71cdb467f8b75b952549952a1d7bc29a6c844f87
GrahamMendick <GrahamMendick@localhost>
2011-05-02 15:52:27 +0100

    Backed out the SelectParameters of CrumbTrailDataSource as it does not work. They work for the NavigationDataSource as the NavigationParameter retrieves its values from StateContext (rather than Crumb).

--- Navigation/CrumbTrailDataSourceView.cs
+++ Navigation/CrumbTrailDataSourceView.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Navigation
 {
@@ -13,38 +11,15 @@ namespace Navigation
 	/// </summary>
 	public class CrumbTrailDataSourceView : DataSourceView
 	{
-		private CrumbTrailDataSource _Owner;
-		private HttpContext _Context;
-		private ParameterCollection _SelectParameters;
-
-		/// <summary>
-		/// Gets the parameters collection used to set default values to help with binding to 
-		/// non-nullable <see cref="System.Web.UI.Control"/> properties
-		/// </summary>
-		public ParameterCollection SelectParameters
-		{
-			get
-			{
-				if (_SelectParameters == null)
-				{
-					_SelectParameters = new ParameterCollection();
-				}
-				return _SelectParameters;
-			}
-		}
-
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Navigation.CrumbTrailDataSourceView"/>
 		/// class
 		/// </summary>
 		/// <param name="owner">The <see cref="Navigation.CrumbTrailDataSource"/> this view is
 		/// associated with</param>
-		/// <param name="context">The current <see cref="System.Web.HttpContext"/></param>
-		public CrumbTrailDataSourceView(CrumbTrailDataSource owner, HttpContext context)
+		public CrumbTrailDataSourceView(CrumbTrailDataSource owner)
 			: base(owner, "") 
 		{
-			_Owner = owner;
-			_Context = context;
 		}
 
 		/// <summary>
@@ -81,8 +56,7 @@ namespace Navigation
 		}
 
 		/// <summary>
-		/// Iterates through the <see cref="Navigation.Crumb"/> contents of <see cref="Navigation.StateController.Crumbs"/>,
-		/// each one is set with any additional values specified in the <see cref="SelectParameters"/> collection
+		/// Iterates through the <see cref="Navigation.Crumb"/> contents of <see cref="Navigation.StateController.Crumbs"/>
 		/// </summary>
 		/// <param name="arguments">This parameter is ignored</param>
 		/// <returns>An <see cref="System.Collections.IEnumerable"/> list of <see cref="Navigation.Crumb"/> items</returns>
@@ -90,11 +64,6 @@ namespace Navigation
 		{
 			foreach (Crumb crumb in StateController.Crumbs)
 			{
-				foreach (DictionaryEntry entry in SelectParameters.GetValues(_Context, _Owner))
-				{
-					if (crumb.Data[(string)entry.Key] == null)
-						crumb.Data[(string)entry.Key] = entry.Value;
-				}
 				yield return crumb;
 			}
 		}
