2ad42675e1a51501ee5b29be341e68384869d86c
GrahamMendick <GrahamMendick@localhost>
2014-02-28 19:50:18 +0000

    Fixed unit tests. Need to write some more for new coherence and default rules

--- NavigationDesigner.Test/CoherenceTest.cs
+++ NavigationDesigner.Test/CoherenceTest.cs
@@ -8,25 +8,6 @@ namespace NavigationDesigner.Test
 	public class CoherenceTest
 	{
 		[TestMethod]
-		public void InitialDialogCoherenceTest()
-		{
-			Store store = new Store(typeof(NavigationLanguageDomainModel));
-			using (Transaction t = store.TransactionManager.BeginTransaction())
-			{
-				NavigationDiagram navigationConfiguration = NavigationLanguageSerializationHelper.Instance.LoadModel(store, "Diagram/InitialDialogCoherence.nav", null, null, null);
-				State state = navigationConfiguration.States[0];
-				state.Initial = false;
-				t.Commit();
-				Assert.AreEqual(string.Empty, state.DialogKey);
-				Assert.AreEqual(string.Empty, state.DialogTitle);
-				Assert.AreEqual(string.Empty, state.DialogResourceType);
-				Assert.AreEqual(string.Empty, state.DialogResourceKey);
-				Assert.AreEqual(string.Empty, state.Path);
-				Assert.AreEqual(0, state.DialogOrder);
-			}
-		}
-
-		[TestMethod]
 		public void DialogInitialCoherenceTest()
 		{
 			Store store = new Store(typeof(NavigationLanguageDomainModel));
@@ -34,14 +15,10 @@ namespace NavigationDesigner.Test
 			{
 				NavigationDiagram navigationConfiguration = NavigationLanguageSerializationHelper.Instance.LoadModel(store, "Diagram/InitialDialogCoherence.nav", null, null, null);
 				State state = navigationConfiguration.States[0];
-				state.DialogKey = null;
-				state.DialogTitle = string.Empty;
-				state.DialogResourceType = null;
-				state.DialogResourceKey = null;
-				state.Path = null;
-				state.DialogOrder = 0;
+				state.Initial = true;
 				t.Commit();
-				Assert.IsFalse(state.Initial);
+				Assert.AreEqual("A", state.DialogKey);
+				Assert.AreEqual("~/A.aspx", state.Path);
 			}
 		}
 	}
