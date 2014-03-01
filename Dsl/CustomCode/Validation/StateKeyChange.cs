49146e4fa10ebd9513a95b540536fc6544d84650
GrahamMendick <GrahamMendick@localhost>
2014-03-01 18:21:26 +0000

    There were problems with defaulting the DialogKey. Using the State Key: Say a State is added, its Key would be defaulted as State1. If the Key is renamed then any new State would be given the Key State1. But both DialogKeys would be the same causing an error. Using the State Count: Say 3 States are added and the first deleted. Any new State would have Key State1 but the State Count would be 2 so the DialogKey would be 3 and would match the second State. Solution: Use the State Key and rename the DialogKey when the State Key changes. Ensure that it's only changed if it matches and if the value matches the default pattern. For consistency also change the Page and Route values. Also, added Route decorator so that Route's more prominent. Routes are as important as Pages and should be clear that it's defaulted

---
+++ Dsl/CustomCode/Validation/StateKeyChange.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.Modeling;
+using System.Text.RegularExpressions;
+
+namespace Navigation.Designer
+{
+	[RuleOn(typeof(State))]
+	public sealed class StateKeyChange : ChangeRule
+	{
+		private static Regex defaultKeyExp = new Regex(@"^State\d+$");
+
+		public override void ElementPropertyChanged(ElementPropertyChangedEventArgs e)
+		{
+			State state = (State)e.ModelElement;
+			if (e.DomainProperty.Id == State.KeyDomainPropertyId && defaultKeyExp.Match((string)e.OldValue).Success)
+			{
+				if (state.Page == string.Format("~/{0}.aspx", e.OldValue))
+					state.Page = string.Format("~/{0}.aspx", e.NewValue);
+				if (state.Route == string.Format("{0}Route", e.OldValue))
+					state.Route = string.Format("{0}Route", e.NewValue);
+				if (state.DialogKey == string.Format("{0}Dialog", e.OldValue))
+					state.DialogKey = string.Format("{0}Dialog", e.NewValue);
+			}
+		}
+	}
+}
