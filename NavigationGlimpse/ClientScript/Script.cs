cfadcd579a5ad9a19ff54266d8fe3ebbd1821fed
GrahamMendick <GrahamMendick@localhost>
2014-04-02 21:04:33 +0100

    Added NavigationGlimpse project. Copied from github. 1. Created folder structure to match Glimpse codebase 2. Renamed Elements to Models to match Glimpse naming standard Set package restore on solution to avoid dll checkin

---
+++ NavigationGlimpse/ClientScript/Script.cs
@@ -0,0 +1,21 @@
+﻿using Glimpse.Core.Extensibility;
+using NavigationGlimpse.Resource;
+
+namespace NavigationGlimpse.ClientScript
+{
+	class Script : IDynamicClientScript
+	{
+		public string GetResourceName()
+		{
+			return ScriptResource.InternalName;
+		}
+
+		public ScriptOrder Order
+		{
+			get
+			{
+				return ScriptOrder.IncludeAfterClientInterfaceScript;
+			}
+		}
+	}
+}
---
+++ NavigationGlimpse/SerializationConverter/TransitionModelConverter.cs
@@ -0,0 +1,20 @@
+﻿using Glimpse.Core.Extensibility;
+using NavigationGlimpse.Model;
+
+namespace NavigationGlimpse.SerializationConverter
+{
+	public class TransitionModelConverter : SerializationConverter<TransitionModel>
+	{
+		public override object Convert(TransitionModel transModel)
+		{
+			return new
+			{
+				transModel.Transition.Key,
+				transModel.X1,
+				transModel.X2,
+				transModel.Y,
+				transModel.H
+			};
+		}
+	}
+}
