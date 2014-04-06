bf7c3a207b85e8dcdc8b67867564959a9dc4e6fe
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-06 10:59:09 +0100

    Added NavigationSample, built from documentation, to improve the F5 experience. NavigationGlimpse shown when glimpse is turned on

---
+++ NavigationSample/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Navigation.Sample
+{
+	public class Person
+	{
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public DateTime DateOfBirth
+		{
+			get;
+			set;
+		}
+	}
+}
