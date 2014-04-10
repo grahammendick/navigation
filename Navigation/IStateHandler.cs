bcb6a2f154cc6ce94e39eb1f123f5728f6eedc8d
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-10 21:10:26 +0100

    Defined method for parsing the link into data, the counterpart to the get link from data. This can be called from framework for Mock Navigation. It can also be called from Adapters (Web Forms, MVC etc) which will use the context parameter instead of the url to get the query data

--- Navigation/IStateHandler.cs
+++ Navigation/IStateHandler.cs
@@ -4,14 +4,16 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Navigation
 {
 	/// <summary>
-	/// 
 	/// </summary>
 	public interface IStateHandler
 	{
-		string GetNavigationLink(State state, NameValueCollection data);
+		string GetNavigationLink(State state, NameValueCollection data, HttpContextBase context);
+
+		NameValueCollection GetNavigationData(State state, string navigationLink, HttpContextBase context);
 	}
 }
