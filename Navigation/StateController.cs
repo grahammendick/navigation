f011bcfbfbde5b3c201993a25ea7ce80a36cacfa
GrahamMendick <GrahamMendick@localhost>
2014-04-12 15:41:22 +0100

    Tidied up usings

--- Navigation/StateController.cs
+++ Navigation/StateController.cs
@@ -1,10 +1,10 @@
+using Navigation.Properties;
 using System;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Configuration;
 using System.Globalization;
 using System.Web;
-using Navigation.Properties;
 
 namespace Navigation
 {
