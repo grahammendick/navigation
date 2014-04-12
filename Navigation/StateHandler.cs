2f61b8d7a63c26f39f5f3b99fc2cda658938aed6
GrahamMendick <GrahamMendick@localhost>
2014-04-12 11:53:34 +0100

    Split up StateHandler to move WebForms specifics into dedicated folder. Mvc and WebApi can directly use StateHandler, no need for derived class. Only catch is have to reference PageStateHandler in State otherwise, pre .NET 4.0, can't default State's Handler

--- Navigation/StateHandler.cs
+++ Navigation/StateHandler.cs
@@ -2,54 +2,26 @@
 using System.Text;
 using System.Web;
 using System.Web.Routing;
-using System.Web.WebPages;
 
 namespace Navigation
 {
-	/// <summary>
-	/// 
-	/// </summary>
 	public class StateHandler : IStateHandler
 	{
-		public string GetNavigationLink(State state, NameValueCollection data, HttpContextBase context)
+		public virtual string GetNavigationLink(State state, NameValueCollection data, HttpContextBase context)
 		{
-			string route = GetRoute(state, context);
-			if (route != null)
+			RouteValueDictionary routeData = new RouteValueDictionary();
+			foreach (string key in data.Keys)
 			{
-				RouteValueDictionary routeData = new RouteValueDictionary();
-				foreach (string key in data.Keys)
-				{
-					if (key != NavigationSettings.Config.StateKey)
-						routeData.Add(key, data[key]);
-				}
-				VirtualPathData virtualPath = RouteTable.Routes.GetVirtualPath(context.Request.RequestContext, route, routeData);
-				if (virtualPath == null)
-					return null;
-				return virtualPath.VirtualPath;
+				if (key != NavigationSettings.Config.StateKey)
+					routeData.Add(key, data[key]);
 			}
-			else
-			{
-				StringBuilder href = new StringBuilder();
-				href.Append(VirtualPathUtility.ToAbsolute(state.GetPage(context.GetOverriddenBrowser().IsMobileDevice), context.Request.ApplicationPath));
-				href.Append("?");
-				href.Append(HttpUtility.UrlEncode(NavigationSettings.Config.StateKey));
-				href.Append("=");
-				href.Append(HttpUtility.UrlEncode(state.DialogStateKey));
-				foreach (string key in data)
-				{
-					if (key != NavigationSettings.Config.StateKey)
-					{
-						href.Append("&");
-						href.Append(HttpUtility.UrlEncode(key));
-						href.Append("=");
-						href.Append(HttpUtility.UrlEncode(data[key]));
-					}
-				}
-				return href.ToString();
-			}
+			VirtualPathData virtualPath = RouteTable.Routes.GetVirtualPath(context.Request.RequestContext, GetRoute(state, context), routeData);
+			if (virtualPath == null)
+				return null;
+			return virtualPath.VirtualPath;
 		}
 
-		public NameValueCollection GetNavigationData(State state, HttpContextBase context)
+		public virtual NameValueCollection GetNavigationData(State state, HttpContextBase context)
 		{
 			//Call from Mock Navigation and StateAdapter
 			//Move GetQueryData logic into here
@@ -58,11 +30,7 @@ namespace Navigation
 
 		protected virtual string GetRoute(State state, HttpContextBase context)
 		{
-			bool mobile = context.GetOverriddenBrowser().IsMobileDevice;
-			if (state.GetRoute(mobile).Length == 0 || RouteTable.Routes[state.GetRouteName(mobile)] == null)
-				return null;
-			else
-				return state.GetRouteName(mobile);
+			return state.Route;
 		}
 	}
 }
