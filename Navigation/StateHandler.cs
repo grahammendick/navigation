99f4365067f41e2aaa050422aefb609264817d74
GrahamMendick <GrahamMendick@localhost>
2014-04-19 10:19:44 +0100

    Added xml documentation

--- Navigation/StateHandler.cs
+++ Navigation/StateHandler.cs
@@ -7,18 +7,20 @@ using System.Web.Routing;
 namespace Navigation
 {
 	/// <summary>
-	/// 
+	/// Implementation of <see cref="Navigation.IStateHandler"/> that builds and parses
+	/// navigation links for a routed <see cref="Navigation.State"/>
 	/// </summary>
 	public class StateHandler : IStateHandler
 	{
 #if NET40Plus
 		/// <summary>
-		/// 
+		/// Returns a routed link that navigates to the <paramref name="state"/> passing 
+		/// the <paramref name="data"/>
 		/// </summary>
-		/// <param name="state"></param>
-		/// <param name="data"></param>
-		/// <param name="context"></param>
-		/// <returns></returns>
+		/// <param name="state">The <see cref="Navigation.State"/> to navigate to</param>
+		/// <param name="data">The data to pass when navigating</param>
+		/// <param name="context">The current context</param>
+		/// <returns>The routed navigation link</returns>
 		public virtual string GetNavigationLink(State state, NameValueCollection data, HttpContextBase context)
 		{
 			RouteValueDictionary routeData = new RouteValueDictionary();
@@ -34,11 +36,11 @@ namespace Navigation
 		}
 
 		/// <summary>
-		/// 
+		/// Returns the data parsed from the Route and QueryString of the <paramref name="context"/>
 		/// </summary>
-		/// <param name="state"></param>
-		/// <param name="context"></param>
-		/// <returns></returns>
+		/// <param name="state">The <see cref="Navigation.State"/> navigated to</param>
+		/// <param name="context">The current context</param>
+		/// <returns>The navigation data</returns>
 		public virtual NameValueCollection GetNavigationData(State state, HttpContextBase context)
 		{
 			NameValueCollection queryData = new NameValueCollection();
@@ -61,11 +63,11 @@ namespace Navigation
 		}
 
 		/// <summary>
-		/// 
+		/// Returns the route of the <paramref name="state"/>
 		/// </summary>
-		/// <param name="state"></param>
-		/// <param name="context"></param>
-		/// <returns></returns>
+		/// <param name="state">The <see cref="Navigation.State"/> to navigate to</param>
+		/// <param name="context">The current context</param>
+		/// <returns>The route name</returns>
 		protected virtual string GetRoute(State state, HttpContextBase context)
 		{
 			return state.Route;
