8ad1cddedfc85c4724199d1246eeab8d3f2917e2
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-24 19:08:55 +0100

    Added a NavigationDataProvider and registered it ahead of other built in Query and RouteData providers. This is needed because an int of 1 (without specify default type) is passed as 12_3 and so can't be converted to an int Controller parameter. The NavigationData holds it as an int and so it works as long as the NavigationDataProvider returns before QueryStringProvider

---
+++ NavigationMvc/NavigationDataValueProviderFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc
+{
+	public sealed class NavigationDataValueProviderFactory : ValueProviderFactory
+	{
+		public override IValueProvider GetValueProvider(ControllerContext controllerContext)
+		{
+			if (controllerContext == null)
+				throw new ArgumentNullException("controllerContext");
+			return new NavigationDataValueProvider();
+		}
+	}
+}
