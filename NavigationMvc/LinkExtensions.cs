12e4d1301120c3dd5e1c7cf38b48badef76a1b12
GrahamMendick <GrahamMendick@localhost>
2014-04-24 21:10:50 +0100

    Start of work exposing StateController GetLink methods as HtmlHelper extensions

---
+++ NavigationMvc/LinkExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc
+{
+	public static class LinkExtensions
+	{
+		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action)
+		{
+			return NavigationLink(htmlHelper, linkText, action, null);
+		}
+
+		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, NavigationData toData)
+		{
+			TagBuilder tagBuilder = new TagBuilder("a")
+			{
+				InnerHtml = !string.IsNullOrEmpty(linkText) ? HttpUtility.HtmlEncode(linkText) : string.Empty
+			};
+			tagBuilder.MergeAttribute("href", StateController.GetNavigationLink(action, toData));
+			return MvcHtmlString.Create(tagBuilder.ToString(TagRenderMode.Normal));
+		}
+	}
+}
