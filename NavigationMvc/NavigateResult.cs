3893ffe798c3f383ed65e7112116763d2a147f71
GrahamMendick <GrahamMendick@localhost>
2014-04-26 00:05:40 +0100

    Added NavigateResult for Controller navigation

---
+++ NavigationMvc/NavigateResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc
+{
+	public class NavigateResult : ActionResult
+	{
+		public NavigateResult(string action) : this(action, null)
+		{
+		}
+
+		public NavigateResult(string action, NavigationData toData)
+		{
+			Action = action;
+			ToData = toData;
+		}
+
+		public string Action
+		{
+			get;
+			private set;
+		}
+
+		public NavigationData ToData
+		{
+			get;
+			private set;
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+			if (context.IsChildAction)
+			{
+				throw new InvalidOperationException();
+			}
+			context.Controller.TempData.Keep();
+			StateController.Navigate(Action, ToData);
+		}
+	}
+}
