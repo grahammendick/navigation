656ac2952809f2ae45224b365ad5a0356fd816f5
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-26 09:36:05 +0100

    Added Navigation FormExtensions. There's some odd code in the Mvc FormExtensions for generating formid when CliendValidationEnabled but Unobtrusive disaabled. I've left this out for now

---
+++ NavigationMvc/FormExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Navigation.Mvc
+{
+	public static class FormExtensions
+	{
+		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action)
+		{
+			return BeginNavigationForm(htmlHelper, action, null);
+		}
+
+		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, NavigationData toData)
+		{
+			return BeginNavigationForm(htmlHelper, action, toData, null);
+		}
+
+		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, NavigationData toData, IDictionary<string, object> htmlAttributes)
+		{
+			return GenerateForm(htmlHelper, StateController.GetNavigationLink(action, toData), htmlAttributes);
+		}
+
+		private static MvcForm GenerateForm(this HtmlHelper htmlHelper, string url, IDictionary<string, object> htmlAttributes)
+		{
+			TagBuilder tagBuilder = new TagBuilder("form");
+			tagBuilder.MergeAttributes<string, object>(htmlAttributes);
+			tagBuilder.MergeAttribute("action", url);
+			tagBuilder.MergeAttribute("method", "post");
+			htmlHelper.ViewContext.Writer.Write(tagBuilder.ToString(TagRenderMode.StartTag));
+			return new MvcForm(htmlHelper.ViewContext);
+		}
+	}
+}
