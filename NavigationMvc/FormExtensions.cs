2312698ee0eca07d639e3233ab9dce88e69b8f19
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-26 09:50:02 +0100

    Added Form Extension methods for back and refresh navigation

--- NavigationMvc/FormExtensions.cs
+++ NavigationMvc/FormExtensions.cs
@@ -26,6 +26,36 @@ namespace Navigation.Mvc
 			return GenerateForm(htmlHelper, StateController.GetNavigationLink(action, toData), htmlAttributes);
 		}
 
+		public static MvcForm BeginNavigationBackForm(this HtmlHelper htmlHelper, int distance)
+		{
+			return BeginNavigationBackForm(htmlHelper, distance, null);
+		}
+
+		public static MvcForm BeginNavigationBackForm(this HtmlHelper htmlHelper, int distance, IDictionary<string, object> htmlAttributes)
+		{
+			return GenerateForm(htmlHelper, StateController.GetNavigationBackLink(distance), htmlAttributes);
+		}
+
+		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper)
+		{
+			return BeginRefreshForm(htmlHelper, null, null);
+		}
+
+		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, NavigationData toData)
+		{
+			return BeginRefreshForm(htmlHelper, toData, null);
+		}
+
+		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, IDictionary<string, object> htmlAttributes)
+		{
+			return BeginRefreshForm(htmlHelper, null, htmlAttributes);
+		}
+
+		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, NavigationData toData, IDictionary<string, object> htmlAttributes)
+		{
+			return GenerateForm(htmlHelper, StateController.GetRefreshLink(toData), htmlAttributes);
+		}
+
 		private static MvcForm GenerateForm(this HtmlHelper htmlHelper, string url, IDictionary<string, object> htmlAttributes)
 		{
 			TagBuilder tagBuilder = new TagBuilder("form");
