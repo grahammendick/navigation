1555ad4f9693466c910da225e7d9d51c7fb171e9
GrahamMendick <GrahamMendick@localhost>
2014-04-26 09:43:58 +0100

    More ActionResults for navigating back and refreshing

---
+++ NavigationMvc/NavigateBackResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc
+{
+	public class NavigateBackResult : ActionResult
+	{
+		public NavigateBackResult(int distance)
+		{
+			Distance = distance;
+		}
+
+		public int Distance
+		{
+			get;
+			private set;
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+			if (context.IsChildAction)
+			{
+				throw new InvalidOperationException();
+			}
+			context.Controller.TempData.Keep();
+			StateController.NavigateBack(Distance);
+		}
+	}
+}
