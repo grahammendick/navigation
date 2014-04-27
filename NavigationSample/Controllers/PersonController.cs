55267cf4cc6c1296fc38a95f1c5ac9c79fc7b0cf
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-27 11:11:41 +0100

    Swapped around if and else

--- NavigationSample/Controllers/PersonController.cs
+++ NavigationSample/Controllers/PersonController.cs
@@ -9,12 +9,14 @@ namespace Navigation.Sample.Controllers
 		public ActionResult Index(PersonSearchModel model, string sortExpression, int startRowIndex, int maximumRows)
 		{
 			DateTime outDate;
-			if (model.MinDateOfBirth != null && !DateTime.TryParse(model.MinDateOfBirth, out outDate))
-				ModelState.AddModelError("MinDateOfBirth", "date error");
-			else
+			if (model.MinDateOfBirth == null || DateTime.TryParse(model.MinDateOfBirth, out outDate))
 			{
 				StateContext.Bag.name = model.Name;
 				StateContext.Bag.minDateOfBirth = model.MinDateOfBirth;
+			}
+			else
+			{
+				ModelState.AddModelError("MinDateOfBirth", "date error");
 			}
 			model.People = new PersonSearch().Search(StateContext.Bag.name, StateContext.Bag.minDateOfBirth, 
 				sortExpression, startRowIndex, maximumRows);
