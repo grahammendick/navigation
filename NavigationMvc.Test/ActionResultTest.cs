125f3ab85b327fc387020b1a6c6893a7c32ffcbf
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-27 14:21:38 +0100

    Added NavigationMvc unit test project and first unit test

---
+++ NavigationMvc.Test/ActionResultTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc.Test
+{
+	[TestClass]
+	public class ActionResultTest
+	{
+		private static ControllerContext ControllerContext
+		{
+			get
+			{
+				return new Mock<ControllerContext> { DefaultValue = DefaultValue.Mock }.Object;
+			}
+		}
+
+		[AssemblyInitialize]
+		public static void AddStateRoutes(TestContext context)
+		{
+			RouteConfig.AddStateRoutes();
+		}
+
+		[TestMethod]
+		public void NavigateResultDialogTest()
+		{
+			NavigateResult result = new NavigateResult("d0");
+			result.ExecuteResult(ControllerContext);
+			Assert.AreEqual(StateInfoConfig.Dialogs[0].States[0], StateContext.State);
+		}
+	}
+}
