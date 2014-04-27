7f17887ab4d4df581bc7a28fccbfbf1c43981345
GrahamMendick <GrahamMendick@localhost>
2014-04-27 18:46:53 +0100

    Added RouteConfig unit tests

---
+++ NavigationMvc.Test/RouteConfigTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Routing;
+
+namespace Navigation.Mvc.Test
+{
+	[TestClass]
+	public class RouteConfigTest
+	{
+		[TestMethod]
+		public void RouteTest()
+		{
+			Assert.AreEqual(4, RouteTable.Routes.Count);
+		}
+
+		[TestMethod]
+		public void MvcStateRouteHandlerTest()
+		{
+			MvcStateRouteHandler handler = ((Route)RouteTable.Routes["Mvc0-0"]).RouteHandler as MvcStateRouteHandler;
+			Assert.IsNotNull(handler);
+			handler = ((Route)RouteTable.Routes["Mvc0-1"]).RouteHandler as MvcStateRouteHandler;
+			Assert.IsNotNull(handler);
+			handler = ((Route)RouteTable.Routes[2]).RouteHandler as MvcStateRouteHandler;
+			Assert.IsNotNull(handler);
+			handler = ((Route)RouteTable.Routes[3]).RouteHandler as MvcStateRouteHandler;
+			Assert.IsNull(handler);
+		}
+	}
+}
