7bf6b0978a60b0748c2f12c1212a765fb7369987
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-27 15:34:25 +0100

    Added NavigationLink HtmlHelper extensions unit tests

---
+++ NavigationMvc.Test/LinkExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Moq;
+
+namespace Navigation.Mvc.Test
+{
+	[TestClass]
+	public class LinkExtensionsTest
+	{
+		private static HtmlHelper HtmlHelper
+		{
+			get
+			{
+				ViewContext context = new Mock<ViewContext>().Object;
+				IViewDataContainer container = new Mock<IViewDataContainer>().Object;
+				return new HtmlHelper(context, container);
+			}
+		}
+
+		[TestMethod]
+		public void NavigationLinkTest()
+		{
+			Assert.AreEqual("<a href=\"/\">link</a>", HtmlHelper.NavigationLink("link", "d0").ToHtmlString());
+		}
+
+		[TestMethod]
+		public void NavigationLinkBlankTextTest()
+		{
+			Assert.AreEqual("<a href=\"/\"></a>", HtmlHelper.NavigationLink(null, "d0").ToHtmlString());
+		}
+
+		[TestMethod]
+		public void NavigationLinkDataTest()
+		{
+			StateController.Navigate("d0");
+			Assert.AreEqual("<a href=\"/r1?a=1\">link</a>", HtmlHelper.NavigationLink("link", "t0", new NavigationData { { "a", "1" } }).ToHtmlString());
+		}
+	}
+}
