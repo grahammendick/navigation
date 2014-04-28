e549aee747ff4dde0cb61969ebafb5e00a738069
GrahamMendick <grahammendick@users.noreply.github.com>
2014-04-28 21:01:48 +0100

    Added LinkExtensions xml documentation

--- NavigationMvc/LinkExtensions.cs
+++ NavigationMvc/LinkExtensions.cs
@@ -17,7 +17,7 @@ namespace Navigation.Mvc
 		/// <param name="linkText">The inner text of the anchor element</param>
 		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
 		/// <see cref="Dialog"/></param>
-		/// <returns>An anchor element (a element).</returns>
+		/// <returns>An anchor element (a element)</returns>
 		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
 		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty;
 		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
@@ -28,16 +28,66 @@ namespace Navigation.Mvc
 			return NavigationLink(htmlHelper, linkText, action, null, null);
 		}
 
+		/// <summary>
+		/// Returns an anchor element (a element) with its href attribute set from a call to
+		/// <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
+		/// </summary>
+		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
+		/// <param name="linkText">The inner text of the anchor element</param>
+		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
+		/// <see cref="Dialog"/></param>
+		/// <param name="toData">The <see cref="NavigationData"/> to be passed to the next
+		/// <see cref="State"/> and stored in the <see cref="StateContext"/></param>
+		/// <returns>An anchor element (a element)</returns>
+		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
+		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty;
+		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
+		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
+		/// be converted to a <see cref="System.String"/></exception>
 		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, NavigationData toData)
 		{
 			return NavigationLink(htmlHelper, linkText, action, toData, null);
 		}
 
+		/// <summary>
+		/// Returns an anchor element (a element) with its href attribute set from a call to
+		/// <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
+		/// </summary>
+		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
+		/// <param name="linkText">The inner text of the anchor element</param>
+		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
+		/// <see cref="Dialog"/></param>
+		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
+		/// element</param>
+		/// <returns>An anchor element (a element)</returns>
+		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
+		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty;
+		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
+		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
+		/// be converted to a <see cref="System.String"/></exception>
 		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, object htmlAttributes)
 		{
 			return NavigationLink(htmlHelper, linkText, action, null, htmlAttributes);
 		}
 
+		/// <summary>
+		/// Returns an anchor element (a element) with its href attribute set from a call to
+		/// <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
+		/// </summary>
+		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
+		/// <param name="linkText">The inner text of the anchor element</param>
+		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
+		/// <see cref="Dialog"/></param>
+		/// <param name="toData">The <see cref="NavigationData"/> to be passed to the next
+		/// <see cref="State"/> and stored in the <see cref="StateContext"/></param>
+		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
+		/// element</param>
+		/// <returns>An anchor element (a element)</returns>
+		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
+		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty;
+		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
+		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
+		/// be converted to a <see cref="System.String"/></exception>
 		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, NavigationData toData, object htmlAttributes)
 		{
 			return GenerateLink(htmlHelper, linkText, StateController.GetNavigationLink(action, toData), htmlAttributes);
