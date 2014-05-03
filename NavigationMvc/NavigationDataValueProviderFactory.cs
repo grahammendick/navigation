fe3311f68d2c4aeb8263f0a1fca0dc78373bc304
GrahamMendick <GrahamMendick@localhost>
2014-05-03 10:16:07 +0100

    Added xml documentation

--- NavigationMvc/NavigationDataValueProviderFactory.cs
+++ NavigationMvc/NavigationDataValueProviderFactory.cs
@@ -3,8 +3,16 @@ using System.Web.Mvc;
 
 namespace Navigation.Mvc
 {
+	/// <summary>
+	/// Represents a factory for creating <see cref="NavigationData"/> value provider objects
+	/// </summary>
 	public sealed class NavigationDataValueProviderFactory : ValueProviderFactory
 	{
+		/// <summary>
+		/// Returns a <see cref="NavigationData"/> value provider for the specified controller context
+		/// </summary>
+		/// <param name="controllerContext">The context within which the result is executed</param>
+		/// <returns><see cref="NavigationData"/> value provider</returns>
 		public override IValueProvider GetValueProvider(ControllerContext controllerContext)
 		{
 			if (controllerContext == null)
