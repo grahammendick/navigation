7784ddc7951fd8e32312dd0dcf583deb3e395a67
GrahamMendick <GrahamMendick@localhost>
2014-05-03 10:13:01 +0100

    Added xml documentation

--- NavigationMvc/NavigationDataValueProvider.cs
+++ NavigationMvc/NavigationDataValueProvider.cs
@@ -4,8 +4,14 @@ using System.Web.Mvc;
 
 namespace Navigation.Mvc
 {
+	/// <summary>
+	/// Represents a value provider for <see cref="NavigationData"/> values
+	/// </summary>
 	public sealed class NavigationDataValueProvider : DictionaryValueProvider<object>
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavigationDataValueProvider"/> class
+		/// </summary>
 		public NavigationDataValueProvider() : base(GetNavigationDataDictionary(), CultureInfo.InvariantCulture)
 		{
 		}
