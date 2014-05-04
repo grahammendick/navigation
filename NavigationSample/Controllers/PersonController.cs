36be6f9a16363774af9584f14a938f6397374b74
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-04 12:18:42 +0100

    Clear sort and start when search changes (same as Web Forms). Cleaner to take data from StateContext instead of parameters because most can change within the method

--- NavigationSample/Controllers/PersonController.cs
+++ NavigationSample/Controllers/PersonController.cs
@@ -6,11 +6,16 @@ namespace Navigation.Sample.Controllers
 {
 	public class PersonController : Controller
 	{
-		public ActionResult Index(PersonSearchModel model, string sortExpression, int startRowIndex, int maximumRows)
+		public ActionResult Index(PersonSearchModel model)
 		{
 			DateTime outDate;
 			if (model.MinDateOfBirth == null || DateTime.TryParse(model.MinDateOfBirth, out outDate))
 			{
+				if (StateContext.Bag.name != model.Name || StateContext.Bag.minDateOfBirth != model.MinDateOfBirth)
+				{
+					StateContext.Bag.startRowIndex = null;
+					StateContext.Bag.sortExpression = null;
+				}
 				StateContext.Bag.name = model.Name;
 				StateContext.Bag.minDateOfBirth = model.MinDateOfBirth;
 			}
@@ -18,8 +23,8 @@ namespace Navigation.Sample.Controllers
 			{
 				ModelState.AddModelError("MinDateOfBirth", "date error");
 			}
-			model.People = new PersonSearch().Search(StateContext.Bag.name, StateContext.Bag.minDateOfBirth, 
-				sortExpression, startRowIndex, maximumRows);
+			model.People = new PersonSearch().Search(StateContext.Bag.name, StateContext.Bag.minDateOfBirth,
+				StateContext.Bag.sortExpression, StateContext.Bag.startRowIndex, StateContext.Bag.maximumRows);
 			return View("Listing", model);
 		}
 
