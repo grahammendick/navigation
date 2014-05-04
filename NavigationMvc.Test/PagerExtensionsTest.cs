20e0bcd24dcb0c8cff36f049f301c2481aa358be
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-04 14:05:28 +0100

    Added PageExtension unit tests

--- NavigationMvc.Test/PagerExtensionsTest.cs
+++ NavigationMvc.Test/PagerExtensionsTest.cs
@@ -18,12 +18,78 @@ namespace Navigation.Mvc.Test
 		}
 
 		[TestMethod]
+		public void PagerEmptyTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.totalRowCount = 0;
+			Assert.AreEqual("<ul><li>&lt;&lt;</li><li>&lt;</li><li>&gt;</li><li>&gt;&gt;</li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+
+		[TestMethod]
 		public void PagerOnePageTest()
 		{
 			StateController.Navigate("d0");
 			StateController.Navigate("t0");
 			StateContext.Bag.totalRowCount = 1;
 			Assert.AreEqual("<ul><li>&lt;&lt;</li><li>&lt;</li><li>&gt;</li><li>&gt;&gt;</li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+
+		[TestMethod]
+		public void PagerOneFullPageTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.totalRowCount = 10;
+			Assert.AreEqual("<ul><li>&lt;&lt;</li><li>&lt;</li><li>&gt;</li><li>&gt;&gt;</li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+
+		[TestMethod]
+		public void PagerFirstPageOfTwoTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.totalRowCount = 11;
+			Assert.AreEqual("<ul><li>&lt;&lt;</li><li>&lt;</li>" + 
+				"<li><a href=\"/r1/10\">&gt;</a></li>" + 
+				"<li><a href=\"/r1/10\">&gt;&gt;</a></li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+
+		[TestMethod]
+		public void PagerSecondPageOfTwoTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.startRowIndex = 10;
+			StateContext.Bag.totalRowCount = 11;
+			Assert.AreEqual("<ul><li><a href=\"/r1\">&lt;&lt;</a></li>" + 
+				"<li><a href=\"/r1\">&lt;</a></li>" + 
+				"<li>&gt;</li><li>&gt;&gt;</li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+
+		[TestMethod]
+		public void PagerSecondFullPageOfTwoTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.startRowIndex = 10;
+			StateContext.Bag.totalRowCount = 20;
+			Assert.AreEqual("<ul><li><a href=\"/r1\">&lt;&lt;</a></li>" + 
+				"<li><a href=\"/r1\">&lt;</a></li>" + 
+				"<li>&gt;</li><li>&gt;&gt;</li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+
+		[TestMethod]
+		public void PagerSecondPageOfThreeTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.startRowIndex = 10;
+			StateContext.Bag.totalRowCount = 21;
+			Assert.AreEqual("<ul><li><a href=\"/r1\">&lt;&lt;</a></li>" + 
+				"<li><a href=\"/r1\">&lt;</a></li>" + 
+				"<li><a href=\"/r1/20\">&gt;</a></li>" + 
+				"<li><a href=\"/r1/20\">&gt;&gt;</a></li></ul>", HtmlHelper.Pager().ToHtmlString());
 		}
 	}
 }
