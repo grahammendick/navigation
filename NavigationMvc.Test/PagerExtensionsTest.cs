c168634970dc548d9a29bbe3f2eb8ce2510062cd
GrahamMendick <GrahamMendick@localhost>
2014-05-04 13:51:33 +0100

    First PagerExtension unit test

---
+++ NavigationMvc.Test/PagerExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc.Test
+{
+	[TestClass]
+	public class PagerExtensionsTest
+	{
+		private static HtmlHelper HtmlHelper
+		{
+			get
+			{
+				ViewContext context = new Mock<ViewContext>().Object;
+				IViewDataContainer container = new Mock<IViewDataContainer>().Object;
+				return new HtmlHelper(context, container);
+			}
+		}
+
+		[TestMethod]
+		public void PagerOnePageTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0");
+			StateContext.Bag.totalRowCount = 1;
+			Assert.AreEqual("<ul><li>&lt;&lt;</li><li>&lt;</li><li>&gt;</li><li>&gt;&gt;</li></ul>", HtmlHelper.Pager().ToHtmlString());
+		}
+	}
+}
