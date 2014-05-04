e623d9ebce80bba8c9cf503d2841b9f2d3ed5191
GrahamMendick <GrahamMendick@localhost>
2014-05-04 18:06:14 +0100

    Changed to specifying htmlAttributes as optional parameters to avoid lots of Extension overloads

--- NavigationMvc/FormExtensions.cs
+++ NavigationMvc/FormExtensions.cs
@@ -15,42 +15,6 @@ namespace Navigation.Mvc
 		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
 		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
 		/// <see cref="Dialog"/></param>
-		/// <returns>An opening &lt;form&gt; tag</returns>
-		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
-		/// <exception cref="System.ArgumentException"><paramref name="action"/> does not match the key of 
-		/// a child <see cref="Transition"/> or the key of a <see cref="Dialog"/>; or there is 
-		/// <see cref="NavigationData"/> that cannot be converted to a <see cref="System.String"/></exception>
-		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action)
-		{
-			return BeginNavigationForm(htmlHelper, action, null, null);
-		}
-
-		/// <summary>
-		/// Writes an opening &lt;form&gt; tag to the response with its action attribute set from
-		/// a call to <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
-		/// <see cref="Dialog"/></param>
-		/// <param name="toData">The <see cref="NavigationData"/> to be passed to the next
-		/// <see cref="State"/> and stored in the <see cref="StateContext"/></param>
-		/// <returns>An opening &lt;form&gt; tag</returns>
-		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
-		/// <exception cref="System.ArgumentException"><paramref name="action"/> does not match the key of 
-		/// a child <see cref="Transition"/> or the key of a <see cref="Dialog"/>; or there is 
-		/// <see cref="NavigationData"/> that cannot be converted to a <see cref="System.String"/></exception>
-		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, NavigationData toData)
-		{
-			return BeginNavigationForm(htmlHelper, action, toData, null);
-		}
-
-		/// <summary>
-		/// Writes an opening &lt;form&gt; tag to the response with its action attribute set from
-		/// a call to <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
-		/// <see cref="Dialog"/></param>
 		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
 		/// element</param>
 		/// <returns>An opening &lt;form&gt; tag</returns>
@@ -58,7 +22,7 @@ namespace Navigation.Mvc
 		/// <exception cref="System.ArgumentException"><paramref name="action"/> does not match the key of 
 		/// a child <see cref="Transition"/> or the key of a <see cref="Dialog"/>; or there is 
 		/// <see cref="NavigationData"/> that cannot be converted to a <see cref="System.String"/></exception>
-		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, object htmlAttributes)
+		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, object htmlAttributes = null)
 		{
 			return BeginNavigationForm(htmlHelper, action, null, htmlAttributes);
 		}
@@ -79,7 +43,7 @@ namespace Navigation.Mvc
 		/// <exception cref="System.ArgumentException"><paramref name="action"/> does not match the key of 
 		/// a child <see cref="Transition"/> or the key of a <see cref="Dialog"/>; or there is 
 		/// <see cref="NavigationData"/> that cannot be converted to a <see cref="System.String"/></exception>
-		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, NavigationData toData, object htmlAttributes)
+		public static MvcForm BeginNavigationForm(this HtmlHelper htmlHelper, string action, NavigationData toData, object htmlAttributes = null)
 		{
 			return GenerateForm(htmlHelper, StateController.GetNavigationLink(action, toData), htmlAttributes);
 		}
@@ -90,26 +54,12 @@ namespace Navigation.Mvc
 		/// </summary>
 		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
 		/// <param name="distance">Starting at 1, the number of <see cref="Crumb"/> steps to go back</param>
-		/// <returns>An opening &lt;form&gt; tag</returns>
-		/// <exception cref="System.ArgumentException"><see cref="StateController.CanNavigateBack"/> returns
-		/// false for this <paramref name="distance"/></exception>
-		public static MvcForm BeginNavigationBackForm(this HtmlHelper htmlHelper, int distance)
-		{
-			return BeginNavigationBackForm(htmlHelper, distance, null);
-		}
-
-		/// <summary>
-		/// Writes an opening &lt;form&gt; tag to the response with its action attribute set from
-		/// a call to <see cref="StateController.GetNavigationBackLink(int)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="distance">Starting at 1, the number of <see cref="Crumb"/> steps to go back</param>
 		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
 		/// element</param>
 		/// <returns>An opening &lt;form&gt; tag</returns>
 		/// <exception cref="System.ArgumentException"><see cref="StateController.CanNavigateBack"/> returns
 		/// false for this <paramref name="distance"/></exception>
-		public static MvcForm BeginNavigationBackForm(this HtmlHelper htmlHelper, int distance, object htmlAttributes)
+		public static MvcForm BeginNavigationBackForm(this HtmlHelper htmlHelper, int distance, object htmlAttributes = null)
 		{
 			return GenerateForm(htmlHelper, StateController.GetNavigationBackLink(distance), htmlAttributes);
 		}
@@ -119,36 +69,10 @@ namespace Navigation.Mvc
 		/// a call to <see cref="StateController.GetRefreshLink(NavigationData)"/>
 		/// </summary>
 		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <returns>An opening &lt;form&gt; tag</returns>
-		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper)
-		{
-			return BeginRefreshForm(htmlHelper, null, null);
-		}
-
-		/// <summary>
-		/// Writes an opening &lt;form&gt; tag to the response with its action attribute set from
-		/// a call to <see cref="StateController.GetRefreshLink(NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="toData">The <see cref="NavigationData"/> to be passed to the current
-		/// <see cref="State"/> and stored in the <see cref="StateContext"/></param>
-		/// <returns>An opening &lt;form&gt; tag</returns>
-		/// <exception cref="System.ArgumentException">There is <see cref="NavigationData"/> that cannot be
-		/// converted to a <see cref="System.String"/></exception>
-		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, NavigationData toData)
-		{
-			return BeginRefreshForm(htmlHelper, toData, null);
-		}
-
-		/// <summary>
-		/// Writes an opening &lt;form&gt; tag to the response with its action attribute set from
-		/// a call to <see cref="StateController.GetRefreshLink(NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
 		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
 		/// element</param>
 		/// <returns>An opening &lt;form&gt; tag</returns>
-		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, object htmlAttributes)
+		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, object htmlAttributes = null)
 		{
 			return BeginRefreshForm(htmlHelper, null, htmlAttributes);
 		}
@@ -165,7 +89,7 @@ namespace Navigation.Mvc
 		/// <returns>An opening &lt;form&gt; tag</returns>
 		/// <exception cref="System.ArgumentException">There is <see cref="NavigationData"/> that cannot be
 		/// converted to a <see cref="System.String"/></exception>
-		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, NavigationData toData, object htmlAttributes)
+		public static MvcForm BeginRefreshForm(this HtmlHelper htmlHelper, NavigationData toData, object htmlAttributes = null)
 		{
 			return GenerateForm(htmlHelper, StateController.GetRefreshLink(toData), htmlAttributes);
 		}
--- NavigationMvc/LinkExtensions.cs
+++ NavigationMvc/LinkExtensions.cs
@@ -17,46 +17,6 @@ namespace Navigation.Mvc
 		/// <param name="linkText">The inner text of the anchor element</param>
 		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
 		/// <see cref="Dialog"/></param>
-		/// <returns>An anchor element (a element)</returns>
-		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
-		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty;
-		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
-		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
-		/// be converted to a <see cref="System.String"/></exception>
-		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action)
-		{
-			return NavigationLink(htmlHelper, linkText, action, null, null);
-		}
-
-		/// <summary>
-		/// Returns an anchor element (a element) with its href attribute set from a call to
-		/// <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="linkText">The inner text of the anchor element</param>
-		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
-		/// <see cref="Dialog"/></param>
-		/// <param name="toData">The <see cref="NavigationData"/> to be passed to the next
-		/// <see cref="State"/> and stored in the <see cref="StateContext"/></param>
-		/// <returns>An anchor element (a element)</returns>
-		/// <exception cref="System.ArgumentNullException"><paramref name="action"/> is null</exception>
-		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty;
-		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
-		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
-		/// be converted to a <see cref="System.String"/></exception>
-		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, NavigationData toData)
-		{
-			return NavigationLink(htmlHelper, linkText, action, toData, null);
-		}
-
-		/// <summary>
-		/// Returns an anchor element (a element) with its href attribute set from a call to
-		/// <see cref="StateController.GetNavigationLink(string, NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="linkText">The inner text of the anchor element</param>
-		/// <param name="action">The key of a child <see cref="Transition"/> or the key of a 
-		/// <see cref="Dialog"/></param>
 		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
 		/// element</param>
 		/// <returns>An anchor element (a element)</returns>
@@ -65,7 +25,7 @@ namespace Navigation.Mvc
 		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
 		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
 		/// be converted to a <see cref="System.String"/></exception>
-		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, object htmlAttributes)
+		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, object htmlAttributes = null)
 		{
 			return NavigationLink(htmlHelper, linkText, action, null, htmlAttributes);
 		}
@@ -88,7 +48,7 @@ namespace Navigation.Mvc
 		/// <paramref name="action"/> does not match the key of a child <see cref="Transition"/> or 
 		/// the key of a <see cref="Dialog"/>; or there is <see cref="NavigationData"/> that cannot 
 		/// be converted to a <see cref="System.String"/></exception>
-		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, NavigationData toData, object htmlAttributes)
+		public static MvcHtmlString NavigationLink(this HtmlHelper htmlHelper, string linkText, string action, NavigationData toData, object htmlAttributes = null)
 		{
 			return GenerateLink(htmlHelper, linkText, StateController.GetNavigationLink(action, toData), htmlAttributes);
 		}
@@ -100,27 +60,12 @@ namespace Navigation.Mvc
 		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
 		/// <param name="linkText">The inner text of the anchor element</param>
 		/// <param name="distance">Starting at 1, the number of <see cref="Crumb"/> steps to go back</param>
-		/// <returns>An anchor element (a element)</returns>
-		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty; or
-		/// <see cref="StateController.CanNavigateBack"/> returns false for this <paramref name="distance"/></exception>
-		public static MvcHtmlString NavigationBackLink(this HtmlHelper htmlHelper, string linkText, int distance)
-		{
-			return NavigationBackLink(htmlHelper, linkText, distance, null);
-		}
-
-		/// <summary>
-		/// Returns an anchor element (a element) with its href attribute set from a call to
-		/// <see cref="StateController.GetNavigationBackLink(int)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="linkText">The inner text of the anchor element</param>
-		/// <param name="distance">Starting at 1, the number of <see cref="Crumb"/> steps to go back</param>
 		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
 		/// element</param>
 		/// <returns>An anchor element (a element)</returns>
 		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty; or
 		/// <see cref="StateController.CanNavigateBack"/> returns false for this <paramref name="distance"/></exception>
-		public static MvcHtmlString NavigationBackLink(this HtmlHelper htmlHelper, string linkText, int distance, object htmlAttributes)
+		public static MvcHtmlString NavigationBackLink(this HtmlHelper htmlHelper, string linkText, int distance, object htmlAttributes = null)
 		{
 			return GenerateLink(htmlHelper, linkText, StateController.GetNavigationBackLink(distance), htmlAttributes);
 		}
@@ -131,40 +76,11 @@ namespace Navigation.Mvc
 		/// </summary>
 		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
 		/// <param name="linkText">The inner text of the anchor element</param>
-		/// <returns>An anchor element (a element)</returns>
-		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty</exception>
-		public static MvcHtmlString RefreshLink(this HtmlHelper htmlHelper, string linkText)
-		{
-			return RefreshLink(htmlHelper, linkText, null, null);
-		}
-
-		/// <summary>
-		/// Returns an anchor element (a element) with its href attribute set from a call to
-		/// <see cref="StateController.GetRefreshLink(NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="linkText">The inner text of the anchor element</param>
-		/// <param name="toData">The <see cref="NavigationData"/> to be passed to the current
-		/// <see cref="State"/> and stored in the <see cref="StateContext"/></param>
-		/// <returns>An anchor element (a element)</returns>
-		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty; or
-		/// there is <see cref="NavigationData"/> that cannot be converted to a <see cref="System.String"/></exception>
-		public static MvcHtmlString RefreshLink(this HtmlHelper htmlHelper, string linkText, NavigationData toData)
-		{
-			return RefreshLink(htmlHelper, linkText, toData, null);
-		}
-
-		/// <summary>
-		/// Returns an anchor element (a element) with its href attribute set from a call to
-		/// <see cref="StateController.GetRefreshLink(NavigationData)"/>
-		/// </summary>
-		/// <param name="htmlHelper">The HTML helper instance that this method extends</param>
-		/// <param name="linkText">The inner text of the anchor element</param>
 		/// <param name="htmlAttributes">An object that contains the HTML attributes to set for the
 		/// element</param>
 		/// <returns>An anchor element (a element)</returns>
 		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty</exception>
-		public static MvcHtmlString RefreshLink(this HtmlHelper htmlHelper, string linkText, object htmlAttributes)
+		public static MvcHtmlString RefreshLink(this HtmlHelper htmlHelper, string linkText, object htmlAttributes = null)
 		{
 			return RefreshLink(htmlHelper, linkText, null, htmlAttributes);
 		}
@@ -182,7 +98,7 @@ namespace Navigation.Mvc
 		/// <returns>An anchor element (a element)</returns>
 		/// <exception cref="System.ArgumentException"><paramref name="linkText"/> is null or empty; or
 		/// there is <see cref="NavigationData"/> that cannot be converted to a <see cref="System.String"/></exception>
-		public static MvcHtmlString RefreshLink(this HtmlHelper htmlHelper, string linkText, NavigationData toData, object htmlAttributes)
+		public static MvcHtmlString RefreshLink(this HtmlHelper htmlHelper, string linkText, NavigationData toData, object htmlAttributes = null)
 		{
 			return GenerateLink(htmlHelper, linkText, StateController.GetRefreshLink(toData), htmlAttributes);
 		}
