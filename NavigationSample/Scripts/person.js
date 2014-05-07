b98db66c5666d42132e350e5a8de2a5ae0bfb924
GrahamMendick <GrahamMendick@localhost>
2014-05-07 21:10:51 +0100

    Added name search to api sample

--- NavigationSample/Scripts/person.js
+++ NavigationSample/Scripts/person.js
@@ -1,7 +1,12 @@
 ﻿function PersonController($scope, $http) {
+    $scope.name;
+    $scope.minDateOfBirth;
     $scope.people;
     $scope.search = function () {
-        $http.get('/WebApiList')
+        var url = '/WebApiList';
+        if ($scope.name)
+            url += '?name=' + $scope.name;
+        $http.get(url)
             .success(function (data) {
                 $scope.people = data;
             }
