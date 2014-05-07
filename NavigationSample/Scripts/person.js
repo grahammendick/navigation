8389588d0f3282a9f9803a045a170455af00317a
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-07 21:26:15 +0100

    Added sort to api sample

--- NavigationSample/Scripts/person.js
+++ NavigationSample/Scripts/person.js
@@ -1,9 +1,13 @@
 ﻿function PersonController($scope, $http) {
     $scope.name;
     $scope.minDateOfBirth;
+    $scope.sortExpression = 'Name';
+    $scope.startRowIndex = 0;
+    $scope.maximumRows = 10;
     $scope.people;
     $scope.search = function () {
-        var url = '/WebApiList';
+        var url = '/WebApiList/' + $scope.startRowIndex + '/'
+            + $scope.maximumRows + '/' + encodeURI($scope.sortExpression);
         if ($scope.name)
             url += '?name=' + $scope.name;
         $http.get(url)
@@ -11,6 +15,10 @@
                 $scope.people = data;
             }
         );
+    }
+    $scope.sort = function () {
+        $scope.sortExpression = $scope.sortExpression == 'Name' ? 'Name DESC' : 'Name';
+        $scope.search();
     }
     $scope.search();
 };
