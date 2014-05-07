01d4f007be6fadb3fcbf24adb2ec9f0a097bb2a9
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-07 19:16:36 +0100

    Start of WebApi Angular sample

---
+++ NavigationSample/Scripts/person.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('app', []);
+
+app.service('PersonService', function ($http, $rootScope) {
+    this.search = function ($scope) {
+        $http.get('/WebApiList')
+            .success(function (data) {
+                $rootScope.people = data;
+            }
+        );
+    }
+})
+
+app.controller('PersonController', function ($scope, PersonService) {
+    $scope.people;
+    PersonService.search();
+});
