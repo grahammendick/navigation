a71c7e7b9bf0a6813aaf63bc77321e11dce5b313
GrahamMendick <GrahamMendick@localhost>
2014-05-07 19:16:36 +0100

    Start of WebApi Angular sample

--- NavigationSample/Controllers/PersonApiController.cs
+++ NavigationSample/Controllers/PersonApiController.cs
@@ -1,9 +1,15 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 
 namespace Navigation.Sample.Controllers
 {
     public class PersonApiController : ApiController
     {
+		public IEnumerable<Person> GetList()
+		{
+			return new PersonSearch().Search(null, null, null, 0, 10);
+		}
+
 		public Person GetDetails(int id)
 		{
 			return new Person();
