aa5932dbfb6c842efdf603a7b0387966a3906b51
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-09 18:04:24 +0100

    Added more NavigationDataValueProvider unit tests

--- NavigationWebApi.Test/NavigationDataValueProviderTest.cs
+++ NavigationWebApi.Test/NavigationDataValueProviderTest.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Navigation.WebApi;
+using System.Web.Http;
+using System.Web.Http.ValueProviders;
+using System.Linq;
+using Navigation;
+using System;
 
-namespace NavigationWebApi.Test
+namespace Navigation.WebApi.Test
 {
 	[TestClass]
 	public class NavigationDataValueProviderTest
@@ -11,6 +16,51 @@ namespace NavigationWebApi.Test
 		{
 			NavigationDataValueProviderFactory factory = new NavigationDataValueProviderFactory();
 			Assert.IsTrue(factory.GetValueProvider(null) is NavigationDataValueProvider);
+		}
+
+		[TestMethod]
+		public void NavigationDataValueProviderFactoryRegisteredTest()
+		{
+			Assert.IsTrue(GlobalConfiguration.Configuration.Services.GetServices(typeof(ValueProviderFactory)).ToList()[0] is NavigationDataValueProviderFactory);
+		}
+
+		[TestMethod]
+		public void NavigationDataValueProviderValueTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0", new NavigationData { { "a", 1 } });
+			NavigationDataValueProvider valueProvider = new NavigationDataValueProvider();
+			Assert.AreEqual(1, ((ValueProviderResult)valueProvider.GetValue("a")).RawValue);
+			Assert.AreEqual("1", ((ValueProviderResult)valueProvider.GetValue("a")).AttemptedValue);
+		}
+
+		[TestMethod]
+		public void NavigationDataValueProviderValueCaseInsensitiveTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0", new NavigationData { { "a", 1 } });
+			NavigationDataValueProvider valueProvider = new NavigationDataValueProvider();
+			Assert.AreEqual(1, ((ValueProviderResult)valueProvider.GetValue("A")).RawValue);
+			Assert.AreEqual("1", ((ValueProviderResult)valueProvider.GetValue("A")).AttemptedValue);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NavigationDataValueProviderNullKeyTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0", new NavigationData { { "a", 1 } });
+			NavigationDataValueProvider valueProvider = new NavigationDataValueProvider();
+			valueProvider.GetValue(null);
+		}
+
+		[TestMethod]
+		public void NavigationDataValueProviderNullValueTest()
+		{
+			StateController.Navigate("d0");
+			StateController.Navigate("t0", new NavigationData { { "a", 1 } });
+			NavigationDataValueProvider valueProvider = new NavigationDataValueProvider();
+			Assert.IsNull(valueProvider.GetValue("b"));
 		}
 	}
 }
