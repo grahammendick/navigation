cf042f9ef1093bcf7d6f17630dc65ab8a3f2790c
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-09 17:47:09 +0100

    Added WebApi unit test project

---
+++ NavigationWebApi.Test/NavigationDataValueProviderTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Navigation.WebApi;
+
+namespace NavigationWebApi.Test
+{
+	[TestClass]
+	public class NavigationDataValueProviderTest
+	{
+		[TestMethod]
+		public void NavigationDataValueProviderFactoryTest()
+		{
+			NavigationDataValueProviderFactory factory = new NavigationDataValueProviderFactory();
+			Assert.IsTrue(factory.GetValueProvider(null) is NavigationDataValueProvider);
+		}
+	}
+}
