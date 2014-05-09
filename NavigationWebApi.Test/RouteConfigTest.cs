a9618b0e448ad9191ac08aa69d400ede8ca52e64
GrahamMendick <GrahamMendick@localhost>
2014-05-09 17:53:42 +0100

    Added RouteConfig unit tests

---
+++ NavigationWebApi.Test/RouteConfigTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Routing;
+
+namespace Navigation.WebApi.Test
+{
+	[TestClass]
+	public class RouteConfigTest
+	{
+		[AssemblyInitialize]
+		public static void AddStateRoutes(TestContext context)
+		{
+			WebApiRouteConfig.AddHttpRoutes();
+			StateController.Navigate("d0");
+		}
+
+		[TestMethod]
+		public void RouteTest()
+		{
+			Assert.AreEqual(4, RouteTable.Routes.Count);
+		}
+
+		[TestMethod]
+		public void WebApiStateRouteHandlerTest()
+		{
+			WebApiStateRouteHandler handler = ((Route)RouteTable.Routes["WebApi0-0"]).RouteHandler as WebApiStateRouteHandler;
+			Assert.IsNotNull(handler);
+			handler = ((Route)RouteTable.Routes["WebApi0-1"]).RouteHandler as WebApiStateRouteHandler;
+			Assert.IsNotNull(handler);
+			handler = ((Route)RouteTable.Routes[2]).RouteHandler as WebApiStateRouteHandler;
+			Assert.IsNotNull(handler);
+			handler = ((Route)RouteTable.Routes[3]).RouteHandler as WebApiStateRouteHandler;
+			Assert.IsNull(handler);
+		}
+	}
+}
