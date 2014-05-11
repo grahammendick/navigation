a017ece382c8f5548ddf820d10eb55fd487f4558
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-11 13:16:22 +0100

    Tuned the WebApi controller response. Create a custom type so it's xml serializable and returned the next link to help consuming clients page

---
+++ NavigationSample/Models/PersonApiSearchModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Navigation.Sample.Models
+{
+	public class PersonApiSearchModel
+	{
+		public IEnumerable<PersonApiModel> People
+		{
+			get;
+			set;
+		}
+
+		public int Total
+		{
+			get;
+			set;
+		}
+
+		public string NextLink
+		{
+			get;
+			set;
+		}
+	}
+}
