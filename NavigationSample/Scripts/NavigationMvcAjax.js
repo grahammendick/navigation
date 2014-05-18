6bd3e714cdf4eeb6cff3486836fe808d5aededc5
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-18 16:03:17 +0100

    Proved the next step where the default link behaviour is intercepted, the udpated panel returned as json and set as innerHTML on the containing span

---
+++ NavigationSample/Scripts/NavigationMvcAjax.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener('click', function (e) {
+    if (e.target.tagName == 'A') {
+        e.preventDefault();
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (req.readyState == 4) {
+                document.getElementById('np1').innerHTML = JSON.parse(req.responseText)['np1'];
+            }
+        };
+        req.open("get", e.target.href, true);
+        req.setRequestHeader('navigation', location.href);
+        req.send();
+    }
+});
