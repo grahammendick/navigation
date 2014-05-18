1f956e89b21781126d222b43fb0683a4f392cd6e
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-18 18:16:14 +0100

    Responded to popstate for history navigation

--- NavigationSample/Scripts/NavigationMvcAjax.js
+++ NavigationSample/Scripts/NavigationMvcAjax.js
@@ -1,15 +1,25 @@
 ﻿document.addEventListener('click', function (e) {
     if (e.target.tagName == 'A') {
         e.preventDefault();
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                document.getElementById('np1').innerHTML = JSON.parse(req.responseText)['np1'];
-                history.pushState('', '', e.target.href);
-            }
-        };
-        req.open("get", e.target.href, true);
-        req.setRequestHeader('navigation', location.pathname + location.search);
-        req.send();
+        navigateAjax(location.pathname + location.search, e.target.getAttribute('href'), true)
     }
 });
+
+window.onpopstate = function (e) {
+    navigateAjax(e.state, location.pathname + location.search, false)
+};
+
+function navigateAjax(oldUrl, newUrl, addHistory)
+{
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            document.getElementById('np1').innerHTML = JSON.parse(req.responseText)['np1'];
+            if (addHistory)
+                history.pushState(newUrl, '', newUrl);
+        }
+    };
+    req.open("get", newUrl, true);
+    req.setRequestHeader('navigation', oldUrl);
+    req.send();
+}
