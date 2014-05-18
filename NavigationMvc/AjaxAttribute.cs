e4c321afec6aa34b356d7f63af9a9b711b2916e5
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-18 15:16:28 +0100

    Proved the concept of wrapping content in panels and only sending back the panel content

---
+++ NavigationMvc/AjaxAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc
+{
+	public class AjaxAttribute : ActionFilterAttribute
+	{
+		public override void OnResultExecuting(ResultExecutingContext filterContext)
+		{
+			//Check if we're in refresh post back mode
+			//if so replace the current writer with an empty one (see onactionexecuting of outputcacheattribute)
+			StringWriter dummyWriter = new StringWriter();
+			TextWriter originalWriter = filterContext.HttpContext.Response.Output;
+			filterContext.HttpContext.Items["originalWriter"] = originalWriter;
+			filterContext.HttpContext.Response.Output = dummyWriter;
+			base.OnResultExecuting(filterContext);
+		}
+
+		public override void OnResultExecuted(ResultExecutedContext filterContext)
+		{
+			//If in refresh post back, get the navigationpanels rendered content
+			//Send that to the browser instead as json
+			filterContext.HttpContext.Response.Output = (TextWriter) filterContext.HttpContext.Items["originalWriter"];
+			filterContext.HttpContext.Response.Write(filterContext.HttpContext.Items["np1"]);
+		}
+	}
+}
---
+++ NavigationMvc/AjaxExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc
+{
+	public static class AjaxExtensions
+	{
+		public static MvcHtmlString AjaxPanel(this HtmlHelper htmlHelper, string navigationDataKeys, Func<dynamic, object> content)
+		{
+			TagBuilder tagBuilder = new TagBuilder("span");
+			tagBuilder.MergeAttribute("id", "np1");
+			tagBuilder.InnerHtml = content(null).ToString();
+			htmlHelper.ViewContext.HttpContext.Items["np1"] = content(null).ToString();
+			return MvcHtmlString.Create(tagBuilder.ToString(TagRenderMode.Normal));
+		}
+	}
+}
