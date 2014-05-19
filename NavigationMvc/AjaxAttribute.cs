5e8a032592690655c384e1b9fa8622cf81a35c34
GrahamMendick <GrahamMendick@localhost>
2014-05-19 20:26:10 +0100

    More code tidying, readying for nested panels

--- NavigationMvc/AjaxAttribute.cs
+++ NavigationMvc/AjaxAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System.Globalization;
 using System.IO;
 using System.Web.Mvc;
 
@@ -9,8 +8,6 @@ namespace Navigation.Mvc
 	{
 		public override void OnResultExecuting(ResultExecutingContext filterContext)
 		{
-			//Check if we're in refresh post back mode
-			//if so replace the current writer with an empty one (see onactionexecuting of outputcacheattribute)
 			StringWriter dummyWriter = new StringWriter();
 			TextWriter originalWriter = filterContext.HttpContext.Response.Output;
 			filterContext.HttpContext.Items["originalWriter"] = originalWriter;
@@ -20,15 +17,13 @@ namespace Navigation.Mvc
 
 		public override void OnResultExecuted(ResultExecutedContext filterContext)
 		{
-			//If in refresh post back, get the navigationpanels rendered content
-			//Send that to the browser instead as json
 			if (filterContext.HttpContext.Request.Headers["navigation"] != null)
 			{
 				filterContext.HttpContext.Response.AppendHeader("Pragma", "no-cache");
 				filterContext.HttpContext.Response.AppendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 				filterContext.HttpContext.Response.AppendHeader("Expires", "0");
 				filterContext.HttpContext.Response.Output = (TextWriter)filterContext.HttpContext.Items["originalWriter"];
-				filterContext.HttpContext.Response.Write(JsonConvert.SerializeObject(filterContext.HttpContext.Items["panels"]));
+				filterContext.HttpContext.Response.Write(JsonConvert.SerializeObject(AjaxNavigationInfo.GetInfo(filterContext.HttpContext).Panels));
 			}
 		}
 	}
---
+++ NavigationMvc/AjaxNavigationInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace Navigation.Mvc
+{
+	internal sealed class AjaxNavigationInfo
+	{
+		private Dictionary<string, string> _Panels = new Dictionary<string, string>();
+
+		internal Dictionary<string, string> Panels
+		{
+			get
+			{
+				return _Panels;
+			}
+		}
+
+		internal string PanelId
+		{
+			get;
+			set;
+		}
+
+		internal static AjaxNavigationInfo GetInfo(HttpContextBase context)
+		{
+			if (context.Items[typeof(AjaxNavigationInfo)] == null)
+				context.Items[typeof(AjaxNavigationInfo)] = new AjaxNavigationInfo();
+			return (AjaxNavigationInfo) context.Items[typeof(AjaxNavigationInfo)];
+		}
+	}
+}
