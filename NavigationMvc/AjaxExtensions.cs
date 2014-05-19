2438a17de070280014824f03745d64f6abf3a320
GrahamMendick <GrahamMendick@localhost>
2014-05-19 20:31:28 +0100

    Tidied code by moving logic into NavigationDataChanged check

--- NavigationMvc/AjaxExtensions.cs
+++ NavigationMvc/AjaxExtensions.cs
@@ -9,13 +9,9 @@ namespace Navigation.Mvc
 		public static MvcHtmlString AjaxPanel(this HtmlHelper htmlHelper, string id, string navigationDataKeys, Func<dynamic, HelperResult> content)
 		{
 			string html = content(null).ToHtmlString();
-			NavigationData data = (NavigationData)htmlHelper.ViewContext.HttpContext.Items["oldData"];
-			if (data != null)
+			if (NavigationDataChanged(htmlHelper, navigationDataKeys))
 			{
-				if (NavigationDataChanged(navigationDataKeys, data))
-				{
-					AjaxNavigationInfo.GetInfo(htmlHelper.ViewContext.HttpContext).Panels[id] = html;
-				}
+				AjaxNavigationInfo.GetInfo(htmlHelper.ViewContext.HttpContext).Panels[id] = html;
 			}
 			TagBuilder tagBuilder = new TagBuilder("span");
 			tagBuilder.MergeAttribute("id", id);
@@ -23,14 +19,18 @@ namespace Navigation.Mvc
 			return MvcHtmlString.Create(tagBuilder.ToString(TagRenderMode.Normal));
 		}
 
-		private static bool NavigationDataChanged(string navigationDataKeys, NavigationData data)
+		private static bool NavigationDataChanged(HtmlHelper htmlHelper, string navigationDataKeys)
 		{
-			if (string.IsNullOrEmpty(navigationDataKeys))
-				return true;
-			foreach (string key in navigationDataKeys.Split(new char[] { ',' }))
+			NavigationData data = (NavigationData)htmlHelper.ViewContext.HttpContext.Items["oldData"];
+			if (data != null)
 			{
-				if (data[key.Trim()] != StateContext.Data[key.Trim()])
+				if (string.IsNullOrEmpty(navigationDataKeys))
 					return true;
+				foreach (string key in navigationDataKeys.Split(new char[] { ',' }))
+				{
+					if (data[key.Trim()] != StateContext.Data[key.Trim()])
+						return true;
+				}
 			}
 			return false;
 		}
