ee21794702a10d06695172ceb94209d7c8651aa9
GrahamMendick <GrahamMendick@localhost>
2014-05-20 20:17:18 +0100

    Tidied up javascript using iife

--- NavigationSample/Scripts/navigation.mvc.js
+++ NavigationSample/Scripts/navigation.mvc.js
@@ -1,29 +1,36 @@
-﻿document.addEventListener('click', function (e) {
-    if (e.target.tagName == 'A') {
-        e.preventDefault();
-        navigateAjax(e.target.getAttribute('href'), true)
-    }
-});
-window.onpopstate = function (e) {
-    navigateAjax(location.pathname + location.search, false)
-};
+﻿(function (win) {
+    
+    if (!(win.history && win.history.pushState))
+        return;
 
-var currentUrl = location.pathname + location.search;
-function navigateAjax(newUrl, addHistory)
-{
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            var panels = JSON.parse(req.responseText);
-            for(var panelId in panels)
-                document.getElementById(panelId).innerHTML = panels[panelId];
-            currentUrl = newUrl;
-            if (addHistory) {
-                history.pushState(newUrl, '', newUrl);
-            }
+    win.document.addEventListener('click', function (e) {
+        if (e.target.tagName && e.target.tagName == 'A') {
+            e.preventDefault();
+            refreshAjax(e.target.getAttribute('href'), true)
         }
+    });
+
+    var link = win.location.pathname + win.location.search;
+    function refreshAjax(newLink, addHistory)
+    {
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (req.readyState == 4 && req.status == 200) {
+                var panels = JSON.parse(req.responseText);
+                for(var id in panels)
+                    document.getElementById(id).innerHTML = panels[id];
+                if (addHistory)
+                    history.pushState(newLink, win.document.title, newLink);
+                link = newLink;
+            }
+        };
+        req.open('get', newLink, true);
+        req.setRequestHeader('Navigation-Link', link);
+        req.send();
+    }
+
+    win.onpopstate = function (e) {
+        refreshAjax(win.location.pathname + win.location.search, false)
     };
-    req.open("get", newUrl, true);
-    req.setRequestHeader('Navigation-Link', currentUrl);
-    req.send();
-}
+
+})(window);
