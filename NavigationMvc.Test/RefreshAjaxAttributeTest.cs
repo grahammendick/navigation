dfa5f17286baacbc8e58a346d375804067e0becc
GrahamMendick <grahammendick@users.noreply.github.com>
2014-05-22 20:29:44 +0100

    Added first unit test for mvc refresh ajax work

---
+++ NavigationMvc.Test/RefreshAjaxAttributeTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Navigation.Mvc.Test
+{
+	[TestClass]
+	public class RefreshAjaxAttributeTest
+	{
+		[TestMethod]
+		public void RefreshAjaxAttributeRegisteredTest()
+		{
+			Assert.IsTrue(GlobalFilters.Filters.First().Instance is RefreshAjaxAttribute);
+		}
+	}
+}
