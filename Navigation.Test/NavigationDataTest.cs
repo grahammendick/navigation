4b19580403a3d6d6acaf47bb107719b3ca9ad12a
GrahamMendick <grahammendick@users.noreply.github.com>
2014-06-17 21:12:54 +0100

    Tidied up usings

--- Navigation.Test/NavigationDataTest.cs
+++ Navigation.Test/NavigationDataTest.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
 using System.Web;
 using System.Web.UI;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Navigation.Test
 {
