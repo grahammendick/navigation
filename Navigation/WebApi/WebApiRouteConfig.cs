63b7ffd52ea9eb9c49e5b92bf61c8600797a2b26
GrahamMendick <grahammendick@users.noreply.github.com>
2014-06-22 19:57:00 +0100

    The default/blank route needs to be registered last because asp.net goes through routes in order and stops when a match is found. For example, if a route of '{sort}' was registered before a route of 'details' then a Url of 'details' would always match the first route. Switching the order around solves the problem. Although registering the default route last won't solve all order problems, it fixes the main one and the others can be solved by prefixing non-default routes with a constant, e.g., list/{sort}

--- Navigation/WebApi/WebApiRouteConfig.cs
+++ Navigation/WebApi/WebApiRouteConfig.cs
@@ -7,35 +7,25 @@ namespace Navigation
 {
 	internal static class WebApiRouteConfig
 	{
-		internal static void AddHttpRoutes()
+		static WebApiRouteConfig()
 		{
-			if (StateInfoConfig.Dialogs == null)
-				return;
 			GlobalConfiguration.Configuration.Services.Insert(typeof(ValueProviderFactory), 0, new NavigationDataValueWebApiProviderFactory());
-			string apiController, action;
-			Route route;
-			RouteValueDictionary defaults;
-			using (RouteTable.Routes.GetWriteLock())
+		}
+
+		internal static void AddHttpRoute(State state)
+		{
+			string apiController = state.Attributes["apiController"] != null ? state.Attributes["apiController"].Trim() : string.Empty;
+			string action = state.Attributes["action"] != null ? state.Attributes["action"].Trim() : string.Empty;
+			if (apiController.Length != 0 && action.Length != 0)
 			{
-				foreach (Dialog dialog in StateInfoConfig.Dialogs)
-				{
-					foreach (State state in dialog.States)
-					{
-						apiController = state.Attributes["apiController"] != null ? state.Attributes["apiController"].Trim() : string.Empty;
-						action = state.Attributes["action"] != null ? state.Attributes["action"].Trim() : string.Empty;
-						if (apiController.Length != 0 && action.Length != 0)
-						{
-							state.StateHandler = new WebApiStateHandler();
-							defaults = StateInfoConfig.GetRouteDefaults(state, state.Route);
-							defaults["controller"] = apiController;
-							defaults["action"] = action;
-							GlobalConfiguration.Configuration.Routes.MapHttpRoute("WebApi" + state.Id, state.Route, defaults);
-							route = (Route) RouteTable.Routes["WebApi" + state.Id];
-							route.DataTokens = new RouteValueDictionary() { { NavigationSettings.Config.StateIdKey, state.Id } };
-							route.RouteHandler = new WebApiStateRouteHandler(state);
-						}
-					}
-				}
+				state.StateHandler = new WebApiStateHandler();
+				RouteValueDictionary defaults = StateInfoConfig.GetRouteDefaults(state, state.Route);
+				defaults["controller"] = apiController;
+				defaults["action"] = action;
+				GlobalConfiguration.Configuration.Routes.MapHttpRoute("WebApi" + state.Id, state.Route, defaults);
+				Route route = (Route) RouteTable.Routes["WebApi" + state.Id];
+				route.DataTokens = new RouteValueDictionary() { { NavigationSettings.Config.StateIdKey, state.Id } };
+				route.RouteHandler = new WebApiStateRouteHandler(state);
 			}
 		}
 	}
--- Navigation/WebForms/PageRouteConfig.cs
+++ Navigation/WebForms/PageRouteConfig.cs
@@ -8,45 +8,38 @@ namespace Navigation
 {
 	internal static class PageRouteConfig
 	{
-		internal static void AddStateRoutes()
+#if NET45Plus
+		private static Type _StateRouteHandlerType;
+
+		static PageRouteConfig()
 		{
-			if (StateInfoConfig.Dialogs == null)
-				return;
-			Route route;
-			using (RouteTable.Routes.GetWriteLock())
+			_StateRouteHandlerType = typeof(StateRouteHandler);
+			if (NavigationSettings.Config.StateRouteHandler.Length != 0)
+				_StateRouteHandlerType = Type.GetType(NavigationSettings.Config.StateRouteHandler);
+		}
+#endif
+
+		internal static void AddRoutes(State state)
+		{
+			if (state.Page.Length != 0 && state.Attributes["route"] != null && state.Attributes["route"].Length != 0)
 			{
-				RouteTable.Routes.Ignore("{resource}.axd/{*pathInfo}");
+				Route route = RouteTable.Routes.MapPageRoute(state.GetRouteName(false), state.GetRoute(false), state.GetPage(false), state.CheckPhysicalUrlAccess,
+					StateInfoConfig.GetRouteDefaults(state, state.GetRoute(false)), null,
+					new RouteValueDictionary() { 
+					{ NavigationSettings.Config.StateIdKey, state.Id }, 
+				});
 #if NET45Plus
-				Type stateRouteHandlerType = typeof(StateRouteHandler);
-				if (NavigationSettings.Config.StateRouteHandler.Length != 0)
-					stateRouteHandlerType = Type.GetType(NavigationSettings.Config.StateRouteHandler);
+				if (state.MobilePage.Length == 0 && state.MobileRoute.Length == 0 && state.MobileMasters.Count == 0 && state.MobileTheme.Length == 0)
+					route.RouteHandler = (StateRouteHandler)Activator.CreateInstance(_StateRouteHandlerType,
+						BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, new object[] { state }, null);
 #endif
-				foreach (Dialog dialog in StateInfoConfig.Dialogs)
-				{
-					foreach (State state in dialog.States)
-					{
-						if (state.Page.Length != 0 && state.Attributes["route"] != null && state.Attributes["route"].Length != 0)
-						{
-							route = RouteTable.Routes.MapPageRoute(state.GetRouteName(false), state.GetRoute(false), state.GetPage(false), state.CheckPhysicalUrlAccess,
-								StateInfoConfig.GetRouteDefaults(state, state.GetRoute(false)), null,
-								new RouteValueDictionary() { 
-								{ NavigationSettings.Config.StateIdKey, state.Id }, 
-							});
-#if NET45Plus
-							if (state.MobilePage.Length == 0 && state.MobileRoute.Length == 0 && state.MobileMasters.Count == 0 && state.MobileTheme.Length == 0)
-								route.RouteHandler = (StateRouteHandler)Activator.CreateInstance(stateRouteHandlerType,
-									BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, new object[] { state }, null);
-#endif
-						}
-						if (state.Page.Length != 0 && state.Attributes["mobileRoute"] != null && state.Attributes["mobileRoute"].Length != 0)
-							RouteTable.Routes.MapPageRoute(state.GetRouteName(true), state.GetRoute(true), state.GetPage(true), state.CheckPhysicalUrlAccess,
-								StateInfoConfig.GetRouteDefaults(state, state.GetRoute(true)), null,
-								new RouteValueDictionary() { 
-								{ NavigationSettings.Config.StateIdKey, state.Id }, 
-							});
-					}
-				}
 			}
+			if (state.Page.Length != 0 && state.Attributes["mobileRoute"] != null && state.Attributes["mobileRoute"].Length != 0)
+				RouteTable.Routes.MapPageRoute(state.GetRouteName(true), state.GetRoute(true), state.GetPage(true), state.CheckPhysicalUrlAccess,
+					StateInfoConfig.GetRouteDefaults(state, state.GetRoute(true)), null,
+					new RouteValueDictionary() { 
+					{ NavigationSettings.Config.StateIdKey, state.Id }, 
+				});
 		}
 	}
 }
