320150e8b382dfe457528404cdc3d4074bc0a904
GrahamMendick <grahammendick@users.noreply.github.com>
2014-07-16 21:18:46 +0100

    Added Mvc and WebApi values server side

--- NavigationGlimpse/Model/StateModel.cs
+++ NavigationGlimpse/Model/StateModel.cs
@@ -6,37 +6,43 @@ namespace Navigation.Glimpse.Model
 	public class StateModel
 	{
 		public StateModel(State state)
-        {
-            State = state;
-        }
+		{
+			State = state;
+		}
 
-        public State State { get; private set; }
+		public State State { get; private set; }
 
-        public int X { get; set; }
+		public int X { get; set; }
 
-        public int Y { get; set; }
+		public int Y { get; set; }
 
-        public int W { get; set; }
+		public int W { get; set; }
 
-        public int H { get; set; }
+		public int H { get; set; }
 
-        public bool Current { get; set; }
+		public bool Current { get; set; }
 
-        public bool Previous { get; set; }
+		public bool Previous { get; set; }
 
-        public int Back { get; set; }
+		public int Back { get; set; }
 
-        public NavigationData Data { get; set; }
+		public NavigationData Data { get; set; }
 
-        public string Page { get; set; }
+		public string Page { get; set; }
 
-        public string Route { get; set; }
+		public string Controller { get; set; }
 
-        public string Theme { get; set; }
+		public string ApiController { get; set; }
 
-        public List<string> Masters { get; set; }
+		public string Action { get; set; }
+
+		public string Route { get; set; }
+
+		public string Theme { get; set; }
+
+		public List<string> Masters { get; set; }
 
 		public List<NavigationLinkModel> NavigationLinks { get; set; }
-    }
+	}
 }
 
