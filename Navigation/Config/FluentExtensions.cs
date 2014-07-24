36ac92738f3408c54101c5c799f7ccdf9ddebddc
GrahamMendick <GrahamMendick@localhost>
2014-07-24 21:18:33 +0100

    Basic classes needed for fluent StateInfo

---
+++ Navigation/Config/FluentExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Navigation
+{
+	public static class FluentExtensions
+	{
+		public static K Defaults<K>(this K state, object defaults) where K : FluentState
+		{
+			state.Defaults = defaults;
+			return state;
+		}
+
+		public static K Theme<K>(this K state, string theme) where K : WebFormsState
+		{
+			state.Theme = theme;
+			return state;
+		}
+	}
+}
---
+++ Navigation/Config/WebFormsState.cs
@@ -0,0 +1,23 @@
+﻿namespace Navigation
+{
+	public class WebFormsState : FluentState
+	{
+		private string Page
+		{ 
+			get;
+			set;
+		}
+
+		internal string Theme
+		{
+			get;
+			set;
+		}
+
+		public WebFormsState(string route, string page) 
+			: base(route)
+		{
+			Page = page;
+		}
+	}
+}
