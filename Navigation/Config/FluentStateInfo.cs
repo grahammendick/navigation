3fc4c70de3f007749bdb1139c3020e8278a14b32
GrahamMendick <GrahamMendick@localhost>
2014-07-25 22:16:52 +0100

    Added start of Fluent StateInfo build logic

--- Navigation/Config/FluentStateInfo.cs
+++ Navigation/Config/FluentStateInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Navigation
 {
@@ -14,6 +15,55 @@ namespace Navigation
 			var dialog = new FluentDialog<TStates, TInitial>(this, key, states, initial(states));
 			_Dialogs.Add(dialog);
 			return dialog;
+		}
+
+		public void Build()
+		{
+			StateInfoCollection<Dialog> dialogs = new StateInfoCollection<Dialog>();
+			Dialog dialog;
+			int dialogIndex = 0;
+			foreach (var fluentDialog in _Dialogs)
+			{
+				dialog = new Dialog();
+				dialog.Index = dialogIndex;
+				dialogIndex++;
+				ProcessStates(dialog, fluentDialog);
+				ProcessTransitions(dialog, fluentDialog);
+			}
+		}
+
+		private static void ProcessStates(Dialog dialog, IFluentDialog fluentDialog)
+		{
+			State state;
+			FluentState fluentState;
+			int stateIndex = 0;
+			var fluentStates = fluentDialog.States.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+			foreach (var stateProperty in fluentStates)
+			{
+				state = new State();
+				fluentState = (FluentState)stateProperty.GetValue(fluentDialog.States);
+				fluentState.Key = stateProperty.Name;
+				state.Parent = dialog;
+				state.Index = stateIndex;
+				stateIndex++;
+				state.Key = fluentState.Key;
+			}
+		}
+
+		private static void ProcessTransitions(Dialog dialog, IFluentDialog fluentDialog)
+		{
+			State state;
+			Transition transition;
+			int transitionIndex = 0;
+			var fluentStates = fluentDialog.States.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+			foreach (var stateProperty in fluentStates)
+			{
+				state = dialog.States[stateProperty.Name];
+				transition = new Transition();
+				transition.Parent = state;
+				transition.Index = transitionIndex;
+				transitionIndex++;
+			}
 		}
 	}
 }
