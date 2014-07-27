7481848d1031ca37ebc6079a4c74e2d06bfe4082
GrahamMendick <GrahamMendick@localhost>
2014-07-27 16:38:27 +0100

    Added WebApi Fluent State

---
+++ Navigation/Config/WebApiState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Navigation
+{
+	public class WebApiState : FluentState
+	{
+		public WebApiState(string route, string controller, string action)
+			: base(route)
+		{
+			if (string.IsNullOrEmpty(controller))
+				throw new ArgumentException("controller");
+			if (string.IsNullOrEmpty(action))
+				throw new ArgumentException("action");
+			AddAttribute("apiController", controller);
+			AddAttribute("action", action);
+		}
+	}
+}
