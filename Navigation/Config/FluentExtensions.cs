4ad1dac1ac6e480da505c744e3ed59f1fca54ed7
GrahamMendick <grahammendick@users.noreply.github.com>
2014-07-27 16:50:21 +0100

    Added Attribute extension method for the fluent interface

--- Navigation/Config/FluentExtensions.cs
+++ Navigation/Config/FluentExtensions.cs
@@ -8,6 +8,12 @@
 			return state;
 		}
 
+		public static K Attribute<K>(this K state, string key, string value) where K : FluentState
+		{
+			state.AddAttribute(key, value);
+			return state;
+		}
+
 		public static K CheckPhysicalUrlAccess<K>(this K state, bool check) where K : WebFormsState
 		{
 			state.AddAttribute("checkPhysicalUrlAccess", check ? "true" : "false");
