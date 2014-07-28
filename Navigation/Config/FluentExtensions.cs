b05f52d27287fcbafd50b147c2126bb7676e26a6
GrahamMendick <grahammendick@users.noreply.github.com>
2014-07-28 19:01:25 +0100

    Added Fluent DefaultTypes

--- Navigation/Config/FluentExtensions.cs
+++ Navigation/Config/FluentExtensions.cs
@@ -1,14 +1,28 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace Navigation
 {
 	public static class FluentExtensions
 	{
+		public static K DefaultTypes<K>(this K state, object defaultTypes) where K : FluentState
+		{
+			foreach (PropertyDescriptor defaultTypeProperty in TypeDescriptor.GetProperties(defaultTypes))
+			{
+				var type = defaultTypeProperty.GetValue(defaultTypes) as Type;
+				if (type == null)
+					throw new ArgumentException("defaultTypes");
+				state.AddDefaultType(defaultTypeProperty.Name, type);
+			}
+			return state;
+		}
+
 		public static K Defaults<K>(this K state, object defaults) where K : FluentState
 		{
 			foreach (PropertyDescriptor defaultProperty in TypeDescriptor.GetProperties(defaults))
 			{
-				state.AddDefault(defaultProperty.Name, defaultProperty.GetValue(defaults));
+				if (defaultProperty.GetValue(defaults) != null)
+					state.AddDefault(defaultProperty.Name, defaultProperty.GetValue(defaults));
 			}
 			return state;
 		}
