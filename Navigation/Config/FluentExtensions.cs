e57775139e4b18d84c9b8545c73466fe2e1494cf
GrahamMendick <GrahamMendick@localhost>
2014-07-29 18:35:12 +0100

    Example of supporting obsolete Fluent attributes. Backward compatability provided throuh Attributes extension method

--- Navigation/Config/FluentExtensions.cs
+++ Navigation/Config/FluentExtensions.cs
@@ -58,5 +58,13 @@ namespace Navigation
 			state.AddAttribute("checkPhysicalUrlAccess", check ? "true" : "false");
 			return state;
 		}
+
+#if !NET40Plus
+		public static K Masters<K>(this K state, params string[] masters) where K : WebFormsState
+		{
+			state.AddAttribute("masters", string.Join(",", masters));
+			return state;
+		}
+#endif
 	}
 }
