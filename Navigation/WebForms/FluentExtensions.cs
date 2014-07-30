d6fef505b3c51464223a72d4e4ea0b01830bea72
GrahamMendick <grahammendick@users.noreply.github.com>
2014-07-30 19:57:07 +0100

    Made FluentExtensions partial so the WebForms specific ones could go in WebForms folder, like was done for State

---
+++ Navigation/WebForms/FluentExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Navigation
+{
+	public static partial class FluentExtensions
+	{
+		public static K CheckPhysicalUrlAccess<K>(this K state, bool check) where K : WebFormsState
+		{
+			state.AddAttribute("checkPhysicalUrlAccess", check ? "true" : "false");
+			return state;
+		}
+
+#if !NET40Plus
+		public static K Masters<K>(this K state, params string[] masters) where K : WebFormsState
+		{
+			state.AddAttribute("masters", string.Join(",", masters));
+			return state;
+		}
+#endif	
+	}
+}
