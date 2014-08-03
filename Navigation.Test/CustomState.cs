99c813365d232e6980cc5b4e42799508c413b7ca
GrahamMendick <GrahamMendick@localhost>
2014-08-03 12:51:06 +0100

    Tidied up usings

--- Navigation.Test/CustomState.cs
+++ Navigation.Test/CustomState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Navigation.Test
+﻿namespace Navigation.Test
 {
 	public class CustomState : FluentState
 	{
