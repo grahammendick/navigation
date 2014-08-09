4aaca4a828fd6d8f8b3ec9cc7c6e20f7ae8863cb
GrahamMendick <grahammendick@users.noreply.github.com>
2014-08-09 14:23:55 +0100

    Added xml documentation

--- Navigation/WebForms/WebFormsState.cs
+++ Navigation/WebForms/WebFormsState.cs
@@ -2,13 +2,26 @@
 
 namespace Navigation
 {
+	/// <summary>
+	/// Configures Web Forms state information. A child of a <see cref="FluentDialog"/>
+	/// element, it represents the endpoint of a navigation
+	/// </summary>
 	public class WebFormsState : FluentState
 	{
 #if !NET40Plus
+		/// <summary>
+		/// Initializes a new instance of the <see cref="WebFormsState"/> class
+		/// </summary>
+		/// <param name="page">The aspx page</param>
 		public WebFormsState(string page)
 		{
 		}
 #else
+		/// <summary>
+		/// Initializes a new instance of the <see cref="WebFormsState"/> class
+		/// </summary>
+		/// <param name="route">The route Url pattern</param>
+		/// <param name="page">The aspx page</param>
 		public WebFormsState(string route, string page)
 			: base(route)
 		{
