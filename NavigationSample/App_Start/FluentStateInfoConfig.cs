e78687a003e03df18f6af2a5b53faed68ae6c7ab
GrahamMendick <grahammendick@users.noreply.github.com>
2014-08-10 09:18:25 +0100

    Switched sample to use Fluent config

---
+++ NavigationSample/App_Start/FluentStateInfoConfig.cs
@@ -0,0 +1,44 @@
+﻿namespace Navigation.Sample
+{
+	public class FluentStateInfoConfig
+	{
+		public static void Register(FluentStateInfo stateInfo)
+		{
+			stateInfo
+				.Dialog("Person", new
+				{
+					Listing = new WebFormsState("List/{startRowIndex}/{maximumRows}/{sortExpression}", "~/Listing.aspx")
+						.Title("Person Search")
+						.Defaults(new { startRowIndex = 0, maximumRows = 10, sortExpression = "Name" })
+						.Derived("totalRowCount")
+						.TrackCrumbTrail(false),
+					Details = new WebFormsState("Person", "~/Details.aspx")
+						.Title("Person Details")
+				}, d => d.Listing)
+					.Transition("Select", d => d.Listing, d => d.Details)
+				.Dialog("MvcPerson", new
+				{
+					Listing = new MvcState("MvcList/{startRowIndex}/{maximumRows}/{sortExpression}", "Person", "List")
+						.Title("Person Search")
+						.Defaults(new { startRowIndex = 0, maximumRows = 10, sortExpression = "Name" })
+						.Derived("totalRowCount")
+						.TrackCrumbTrail(false),
+					Details = new MvcState("MvcPerson", "Person", "GetDetails")
+						.Title("Person Details")
+				}, d => d.Listing)
+					.Transition("Select", d => d.Listing, d => d.Details)
+				.Dialog("WebApiPerson", new
+				{
+					Listing = new WebApiState("WebApiList/{startRowIndex}/{maximumRows}/{sortExpression}", "PersonApi", "GetList")
+						.Title("Person Search")
+						.Defaults(new { startRowIndex = 0, maximumRows = 10, sortExpression = "Name" })
+						.Derived("totalRowCount")
+						.TrackCrumbTrail(false),
+					Details = new WebApiState("WebApiPerson", "PersonApi", "GetDetails")
+						.Title("Person Details")
+				}, d => d.Listing)
+					.Transition("Select", d => d.Listing, d => d.Details)
+				.Build();
+		}
+	}
+}
---
+++ NavigationSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Navigation.Sample.Global" Language="C#" %>
