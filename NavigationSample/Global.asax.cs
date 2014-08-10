81ef9b8e668e597c0ada2585fd86cfce469b6937
GrahamMendick <GrahamMendick@localhost>
2014-08-10 09:18:25 +0100

    Switched sample to use Fluent config

---
+++ NavigationSample/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+
+namespace Navigation.Sample
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			FluentStateInfoConfig.Register(StateInfoConfig.Fluent);
+		}
+	}
+}
