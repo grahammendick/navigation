92961291a455d87c3deda68654a429c475c47b90
GrahamMendick <GrahamMendick@localhost>
2014-08-14 18:28:17 +0100

    Shared the onready ajax response handler between hyperlink and form methods

--- NavigationSample/Scripts/navigation.mvc.js
+++ NavigationSample/Scripts/navigation.mvc.js
@@ -21,10 +21,8 @@
         }
     });
 
-    win.document.addEventListener('submit', function (e) {
-        var els = e.target.elements;
-        var req = new win.XMLHttpRequest();
-        req.onreadystatechange = function () {
+    function onReady(req, addHistory, newLink) {
+        return function () {
             if (req.readyState === 4 && req.status === 200) {
                 var panels = JSON.parse(req.responseText).Panels;
                 for (var id in panels) {
@@ -39,11 +37,19 @@
                     }
                     panel.dispatchEvent(evt);
                 }
-                var newLink = JSON.parse(req.responseText).Link;
-                history.pushState(newLink, win.document.title, newLink);
+                if (!newLink)
+                    newLink = JSON.parse(req.responseText).Link;
+                if (addHistory)
+                    win.history.pushState(newLink, win.document.title, newLink);
                 link = newLink;
             }
         };
+    }
+
+    win.document.addEventListener('submit', function (e) {
+        var els = e.target.elements;
+        var req = new win.XMLHttpRequest();
+        req.onreadystatechange = onReady(req, true, null);
         var data = {};
         for (var i = 0; i < els.length; i++) {
             data[els[0].name] = els[0].value;
@@ -64,26 +70,7 @@
     var link = win.location.pathname + win.location.search;
     function refreshAjax(newLink, addHistory) {
         var req = new win.XMLHttpRequest();
-        req.onreadystatechange = function () {
-            if (req.readyState === 4 && req.status === 200) {
-                var panels = JSON.parse(req.responseText).Panels;
-                for (var id in panels) {
-                    var panel = win.document.getElementById(id);
-                    panel.innerHTML = panels[id];
-                    var evt;
-                    if (typeof Event === 'function')
-                        evt = new win.Event('refreshajax');
-                    else {
-                        evt = win.document.createEvent('Event');
-                        evt.initEvent('refreshajax', false, false);
-                    }
-                    panel.dispatchEvent(evt);
-                }
-                if (addHistory)
-                    history.pushState(newLink, win.document.title, newLink);
-                link = newLink;
-            }
-        };
+        req.onreadystatechange = onReady(req, addHistory, newLink);
         var uniqueLink = newLink;
         uniqueLink += uniqueLink.indexOf('?') > 0 ? '&' : '?';
         uniqueLink += 'refreshajax=' + win.encodeURIComponent(link);
