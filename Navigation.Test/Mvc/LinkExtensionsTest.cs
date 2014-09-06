4f1939b60c784d7317f043e6b95d8775b4b94a5f
GrahamMendick <GrahamMendick@localhost>
2014-09-06 19:31:16 +0100

    Added current data keys refresh link test

--- Navigation.Test/Mvc/LinkExtensionsTest.cs
+++ Navigation.Test/Mvc/LinkExtensionsTest.cs
@@ -193,6 +193,15 @@ namespace Navigation.Test.Mvc
 			Assert.AreEqual("<a data-include-current=\"true\" data-navigation=\"refresh\" href=\"/r1?b=0&amp;a=1\" title=\"details\">link</a>",
 				HtmlHelper.RefreshLink("link", new NavigationData { { "a", "1" } }, true, new { title = "details" }).ToHtmlString());
 		}
+
+		[TestMethod]
+		public void RefreshLinkCurrentDataKeysTest()
+		{
+			StateController.Navigate("d7");
+			StateController.Navigate("t0", new NavigationData { { "b", "0" }, { "c", "2" }, { "d", "3" } });
+			Assert.AreEqual("<a data-current-keys=\" b , c\" data-navigation=\"refresh\" href=\"/r1?b=0&amp;c=2&amp;a=1\">link</a>",
+				HtmlHelper.RefreshLink("link", new NavigationData { { "a", "1" } }, " b , c").ToHtmlString());
+		}
 	}
 }
 #endif
