f96de0c55a469d70fed2b7a46003254cb980556d
GrahamMendick <GrahamMendick@localhost>
2014-09-24 21:37:05 +0100

    Redirecting with an ajax doesn't work. Added a Module to detect refresh ajax redirects and conver them from 302 into 200. The javascript checks for the redirect link and sets the new location.

---
+++ Navigation/Mvc/RefreshAjaxModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Navigation
+{
+	public class RefreshAjaxModule : IHttpModule
+	{
+		public void Init(HttpApplication context)
+		{
+			context.EndRequest += context_EndRequest;
+		}
+
+		private void context_EndRequest(object sender, EventArgs e)
+		{
+			var context = (HttpApplication) sender;
+			var response = context.Response;
+			var info = RefreshAjaxInfo.GetInfo(new HttpContextWrapper(context.Context));
+			if (response.StatusCode == 302 && info.Data != null)
+			{
+				var link = response.RedirectLocation;
+				response.RedirectLocation = null;
+				response.ClearContent();
+				response.StatusCode = 200;
+				response.Write(new JavaScriptSerializer().Serialize(new
+					{
+						RedirectLink = link
+					}));
+			}
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
