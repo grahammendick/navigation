c34a0fe8a28fa509a143c10074f11ee875113f1e
GrahamMendick <grahammendick@users.noreply.github.com>
2014-12-17 16:49:14 +0000

    Created Child Action, following design principles established by Navigation Gmail sample

--- NavigationSample/Controllers/TodoController.cs
+++ NavigationSample/Controllers/TodoController.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Navigation.Sample.Controllers
 {
     public class TodoController : Controller
     {
-        //
-        // GET: /Todo/
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult _Content()
+		{
+			return View();
+		}
 	}
 }
