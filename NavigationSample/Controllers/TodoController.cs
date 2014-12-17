b64315094b89177f5bcdc77dec6076a9f2f952a8
GrahamMendick <GrahamMendick@localhost>
2014-12-17 17:08:23 +0000

    Added basic add functionality and list rendering

--- NavigationSample/Controllers/TodoController.cs
+++ NavigationSample/Controllers/TodoController.cs
@@ -1,9 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using Navigation.Sample.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Navigation.Sample.Controllers
 {
     public class TodoController : Controller
     {
+		private List<Todo> Todos
+		{
+			get
+			{
+				if (Session["todos"] == null)
+					Session["todos"] = new List<Todo>();
+				return (List<Todo>) Session["todos"];
+			}
+		}
+
 		[ActionSelector]
 		public ActionResult Index()
 		{
@@ -12,12 +24,13 @@ namespace Navigation.Sample.Controllers
 
 		public ActionResult _Content()
 		{
-			return View();
+			return View(new TodoModel { Todos = Todos });
 		}
 
 		[ActionSelector]
-		public ActionResult Add()
+		public ActionResult Add(TodoModel todoModel)
 		{
+			Todos.Add(new Todo { Title = todoModel.Title });
 			return View();
 		}
 	}
