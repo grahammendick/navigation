6e51b804f94e0b1b56c1048e36b86eb4785c83cf
GrahamMendick <grahammendick@users.noreply.github.com>
2014-12-17 16:55:08 +0000

    Added Action for adding new Todo, using ActionSelectorAttribute as per Navigation Gmail

--- NavigationSample/Controllers/TodoController.cs
+++ NavigationSample/Controllers/TodoController.cs
@@ -4,12 +4,19 @@ namespace Navigation.Sample.Controllers
 {
     public class TodoController : Controller
     {
+		[ActionSelector]
 		public ActionResult Index()
 		{
 			return View();
 		}
 
 		public ActionResult _Content()
+		{
+			return View();
+		}
+
+		[ActionSelector]
+		public ActionResult Add()
 		{
 			return View();
 		}
