97558e56d7dbb8637615c826e3c8d9afb9daaf5b
GrahamMendick <grahammendick@users.noreply.github.com>
2014-12-17 15:57:13 +0000

    Controller and View for TodoMVC sample

---
+++ NavigationSample/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Navigation.Sample.Controllers
+{
+    public class TodoController : Controller
+    {
+        //
+        // GET: /Todo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
