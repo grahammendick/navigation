4b06ebcfe03863e3b8685a56eebf7f2dd2855aab
GrahamMendick <GrahamMendick@localhost>
2014-12-30 13:04:00 +0000

    Moved the data-todo attribute onto the form now the form is always passed as the target - form submit of custom navigation. Don't want to return different responses if the todo is found or not, so in edit Controller Action always set the 'edit' to true. The client will take care of deleting the todo if it's not found. Simplified client and Controller by putting the cancel functionality into the Edit action.

--- NavigationSample/Scripts/app.js
+++ NavigationSample/Scripts/app.js
@@ -5,7 +5,7 @@
     var edit = false;
 
     refreshAjax.navigating(function (req, resp) {
-        edit = req.data && (req.data.action == 'edit' || req.data.action == 'clear');
+        edit = req.data && req.data.action == 'edit';
     });
 
     refreshAjax.updating(function (req, resp) {
@@ -21,7 +21,7 @@
                     .insertAdjacentHTML('beforeend', '<li><span id="todo' + newTodoId + '" /></li>');
             }
         }
-        if (req.data && (req.data.action == 'edit' || req.data.action == 'clear') && !edit)
+        if (req.data && req.data.action == 'edit' && !edit)
             resp.panels = null;
     });
 
@@ -50,7 +50,7 @@
             });
             el.addEventListener('keyup', function (e) {
                 if (!edit && e.keyCode == 27)
-                    refreshAjax.navigate({ action: 'clear' }, el.form);
+                    refreshAjax.navigate({ action: 'edit', cancel: true }, el.form);
             });
         } else if (req.target.id === 'todo-form') {
             document.getElementById('new-todo').focus();
