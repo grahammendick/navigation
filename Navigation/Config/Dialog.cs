fe7a6f89101a4e3c643183845e675f9363c681d4
GrahamMendick <grahammendick@users.noreply.github.com>
2015-01-16 15:17:41 +0000

    Tidied up usings

--- Navigation/Config/Dialog.cs
+++ Navigation/Config/Dialog.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Threading;
-using System.Web;
 
 namespace Navigation
 {
--- Navigation/Config/State.cs
+++ Navigation/Config/State.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
-using System.Threading;
-using System.Web;
 
 namespace Navigation
 {
