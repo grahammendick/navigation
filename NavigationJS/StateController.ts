b5d279bdf0e3fca7d3f8adccb9625e6b1fbbd09f
GrahamMendick <grahammendick@users.noreply.github.com>
2015-01-28 15:09:38 +0000

    Skeleton navigate TypeScript methods

--- NavigationJS/StateController.ts
+++ NavigationJS/StateController.ts
@@ -1,1 +1,10 @@
-﻿ 
+﻿module Navigation {
+    export class StateController {
+        static navigate(action: string, toData?: NavigationData) {
+        }
+        private static GetNavigationLink(action: string, toData: NavigationData, returnData: NavigationData) {
+        }
+        private static NavigateLink(state: State, url: string) {
+        }
+    }
+}
