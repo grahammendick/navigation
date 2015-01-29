454b89bbf831bebb2b9eaf95edaa39056083ee7d
GrahamMendick <GrahamMendick@localhost>
2015-01-29 11:30:44 +0000

    Added getNextState to StateController. Chose to have two transition lists on State, one accessed by index and the other by key.

--- NavigationJS/Navigation.ts
+++ NavigationJS/Navigation.ts
@@ -8,7 +8,8 @@
     }
 
     export class State {
-        states: Array<Transition>;
+        transitions: Array<Transition>;
+        _transitions: { [index: string]: Transition };
         parent: Dialog;
         index: number;
         key: string;
@@ -17,6 +18,9 @@
         trackCrumbTrail: boolean;
         attributes: Array<string>;
         stateHandler: IStateHandler;
+        Id(): string{
+            return this.parent.index.toString() + '-' + this.index.toString();
+        }
     }
 
     export class Transition {
@@ -39,7 +43,15 @@
     }
 
     export class StateInfoConfig {
-        dialogs: Array<Dialog>;
+        static dialogs: Array<Dialog>;
+        static _dialogs: { [index: string]: Dialog };
+    }
+
+    export class StateContext {
+        static previousState: State;
+        static previousDialog: Dialog;
+        static state: State;
+        static dialog: Dialog;
     }
 
     class CrumbTrailManager {
@@ -53,10 +65,23 @@
         }
 
         static getNavigationLink(action: string, toData?: any): string {
-            return CrumbTrailManager.getHref(action, toData, null);
+            return CrumbTrailManager.getHref(this.getNextState(action).Id(), toData, null);
         }
 
         private static navigateLink(state: State, url: string) {
+        }
+
+        private static getNextState(action: string): State {
+            if (!action)
+                throw "action is required";
+            var nextState: State = null;
+            if (StateContext.state && StateContext.state._transitions[action])
+                nextState = StateContext.state._transitions[action].to;
+            if (!nextState && StateInfoConfig._dialogs[action])
+                nextState = StateInfoConfig._dialogs[action].initial;
+            if (!nextState)
+                throw "invalid action";
+            return nextState;
         }
     }
 }
