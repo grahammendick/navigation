2bb1bbb4b9fc27823b145f669cc46f5208175a99
GrahamMendick <GrahamMendick@localhost>
2015-02-16 18:31:27 +0000

    Start of Navigation Router. Couldn't find a javascript router that supported defaults (like .net route defaults), e.g., x/{y} and defaults = { y: 'a' }. Passing null should produce x and parsing x should return y = 'a'. None of them supported parsing a route into data, all expected a callback to receive the data. Don't need to worry about query strings or slugs, so code should be simpler?

---
+++ NavigationJS/Router.ts
@@ -0,0 +1,70 @@
+﻿module Navigation {
+    export class Router {
+    }
+
+    export class Route {
+        path: string;
+        defaults: any;
+        dataTokens: any;
+        private segments: Array<Segment> = [];
+        private pattern: RegExp;
+
+        constructor(path: string, defaults?: any, dataTokens?: any) {
+            this.path = path;
+            this.defaults = defaults;
+            this.dataTokens = dataTokens;
+        }
+
+        private parse() {
+            var subPaths = this.path.split('/').reverse();
+            var segments: Array<Segment> = [];
+            var mandatory = false;
+            for (var i = 0; i < subPaths.length; i++) {
+                var segment = new Segment(subPaths[i], mandatory);
+                segment.parse(this.defaults);
+                segments.push(segment);
+                mandatory = mandatory || segment.mandatory;
+            }
+            this.segments = segments.reverse();
+            var subPatterns: Array<string> = [];
+            for (var i = 0; i < this.segments.length; i++) {
+                subPatterns.push(this.segments[i].pattern.source);
+            }
+            this.pattern = new RegExp(subPatterns.join('\/'));
+        }
+    }
+
+    class Segment {
+        path: string;
+        mandatory: boolean;
+        pattern: RegExp;
+        paramsPattern: RegExp = /\{([^}]+)\}/g;
+        escapePattern: RegExp = /[\.+*\^$\[\](){}']/g;
+        params: Array<string> = [];
+
+        constructor(path: string, mandatory: boolean) {
+            this.path = path;
+            this.mandatory = mandatory;
+        }
+
+        parse(defaults: any) {
+            var match: RegExpExecArray;
+            this.paramsPattern.lastIndex = 0;
+            var optional = false;
+            while (match = this.paramsPattern.exec(this.path)) {
+                var param = match[1];
+                if (param.slice(-1) === '?')
+                    param = param.substring(0, param.length - 1);
+                this.params.push(param);
+                if (this.path.length === match[0].length && (param.slice(-1) === '?' || defaults[param]))
+                    optional = true;
+            }
+            this.paramsPattern.lastIndex = 0;
+            this.mandatory = this.mandatory || !optional;
+            var pattern = this.path.replace(this.paramsPattern, '?');
+            pattern = pattern.replace(this.escapePattern, '\\$&');
+            pattern = pattern.replace(/\\?/g, '([^/]+)' + (this.mandatory ? '' : '?'));
+            this.pattern = new RegExp(pattern);
+        }    
+    }
+} 
