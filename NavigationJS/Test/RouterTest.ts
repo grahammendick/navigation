60f78e4304a2686a0eac8e3c6132801fcd65bdfb
GrahamMendick <grahammendick@users.noreply.github.com>
2015-02-17 11:32:57 +0000

    Added first real Router unit tests

--- NavigationJS/Test/RouterTest.ts
+++ NavigationJS/Test/RouterTest.ts
@@ -1,4 +1,42 @@
 ﻿module RouterTest {
+    QUnit.test('NoParamRouteMatchTest', function (assert) {
+        var router = new Navigation.Router();
+        var route = router.addRoute('abc');
+        var routeMatch = router.match('abc');
+        assert.equal(routeMatch.route, route);
+        assert.equal(Object.keys(routeMatch.data).length, 0);
+    });
+
+    QUnit.test('NoParamRouteNonMatchTest', function (assert) {
+        var router = new Navigation.Router();
+        var route = router.addRoute('abc');
+        assert.equal(router.match('abd'), null);
+        assert.equal(router.match('abcd'), null);
+        assert.equal(router.match('dbc'), null);
+        assert.equal(router.match('ab/c'), null);
+        assert.equal(router.match('adc'), null);
+        assert.equal(router.match('aabc'), null);
+    });
+
+    QUnit.test('NoParamRouteMultipleSegmentMatchTest', function (assert) {
+        var router = new Navigation.Router();
+        var route = router.addRoute('ab/c');
+        var routeMatch = router.match('ab/c');
+        assert.equal(routeMatch.route, route);
+        assert.equal(Object.keys(routeMatch.data).length, 0);
+    });
+
+    QUnit.test('NoParamRouteMultipleSegmentNonMatchTest', function (assert) {
+        var router = new Navigation.Router();
+        var route = router.addRoute('ab/c');
+        assert.equal(router.match('ab/d'), null);
+        assert.equal(router.match('ab/cd'), null);
+        assert.equal(router.match('a/b/c'), null);
+        assert.equal(router.match('abc'), null);
+        assert.equal(router.match('ad/c'), null);
+        assert.equal(router.match('aab/c'), null);
+    });
+
     QUnit.test('a', function (assert) {
         var router = new Navigation.Router();
         var route = router.addRoute('a/{x}');
