5d764754bdb90bf066c8b7f62612db2f8aa739f2
GrahamMendick <grahammendick@users.noreply.github.com>
2015-02-19 19:14:04 +0000

    Renamed function for clarity

--- NavigationJS/Router.ts
+++ NavigationJS/Router.ts
@@ -104,14 +104,14 @@
 
         private parse() {
             var optional = this.path.length === 0;
-            var replace = (match: string, param: string) => {
+            var replaceParam = (match: string, param: string) => {
                 var name = param.slice(-1) === '?' ? param.substring(0, param.length - 1) : param;
                 this.params.push(name);
                 var optionalOrDefault = param.slice(-1) === '?' || this.defaults[name];
                 optional = this.path.length === match.length && optionalOrDefault;
                 return '?';
             }
-            this.pattern = this.path.replace(this.paramsPattern, replace);
+            this.pattern = this.path.replace(this.paramsPattern, replaceParam);
             this.optional = this.optional && optional;
             this.pattern = this.pattern.replace(this.escapePattern, '\\$&');
             if (!this.optional)
@@ -122,12 +122,12 @@
 
         build(data?: any): { path: string; optional: boolean } {
             var optional = this.optional;
-            var replace = (match: string, param: string) => {
+            var replaceParam = (match: string, param: string) => {
                 var name = param.slice(-1) === '?' ? param.substring(0, param.length - 1) : param;
                 optional = optional && (data[name] == null || data[name] === this.defaults[name]);
                 return data[name] != null ? data[name] : this.defaults[name];
             }
-            return { path: this.path.replace(this.paramsPattern, replace), optional: optional };
+            return { path: this.path.replace(this.paramsPattern, replaceParam), optional: optional };
         }
     }
 }
