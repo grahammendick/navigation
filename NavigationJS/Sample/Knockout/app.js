34a6189e6a30671fcaf510280d198a6f938da78e
GrahamMendick <GrahamMendick@localhost>
2015-02-21 15:43:21 +0000

    Added files for Navigation Knockout sample

---
+++ NavigationJS/Sample/Knockout/app.js
@@ -0,0 +1,14 @@
+﻿function WebmailViewModel() {
+	// Data
+	var self = this;
+	self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+	self.chosenFolderId = ko.observable();
+	self.chosenFolderData = ko.observable();
+	self.chosenMailData = ko.observable();
+
+	// Behaviours    
+	self.goToFolder = function (folder) { };
+	self.goToMail = function (mail) { };
+};
+
+ko.applyBindings(new WebmailViewModel());
