c2f6a7cb0d6fdc241faeded9a73b3bb054052759
GrahamMendick <GrahamMendick@localhost>
2015-02-24 11:50:11 +0000

    Added Navigation Knockout plug in. Implemented a navigationLink custom binding. Replaced the view model sample code with data bind statements in the html.

---
+++ NavigationJS/Navigation.Knockout.ts
@@ -0,0 +1,21 @@
+﻿module NavigationKnockout {
+    ko.bindingHandlers['navigationLink'] = {
+        update: (element: Element, valueAccessor, allBindings: KnockoutAllBindingsAccessor) => {
+            var action = ko.utils.unwrapObservable<string>(valueAccessor());
+            var dataValue = allBindings.get('toData');
+            var data = {};
+            for (var key in dataValue) {
+                data[key] = ko.utils.unwrapObservable(dataValue[key]);
+            } 
+            var navigate = (e) => {
+                if (!e.ctrlKey && !e.shiftKey) {
+                    e.preventDefault();
+                    Navigation.StateController.navigate(action, data);
+                }
+            }
+            element['href'] = '#' + Navigation.StateController.getNavigationLink(action, data);
+            element.removeEventListener('click', navigate);
+            element.addEventListener('click', navigate);
+        }
+    };
+}
