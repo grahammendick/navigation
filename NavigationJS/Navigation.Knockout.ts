3674e7e4ed7717069b7d36cacf99c28a87aa0c8e
GrahamMendick <grahammendick@users.noreply.github.com>
2015-02-25 12:54:36 +0000

    Renamed dataValue to toData

--- NavigationJS/Navigation.Knockout.ts
+++ NavigationJS/Navigation.Knockout.ts
@@ -2,16 +2,16 @@
     ko.bindingHandlers['navigationLink'] = {
         update: (element: Element, valueAccessor, allBindings: KnockoutAllBindingsAccessor) => {
             var action = ko.utils.unwrapObservable<string>(valueAccessor());
-            var dataValue = allBindings.get('toData');
+            var toData = allBindings.get('toData');
             var includeCurrentData: boolean = allBindings.get('includeCurrentData');
             var currentDataKeys: string = allBindings.get('currentDataKeys');
             var navigate = (e) => {
                 if (!e.ctrlKey && !e.shiftKey) {
                     e.preventDefault();
-                    Navigation.StateController.navigate(action, getData(dataValue, includeCurrentData, currentDataKeys));
+                    Navigation.StateController.navigate(action, getData(toData, includeCurrentData, currentDataKeys));
                 }
             }
-            var link = Navigation.StateController.getNavigationLink(action, getData(dataValue, includeCurrentData, currentDataKeys));
+            var link = Navigation.StateController.getNavigationLink(action, getData(toData, includeCurrentData, currentDataKeys));
             element['href'] = Navigation.historyManager.getHref(link);
             element.removeEventListener('click', navigate);
             element.addEventListener('click', navigate);
@@ -36,30 +36,30 @@
 
     ko.bindingHandlers['refreshLink'] = {
         update: (element: Element, valueAccessor, allBindings: KnockoutAllBindingsAccessor) => {
-            var dataValue = ko.utils.unwrapObservable<any>(valueAccessor());
+            var toData = ko.utils.unwrapObservable<any>(valueAccessor());
             var includeCurrentData: boolean = allBindings.get('includeCurrentData');
             var currentDataKeys: string = allBindings.get('currentDataKeys');
             var navigate = (e) => {
                 if (!e.ctrlKey && !e.shiftKey) {
                     e.preventDefault();
-                    Navigation.StateController.refresh(getData(dataValue, includeCurrentData, currentDataKeys));
+                    Navigation.StateController.refresh(getData(toData, includeCurrentData, currentDataKeys));
                 }
             }
-            var link = Navigation.StateController.getRefreshLink(getData(dataValue, includeCurrentData, currentDataKeys));
+            var link = Navigation.StateController.getRefreshLink(getData(toData, includeCurrentData, currentDataKeys));
             element['href'] = Navigation.historyManager.getHref(link);
             element.removeEventListener('click', navigate);
             element.addEventListener('click', navigate);
         }
     };
 
-    function getData(dataValue: any, includeCurrentData: boolean, currentDataKeys: string) {
+    function getData(toData: any, includeCurrentData: boolean, currentDataKeys: string) {
         var data = {};
         if (currentDataKeys)
             data = Navigation.StateContext.newCurrentData(currentDataKeys.trim().split(/\s*,\s*/));
         if (includeCurrentData)
             data = Navigation.StateContext.newCurrentData();
-        for (var key in dataValue) {
-            data[key] = ko.utils.unwrapObservable(dataValue[key]);
+        for (var key in toData) {
+            data[key] = ko.utils.unwrapObservable(toData[key]);
         }
         return data;
     }
