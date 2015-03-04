687eefebccf86f228f23a3f92247a50751494e91
GrahamMendick <GrahamMendick@localhost>
2015-03-04 19:11:38 +0000

    Added NavigationLink component. Haven't put it in typescript namespace because 1. Navigation.React module name clashed with React module name 2. Couldn't use it if inside namespace (need to see if CommonJS modules work)

--- NavigationJS/Navigation.React.ts
+++ NavigationJS/Navigation.React.ts
@@ -1,2 +1,22 @@
-﻿module Navigation.React {
-}
+﻿var NavigationLink = React.createClass({
+    navigate: function (e) {
+        if (!e.ctrlKey && !e.shiftKey) {
+            e.preventDefault();
+            Navigation.StateController.navigate(this.props.action, this.props.toData);
+        }
+    },
+    render: function () {
+        var data: any = {};
+        for (var key in this.props) {
+            data[key] = this.props[key];
+        }
+        var link = Navigation.StateController.getNavigationLink(this.props.action, this.props.toData);
+        data.href = Navigation.historyManager.getHref(link);
+        data.onClick = this.navigate;
+        delete data.action;
+        delete data.toData;
+        return (
+            React.createElement('a', data)
+        )
+    }
+});
