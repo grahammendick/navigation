c9caf1193846d806defedf0e8e522a57c90e7579
GrahamMendick <grahammendick@users.noreply.github.com>
2015-03-05 12:20:05 +0000

    Added currentData props to NavigationLink. Worked out how to put Navigation Router in a module - declared NavigationLink outside the module pointing to NavigationLink from the module (this is like the require statement of commonjs modules). Need to revisit when it comes to publishing code. Put the Navigation.start() call in top WebMail component otherwise State isn't set up and NavigationLinks won't work. Could pull the state navigated code outside all components and pass the data in - better for server rendering? To match, put the start back in the ViewModel in Knockout sample

--- NavigationJS/Navigation.React.ts
+++ NavigationJS/Navigation.React.ts
@@ -1,22 +1,51 @@
-﻿var NavigationLink = React.createClass({
-    navigate: function (e) {
-        if (!e.ctrlKey && !e.shiftKey) {
-            e.preventDefault();
-            Navigation.StateController.navigate(this.props.action, this.props.toData);
+﻿module NavigationReact {
+    export var NavigationLink = React.createClass({
+        render: function () {
+            var props = cloneProps(this);
+            var includeCurrentData = props.includeCurrentData;
+            var currentDataKeys = props.currentDataKeys;
+            var action = props.action;
+            var toData = getData(props.toData, includeCurrentData, currentDataKeys);
+            var link = Navigation.StateController.getNavigationLink(action, toData);
+            props.href = Navigation.historyManager.getHref(link);
+            props.onClick = getClickListener(() => Navigation.StateController.navigate(action, toData))
+            clearProps(props);
+            return (
+                React.createElement('a', props)
+            );
         }
-    },
-    render: function () {
-        var data: any = {};
-        for (var key in this.props) {
-            data[key] = this.props[key];
+    });
+
+    function cloneProps(elem: React.ReactElement<any, any>): any {
+        var props = {};
+        for (var key in elem.props) {
+            props[key] = elem.props[key];
         }
-        var link = Navigation.StateController.getNavigationLink(this.props.action, this.props.toData);
-        data.href = Navigation.historyManager.getHref(link);
-        data.onClick = this.navigate;
-        delete data.action;
-        delete data.toData;
-        return (
-            React.createElement('a', data)
-        );
+        return props;
     }
-});
+
+    function getData(toData, includeCurrentData: boolean, currentDataKeys: string) {
+        if (includeCurrentData)
+            toData = Navigation.StateContext.includeCurrentData(toData);
+        if (currentDataKeys)
+            toData = Navigation.StateContext.includeCurrentData(toData, currentDataKeys.trim().split(/\s*,\s*/));
+        return toData;
+    }
+
+    function getClickListener(listener: () => void) {
+        return (e: MouseEvent) => {
+            if (!e.ctrlKey && !e.shiftKey) {
+                e.preventDefault();
+                listener();
+            }
+        };
+    }
+
+    function clearProps(props: any) {
+        delete props.action;
+        delete props.toData;
+        delete props.includeCurrentData;
+        delete props.currentDataKeys;
+    }
+}
+var NavigationLink = NavigationReact.NavigationLink;
