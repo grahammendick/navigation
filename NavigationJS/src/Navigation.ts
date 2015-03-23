167d7b3e77465b6ed866bc2f7e37630a47c25cbb
Graham Mendick <graham.mendick@gmail.com>
2015-03-23 15:36:16 +0000

    Exposed public api via Navigation export

--- NavigationJS/src/Navigation.ts
+++ NavigationJS/src/Navigation.ts
@@ -1,1 +1,18 @@
-﻿ 
+﻿import historyManager = require('history/historyManager');
+import router = require('router');
+import settings = require('settings');
+import start = require('start');
+import StateContext = require('StateContext');
+import StateController = require('StateController');
+import StateInfoConfig = require('config/StateInfoConfig');
+
+class Navigation {
+    static StateContext = StateContext;
+    static StateController = StateController;
+    static StateInfoConfig = StateInfoConfig;
+    static historyManager = historyManager;
+    static router = router;
+    static settings = settings;
+    static start = start;
+} 
+export = Navigation;
