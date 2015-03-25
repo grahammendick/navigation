47576e39132df96a94cb141642018d746da8ca92
Graham Mendick <graham.mendick@gmail.com>
2015-03-25 11:55:36 +0000

    Piped derequire
    As per advice on browserify when runnning in standalone mode,
    https://github.com/substack/node-browserify (also, noticed React had
    done it)

--- NavigationJS/gulpfile.js
+++ NavigationJS/gulpfile.js
@@ -1,18 +1,18 @@
 ﻿var browserify = require('browserify');
 var gulp = require('gulp');
+var derequire = require('gulp-derequire');
 var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
 var streamify = require('gulp-streamify');
 var uglify = require('gulp-uglify');
-var source = require('vinyl-source-stream');
+
 
 gulp.task('build', function () {
-	//var b = browserify();
-	//b.add('./src/Navigation.js', {standalone : 'Navigation'});
-	var bundleStream = browserify('./src/Navigation.js', { standalone: 'Navigation' }).bundle()
-
-	bundleStream
-	  .pipe(source('navigation.js'))
-	  //.pipe(streamify(uglify()))
-	  .pipe(rename('navigation.js'))
-	  .pipe(gulp.dest('./build'))
+	browserify('./src/Navigation.js', { standalone: 'Navigation' })
+		.bundle()
+		.pipe(source('navigation.js'))
+		//.pipe(streamify(uglify()))
+		.pipe(rename('navigation.js'))
+		.pipe(derequire())
+		.pipe(gulp.dest('./build'))
 })
