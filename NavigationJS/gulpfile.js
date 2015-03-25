98492b36469f35827eef3eea5f50388db3015657
Graham Mendick <graham.mendick@gmail.com>
2015-03-25 12:05:26 +0000

    Added mocha test gulp task

--- NavigationJS/gulpfile.js
+++ NavigationJS/gulpfile.js
@@ -1,18 +1,23 @@
 ﻿var browserify = require('browserify');
 var gulp = require('gulp');
 var derequire = require('gulp-derequire');
+var mocha = require('gulp-mocha');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var streamify = require('gulp-streamify');
 var uglify = require('gulp-uglify');
 
+gulp.task('test', function () {
+	return gulp.src('./test/*.js', { read: false })
+        .pipe(mocha({ reporter: 'nyan' }));
+});
 
 gulp.task('build', function () {
 	browserify('./src/Navigation.js', { standalone: 'Navigation' })
 		.bundle()
 		.pipe(source('navigation.js'))
-		//.pipe(streamify(uglify()))
 		.pipe(rename('navigation.js'))
 		.pipe(derequire())
+		//.pipe(streamify(uglify()))
 		.pipe(gulp.dest('./build'))
 })
