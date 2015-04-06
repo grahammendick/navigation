fc5f23431081b1c7249f2417f9f28e476c4abacf
Graham Mendick <graham.mendick@gmail.com>
2015-04-06 22:37:14 +0100

    Added code mirror and navigation
    Converted textarea into code mirror and eval'ed the code when run's
    clicked

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -1,1 +1,8 @@
-document.getElementById('code').value = tutorial.exercise;
+var code = document.getElementById('code');
+code.value = tutorial.exercise;
+var codeMirror = CodeMirror.fromTextArea(code, {
+	lineNumbers: true,
+});
+document.getElementById('run').addEventListener('click', function () {
+	eval('(' + codeMirror.getValue() + ')')({});
+});
