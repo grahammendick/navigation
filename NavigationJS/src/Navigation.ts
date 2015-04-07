cb46b2f2f97c969a4374f612de06db20c3ff82b1
Graham Mendick <graham.mendick@gmail.com>
2015-04-07 17:51:15 +0100

    Added more types to Navigation api
    All public types should go on api otherwise there won't be type on
    Navigation global

--- NavigationJS/src/Navigation.ts
+++ NavigationJS/src/Navigation.ts
@@ -1,19 +1,33 @@
 ﻿import StateContext = require('./StateContext');
 import StateController = require('./StateController');
+import Dialog = require('./config/Dialog');
+import State = require('./config/State');
+import Transition = require('./config/Transition');
 import StateInfoConfig = require('./config/StateInfoConfig');
 import HistoryNavigator = require('./history/HistoryNavigator');
 import HashHistoryManager = require('./history/HashHistoryManager');
 import HTML5HistoryManager = require('./history/HTML5HistoryManager');
+import Crumb = require('./Crumb');
+import StateHandler = require('./StateHandler');
+import StateRouter = require('./StateRouter');
+import NavigationSettings = require('./NavigationSettings');
 import router = require('./router');
 import settings = require('./settings');
 
 class Navigation {
+    static Dialog = Dialog;
+    static State = State;
+    static Transition = Transition;
     static StateInfoConfig = StateInfoConfig;
-    static StateContext = StateContext;
-    static StateController = StateController;
     static HashHistoryManager = HashHistoryManager;
     static HTML5HistoryManager = HTML5HistoryManager;
     static historyManager = HistoryNavigator.historyManager;
+    static Crumb = Crumb;
+    static NavigationSettings = NavigationSettings;
+    static StateContext = StateContext;
+    static StateController = StateController;
+    static StateHandler = StateHandler;
+    static StateRouter = StateRouter;
     static router = router;
     static settings = settings;
     static start = (url?: string) => {
