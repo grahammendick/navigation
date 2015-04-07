51657c5e0e69372b2561e14638ae72a4cb671471
Graham Mendick <graham.mendick@gmail.com>
2015-04-07 13:39:32 +0100

    Extracted state info names from code
    Validate state names for dot property syntax, dialog.states.name.
    Otherwise revert to default names

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -26,17 +26,41 @@
 	var codeMirror = CodeMirror.fromTextArea(code, {
 		lineNumbers: true,
 	});
+	function runCode() {
+		eval('(' + codeMirror.getValue() + ')')({});
+		var dialog = Navigation.StateInfoConfig._dialogs[0];
+		var listing = dialog._states[0];
+		var details = dialog._states[1];
+		var dialogName = 'masterDetails';
+		var listingName = 'listing';
+		var detailsName = 'details';
+		var transitionName = 'select';
+		var nameRegex = /^[a-zA-Z_$][0-9a-zA-Z_$]*$/;
+		if (nameRegex.exec(listing.key) && nameRegex.exec(details.key)) {
+			dialogName = dialog.key;
+			listingName = listing.key;
+			detailsName = details.key;
+			if (listing._transitions.length !== 0)
+				transitionName = listing._transitions[0].key;
+		}
+		localStorage.setItem('tutorial', JSON.stringify({
+			dialog: dialogName,
+			listing: listingName,
+			details: detailsName,
+			transition: transitionName
+		}));
+	}
 	var run = document.getElementById('run');
 	var cheat = document.getElementById('cheat');
 	var next = document.getElementById('next');
 	run.addEventListener('click', function () {
 		next.style.display = 'none';
-		eval('(' + codeMirror.getValue() + ')')({});
+		runCode();
 		next.style.display = 'block';
 	});
 	cheat.addEventListener('click', function () {
 		codeMirror.setValue(tutorial.answer);
-		eval('(' + codeMirror.getValue() + ')')({});
+		runCode();
 		next.style.display = 'block';
 	});
 	next.addEventListener('click', function () {
