e460234e6a8c6d979513d508803b225f08bcc0af
Graham Mendick <graham.mendick@gmail.com>
2015-04-08 22:32:53 +0100

    Added click listener
    Very similar to NavigationReact plugin

--- NavigationJS/src/angular/LinkUtility.ts
+++ NavigationJS/src/angular/LinkUtility.ts
@@ -1,12 +1,30 @@
 ﻿import Navigation = require('../Navigation');
 
 class LinkUtility {
+    static setLink(element: ng.IAugmentedJQuery, attrs: ng.IAttributes, linkAccessor: () => string) {
+        try {
+            attrs.$set('href', Navigation.historyManager.getHref(linkAccessor()));
+        element.bind('click', (e) => this.onClick(e, <HTMLAnchorElement> element[0]));
+        } catch (e) {
+            attrs.$set('href', null);
+        }
+    }
+
     static getData(toData, includeCurrentData: boolean, currentDataKeys: string): any {
         if (currentDataKeys)
             toData = Navigation.StateContext.includeCurrentData(toData, currentDataKeys.trim().split(/\s*,\s*/));
         if (includeCurrentData)
             toData = Navigation.StateContext.includeCurrentData(toData);
         return toData;
+    }
+
+    static onClick(e: JQueryEventObject, element: HTMLAnchorElement) {
+        if (!e.ctrlKey && !e.shiftKey) {
+            if (element.href) {
+                e.preventDefault();
+                Navigation.StateController.navigateLink(Navigation.historyManager.getUrl(element));
+            }
+        }
     }
 }
 export = LinkUtility; 
--- NavigationJS/src/angular/NavigationLink.ts
+++ NavigationJS/src/angular/NavigationLink.ts
@@ -2,27 +2,21 @@
 import Navigation = require('../Navigation');
 
 var NavigationLink = () => {
-        return {
-            restrict: 'A',
-            link: (scope: ng.IScope, element, attrs: ng.IAttributes) => {
-                scope.$watchGroup([
-                    attrs['navigationLink'],
-                    attrs['toData'],
-                    attrs['includeCurrentData'],
-                    attrs['currentDataKeys']
-                ], function (values) {
-                    try {
-                        var toData = LinkUtility.getData(values[1], values[2], values[3]);
-                        var navigationLink = Navigation.StateController.getNavigationLink(values[0], toData);
-                        attrs.$set('href', Navigation.historyManager.getHref(navigationLink));
-                    } catch (e) {
-                        attrs.$set('href', null);
-                    }
-                });
-            }
+    return {
+        restrict: 'EA',
+        link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+            scope.$watchGroup([
+                attrs['navigationLink'],
+                attrs['toData'],
+                attrs['includeCurrentData'],
+                attrs['currentDataKeys']
+            ], function (values) {
+                var toData = LinkUtility.getData(values[1], values[2], values[3]);
+                LinkUtility.setLink(element, attrs, () => Navigation.StateController.getNavigationLink(values[0], toData));
+            });
         }
-    };
-
+    }
+};
 angular.module('NavigationAngular', [])
     .directive('navigationLink', NavigationLink);
 export = NavigationLink;
