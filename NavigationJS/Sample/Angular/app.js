1e5f9a549dabf924eb7cbe02d2b4efc2b7e7f396
Graham Mendick <graham.mendick@gmail.com>
2015-04-08 14:38:04 +0100

    Added basic angular sample to test directive

---
+++ NavigationJS/Sample/Angular/app.js
@@ -0,0 +1,11 @@
+﻿function PersonController($scope) {
+	$scope.action = 'select';
+}
+
+Navigation.StateInfoConfig.build([
+	{ key: 'person', initial: 'list', states: [
+		{ key: 'list', route: '{startRowIndex}/{maximumRows}/{sortExpression}', defaults: { startRowIndex: 0, maximumRows: 10, sortExpression: 'Name'}, trackCrumbTrail: false, title: 'Person Search', transitions: [
+			{ key: 'select', to: 'details' }]},
+		{ key: 'details', route: 'person', title: 'Person Details', }]}
+]);
+Navigation.start();
