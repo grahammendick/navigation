77b290b3133e33af45e1b14be283a67c2be0b7cf
Graham Mendick <graham.mendick@gmail.com>
2015-04-08 14:28:14 +0100

    Started NavigationLink angular directive

---
+++ NavigationJS/src/angular/NavigationLink.ts
@@ -0,0 +1,18 @@
+﻿import Navigation = require('../Navigation');
+
+var NavigationLink = () => {
+        return {
+            restrict: 'A',
+            link: (scope, element, attrs) => {
+                try {
+                    var navigationLink = Navigation.StateController.getNavigationLink(attrs.navigationLink);
+                    element.href = Navigation.historyManager.getHref(navigationLink);
+                } catch (e) {
+                }
+            }
+        }
+    };
+
+angular.module('NavigationAngular')
+    .directive('NavigationLink', NavigationLink);
+export = NavigationLink;
