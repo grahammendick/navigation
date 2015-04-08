fe1952a73a78a06c4d6cd61dfdf28f79c8f605eb
Graham Mendick <graham.mendick@gmail.com>
2015-04-08 22:17:16 +0100

    Watched other attributes

---
+++ NavigationJS/src/angular/LinkUtility.ts
@@ -0,0 +1,12 @@
+﻿import Navigation = require('../Navigation');
+
+class LinkUtility {
+    static getData(toData, includeCurrentData: boolean, currentDataKeys: string): any {
+        if (currentDataKeys)
+            toData = Navigation.StateContext.includeCurrentData(toData, currentDataKeys.trim().split(/\s*,\s*/));
+        if (includeCurrentData)
+            toData = Navigation.StateContext.includeCurrentData(toData);
+        return toData;
+    }
+}
+export = LinkUtility; 
--- NavigationJS/src/angular/NavigationLink.ts
+++ NavigationJS/src/angular/NavigationLink.ts
@@ -1,12 +1,19 @@
-﻿import Navigation = require('../Navigation');
+﻿import LinkUtility = require('./LinkUtility');
+import Navigation = require('../Navigation');
 
 var NavigationLink = () => {
         return {
             restrict: 'A',
             link: (scope: ng.IScope, element, attrs: ng.IAttributes) => {
-                scope.$watchGroup([attrs['navigationLink'], attrs['toData']], function (values) {
+                scope.$watchGroup([
+                    attrs['navigationLink'],
+                    attrs['toData'],
+                    attrs['includeCurrentData'],
+                    attrs['currentDataKeys']
+                ], function (values) {
                     try {
-                        var navigationLink = Navigation.StateController.getNavigationLink(values[0], values[1]);
+                        var toData = LinkUtility.getData(values[1], values[2], values[3]);
+                        var navigationLink = Navigation.StateController.getNavigationLink(values[0], toData);
                         attrs.$set('href', Navigation.historyManager.getHref(navigationLink));
                     } catch (e) {
                         attrs.$set('href', null);
