c1f0e10a39e2ef10f905caef66e9a1d020d6891b
Graham Mendick <graham.mendick@gmail.com>
2015-04-09 09:45:22 +0100

    Separated out attrs
    Couldn't hold onto built toData because it needs to be reevaluated
    onNavigate because the StateContext data's changed

--- NavigationJS/src/angular/NavigationLink.ts
+++ NavigationJS/src/angular/NavigationLink.ts
@@ -5,25 +5,26 @@ var NavigationLink = () => {
     return {
         restrict: 'EA',
         link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
-            var action, toData;
+            var action, toData, includeCurrentData, currentDataKeys;
             LinkUtility.addClickListener(element);
-            LinkUtility.addNavigateHandler(element, () => setNavigationLink(element, attrs, action, toData));
-            scope.$watchGroup([
-                attrs['navigationLink'],
-                attrs['toData'],
-                attrs['includeCurrentData'],
-                attrs['currentDataKeys']
-            ], function (values) {
+            LinkUtility.addNavigateHandler(element, () => setNavigationLink(element, attrs, action, toData, includeCurrentData, currentDataKeys));
+            var watchAttrs = [attrs['navigationLink'], attrs['toData'], attrs['includeCurrentData'], attrs['currentDataKeys']];
+            scope.$watchGroup(watchAttrs, function (values) {
                 action = values[0];
-                toData = LinkUtility.getData(values[1], values[2], values[3]);
-                setNavigationLink(element, attrs, action, toData);
+                toData = values[1];
+                includeCurrentData = values[2];
+                currentDataKeys = values[3];
+                setNavigationLink(element, attrs, action, toData, includeCurrentData, currentDataKeys);
             });
         }
     }
 };
 
-function setNavigationLink(element: ng.IAugmentedJQuery, attrs: ng.IAttributes, action: string, toData: any) {
-    LinkUtility.setLink(element, attrs, () => Navigation.StateController.getNavigationLink(action, toData));
+function setNavigationLink(element: ng.IAugmentedJQuery, attrs: ng.IAttributes,
+    action: string, toData: any, includeCurrentData: boolean, currentDataKeys: string) {
+    LinkUtility.setLink(element, attrs, () => Navigation.StateController.getNavigationLink(action,
+        LinkUtility.getData(toData, includeCurrentData, currentDataKeys))
+    );
 }
 
 angular.module('NavigationAngular', [])
