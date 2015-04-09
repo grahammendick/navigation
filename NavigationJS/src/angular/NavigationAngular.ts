3848c48f1c360d421d3ab9de35a7af72c1625e36
Graham Mendick <graham.mendick@gmail.com>
2015-04-09 10:33:37 +0100

    Implemented NavigationAngular refresh link

--- NavigationJS/src/angular/NavigationAngular.ts
+++ NavigationJS/src/angular/NavigationAngular.ts
@@ -2,12 +2,15 @@
 /// <reference path="jquery.d.ts" />
 import NavigationBackLink = require('./NavigationBackLink');
 import NavigationLink = require('./NavigationLink');
+import RefreshLink = require('./RefreshLink');
 
 class NavigationAngular {
     static NavigationBackLink = NavigationBackLink;
     static NavigationLink = NavigationLink;
+    static RefreshLink = RefreshLink;
 }
 angular.module('NavigationAngular', [])
     .directive('navigationBackLink', NavigationBackLink)
-    .directive('navigationLink', NavigationLink);
+    .directive('navigationLink', NavigationLink)
+    .directive('refreshLink', RefreshLink);
 export = NavigationAngular; 
--- NavigationJS/src/angular/RefreshLink.ts
+++ NavigationJS/src/angular/RefreshLink.ts
@@ -1,1 +1,28 @@
-﻿ 
+﻿import LinkUtility = require('./LinkUtility');
+import Navigation = require('../Navigation');
+
+var RefreshLink = () => {
+    return {
+        restrict: 'EA',
+        link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+            var toData, includeCurrentData, currentDataKeys;
+            LinkUtility.addClickListener(element);
+            LinkUtility.addNavigateHandler(element, () => setRefreshLink(element, attrs, toData, includeCurrentData, currentDataKeys));
+            var watchAttrs = [attrs['refreshLink'], attrs['includeCurrentData'], attrs['currentDataKeys']];
+            scope.$watchGroup(watchAttrs, function (values) {
+                toData = values[0];
+                includeCurrentData = values[1];
+                currentDataKeys = values[2];
+                setRefreshLink(element, attrs, toData, includeCurrentData, currentDataKeys);
+            });
+        }
+    }
+};
+
+function setRefreshLink(element: ng.IAugmentedJQuery, attrs: ng.IAttributes,
+    toData: any, includeCurrentData: boolean, currentDataKeys: string) {
+    LinkUtility.setLink(element, attrs, () => Navigation.StateController.getRefreshLink(
+        LinkUtility.getData(toData, includeCurrentData, currentDataKeys))
+    );
+}
+export = RefreshLink;
