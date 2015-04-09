ef030a0f15d64eff02c0d30a73184b3619105537
Graham Mendick <graham.mendick@gmail.com>
2015-04-09 11:04:58 +0100

    Added details State and back navigation

--- NavigationJS/Sample/Angular/app.js
+++ NavigationJS/Sample/Angular/app.js
@@ -4,6 +4,8 @@ app.controller('PersonController', function ($scope) {
 	$scope.id;
 	$scope.name;
 	$scope.people;
+	$scope.personName;
+	$scope.dateOfBirth;
 
 	var personStates = Navigation.StateInfoConfig.dialogs.person.states;
 	var subscription;
@@ -12,6 +14,13 @@ app.controller('PersonController', function ($scope) {
 		var totalRowCount = people.length;
 		$scope.people = people.slice(data.startRowIndex, data.startRowIndex + data.maximumRows);
 	};
+	personStates.details.navigated = function (data) {
+		$scope.id = data.id;
+		var person = personSearch.getDetails(data.id);
+		$scope.personName = person.name;
+		$scope.dateOfBirth = person.dateOfBirth;
+	};
+	personStates.details.dispose = function () { $scope.id = null };
 	Navigation.StateController.onNavigate(function () {
 		if (!$scope.$$phase)
 			$scope.$apply();
