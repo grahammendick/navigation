72d5a56cc2a8b4036dd85ef3ba1e35c4adb0d94f
Graham Mendick <graham.mendick@gmail.com>
2015-05-31 14:54:53 +0100

    Added more definitelytyped tests

--- NavigationJS/test/navigation-tests.ts
+++ NavigationJS/test/navigation-tests.ts
@@ -1,5 +1,6 @@
 /// <reference path="../src/navigation.d.ts" />
 
+// Configuration
 Navigation.StateInfoConfig.build([
 	{ key: 'home', initial: 'page', states: [
 		{ key: 'page', route: '' }
@@ -12,8 +13,24 @@ Navigation.StateInfoConfig.build([
 	]}
 ]);
 
-Navigation.start('home');
+// StateInfo
+var dialogs = Navigation.StateInfoConfig.dialogs;
+var home = dialogs['home'];
+var homePage = home.states['page'];
+var homeKey = home.key;
+var homePageKey = homePage.key;
+homePage = home.initial;
+var person = dialogs['person'];
+var personList = person.states['list'];
+var personDetails = person.states['details'];
+var personListSelect = personList.transitions['select'];
+personList = personListSelect.parent;
+personDetails = personListSelect.to;
+var pageDefault = personList.defaults.page;
+var idDefaultType = personDetails.defaultTypes.id;
 
+// Navigation
+Navigation.start('home');
 Navigation.StateController.navigate('person');
 Navigation.StateController.refresh();
 Navigation.StateController.refresh({ page: 2 });
@@ -22,11 +39,13 @@ if (Navigation.StateController.canNavigateBack(1)){
 	Navigation.StateController.navigateBack(1);	
 }
 
+// Navigation Link
 var link: string = Navigation.StateController.getNavigationLink('person');
 link = Navigation.StateController.getRefreshLink();
 link = Navigation.StateController.getRefreshLink({ page: 2 });
 link = Navigation.StateController.getNavigationLink('select', { id: 10 });
 Navigation.StateController.navigateLink(link);
-link = Navigation.StateController.getNavigationBackLink(1);	
-
+link = Navigation.StateController.getNavigationBackLink(1);
+var crumb: Navigation.Crumb = Navigation.StateController.crumbs[0];
+link = crumb.navigationLink;
 
