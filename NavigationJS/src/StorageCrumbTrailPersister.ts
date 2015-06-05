b03e1aadd754e44e27f35fb8d312c32083908944
Graham Mendick <graham.mendick@gmail.com>
2015-06-05 12:51:40 +0100

    Converted crumb trail to key
    dialogIndex + stateIndex + count. Turn the numbers into aphas using 0 =
    a,...,25 = z, 26 = A,...,51 = Z. If greater than 51 then using a number
    as well 52 = a1,...,77 =z1, 78 = A1,...103=Z1 etc.

--- NavigationJS/src/StorageCrumbTrailPersister.ts
+++ NavigationJS/src/StorageCrumbTrailPersister.ts
@@ -1,10 +1,12 @@
 import CrumbTrailPersister = require('./CrumbTrailPersister');
+import StateContext = require('./StateContext');
 import settings = require('./settings');
 
 class StorageCrumbTrailPersister extends CrumbTrailPersister {
 	private maxLength: number = 500;
 	private historySize: number = 50;
 	private storage: Storage;
+	private count: number = 0;
 	
 	constructor(storage?: Storage) {
 		super();
@@ -26,7 +28,27 @@ class StorageCrumbTrailPersister extends CrumbTrailPersister {
 	}
 	
 	save(crumbTrail: string): string {
+		if (!crumbTrail)
+			return crumbTrail;
+		if (crumbTrail.length > this.maxLength){
+			var dialogCode = this.getCode(StateContext.state.parent.index);
+			var stateCode = this.getCode(StateContext.state.index);
+			var countCode = this.getCode(this.count);
+			var key = dialogCode + stateCode + countCode;
+			if (this.count >= this.historySize)
+				this.storage.removeItem((this.count - this.historySize).toString());
+			this.storage.setItem(this.count.toString(), key + '=' + crumbTrail);
+			this.count++;
+			return key;
+		}
 		return crumbTrail;
+	}
+	
+	private getCode(val: number): string {
+		var rem = val & 52;
+		var div = Math.floor(val / 52);
+		var baseCharCode = rem < 26 ? 97 : 65 - 26; 
+		return String.fromCharCode(baseCharCode + rem) + rem.toString();
 	}
 }
 
@@ -35,6 +57,10 @@ class InProcStorage implements Storage {
 	private store: any = {};
     [key: string]: any;
     [index: number]: string;
+	
+	clear() { throw new Error('Not implemented'); }
+	
+	key(index: number): string { throw new Error('Not implemented'); }
 	
 	getItem(key: string): any {
 		return this.store[key];
@@ -48,7 +74,5 @@ class InProcStorage implements Storage {
 		delete this.store[key];
 	}
 	
-	clear() { throw new Error('Not implemented'); }
-	key(index: number): string { throw new Error('Not implemented'); }
 }
 export = StorageCrumbTrailPersister;
