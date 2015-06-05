faf6c40b48fd53c000b19df3b3c11802f64b0497
Graham Mendick <graham.mendick@gmail.com>
2015-06-05 11:15:02 +0100

    Added properties to persister
    These match the .NET properties on the SessionCrumbTrailPersister

--- NavigationJS/src/LocalStorageCrumbTrailPersister.ts
+++ NavigationJS/src/LocalStorageCrumbTrailPersister.ts
@@ -1,6 +1,9 @@
 import CrumbTrailPersister = require('./CrumbTrailPersister');
 
 class LocalStorageCrumbTrailPersister extends CrumbTrailPersister {
+	maxLength: number = 500;
+	historySize: number = 50;
+	
 	load(crumbTrail: string): string {
 		return crumbTrail;
 	}
