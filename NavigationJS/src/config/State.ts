a7b5ca5a1ba384da564c9429d2dd31e110c8b5a8
Graham Mendick <graham.mendick@gmail.com>
2015-06-08 15:50:30 +0100

    Tweaked formatting

--- NavigationJS/src/config/State.ts
+++ NavigationJS/src/config/State.ts
@@ -18,8 +18,10 @@ class State implements IState<{ [index: string]: Transition }> {
     route: string;
     trackCrumbTrail: boolean = true;
     stateHandler: IStateHandler = new StateHandler();
-    unloading: (state: State, data: any, url: string, unload: () => void, history: boolean) => void = function (state, data, url, unload, history) { unload(); };
-    navigating: (data: any, url: string, navigate: () => void, history: boolean) => void = function (data, url, navigate, history) { navigate(); };
+    unloading: (state: State, data: any, url: string, unload: () => void, history: boolean) => void 
+        = function (state, data, url, unload, history) { unload(); };
+    navigating: (data: any, url: string, navigate: () => void, history: boolean) => void 
+        = function (data, url, navigate, history) { navigate(); };
     dispose: () => void = function () { };
     navigated: (data: any) => void = function (data: any) { };
 }
