bca28537a10a500b3ad94dd3edd267b2bad81c28
Graham Mendick <graham.mendick@gmail.com>
2015-06-16 20:36:48 +0100

    Fixed whitespace

--- NavigationJS/src/config/IState.ts
+++ NavigationJS/src/config/IState.ts
@@ -1,11 +1,11 @@
 interface IState<TTransitions> {
-	transitions?: TTransitions;
-	key: string;
-	defaults?: any;
-	defaultTypes?: any;
-	title?: string;
-	route: string | string[];
-	trackCrumbTrail?: boolean;
+    transitions?: TTransitions;
+    key: string;
+    defaults?: any;
+    defaultTypes?: any;
+    title?: string;
+    route: string | string[];
+    trackCrumbTrail?: boolean;
     trackTypes?: boolean;
 }
 export = IState;
