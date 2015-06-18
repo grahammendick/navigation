8adc59726bd2eeca62bb707b75d22cd293d2656b
Graham Mendick <graham.mendick@gmail.com>
2015-06-18 11:06:10 +0100

    Added changes into NavigationLink
    The page size links didn't get hrefs to start with when set to lazy.
    Wanted to check this was just a refresh navigation thing. After changing
    them to NavigationLinks they did get hrefs. A RefreshLink that is on the
    screen before a navigation won't get an href if set to lazy because it
    won't run after a navigation - should fix this? Probably not because
    even if run an initial navigation there's no guarantee that the
    RefreshLink will return non null

--- NavigationKnockout/src/NavigationLink.ts
+++ NavigationKnockout/src/NavigationLink.ts
@@ -4,8 +4,11 @@ import ko = require('knockout');
 
 var NavigationLink = ko.bindingHandlers['navigationLink'] = {
     init: (element, valueAccessor, allBindings: KnockoutAllBindingsAccessor) => {
-        LinkUtility.addClickListener(element);
-        LinkUtility.addNavigateHandler(element, () => setNavigationLink(element, valueAccessor, allBindings));
+        LinkUtility.addClickListener(element, () => setNavigationLink(element, valueAccessor, allBindings));
+        LinkUtility.addNavigateHandler(element, () => {
+            if (!allBindings.get('lazy'))
+                setNavigationLink(element, valueAccessor, allBindings);
+        });
     },
     update: (element, valueAccessor, allBindings: KnockoutAllBindingsAccessor) => {
         setNavigationLink(element, valueAccessor, allBindings);
