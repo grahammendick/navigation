83279ccbb0dc55a649ea678b4d9b99b2f2851082
Graham Mendick <graham.mendick@gmail.com>
2015-07-01 12:01:43 +0100

    Made react navigation links lazy
    Similar to the lazy knockout changes. Couldn't add the onNavigate
    listener in LinkUtility because can only remove from
    componentWillUnmount. In the onClick, directly set the element's href,
    not just props' href, so that keyboard enter works. For mouse click the
    mouse down will have forced href update, but keyboard enter won't have
    done.

--- NavigationReact/src/NavigationLink.ts
+++ NavigationReact/src/NavigationLink.ts
@@ -6,18 +6,22 @@ var NavigationLink = React.createClass({
     onNavigate: function () {
         this.forceUpdate();
     },
+    setNavigationLink: function() {
+        var toData = LinkUtility.getData(this.props.toData, this.props.includeCurrentData, this.props.currentDataKeys);
+        LinkUtility.setLink(this, this.props, () => Navigation.StateController.getNavigationLink(this.props.action, toData));
+    },
     componentDidMount: function () {
-        Navigation.StateController.onNavigate(this.onNavigate);
+        if (!this.props.lazy)
+            Navigation.StateController.onNavigate(this.onNavigate);
     },
     componentWillUnmount: function () {
-        Navigation.StateController.offNavigate(this.onNavigate);
+        if (!this.props.lazy)
+            Navigation.StateController.offNavigate(this.onNavigate);
     },
     render: function () {
-        var props = LinkUtility.cloneProps(this);
-        var action = props.action;
-        var toData = LinkUtility.getData(props.toData, props.includeCurrentData, props.currentDataKeys);
-        LinkUtility.setLink(this, props, () => Navigation.StateController.getNavigationLink(action, toData));
-        return LinkUtility.createElement(props);
+        LinkUtility.addListeners(this, this.props, () => this.setNavigationLink(), !!this.props.lazy);
+        this.setNavigationLink();
+        return LinkUtility.createElement(this.props);
     }
 });
 export = NavigationLink;
