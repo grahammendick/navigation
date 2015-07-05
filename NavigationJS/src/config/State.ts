d1edf0bd84e5f7e35eb3bdbe6f1dbd5bdf49d750
Graham Mendick <graham.mendick@gmail.com>
2015-07-05 17:49:25 +0100

    Passed data from navigating to navigated
    A way to pass data retrieved asynchronously in navigating before
    continuing the navigation. Pass it into the continuation and receive it
    in navigated

--- NavigationJS/src/config/State.ts
+++ NavigationJS/src/config/State.ts
@@ -20,8 +20,8 @@ class State implements IState<{ [index: string]: Transition }> {
     trackTypes: boolean = true;
     stateHandler: IStateHandler = new StateHandler();
     unloading: (state: State, data: any, url: string, unload: () => void, history?: boolean) => void = function (state, data, url, unload) { unload(); };
-    navigating: (data: any, url: string, navigate: () => void, history?: boolean) => void = function (data, url, navigate) { navigate(); };
+    navigating: (data: any, url: string, navigate: (asyncData?: any) => void, history?: boolean) => void = function (data, url, navigate) { navigate(); };
     dispose: () => void = function () { };
-    navigated: (data: any) => void = function (data: any) { };
+    navigated: (data: any, asyncData?: any) => void = function (data: any) { };
 }
 export = State;
