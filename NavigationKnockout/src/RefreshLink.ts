fb141fc44ca2c1e452eabad342d684f7babf3e10
Graham Mendick <graham.mendick@gmail.com>
2015-07-21 13:03:30 +0100

    Had first crack at active check
    Started working on inert first but think that active's more important.
    Probably don't need inert if have activeCss and disableActive
    attributes?

--- NavigationKnockout/src/RefreshLink.ts
+++ NavigationKnockout/src/RefreshLink.ts
@@ -12,6 +12,19 @@ var RefreshLink = ko.bindingHandlers['refreshLink'] = {
 };
 
 function setRefreshLink(element: HTMLAnchorElement, valueAccessor: () => any, allBindings: KnockoutAllBindingsAccessor) {
+    var data = {};
+    var toData = valueAccessor();
+    toData = ko.unwrap(toData);
+    var trackTypes = Navigation.StateContext.state.trackTypes;
+    var active = true;
+    for (var key in toData) {
+        var val = ko.unwrap(toData[key]);
+        data[key] = val;
+        var currentVal = Navigation.StateContext.data[key];
+        if (val != null && val.toString()) {
+            active = active && currentVal != null && (trackTypes ? val !== currentVal : val.toString() != currentVal.toString());
+        }
+    }
     LinkUtility.setLink(element, () => Navigation.StateController.getRefreshLink(
         LinkUtility.getData(valueAccessor(), allBindings.get('includeCurrentData'), allBindings.get('currentDataKeys')))
     );
