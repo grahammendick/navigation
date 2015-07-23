cec54104cf2df85c324f5cfa36e6f6f9b82b78b4
Graham Mendick <graham.mendick@gmail.com>
2015-07-23 15:20:14 +0100

    Changed from mousedown to contextmenu
    Lazy links must be updated when right clicked and opened in new window.
    Right click doesn't fire onclick, so the mousedown was handling this
    case. But mousedown doesn't handle keyboard right click. Changed to
    oncontextmenu instead - fired whenever the right click context menu is
    triggered

--- NavigationReact/src/LinkUtility.ts
+++ NavigationReact/src/LinkUtility.ts
@@ -55,7 +55,7 @@ class LinkUtility {
             }
         };
         if (lazy)
-            props.onMouseDown = (e: MouseEvent) => component.forceUpdate();
+            props.onContextMenu = (e: MouseEvent) => component.forceUpdate();
     }
 }
 export = LinkUtility;
