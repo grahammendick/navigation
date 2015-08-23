3ae3b0e9b971b6e88e8ce303f12d71821b4219d8
Graham Mendick <graham.mendick@gmail.com>
2015-08-23 11:44:28 +0100

    Reset State context after async returns

--- NavigationReact/sample/isomorphic/index.js
+++ NavigationReact/sample/isomorphic/index.js
@@ -6,6 +6,7 @@ var server = http.createServer(function(req, res) {
 	if (req.url !== '/favicon.ico') {
 		Navigation.StateController.navigateLink(req.url);
 		NavigateServer.getProps(function(props){
+			Navigation.StateController.navigateLink(req.url);
 			res.write(NavigateServer.render(props));
 			res.end();
 		});
