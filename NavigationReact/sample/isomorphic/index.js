1e36baf4afba95af0d714f63c3f5ebc273ee1603
Graham Mendick <graham.mendick@gmail.com>
2015-08-23 14:14:03 +0100

    Added basic style, changed people to match example

--- NavigationReact/sample/isomorphic/index.js
+++ NavigationReact/sample/isomorphic/index.js
@@ -7,7 +7,11 @@ http.createServer(function(req, res) {
 		Navigation.StateController.navigateLink(req.url);
 		NavigateServer.getProps(function(props){
 			Navigation.StateController.navigateLink(req.url);
+			res.write('<html><head><style>')
+			res.write('table{border-collapse:collapse;}table,td,th{border:1px #000 solid;}')
+			res.write('</style></head><body><div id="content">')
 			res.write(NavigateServer.render(props));
+			res.write('</div></body></html>')
 			res.end();
 		});
 	} else {
