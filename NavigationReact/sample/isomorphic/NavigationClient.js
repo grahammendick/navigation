00f889b0cd5aab8dc3552edf92af40df256ad723
Graham Mendick <graham.mendick@gmail.com>
2015-08-23 20:18:33 +0100

    Register StateNavigators after client rendered
    Can't register them before Navigation.start because that would trigger
    them unnecessarily

--- NavigationReact/sample/isomorphic/NavigationClient.js
+++ NavigationReact/sample/isomorphic/NavigationClient.js
@@ -2,7 +2,14 @@ var React = require('react');
 var Navigation = require('navigation');
 var NavigationShared = require('./NavigationShared');
 
+function registerNavigators() {
+	var states = Navigation.StateInfoConfig.dialogs.masterDetails.states;
+	states.listing.navigated = function(data) {
+	}	
+}
+
 exports.start = function(props) {
 	Navigation.start();
 	React.render(React.createElement(NavigationShared.getContent(), props), document.getElementById('content'));
+	registerNavigators();
 }
--- NavigationReact/sample/isomorphic/NavigationServer.js
+++ NavigationReact/sample/isomorphic/NavigationServer.js
@@ -2,17 +2,16 @@ var Navigation = require('navigation');
 var PersonSearch = require('./PersonSearch');
 
 var states = Navigation.StateInfoConfig.dialogs.masterDetails.states;
-
 states.listing.getProps = function(data, callback) {
 	PersonSearch.search(data.pageNumber, function(people){
 		callback({ people: people });
-	})
+	});
 }
 
 states.details.getProps = function(data, callback) {
 	PersonSearch.getDetails(data.id, function(person){
 		callback({ person: person });
-	})
+	});
 }
 
 exports.getProps = function(callback) {
