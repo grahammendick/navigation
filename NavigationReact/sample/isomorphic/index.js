2224ae1eac1a9b769fd31ecfc7107b50b4ec316f
Graham Mendick <graham.mendick@gmail.com>
2015-08-23 10:11:58 +0100

    Navigated to the requested url

--- NavigationReact/sample/isomorphic/index.js
+++ NavigationReact/sample/isomorphic/index.js
@@ -1,10 +1,12 @@
 var http = require('http');
+var Navigation = require('../../../build/dist/Navigation');
 var StateInfoConfig = require('./StateInfoConfig');
 
 StateInfoConfig.register();
 
 var server = http.createServer(function(req, res) {
-	res.write('Hello World');
+	Navigation.StateController.navigateLink(req.url);
+	res.write(Navigation.StateContext.state.key);
 	res.end();
 });
 
