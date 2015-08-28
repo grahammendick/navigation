8fa56724653fff1f99ad1810a0e58c6e7ed1f905
Graham Mendick <graham.mendick@gmail.com>
2015-08-28 13:17:32 +0100

    Moved getData into navigating function
    There's only one navigating function now so no need for separate getData
    function

--- NavigationReact/sample/isomorphic/NavigationClient.js
+++ NavigationReact/sample/isomorphic/NavigationClient.js
@@ -26,26 +26,20 @@ function registerNavigators() {
 	for(var key in states) {
 		var state = states[key];
 		state.navigating = function(data, url, navigate) {
-			getData(url, function(data){
-				navigate(data);
-			})
+			var req = new XMLHttpRequest();
+			req.onreadystatechange = function() {
+				if (req.readyState === 4){
+					navigate(JSON.parse(req.responseText));
+				}
+			};
+			req.open('get', url);
+			req.setRequestHeader('Accept', 'application/json');
+			req.send(null);
 		}
 		state.navigated = function(data, asyncData) {
 			render(asyncData);
 		}
 	}
-}
-
-function getData(url, callback) {
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function() {
-		if (req.readyState === 4){
-			callback(JSON.parse(req.responseText));
-		}
-	};
-	req.open('get', url);
-	req.setRequestHeader('Accept', 'application/json');
-	req.send(null);
 }
 	
 
