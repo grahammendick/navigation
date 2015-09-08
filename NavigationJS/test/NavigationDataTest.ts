2954e86af565fcd18514de64854d5cb95b65f534
Graham Mendick <graham.mendick@gmail.com>
2015-09-08 11:46:24 +0100

    Refactored Separator Url Character Data unit tests

--- NavigationJS/test/NavigationDataTest.ts
+++ NavigationJS/test/NavigationDataTest.ts
@@ -500,25 +500,45 @@ describe('Navigation Data', function () {
         }
     });
 
-    it('SeparatorUrlCharacterDataTest', function () {
+    describe('Separator Url Character Data', function() {
+        beforeEach(function() {
+            Navigation.StateInfoConfig.build([
+                { key: 'd', initial: 's0', states: [
+                    { key: 's0', route: 'r0', transitions: [
+                        { key: 't', to: 's1' }
+                    ]},
+                    { key: 's1', route: 'r1' }]}
+                ]);
+        });
         var data = {};
         data['_0_1_2_3_4_5_'] = '__00__11__22__33__44__55__';
-        Navigation.StateController.navigate('d0', data);
-        Navigation.StateController.navigate('t0');
-        Navigation.StateController.navigateBack(1);
-        assert.strictEqual(Navigation.StateContext.data['_0_1_2_3_4_5_'], '__00__11__22__33__44__55__');
-    });
+        
+        describe('Navigate', function() {
+            beforeEach(function() {
+                Navigation.StateController.navigate('d', data);
+                Navigation.StateController.navigate('t');
+                Navigation.StateController.navigateBack(1);
+            });
+            test();
+        });
 
-    it('SeparatorUrlCharacterDataLinkTest', function () {
-        var data = {};
-        data['_0_1_2_3_4_5_'] = '__00__11__22__33__44__55__';
-        var link = Navigation.StateController.getNavigationLink('d0', data);
-        Navigation.StateController.navigateLink(link);
-        link = Navigation.StateController.getNavigationLink('t0');
-        Navigation.StateController.navigateLink(link);
-        link = Navigation.StateController.getNavigationBackLink(1);
-        Navigation.StateController.navigateLink(link);
-        assert.strictEqual(Navigation.StateContext.data['_0_1_2_3_4_5_'], '__00__11__22__33__44__55__');
+        describe('Navigate Link', function() {
+            beforeEach(function() {
+                var link = Navigation.StateController.getNavigationLink('d', data);
+                Navigation.StateController.navigateLink(link);
+                link = Navigation.StateController.getNavigationLink('t');
+                Navigation.StateController.navigateLink(link);
+                link = Navigation.StateController.getNavigationBackLink(1);
+                Navigation.StateController.navigateLink(link);
+            });
+            test();
+        });
+
+        function test() {
+            it('should populate data', function () {
+                assert.strictEqual(Navigation.StateContext.data['_0_1_2_3_4_5_'], '__00__11__22__33__44__55__');
+            });
+        }
     });
 
     it('EmptyStringDataNavigateTest', function () {
