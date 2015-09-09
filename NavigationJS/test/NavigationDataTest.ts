833804deb7eb2a11b3016986dc9e766a37eddc8b
Graham Mendick <graham.mendick@gmail.com>
2015-09-09 10:35:17 +0100

    Refactored Back Override Defaults unit tests

--- NavigationJS/test/NavigationDataTest.ts
+++ NavigationJS/test/NavigationDataTest.ts
@@ -2283,33 +2283,52 @@ describe('Navigation Data', function () {
         }
     });
 
-    it('NavigateBackOverrideDefaultsTest', function () {
-        Navigation.StateController.navigate('d0');
-        Navigation.StateController.navigate('t0');
+    describe('Back Override Defaults', function() {
+        beforeEach(function() {
+            Navigation.StateInfoConfig.build([
+                { key: 'd', initial: 's0', states: [
+                    { key: 's0', route: 'r0', transitions: [
+                        { key: 't', to: 's1' }
+                    ]},
+                    { key: 's1', route: 'r1', defaults: { emptyString: '', 'number': 4, char: 7 }, transitions: [
+                        { key: 't', to: 's2' }
+                    ]},
+                    { key: 's2', route: 'r2' }]}
+                ]);
+        });
         var data = { emptyString: 'World', 'number': 1, char: 5 };
-        Navigation.StateController.navigate('t0', data);
-        Navigation.StateController.navigate('t0');
-        Navigation.StateController.navigateBack(1);
-        assert.strictEqual(Navigation.StateContext.data['emptyString'], 'World');
-        assert.strictEqual(Navigation.StateContext.data['number'], 1);
-        assert.strictEqual(Navigation.StateContext.data['char'], 5);
-    });
+        
+        describe('Navigate', function() {
+            beforeEach(function() {
+                Navigation.StateController.navigate('d');
+                Navigation.StateController.navigate('t', data);
+                Navigation.StateController.navigate('t');
+                Navigation.StateController.navigateBack(1);
+            });
+            test();
+        });
 
-    it('NavigateBackOverrideDefaultsLinkTest', function () {
-        var link = Navigation.StateController.getNavigationLink('d0');
-        Navigation.StateController.navigateLink(link);
-        link = Navigation.StateController.getNavigationLink('t0');
-        Navigation.StateController.navigateLink(link);
-        var data = { emptyString: 'World', 'number': 1, char: 5 };
-        link = Navigation.StateController.getNavigationLink('t0', data);
-        Navigation.StateController.navigateLink(link);
-        link = Navigation.StateController.getNavigationLink('t0');
-        Navigation.StateController.navigateLink(link);
-        link = Navigation.StateController.getNavigationBackLink(1);
-        Navigation.StateController.navigateLink(link);
-        assert.strictEqual(Navigation.StateContext.data['emptyString'], 'World');
-        assert.strictEqual(Navigation.StateContext.data['number'], 1);
-        assert.strictEqual(Navigation.StateContext.data['char'], 5);
+        describe('Navigate Link', function() {
+            beforeEach(function() {
+                var link = Navigation.StateController.getNavigationLink('d');
+                Navigation.StateController.navigateLink(link);
+                link = Navigation.StateController.getNavigationLink('t', data);
+                Navigation.StateController.navigateLink(link);
+                link = Navigation.StateController.getNavigationLink('t');
+                Navigation.StateController.navigateLink(link);
+                link = Navigation.StateController.getNavigationBackLink(1);
+                Navigation.StateController.navigateLink(link);
+            });
+            test();
+        });
+
+        function test() {
+            it('should populate data', function () {
+                assert.strictEqual(Navigation.StateContext.data['emptyString'], 'World');
+                assert.strictEqual(Navigation.StateContext.data['number'], 1);
+                assert.strictEqual(Navigation.StateContext.data['char'], 5);
+            });
+        }
     });
 
     it('NavigateBackOverrideDefaultsRouteTest', function () {
