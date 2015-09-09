b74ba9df566003554572654ac010c71d65001d2b
Graham Mendick <graham.mendick@gmail.com>
2015-09-09 10:15:32 +0100

    Refactored Clear Data And Defaults unit tests

--- NavigationJS/test/NavigationDataTest.ts
+++ NavigationJS/test/NavigationDataTest.ts
@@ -2003,33 +2003,47 @@ describe('Navigation Data', function () {
         }
     });
 
-    it('ClearDataAndDefaultsTest', function () {
-        Navigation.StateController.navigate('d0');
-        Navigation.StateController.navigate('t0');
+    describe('Clear Data And Defaults', function() {
+        beforeEach(function() {
+            Navigation.StateInfoConfig.build([
+                { key: 'd', initial: 's0', states: [
+                    { key: 's0', route: 'r0', transitions: [
+                        { key: 't', to: 's1' }
+                    ]},
+                    { key: 's1', route: 'r1', defaults: { emptyString: '', 'number': 4, char: 7 } }]}
+                ]);
+        });
         var data = { s: 1, t: '2' };
-        Navigation.StateController.navigate('t0', data);
-        Navigation.StateContext.clear();
-        assert.strictEqual(Navigation.StateContext.data['emptyString'], '');
-        assert.strictEqual(Navigation.StateContext.data['number'], 4);
-        assert.strictEqual(Navigation.StateContext.data['char'], 7);
-        assert.strictEqual(Navigation.StateContext.data['s'], undefined);
-        assert.strictEqual(Navigation.StateContext.data['t'], undefined);
-    });
+        
+        describe('Navigate', function() {
+            beforeEach(function() {
+                Navigation.StateController.navigate('d');
+                Navigation.StateController.navigate('t', data);
+                Navigation.StateContext.clear();
+            });
+            test();
+        });
 
-    it('ClearDataAndDefaultsLinkTest', function () {
-        var link = Navigation.StateController.getNavigationLink('d0');
-        Navigation.StateController.navigateLink(link);
-        link = Navigation.StateController.getNavigationLink('t0');
-        Navigation.StateController.navigateLink(link);
-        var data = { s: 1, t: '2' };
-        link = Navigation.StateController.getNavigationLink('t0', data);
-        Navigation.StateController.navigateLink(link);
-        Navigation.StateContext.clear();
-        assert.strictEqual(Navigation.StateContext.data['emptyString'], '');
-        assert.strictEqual(Navigation.StateContext.data['number'], 4);
-        assert.strictEqual(Navigation.StateContext.data['char'], 7);
-        assert.strictEqual(Navigation.StateContext.data['s'], undefined);
-        assert.strictEqual(Navigation.StateContext.data['t'], undefined);
+        describe('Navigate Link', function() {
+            beforeEach(function() {
+                var link = Navigation.StateController.getNavigationLink('d');
+                Navigation.StateController.navigateLink(link);
+                link = Navigation.StateController.getNavigationLink('t', data);
+                Navigation.StateController.navigateLink(link);
+                Navigation.StateContext.clear();
+            });
+            test();
+        });
+
+        function test() {
+            it('should populate data', function () {
+                assert.strictEqual(Navigation.StateContext.data['emptyString'], '');
+                assert.strictEqual(Navigation.StateContext.data['number'], 4);
+                assert.strictEqual(Navigation.StateContext.data['char'], 7);
+                assert.strictEqual(Navigation.StateContext.data['s'], undefined);
+                assert.strictEqual(Navigation.StateContext.data['t'], undefined);
+            });
+        }
     });
 
     it('ClearDataAndDefaultsRouteTest', function () {
