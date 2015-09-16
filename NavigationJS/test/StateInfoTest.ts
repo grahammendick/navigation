3ad0aef6cdec01a7bbc1c1b29900e4bcda6dff5c
Graham Mendick <graham.mendick@gmail.com>
2015-09-16 14:21:17 +0100

    Refactored Transition Parent unit test

--- NavigationJS/test/StateInfoTest.ts
+++ NavigationJS/test/StateInfoTest.ts
@@ -111,17 +111,18 @@ describe('StateInfoTest', function () {
         })
     });
 
-    it('TransitionParentTest', function () {
-        for (var i = 0; i < Navigation.StateInfoConfig._dialogs.length; i++) {
-            var dialog = Navigation.StateInfoConfig._dialogs[i];
-            for (var j = 0; j < dialog._states.length; j++) {
-                var state = dialog._states[j];
-                for (var k = 0; k < state._transitions.length; k++) {
-                    var transition = state._transitions[k];
-                    assert.equal(transition.parent, state);
-                }
-            }
-        }
+    describe('Transition Parent', function () {
+        it('should configure State Info', function() {
+            Navigation.StateInfoConfig.build([
+                { key: 'd', initial: 's0', states: [
+                    { key: 's0', route: 'r0', transitions: [
+                        { key: 't', to: 's1' }]},
+                    { key: 's1', route: 'r1' }]}
+                ]);
+            var dialog = Navigation.StateInfoConfig._dialogs[0];
+            var state = dialog._states[0];
+            assert.equal(state._transitions[0].parent, state);
+        })
     });
 
     it('TransitionToTest', function () {
