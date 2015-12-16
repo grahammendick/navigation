af330b490c375516c0346a2777e0160af21bd10c
Graham Mendick <graham.mendick@gmail.com>
2015-12-16 11:54:10 +0000

    Moved getName onto TypeConverter

--- NavigationJS/src/converter/ConverterFactory.ts
+++ NavigationJS/src/converter/ConverterFactory.ts
@@ -29,26 +29,9 @@ class ConverterFactory {
     private static getKey(type: string) {
         return this.nameToKeyList[type];
     }
-    
-    private static getName(obj: any) {
-        var fullName = typeof obj;
-        var subName: string;
-        if (Object.prototype.toString.call(obj) === '[object Array]') {
-            var arr: any[] = obj;
-            subName = 'string';
-            for (var i = 0; i < arr.length; i++) {
-                if (arr[i] != null && arr[i].toString()) {
-                    subName = typeof arr[i];
-                    break;
-                }
-            }
-            fullName = subName + 'array';
-        }
-        return fullName;
-    }
 
     private static getKeyFromObject(obj: any) {
-        var name = this.getName(obj);
+        var name = TypeConverter.getName(obj);
         if (!this.nameToKeyList[name])
             throw new Error('No TypeConverter found for ' + name);
         return this.nameToKeyList[name];
--- NavigationJS/src/converter/TypeConverter.ts
+++ NavigationJS/src/converter/TypeConverter.ts
@@ -14,5 +14,22 @@
     convertTo(val: any): { val: string, queryStringVal?: string[] } {
         return null;
     }
+    
+    static getName(obj: any) {
+        var fullName = typeof obj;
+        var subName: string;
+        if (Object.prototype.toString.call(obj) === '[object Array]') {
+            var arr: any[] = obj;
+            subName = 'string';
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i] != null && arr[i].toString()) {
+                    subName = typeof arr[i];
+                    break;
+                }
+            }
+            fullName = subName + 'array';
+        }
+        return fullName;
+    }
 }
 export = TypeConverter;
