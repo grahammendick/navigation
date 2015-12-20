67d6ccb766f8d7f52bf31cbd63e2827861bd6fd5
Graham Mendick <graham.mendick@gmail.com>
2015-12-20 10:56:55 +0000

    Used toString.call for all type lookups
    Browsers consistently report toString.call on objects (checked in
    BrowserStack). Changed Type name lookup to use it so that it
    automatically supports new TypeConverters, e.g., DateConverter
    
    Object.prototype.toString.call('')
    ; //'[object String]'
    Object.prototype.toString.call(true)
    ; //'[object Boolean]'
    Object.prototype.toString.call(1)
    ; //'[object Number]'
    Object.prototype.toString.call(new Date())
    ; //'[object Date]'
    Object.prototype.toString.call([]); //'[object Array]'

--- NavigationJS/src/converter/TypeConverter.ts
+++ NavigationJS/src/converter/TypeConverter.ts
@@ -16,18 +16,18 @@
     }
     
     static getName(obj: any) {
-        var fullName = typeof obj;
-        var subName: string;
-        if (Object.prototype.toString.call(obj) === '[object Array]') {
+        var fullName: string = Object.prototype.toString.call(obj);
+        fullName = fullName.substring(8, fullName.length - 1).toLowerCase();
+        if (fullName === 'array') {
             var arr: any[] = obj;
-            subName = 'string';
+            var subName = 'string';
             for (var i = 0; i < arr.length; i++) {
                 if (arr[i] != null && arr[i].toString()) {
                     subName = typeof arr[i];
                     break;
                 }
             }
-            fullName = subName + 'array';
+            fullName = subName + fullName;
         }
         return fullName;
     }
