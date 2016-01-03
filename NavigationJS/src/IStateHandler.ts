1aa517f60abb2e63c6e02993da5cd83522960dba
Graham Mendick <graham.mendick@gmail.com>
2016-01-03 13:05:20 +0000

    Moved functions around to match StateHandler

--- NavigationJS/src/IStateHandler.ts
+++ NavigationJS/src/IStateHandler.ts
@@ -5,8 +5,8 @@ interface IStateHandler {
     getNavigationLink(state: State, data: any, queryStringData?: { [index: string]: string[] }): string;
     navigateLink(oldState: State, state: State, url: string): void;
     getNavigationData(state: State, url: string, queryStringData?: any): any;
-    truncateCrumbTrail(state: State, crumbs: Crumb[]): Crumb[];
     urlEncode?(state: State, key: string, val: string, queryString: boolean): string;
     urlDecode?(state: State, key: string, val: string, queryString: boolean): string;
+    truncateCrumbTrail(state: State, crumbs: Crumb[]): Crumb[];
 }
 export = IStateHandler;
