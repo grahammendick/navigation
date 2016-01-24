af8c804681a3ada69626d29fddd5febb1566fa36
Graham Mendick <graham.mendick@gmail.com>
2016-01-24 19:51:02 +0000

    Added reminders

--- NavigationCycle/src/NavigationDriver.ts
+++ NavigationCycle/src/NavigationDriver.ts
@@ -1,4 +1,6 @@
 // Should take in anchors and wrap navigate, refresh and back StateController functions
+// queryable navigated stream, different events, disposing, navigated etc.
+// accept url to pass to Navigation start?
 function NavigationDriver() {
     
 }
