5723deeae7ccea768327c78f5821bb66ad25c523
Graham Mendick <graham.mendick@gmail.com>
2016-01-26 19:48:55 +0000

    Copied props in navigation back link

--- NavigationCycle/src/NavigationBackLink.ts
+++ NavigationCycle/src/NavigationBackLink.ts
@@ -3,6 +3,8 @@ import CycleDOM = require('@cycle/dom');
 
 var NavigationBackLink = (properties: any, children: any) => {
     var newProperties: any = {};
+    for(var key in properties)
+        newProperties[key] = properties[key];
     var link = Navigation.StateController.getNavigationBackLink(properties.distance);
     newProperties.href = Navigation.settings.historyManager.getHref(link);
     return CycleDOM.h(newProperties.href ? 'a' : 'span', newProperties, children);
