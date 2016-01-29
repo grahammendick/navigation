784630d3f1c787d95257827eea6bc07dd8c65cc4
Graham Mendick <graham.mendick@gmail.com>
2016-01-29 17:45:09 +0000

    Added navigate events to Navigation sink
    Converted these to StateController navigate calls

--- NavigationCycle/src/NavigationDriver.ts
+++ NavigationCycle/src/NavigationDriver.ts
@@ -1,6 +1,18 @@
 import Navigation = require('navigation');
 import Rx = require('rx');
 
+function navigate(properties) {
+    if (properties.action) {
+        Navigation.StateController.navigate(properties.action, properties.toData, properties.historyAction);
+    } else {
+        if (properties.toData) {
+            Navigation.StateController.refresh(properties.toData, properties.historyAction);
+        } else {
+            Navigation.StateController.navigateBack(properties.distance, properties.historyAction);
+        }
+    }             
+}
+
 var NavigationDriver = (url) => {
     return (navigate$) => {
         var started = false;
@@ -10,10 +22,14 @@ var NavigationDriver = (url) => {
                 started = true;
                 return;
             }
-            if (!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKey && !e.button) {
-                e.preventDefault();
-                var link = Navigation.settings.historyManager.getUrl(e.target);
-                Navigation.StateController.navigateLink(link);
+            if (e.target) {
+                if (!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKey && !e.button) {
+                    e.preventDefault();
+                    var link = Navigation.settings.historyManager.getUrl(e.target);
+                    Navigation.StateController.navigateLink(link);
+                }
+            } else {
+                navigate(e);
             }
         });
         var navigated$ = new Rx.ReplaySubject(1);
