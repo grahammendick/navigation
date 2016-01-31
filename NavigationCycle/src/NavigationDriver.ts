47f768a7fab95c503bbd9787fbc1f0d722409e66
Graham Mendick <graham.mendick@gmail.com>
2016-01-31 10:59:22 +0000

    Allowed string as history action
    Any plugin navigation should accept string as history action, only
    calling the StateController functions directly doesn't accept strings

--- NavigationCycle/src/NavigationDriver.ts
+++ NavigationCycle/src/NavigationDriver.ts
@@ -2,14 +2,17 @@ import Navigation = require('navigation');
 import Rx = require('rx');
 
 function navigate(e) {
+    var historyAction = e.historyAction;
+    if (typeof historyAction === 'string')
+        historyAction = Navigation.HistoryAction[historyAction];
     if (e.action)
-        Navigation.StateController.navigate(e.action, e.toData, e.historyAction);
+        Navigation.StateController.navigate(e.action, e.toData, historyAction);
     if (!e.action && e.toData)
-        Navigation.StateController.refresh(e.toData, e.historyAction);
+        Navigation.StateController.refresh(e.toData, historyAction);
     if (e.distance)
-        Navigation.StateController.navigateBack(e.distance, e.historyAction);
+        Navigation.StateController.navigateBack(e.distance, historyAction);
     if (e.url)
-        Navigation.StateController.navigateLink(e.url);
+        Navigation.StateController.navigateLink(e.url, false, historyAction);
 }
 
 function isolate(NavigationSource, key) {
