308fcc3dc0941b2abe87288109e0bc66dc59603f
Graham Mendick <graham.mendick@gmail.com>
2016-02-28 13:45:47 +0000

    Created proper state controller functions

--- NavigationJS/src/config/State.ts
+++ NavigationJS/src/config/State.ts
@@ -13,10 +13,20 @@ class State implements IState {
     crumbTrailKey: string = 'crumb';
     trackTypes: boolean = true;
     stateHandler: StateHandler = new StateHandler();
-    unloading: (state: State, data: any, url: string, unload: () => void, history: boolean) => void = function (state, data, url, unload) { unload(); };
-    navigating: (data: any, url: string, navigate: (asyncData?: any) => void, history: boolean) => void = function (data, url, navigate) { navigate(); };
-    dispose: () => void = function () { };
-    navigated: (data: any, asyncData?: any) => void = function (data: any) { };
+    
+    unloading(state: State, data: any, url: string, unload: () => void, history: boolean) { 
+        unload()
+    };
+    
+    navigating(data: any, url: string, navigate: (asyncData?: any) => void, history: boolean) {
+        navigate();
+    };
+    
+    dispose() { 
+    };
+    
+    navigated(data: any, asyncData?: any) {
+    };
     
     urlEncode(state: State, key: string, val: string, queryString: boolean): string {
         return encodeURIComponent(val);
