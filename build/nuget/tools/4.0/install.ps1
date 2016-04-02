642163438ddfb1eb98139ee90138f5625865c170
Graham Mendick <graham.mendick@gmail.com>
2016-04-02 19:52:39 +0100

    Removed nuget build

--- build/nuget/tools/4.0/install.ps1
+++
@@ -1,8 +0,0 @@
-param($rootPath, $toolsPath, $package, $project)
-
-if ($project.Type -eq "VB.NET") {
-	$project.ProjectItems.Item("App_Start").ProjectItems.Item("FluentStateInfoConfig.cs").Delete()
-}
-if ($project.Type -eq "C#") {
-	$project.ProjectItems.Item("App_Start").ProjectItems.Item("FluentStateInfoConfig.vb").Delete()
-}
