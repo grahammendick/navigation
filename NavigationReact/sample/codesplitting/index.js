a5314361eff79279855442beda5bac6a93c2c63c
Graham Mendick <graham.mendick@gmail.com>
2016-04-05 17:05:17 +0100

    Added basic codesplitting structure
    Separate module for each State so they can be split

---
+++ NavigationReact/sample/codesplitting/index.js
@@ -0,0 +1,6 @@
+var Navigation = require('navigation');
+
+var stateNavigator = new Navigation.StateNavigator([
+    {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1 }},
+    {key: 'person', route: 'person/{id}', defaults: {id: 0 }}
+]);
