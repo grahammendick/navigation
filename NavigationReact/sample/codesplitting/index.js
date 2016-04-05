fd00716803ee1a2f00cceabd762ba3b2c775d101
Graham Mendick <graham.mendick@gmail.com>
2016-04-05 17:16:36 +0100

    Added code splitting theory

--- NavigationReact/sample/codesplitting/index.js
+++ NavigationReact/sample/codesplitting/index.js
@@ -4,3 +4,19 @@ var stateNavigator = new Navigation.StateNavigator([
     {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1 }},
     {key: 'person', route: 'person/{id}', defaults: {id: 0 }}
 ]);
+
+stateNavigator.states.people.navigating = function(data, url, navigate) {
+    require.ensure(['./People'], function(require) {
+        var People = require('./People');
+        People.createController(stateNavigator);
+        navigate();
+    });
+}
+
+stateNavigator.states.person.navigating = function(data, url, navigate) {
+    require.ensure(['./Person'], function(require) {
+        var Person = require('./Person');
+        Person.createController(stateNavigator);
+        navigate();
+    });
+}
