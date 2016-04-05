a23ac79801df0be5650aeda7f6283f21bd5c37f6
Graham Mendick <graham.mendick@gmail.com>
2016-04-05 17:43:45 +0100

    Created People and Person chunks
    Moved common requires, like React and NavigationReact, into index entry
    so that they don't appear in the chunks. Named the chunks for clarity -
    should help when it comes to writing out the right chunk for the request

--- NavigationReact/sample/codesplitting/index.js
+++ NavigationReact/sample/codesplitting/index.js
@@ -1,4 +1,8 @@
+var Data = require('./Data');
 var Navigation = require('navigation');
+var NavigationReact = require('navigation-react');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 var stateNavigator = new Navigation.StateNavigator([
     {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1 }},
@@ -10,7 +14,7 @@ stateNavigator.states.people.navigating = function(data, url, navigate) {
         var People = require('./People');
         People.createController(stateNavigator);
         navigate();
-    });
+    }, 'People');
 }
 
 stateNavigator.states.person.navigating = function(data, url, navigate) {
@@ -18,5 +22,5 @@ stateNavigator.states.person.navigating = function(data, url, navigate) {
         var Person = require('./Person');
         Person.createController(stateNavigator);
         navigate();
-    });
+    }, 'Person');
 }
--- NavigationReact/sample/codesplitting/webpack.config.js
+++ NavigationReact/sample/codesplitting/webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: __dirname,
-        filename: "app.js"
+        filename: "app.js",
+        chunkFilename: "[name].chunk.js"
     }
 };
