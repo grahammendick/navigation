89b25fe2fa50f3c27f0bccae5aafa279aeb79978
Graham Mendick <graham.mendick@gmail.com>
2016-04-14 13:48:31 +0100

    Moved component code into Person/People js

--- NavigationReact/sample/codesplitting/People.js
+++ NavigationReact/sample/codesplitting/People.js
@@ -1,7 +1,9 @@
-var Component = require('./Component');
 var Data = require('./Data');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var NavigationReact = require('navigation-react');
+var NavigationLink = NavigationReact.NavigationLink;
+var RefreshLink = NavigationReact.RefreshLink;
 
 /**
  * Attaches the navigated hook to the People State. Fired when the State is
@@ -11,15 +13,43 @@ var ReactDOM = require('react-dom');
  * e.g., stateNavigator.states.people.state = Component.Listing
  */
 exports.createController = function(stateNavigator) {
-    stateNavigator.states.people.navigating = function(data, url, navigate) {
-        navigate();
-    }
-    
     stateNavigator.states.people.navigated = function(data) {
         var people = Data.searchPeople(data.pageNumber);
         ReactDOM.render(
-            React.createElement(Component.Listing, {people: people, stateNavigator: stateNavigator}),
+            React.createElement(Listing, {people: people, stateNavigator: stateNavigator}),
             document.getElementById('content')
         );		
     }
 }
+
+var Listing = React.createClass({
+    render: function() {
+        var stateNavigator = this.props.stateNavigator;
+        var people = this.props.people.map(function (person) {
+            return (
+                React.createElement("tr", {key: person.id}, 
+                    React.createElement("td", null, React.createElement(NavigationLink, {stateKey: "person", navigationData: {id: person.id}, stateNavigator: stateNavigator}, person.name)), 
+                    React.createElement("td", null, person.dateOfBirth)
+                )
+            );
+        });
+        return (
+            React.createElement("div", {id: "listing"}, 
+                React.createElement("table", null, 
+                    React.createElement("thead", null, 
+                        React.createElement("tr", null, 
+                            React.createElement("th", null, "Name"), 
+                            React.createElement("th", null, "Date of Birth")
+                        )
+                    ), 
+                    React.createElement("tbody", null, people)
+                ), 
+                React.createElement("div", {id: "pager"}, 
+                    "Go to page", 
+                    React.createElement(RefreshLink, {navigationData: {pageNumber: 1}, disableActive: true, stateNavigator: stateNavigator}, "1"), 
+                    React.createElement(RefreshLink, {navigationData: {pageNumber: 2}, disableActive: true, stateNavigator: stateNavigator}, "2")
+                )
+            )
+        );
+    }
+})
--- NavigationReact/sample/codesplitting/Person.js
+++ NavigationReact/sample/codesplitting/Person.js
@@ -1,7 +1,8 @@
-var Component = require('./Component');
 var Data = require('./Data');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var NavigationReact = require('navigation-react');
+var NavigationBackLink = NavigationReact.NavigationBackLink;
 
 /**
  * Attaches the navigated hook to the Person State. Fired when the State is
@@ -11,15 +12,31 @@ var ReactDOM = require('react-dom');
  * e.g., stateNavigator.states.person.state = Component.Details
  */
 exports.createController = function(stateNavigator) {
-    stateNavigator.states.person.navigating = function(data, url, navigate) {
-        navigate();
-    }
-    
     stateNavigator.states.person.navigated = function(data) {
         var person = Data.getPerson(data.id);
         ReactDOM.render(
-            React.createElement(Component.Details, {person: person, stateNavigator: stateNavigator}),
+            React.createElement(Details, {person: person, stateNavigator: stateNavigator}),
             document.getElementById('content')
         );		
     }
 }
+
+var Details = React.createClass({
+    render: function() {
+        var person = this.props.person;
+        return (
+            React.createElement("div", {id: "details"}, 
+                React.createElement(NavigationBackLink, {distance: 1, stateNavigator: this.props.stateNavigator}, "People"), 
+                React.createElement("div", {id: "info"}, 
+                    React.createElement("h2", null, person.name), 
+                    React.createElement("div", {className: "label"}, "Date of Birth"), 
+                    React.createElement("div", null, person.dateOfBirth), 
+                    React.createElement("div", {className: "label"}, "Email"), 
+                    React.createElement("div", null, person.email), 
+                    React.createElement("div", {className: "label"}, "Phone"), 
+                    React.createElement("div", null, person.phone)
+                )
+            )
+        );
+    }
+})
