fa1d6ba4bcaeafe959e045ecc62dd9542f02154f
Graham Mendick <graham.mendick@gmail.com>
2016-07-07 15:23:09 +0100

    Copied across other files from React

---
+++ NavigationInferno/src/NavigationBackLink.ts
@@ -0,0 +1,36 @@
+import LinkUtility = require('./LinkUtility');
+import Navigation = require('navigation');
+import InfernoComponent = require('inferno-component');
+import createElement = require('inferno-create-element');
+
+class NavigationBackLink extends InfernoComponent {
+    private onNavigate = () => this.forceUpdate();
+    
+    private getStateNavigator(): Navigation.StateNavigator {
+        return this.props.stateNavigator || (<any> this.context).stateNavigator;
+    }
+    
+    private getNavigationBackLink(): string {
+        return LinkUtility.getLink(this.getStateNavigator(), () => this.getStateNavigator().getNavigationBackLink(this.props.distance));
+    }
+    
+    componentDidMount() {
+        if (!this.props.lazy)
+            this.getStateNavigator().onNavigate(this.onNavigate);
+    }
+    
+    componentWillUnmount() {
+        if (!this.props.lazy)
+            this.getStateNavigator().offNavigate(this.onNavigate);
+    }
+    
+    render() {
+        var props: any = {};
+        for(var key in this.props)
+            props[key] = this.props[key];
+        props.href = this.getNavigationBackLink();
+        LinkUtility.addListeners(this, this.getStateNavigator(), props, () => this.getNavigationBackLink());
+        return createElement(props.href ? 'a' : 'span', props);
+    }
+};
+export = NavigationBackLink;
---
+++ NavigationInferno/src/RefreshLink.ts
@@ -0,0 +1,43 @@
+import LinkUtility = require('./LinkUtility');
+import Navigation = require('navigation');
+import InfernoComponent = require('inferno-component');
+import createElement = require('inferno-create-element');
+
+class RefreshLink extends InfernoComponent {
+    private onNavigate = () => this.forceUpdate();
+    
+    private getStateNavigator(): Navigation.StateNavigator {
+        return this.props.stateNavigator || (<any> this.context).stateNavigator;
+    }
+    
+    getRefreshLink(): string {
+        var navigationData = LinkUtility.getData(this.getStateNavigator(), this.props.navigationData, this.props.includeCurrentData, this.props.currentDataKeys);
+        return LinkUtility.getLink(this.getStateNavigator(), () => this.getStateNavigator().getRefreshLink(navigationData));
+    }
+    
+    componentDidMount() {
+        if (!this.props.lazy)
+            this.getStateNavigator().onNavigate(this.onNavigate);
+    }
+    
+    componentWillUnmount() {
+        if (!this.props.lazy)
+            this.getStateNavigator().offNavigate(this.onNavigate);
+    }
+    
+    render() {
+        var props: any = {};
+        for(var key in this.props)
+            props[key] = this.props[key];
+        var active = true;
+        for (var key in this.props.navigationData) {
+            active = active && LinkUtility.isActive(this.getStateNavigator(), key, this.props.navigationData[key]);
+        }
+        props.href = this.getRefreshLink();
+        LinkUtility.addListeners(this, this.getStateNavigator(), props, () => this.getRefreshLink());
+        active = active && !!props.href;
+        LinkUtility.setActive(props, active, this.props.activeCssClass, this.props.disableActive);
+        return createElement(props.href ? 'a' : 'span', props);
+    }
+};
+export = RefreshLink;
