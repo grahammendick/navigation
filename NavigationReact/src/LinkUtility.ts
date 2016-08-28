f0238efe62a36380652b71eaf2e47940a3b89747
Graham Mendick <graham.mendick@gmail.com>
2016-08-28 17:17:16 +0100

    Supported dates and arrays in active check

--- NavigationReact/src/LinkUtility.ts
+++ NavigationReact/src/LinkUtility.ts
@@ -23,15 +23,24 @@ class LinkUtility {
     static isActive(stateNavigator: Navigation.StateNavigator, key: string, val: any): boolean {
         if (!stateNavigator.stateContext.state)
             return false;
-        if (val != null) {
-            var trackTypes = stateNavigator.stateContext.state.trackTypes;
-            var currentVal = stateNavigator.stateContext.data[key];
-            if (currentVal != null)
-                return trackTypes ? val === currentVal : val.toString() == currentVal.toString();
-            else
-                return val === '';
+        return val == null || this.areEqual(val, stateNavigator.stateContext.data[key]);
+    }
+
+    private static areEqual(val: any, currentVal: any): boolean {
+        if (currentVal == null)
+            return val == null || val === '';
+        var valType = Object.prototype.toString.call(val);
+        if (valType !== Object.prototype.toString.call(currentVal))
+            return false;
+        if (valType === '[object Array]') {
+            var active = val.length !== currentVal.length;
+            for(var i = 0; active && i < val.length; i++) {
+                active = this.areEqual(val[i], currentVal[i]);
+            }
+            return active;
+        } else {
+            return isNaN(val) ? val === currentVal : +val === +currentVal;
         }
-        return true;
     }
 
     static setActive(props: any, active: boolean, activeCssClass: string, disableActive: boolean) {
