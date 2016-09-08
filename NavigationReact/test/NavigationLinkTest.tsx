8e1e7dbde988242925a107e0a6e4bf4ff14cb399
Graham Mendick <graham.mendick@gmail.com>
2016-09-08 15:36:18 +0100

    Tested rendered output using React Test Utils
    Was going to use enzyme but it gave compile errors, plus it was using
    react test utils under the covers anyway. Updated d.ts files to match
    the test utils one

--- NavigationReact/test/NavigationLinkTest.tsx
+++ NavigationReact/test/NavigationLinkTest.tsx
@@ -1,15 +1,27 @@
 /// <reference path="assert.d.ts" />
 /// <reference path="mocha.d.ts" />
 /// <reference path="../src/react.d.ts" />
+/// <reference path="react-addons-test-utils.d.ts" />
 import assert = require('assert');
+import Navigation = require('../../Navigation/src/Navigation');
 import NavigationReact = require('../src/NavigationReact');
 import React = require('react');
+import ReactTestUtils = require('react-addons-test-utils');
 
 describe('NavigationLinkTest', function () {
     describe('<>', function () {
         it('should work', function(){
-            var x = <NavigationReact.NavigationLink />;
-            assert.notEqual(null, x);
+            var stateNavigator = new Navigation.StateNavigator([
+                { key: 's', route: '' }
+            ]);
+            var renderer = ReactTestUtils.createRenderer();
+            renderer.render(
+                <NavigationReact.NavigationLink
+                    stateKey="s"
+                    stateNavigator={stateNavigator} />
+            );
+            var link = renderer.getRenderOutput();
+            assert.equal(link.props['href'], '#/');
         })
     });  
 });
