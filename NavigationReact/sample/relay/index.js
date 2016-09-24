65033d463effc490866267ec84ae20e58d247f73
Graham Mendick <graham.mendick@gmail.com>
2016-09-24 19:35:14 +0100

    Removed commented code

--- NavigationReact/sample/relay/index.js
+++ NavigationReact/sample/relay/index.js
@@ -5,95 +5,10 @@ import Listing from './People.js';
 import Details from './Person.js';
 import { StateNavigator } from 'Navigation';
 
-/*class Score extends React.Component {
-  render() {
-    var {initials, score} = this.props.score;
-    return (
-      <li key={initials}>
-        <strong>{initials}</strong> {score}
-      </li>
-    );
-  }
-}
-Score = Relay.createContainer(Score, {
-  fragments: {
-    score: () => Relay.QL`
-      fragment on Score {
-        initials,
-        score,
-      }
-    `,
-  },
-});
-
-class Game extends React.Component {
-  _handleCountChange = (e) => {
-    this.props.relay.setVariables({
-      numToShow: e.target.value
-        ? parseInt(e.target.value, 10)
-        : 0,
-    });
-  }
-  _handleSortChange = (e) => {
-    this.props.relay.setVariables({
-      sortDirection: e.target.value,
-    });
-  }
-  render() {
-    var {scores} = this.props.game;
-    var {
-      numToShow,
-      sortDirection,
-    } = this.props.relay.variables;
-    return (
-      <div>
-        <h1>High Scores</h1>
-        <select
-          defaultValue={sortDirection}
-          onChange={this._handleSortChange}>
-          <option value="asc">Bottom</option>
-          <option value="desc">Top</option>
-        </select>{' '}
-        <input
-          onChange={this._handleCountChange}
-          min="0"
-          style={{width: 44}}
-          type="number"
-          value={numToShow}
-        />
-        <ul>
-          {scores.map(
-            score => <Score score={score} />
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
-Game = Relay.createContainer(Game, {
-  initialVariables: {
-    numToShow: 10,
-    sortDirection: 'desc',
-  },
-  fragments: {
-    game: () => Relay.QL`
-      fragment on Game {
-        scores(
-          numToShow: $numToShow,
-          sortDirection: $sortDirection
-        ) {
-          ${Score.getFragment('score')},
-        },
-      }
-    `,
-  },
-});*/
-
 var stateNavigator = new StateNavigator([
     {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1}},
     {key: 'person', route: 'person/{id}', defaults: {id: 0}, trackCrumbTrail: true}
 ]);
-
 
 var Person = Relay.createContainer(Details, {
   fragments: {
@@ -108,7 +23,6 @@ var Person = Relay.createContainer(Details, {
   },
 });
 
-
 var People = Relay.createContainer(Listing, {
   fragments: {
     people: () => Relay.QL`
@@ -122,17 +36,6 @@ var People = Relay.createContainer(Listing, {
     `,
   },
 });
-
-/*class GameHomeRoute extends Relay.Route {
-  static routeName = 'Home';
-  static queries = {
-    game: (Component) => Relay.QL`
-      query GameQuery {
-        game { ${Component.getFragment('game')} },
-      }
-    `,
-  };
-}*/
 
 class PeopleRoute extends Relay.Route {
   static routeName = 'People';
--- NavigationReact/sample/relay/schema.js
+++ NavigationReact/sample/relay/schema.js
@@ -6,26 +6,6 @@ import {
   GraphQLString,
 } from 'graphql';
 
-/*const GAME = {
-  scores: [
-    {initials: 'SDL', score: 134812},
-    {initials: '_TY', score: 897243},
-    {initials: 'AAA', score: 348234},
-    {initials: '_LK', score: 903244},
-    {initials: '_JK', score: 890324},
-    {initials: 'GLH', score: 248721},
-    {initials: '_JS', score: 257893},
-    {initials: 'Y~Z', score: 752323},
-    {initials: 'J/D', score: 982354},
-    {initials: 'L!B', score: 252432},
-    {initials: 'N*S', score: 982523},
-    {initials: '*DS', score: 278347},
-    {initials: '+AZ', score: 178954},
-    {initials: 'FC;', score: 897252},
-    {initials: '#BK', score: 547840},
-  ],
-};*/
-
 var PEOPLE = [
     {id: 1, name: 'Bell Halvorson', dateOfBirth: '01/01/1980', email: 'bell@navigation.com', phone: '555 0001'},
     {id: 2, name: 'Aditya Larson', dateOfBirth: '01/02/1980', email: 'aditya@navigation.com', phone: '555 0002'},
@@ -58,50 +38,6 @@ var PeopleType = new GraphQLObjectType({
     persons: {type: new GraphQLList(PersonType)},
   }),
 });
-
-/*var ScoreType = new GraphQLObjectType({
-  name: 'Score',
-  fields: () => ({
-    initials: {type: GraphQLString},
-    score: {type: GraphQLInt},
-  }),
-});
-
-var GameType = new GraphQLObjectType({
-  name: 'Game',
-  fields: () => ({
-    scores: {
-      args: {
-        numToShow: {type: GraphQLInt},
-        sortDirection: {type: GraphQLString},
-      },
-      type: new GraphQLList(ScoreType),
-      resolve: (game, {numToShow, sortDirection}) => {
-        if (numToShow == null) {
-          numToShow = game.scores.length;
-        }
-        var sortMult = sortDirection === 'asc' ? 1 : -1;
-        return game.scores
-          .sort((a, b) =>
-            sortMult * (a.score - b.score)
-          )
-          .slice(0, numToShow);
-      },
-    },
-  }),
-});
-
-export default new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: () => ({
-      game: {
-        type: GameType,
-        resolve: () => GAME,
-      },
-    }),
-  }),
-});*/
 
 export default new GraphQLSchema({
   query: new GraphQLObjectType({
