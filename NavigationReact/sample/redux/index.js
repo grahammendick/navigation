94287f004c73b76adc316632baef46c58e297500
Graham Mendick <graham.mendick@gmail.com>
2016-10-06 14:00:27 +0100

    Sketched in idea for store subscribe and onNavigate

--- NavigationReact/sample/redux/index.js
+++ NavigationReact/sample/redux/index.js
@@ -2,25 +2,45 @@ import { Listing, Detail } from './Component';
 import { StateNavigator } from 'navigation';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 var stateNavigator = new StateNavigator([
     {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1 }},
     {key: 'person', route: 'person/{id}', defaults: {id: 0 }}
 ]);
 
-stateNavigator.states.people.render = function(data, state) {
+var store = createStore();
+
+store.subscribe(() => {
+    var { stateContext: { state, data} } = stateNavigator;
+    state.render(data, store);
+})
+
+stateNavigator.onNavigate((oldState, state, data) => {
+    state.render(data, store);
+});
+
+stateNavigator.states.people.render = function(data, store) {
+    var people = store.getState();
 	var start = (data.pageNumber - 1) * 10;
-	var people = state.slice(start, start + 10);
+	people = people.slice(start, start + 10);
     ReactDOM.render(
-        <Listing people={people} stateNavigator={stateNavigator} />,
+        <Listing
+            people={people}
+            store={store}
+            stateNavigator={stateNavigator} />,
         document.getElementById('content')
     );		
 }
 
 stateNavigator.states.person.navigated = function(data, state) {
-    var person = state.filter((person) => person.id === data.id)[0];
+    var people = store.getState();
+    var person = people.filter((person) => person.id === data.id)[0];
     ReactDOM.render(
-        <Details person={person} stateNavigator={stateNavigator} />,
+        <Details
+            person={person}
+            store={store}
+            stateNavigator={stateNavigator} />,
         document.getElementById('content')
     );		
 }
