d8ae9209cd88f83af891fb2a175bb32c3e720f02
Graham Mendick <graham.mendick@gmail.com>
2016-10-06 19:49:00 +0100

    Commented code

--- NavigationReact/sample/redux/index.js
+++ NavigationReact/sample/redux/index.js
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import reducer from './reducer';
 
+/**
+ * Configures the states for the two views.
+ */
 var stateNavigator = new StateNavigator([
     {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1 }},
     {key: 'person', route: 'person/{id}', defaults: {id: 0 }, trackCrumbTrail: true}
@@ -12,15 +15,27 @@ var stateNavigator = new StateNavigator([
 
 var store = createStore(reducer);
 
+/**
+ * Subscribes to store changes. Calls render on the current state, passing the
+ * current data and store
+ */
 store.subscribe(() => {
     var { stateContext: { state, data} } = stateNavigator;
     state.render(data, store);
 })
 
+/**
+ * Subscribes to navigation events. Calls render on the new state, passing the
+ * new data and store.
+ */
 stateNavigator.onNavigate((oldState, state, data) => {
     state.render(data, store);
 });
 
+/**
+ * Filters the people in the store by the page number in the navigation data.
+ * Renders the Listing Component with the filtered data.
+ */
 stateNavigator.states.people.render = function(data, store) {
     var people = store.getState();
 	var start = (data.pageNumber - 1) * 10;
@@ -33,6 +48,10 @@ stateNavigator.states.people.render = function(data, store) {
     );
 };
 
+/**
+ * Finds the person from the people in the store and id in the navigation data.
+ * Renders the Details Component with the person.
+ */
 stateNavigator.states.person.render = function(data, store) {
     var people = store.getState();
     var person = people.filter((person) => person.id === data.id)[0];
