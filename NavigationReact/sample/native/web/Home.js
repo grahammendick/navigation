950b6936ebd206bb03059004ed8d57ce2ff94116
Graham Mendick <graham.mendick@gmail.com>
2016-11-10 22:25:48 +0000

    Added border to home scene

--- NavigationReact/sample/native/web/Home.js
+++ NavigationReact/sample/native/web/Home.js
@@ -1,10 +1,12 @@
+import Back from './Back.js';
 import React from 'react';
 
 export default ({stateNavigator}) => (
-    <div>
+    <div style={styles.home}>
         <div style={styles.nav}>
-            <svg style={styles.home} viewBox="0 0 64 72"><g><path d="M60.034 33.795l-26-23.984a2.997 2.997 0 0 0-4.068 0l-26 23.984a3 3 0 0 0 4.068 4.41l2.265-2.09 6.809 24.683A3 3 0 0 0 20 63h24a3 3 0 0 0 2.892-2.202l6.809-24.683 2.265 2.09a2.988 2.988 0 0 0 2.033.795 3 3 0 0 0 2.035-5.205zM32 53a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0-11a6.999 6.999 0 1 1 0-13.998A7 7 0 1 1 32 42z"></path></g></svg>
+            <svg style={styles.icon} viewBox="0 0 64 72"><g><path d="M60.034 33.795l-26-23.984a2.997 2.997 0 0 0-4.068 0l-26 23.984a3 3 0 0 0 4.068 4.41l2.265-2.09 6.809 24.683A3 3 0 0 0 20 63h24a3 3 0 0 0 2.892-2.202l6.809-24.683 2.265 2.09a2.988 2.988 0 0 0 2.033.795 3 3 0 0 0 2.035-5.205zM32 53a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0-11a6.999 6.999 0 1 1 0-13.998A7 7 0 1 1 32 42z"></path></g></svg>
             <h1 style={styles.heading}>Home</h1>
+            <Back stateNavigator={stateNavigator}/>
         </div>
         <ul style={styles.tweets}>
             <li style={styles.tweet} onClick={() => stateNavigator.navigate('tweet')}>
@@ -19,6 +21,10 @@ export default ({stateNavigator}) => (
 
 var styles = {
     home: {
+        border: '1px solid #ccd6dd',
+        height: '400px'
+    },
+    icon: {
         display: 'inline-block',
         fill: '#1da1f2',
         width: '10%',
