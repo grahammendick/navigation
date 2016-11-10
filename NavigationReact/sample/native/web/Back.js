1dc014eda3c1df583357961f6957766894fd878f
Graham Mendick <graham.mendick@gmail.com>
2016-11-10 22:27:39 +0000

    Simplified Back component

--- NavigationReact/sample/native/web/Back.js
+++ NavigationReact/sample/native/web/Back.js
@@ -1,22 +1,8 @@
 import React, { Component } from 'react';
 
-class Back extends Component {
-    constructor(props) {
-        super(props);
-        this.onNavigate = () => this.forceUpdate();
-    }
-    componentDidMount() {
-        this.props.stateNavigator.onNavigate(this.onNavigate);
-    }
-    componentWillUnmount() {
-        this.props.stateNavigator.offNavigate(this.onNavigate);
-    }
-    render() {
-        var {stateNavigator} = this.props;
-        if (stateNavigator.canNavigateBack(1))
-            return <button onClick={(() => stateNavigator.navigateBack(1))}>Back</button>;
-        return null;
-    }
+export default ({stateNavigator}) => {
+    if (stateNavigator.canNavigateBack(1))
+        return <button onClick={(() => stateNavigator.navigateBack(1))}>Back</button>;
+    return null;
 }
 
-export default Back;
