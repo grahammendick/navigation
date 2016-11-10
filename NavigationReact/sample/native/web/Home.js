7eea40bf7d5e75f55cabc5047ebc2a9d0ec27ba8
Graham Mendick <graham.mendick@gmail.com>
2016-11-10 14:16:46 +0000

    Moved hard-coded html into Home component

---
+++ NavigationReact/sample/native/web/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <div id="nav">
+            <svg id="home" viewBox="0 0 64 72"><g><path d="M60.034 33.795l-26-23.984a2.997 2.997 0 0 0-4.068 0l-26 23.984a3 3 0 0 0 4.068 4.41l2.265-2.09 6.809 24.683A3 3 0 0 0 20 63h24a3 3 0 0 0 2.892-2.202l6.809-24.683 2.265 2.09a2.988 2.988 0 0 0 2.033.795 3 3 0 0 0 2.035-5.205zM32 53a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0-11a6.999 6.999 0 1 1 0-13.998A7 7 0 1 1 32 42z"></path></g></svg>
+            <h1>Home</h1>
+        </div>
+        <ul>
+            <li>
+                <h2>React</h2> <span>@reactjs</span><span>3h</span>
+                <img src="react.png" />
+                <div>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+            </li>
+            <li>
+                <h2>React</h2> <span>@reactjs</span><span>3h</span>
+                <img src="react.png" />
+                <div>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+            </li>
+            <li>
+                <h2>React</h2> <span>@reactjs</span><span>3h</span>
+                <img src="react.png" />
+                <div>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+            </li>
+            <li>
+                <h2>React</h2> <span>@reactjs</span><span>3h</span>
+                <img src="react.png" />
+                <div>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+            </li>
+            <li>
+                <h2>React</h2> <span>@reactjs</span><span>3h</span>
+                <img src="react.png" />
+                <div>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+            </li>
+            <li>
+                <h2>React</h2> <span>@reactjs</span><span>3h</span>
+                <img src="react.png" />
+                <div>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+            </li>
+        </ul>
+    </div>
+);
--- NavigationReact/sample/native/web/index.js
+++ NavigationReact/sample/native/web/index.js
@@ -1,6 +1,7 @@
 import Back from './Back.js';
 import ComponentA from './ComponentA.js';
 import ComponentB from './ComponentB.js';
+import Home from './Home.js';
 import {spring} from 'react-motion';
 import {StateNavigator} from 'navigation';
 import SceneNavigator from './SceneNavigator.js';
@@ -14,7 +15,7 @@ var stateNavigator = new StateNavigator([
 ]);
 
 var {first, second, third} = stateNavigator.states;
-first.renderScene = () => <ComponentA title="First" stateNavigator={stateNavigator} next="second"/>;
+first.renderScene = () => <Home stateNavigator={stateNavigator}/>;
 second.renderScene = () => <ComponentA title="Second" stateNavigator={stateNavigator} next="third"/>;
 third.renderScene = () => <ComponentB title="Third" stateNavigator={stateNavigator}/>;
 
@@ -25,13 +26,11 @@ ReactDOM.render(
     <div>
         <Back stateNavigator={stateNavigator} />
         <SceneNavigator
-            getDefaultStyle={(state) => ({x: state == first ? 200 : 400})}
-            getStyle={(active) => ({x: spring(!active ? 0 : 200)})}
+            getDefaultStyle={(state) => ({x: state == first ? 0 : 400})}
+            getStyle={(active) => ({x: spring(!active ? 0 : 0)})}
             interpolateStyle={({x}, active) => ({
                 position: 'absolute',
                 display: !active ? 'none' : 'block',
-                width: '200px',
-                height: '500px',
                 backgroundColor: '#fff',
                 transform: `translate3d(${x}px, 0, 0)`})}
             stateNavigator={stateNavigator} />
