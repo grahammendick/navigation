2717dfcbd99c31982a0c059788892667645187e2
Graham Mendick <graham.mendick@gmail.com>
2016-11-11 12:35:49 +0000

    Made content scrollable

--- NavigationReact/sample/native/web/Home.js
+++ NavigationReact/sample/native/web/Home.js
@@ -6,14 +6,16 @@ export default ({stateNavigator}) => (
             <svg style={styles.icon} viewBox="0 0 64 72"><g><path d="M60.034 33.795l-26-23.984a2.997 2.997 0 0 0-4.068 0l-26 23.984a3 3 0 0 0 4.068 4.41l2.265-2.09 6.809 24.683A3 3 0 0 0 20 63h24a3 3 0 0 0 2.892-2.202l6.809-24.683 2.265 2.09a2.988 2.988 0 0 0 2.033.795 3 3 0 0 0 2.035-5.205zM32 53a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0-11a6.999 6.999 0 1 1 0-13.998A7 7 0 1 1 32 42z"></path></g></svg>
             <h1 style={styles.heading}>Home</h1>
         </div>
-        <ul style={styles.tweets}>
-            <li style={styles.tweet} onClick={() => stateNavigator.navigate('tweet')}>
-                <h2 style={styles.name}>React</h2>
-                <span style={styles.username}>@reactjs</span><span style={styles.time}>3h</span>
-                <img src="react.png" style={styles.logo} />
-                <div style={styles.text}>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
-            </li>
-       </ul>
+        <div style={styles.content}>
+            <ul style={styles.tweets}>
+                <li style={styles.tweet} onClick={() => stateNavigator.navigate('tweet')}>
+                    <h2 style={styles.name}>React</h2>
+                    <span style={styles.username}>@reactjs</span><span style={styles.time}>3h</span>
+                    <img src="react.png" style={styles.logo} />
+                    <div style={styles.text}>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+                </li>
+            </ul>
+        </div>
     </div>
 );
 
@@ -40,12 +42,14 @@ var styles = {
         boxShadow: '0 0 4px #657786',
         height: '30px'
     },   
+    content: {
+        height: '350px',
+        overflow: 'auto'
+    },
     tweets: {
         padding: 0,
         margin: 0,
         listStyle: 'none',
-        height: '351px',
-        overflow: 'auto'
     },
     logo: {
         width: '20%',
--- NavigationReact/sample/native/web/Tweet.js
+++ NavigationReact/sample/native/web/Tweet.js
@@ -7,25 +7,27 @@ export default ({stateNavigator}) => (
             <svg onClick={() => stateNavigator.navigateBack(1)} style={styles.icon} viewBox="0 0 46 72"><g><path d="M40 33H15.243l7.878-7.879a2.998 2.998 0 0 0 0-4.242 2.998 2.998 0 0 0-4.242 0l-13 13a2.998 2.998 0 0 0 0 4.242l13 13c.585.586 1.353.879 2.121.879s1.536-.293 2.121-.879a2.998 2.998 0 0 0 0-4.242L15.243 39H40a3 3 0 1 0 0-6z"></path></g></svg>
             <h1 style={styles.heading}>Tweet</h1>
         </div>
-        <div style={styles.detail}>
-            <img src="react.png" style={styles.logo} />
-            <h2 style={styles.name}>React</h2>
-            <div style={styles.username}>@reactjs</div>
-            <div style={styles.message}>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
-            <div style={styles.fullTime}>12:04 pm · 11 Nov 2016</div>
-            <div style={styles.interactions}>
-                <span style={styles.count}>12</span><span>RETWEETS</span>
-                <span style={styles.count}>14</span><span>LIKES</span>
-            </div>
-        </div>
-        <ul style={styles.tweets}>
-            <li style={styles.tweet} onClick={() => stateNavigator.navigate('tweet')}>
-                <h2 style={styles.name}>React</h2>
-                <span style={styles.username}>@reactjs</span><span style={styles.time}>3h</span>
+        <div style={styles.content}>
+            <div style={styles.detail}>
                 <img src="react.png" style={styles.logo} />
-                <div style={styles.text}>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
-            </li>
-       </ul>
+                <h2 style={styles.name}>React</h2>
+                <div style={styles.username}>@reactjs</div>
+                <div style={styles.message}>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+                <div style={styles.fullTime}>12:04 pm · 11 Nov 2016</div>
+                <div style={styles.interactions}>
+                    <span style={styles.count}>12</span><span>RETWEETS</span>
+                    <span style={styles.count}>14</span><span>LIKES</span>
+                </div>
+            </div>
+            <ul style={styles.tweets}>
+                <li style={styles.tweet} onClick={() => stateNavigator.navigate('tweet')}>
+                    <h2 style={styles.name}>React</h2>
+                    <span style={styles.username}>@reactjs</span><span style={styles.time}>3h</span>
+                    <img src="react.png" style={styles.logo} />
+                    <div style={styles.text}>Have you tried our 15.4 release candidate? Give it a shot and let us know if you have any issues before we ship it! </div>
+                </li>
+            </ul>
+        </div>
     </div>
 );
 
@@ -51,6 +53,10 @@ var styles = {
         padding: '10px 0',
         boxShadow: '0 0 4px #657786',
         height: '30px'
+    },
+    content: {
+        height: '350px',
+        overflow: 'auto'
     },
     detail: {
         padding: '5% 3%',
@@ -78,9 +84,7 @@ var styles = {
     tweets: {
         padding: 0,
         margin: 0,
-        listStyle: 'none',
-        height: '351px',
-        overflow: 'auto'
+        listStyle: 'none'
     },
     logo: {
         width: '20%',
