0ddddef45aa5ce662b14f6981aa97084c1d57743
Graham Mendick <graham.mendick@gmail.com>
2016-11-11 09:23:06 +0000

    Renamed leave to parent because for enter/leave

--- NavigationReact/sample/native/web/SceneNavigator.js
+++ NavigationReact/sample/native/web/SceneNavigator.js
@@ -20,10 +20,10 @@ class SceneNavigator extends Component{
             });
         });
     }
-    renderMotion({key, data: {scene, state, data, mount}, style: {leave,...style}}) {
+    renderMotion({key, data: {scene, state, data, mount}, style: {parent,...parentStyle}}) {
         var {getMountStyle, getMountedStyle, interpolateStyle} = this.props;
         return <Motion key={key}
-            style={(leave ? style : (mount ? getMountStyle : getMountedStyle)(state, data))}>
+            style={(parent ? parentStyle : (mount ? getMountStyle : getMountedStyle)(state, data))}>
             {(interpolatingStyle) => <div style={interpolateStyle(interpolatingStyle)}>{scene}</div>}
         </Motion>;
     }
@@ -31,12 +31,12 @@ class SceneNavigator extends Component{
         var {oldState, state, data, url, crumbs} = this.props.stateNavigator.stateContext;
         var {getUnmountedStyle, getMountStyle, getUnmountStyle} = this.props;
         var sceneContexts = crumbs.concat({state, data, url, mount: true});
-        return <TransitionMotion willEnter={() => ({...getUnmountedStyle(state, data), leave: 1})} 
-            willLeave={() => ({...getUnmountStyle(state, data), leave: 1})}
+        return <TransitionMotion willEnter={() => ({...getUnmountedStyle(state, data), parent: 1})} 
+            willLeave={() => ({...getUnmountStyle(state, data), parent: 1})}
             styles={sceneContexts.map(({state, data, url, mount}) => ({
                 key: url,
                 data: {scene: this.state.scenes[url], state, data, mount},
-                style: {...getMountStyle(state, data), leave: spring(0)}
+                style: {...getMountStyle(state, data), parent: spring(0)}
             }))}>
             {interpolatingStyles =>
                 <div>{interpolatingStyles.map((config) => this.renderMotion(config))}</div>}
