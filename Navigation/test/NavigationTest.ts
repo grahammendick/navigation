b14c4f5b90629f0329bab73bba4f5cea81e04fa8
Graham Mendick <graham.mendick@gmail.com>
2016-11-22 16:44:32 +0000

    Added fluent transition with trail test

--- Navigation/test/NavigationTest.ts
+++ Navigation/test/NavigationTest.ts
@@ -4996,4 +4996,19 @@ describe('Navigation', function () {
             assert.strictEqual(stateNavigator.stateContext.url, null);
         });
     });
+
+    describe('Fluent Transition With Trail', function () {
+        it('should navigate', function() {
+            var stateNavigator = new StateNavigator([
+                { key: 's0', route: 'r0' },
+                { key: 's1', route: 'r1', trackCrumbTrail: true },
+            ]);
+            var url = stateNavigator.fluent()
+                .navigate('s0')
+                .navigate('s1')
+                .url;
+            assert.strictEqual(url, '/r1?crumb=%2Fr0');
+            assert.strictEqual(stateNavigator.stateContext.url, null);
+        });
+    });
 });
