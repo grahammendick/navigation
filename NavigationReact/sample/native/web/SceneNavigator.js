fb6d67eeb486415b83568d03365bff4f8ffb07d6
Graham Mendick <graham.mendick@gmail.com>
2016-11-28 13:48:11 +0000

    Reverted to getStyle, passing in style and data

--- NavigationReact/sample/native/web/SceneNavigator.js
+++ NavigationReact/sample/native/web/SceneNavigator.js
@@ -28,7 +28,7 @@ class SceneNavigator extends Component{
     //TODO - rename parent to something more obscure so no fear of user choosing same style prop
     renderScene({key, data: {scene, state, data, mount}, style: {parent,...parentStyle}}) {
         var {mountedStyle, mountedBackStyle, children} = this.props;
-        var style = getStyleAccessor(mount ? mountedStyle : mountedBackStyle)(state, data);
+        var style = getStyle(mount ? mountedStyle : mountedBackStyle, state, data);
         return (
             <Motion key={key} style={parent ? parentStyle : style}>
                 {(tweenStyle) => children(tweenStyle, scene, state, data)}
@@ -41,12 +41,12 @@ class SceneNavigator extends Component{
         var sceneContexts = crumbs.concat({state, data, url, mount: true});
         return (
             <TransitionMotion
-                willEnter={() => ({...stripStyle(getStyleAccessor(unmountedStyle)(state, data)), parent: 1})} 
-                willLeave={() => ({...getStyleAccessor(unmountedStyle)(oldState, oldData), parent: 1})}
+                willEnter={() => ({...stripStyle(getStyle(unmountedStyle, state, data)), parent: 1})} 
+                willLeave={() => ({...getStyle(unmountedStyle, oldState, oldData), parent: 1})}
                 styles={sceneContexts.map(({state, data, url, mount}) => ({
                     key: url,
                     data: {scene: this.state.scenes[url], state, data, mount},
-                    style: {...getStyleAccessor(mountedStyle)(state, data), parent: spring(0)}
+                    style: {...getStyle(mountedStyle, state, data), parent: spring(0)}
                 }))}>
                 {tweenStyles => <div>{tweenStyles.map((config) => this.renderScene(config))}</div>}
             </TransitionMotion>
@@ -61,8 +61,8 @@ function stripStyle(style) {
     return strippedStyle;
 }
 
-function getStyleAccessor(styleProp) {
-    return typeof styleProp === 'function' ? styleProp : () => styleProp;
+function getStyle(styleProp, state, data) {
+    return typeof styleProp === 'function' ? styleProp(state, data) : styleProp;
 }
 
 export default SceneNavigator;
