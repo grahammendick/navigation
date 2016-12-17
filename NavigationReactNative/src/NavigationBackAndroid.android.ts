56daa5974b80d786c56b596dc6bcaadc7e34f119
Graham Mendick <graham.mendick@gmail.com>
2016-12-17 16:54:12 +0000

    Return false if context state doesn't match state
    Each scene will have its own NavigationBackAndroid component but only
    the one corresponding to the current state should handle the back press

--- NavigationReactNative/src/NavigationBackAndroid.android.ts
+++ NavigationReactNative/src/NavigationBackAndroid.android.ts
@@ -5,11 +5,20 @@ import { BackAndroid } from 'react-native';
 class NavigationBackAndroid extends React.Component<any, any> {
     private onBack = () => {
         var stateNavigator = this.getStateNavigator();
-        if (stateNavigator.canNavigateBack(1)) {
-            stateNavigator.navigateBack(1);
+        if (this.state.state === stateNavigator.stateContext.state) {
+            var listener = this.props.navigating;
+            var navigate = true;
+            if (listener)
+                navigate = listener();
+            if (navigate && stateNavigator.canNavigateBack(1))
+                stateNavigator.navigateBack(1);
             return true;
         }
         return false;
+    }
+    constructor(props, context) {
+        super(props, context);
+        this.state = { state: this.getStateNavigator().stateContext.state };
     }
     static contextTypes = {
         stateNavigator: React.PropTypes.object
