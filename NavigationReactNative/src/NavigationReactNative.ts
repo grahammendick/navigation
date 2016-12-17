d91b3881f1e6dd17dc3daa526d2038a53d37042c
Graham Mendick <graham.mendick@gmail.com>
2016-12-17 17:45:39 +0000

    Added default to match the transpiled imports

--- NavigationReactNative/src/NavigationReactNative.ts
+++ NavigationReactNative/src/NavigationReactNative.ts
@@ -1,6 +1,6 @@
 import NavigationBackAndroid_android from './NavigationBackAndroid.android';
 import NavigationBackAndroid_ios from './NavigationBackAndroid.ios';
-var NavigationBackAndroid = require('./NavigationBackAndroid');
+var NavigationBackAndroid = require('./NavigationBackAndroid').default;
 import SceneNavigator from './SceneNavigator';
 import spring from './spring';
 
