1f0eb26e8d1aec0d680096518789df469d2f1a17
Graham Mendick <graham.mendick@gmail.com>
2016-12-28 19:03:41 +0000

    Rendered replies

--- NavigationReactNative/sample/twitter/Tweet.js
+++ NavigationReactNative/sample/twitter/Tweet.js
@@ -1,28 +1,74 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+import {StyleSheet, Text, Image, View, ListView, TouchableHighlight} from 'react-native';
 
-export default ({tweet: {name}, stateNavigator}) => (
-  <View style={styles.view}>
-    <TouchableHighlight underlayColor="white" onPress={() => {
-      stateNavigator.navigateBack(1);
-    }}>
-      <Text>Back</Text>
-    </TouchableHighlight>
-    <Text style={styles.tweet}>Tweet</Text>
-    <TouchableHighlight underlayColor="white" onPress={() => {
-      stateNavigator.navigate('tweet', {id: 1});
-    }}>
-      <Text>{name} {stateNavigator.stateContext.crumbs.length}</Text>
-    </TouchableHighlight>
-  </View>
+export default ({tweet: {name, replies}, stateNavigator}) => {
+  const dataSource = new ListView
+    .DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    .cloneWithRows(replies);
+  return (
+    <View style={styles.view}>
+      <TouchableHighlight underlayColor="white" onPress={() => {
+        stateNavigator.navigateBack(1);
+      }}>
+        <Text>Back</Text>
+      </TouchableHighlight>
+      <Text style={styles.title}>Tweet</Text>
+      <ListView
+        dataSource={dataSource}
+        renderRow={reply => (
+          <Reply
+            {...reply}
+            stateNavigator={stateNavigator} />
+        )} />
+    </View>
+  );
+};
+
+
+const Reply = ({id, name, logo, text, stateNavigator}) => (
+  <TouchableHighlight
+    underlayColor="white"
+    onPress={() => {
+      stateNavigator.navigate('tweet', {id});
+  }}>
+    <View style={styles.tweet}>
+      <Image
+        style={styles.logo}
+        source={{uri: logo}}
+      />
+      <View style={styles.details}>
+        <Text style={styles.name}>{name}</Text>
+        <Text>{text}</Text>
+      </View>
+    </View>
+  </TouchableHighlight>
 );
+
 
 const styles = StyleSheet.create({
   view: {
     margin: 20,
   },
-  tweet: {
+  title: {
     fontSize: 50,
     marginTop: 50,
+  },
+  tweet: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomColor: '#ccd6dd',
+    borderBottomWidth: 1,
+  },
+  details: {
+    flex: 1,
+  },
+  name: {
+    fontWeight: 'bold',
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+    marginRight: 8,
   },
 });
