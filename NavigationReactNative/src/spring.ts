79f2f3732e2d4415bccf58372d5081ad4c88df20
Graham Mendick <graham.mendick@gmail.com>
2016-12-28 13:40:32 +0000

    Defaulted spring precision to 0.1 instead of 0.01

--- NavigationReactNative/src/spring.ts
+++ NavigationReactNative/src/spring.ts
@@ -1,4 +1,6 @@
 import { OpaqueConfig, spring, SpringHelperConfig } from 'react-motion';
 
-export default (val: number, config?: SpringHelperConfig): OpaqueConfig => spring(val, config);
+export default (val: number, config?: SpringHelperConfig): OpaqueConfig => (
+    spring(val, { precision: 0.1, ...config })
+);
 
