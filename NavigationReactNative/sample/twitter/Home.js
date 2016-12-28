e034725a913114fa18c87bd08dfc2458c1f7b76c
Graham Mendick <graham.mendick@gmail.com>
2016-12-28 14:29:22 +0000

    Removed dummy tweet

--- NavigationReactNative/sample/twitter/Home.js
+++ NavigationReactNative/sample/twitter/Home.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, Image, View, ListView, TouchableHighlight } from 'react-native';
 
 export default ({ tweets, stateNavigator }) => {
-  const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-  const dataSource = ds.cloneWithRows(tweets);
+  var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  var dataSource = ds.cloneWithRows(tweets);
   return (
     <View style={styles.view}>
       <Text style={styles.home}>Home</Text>
-      <TouchableHighlight underlayColor="white" onPress={() => {
-        stateNavigator.navigate('tweet');
-      } }>
-        <Text>Tweet</Text>
-      </TouchableHighlight>
       <ListView
         dataSource={dataSource}
         renderRow={tweet => <Row {...tweet} stateNavigator={stateNavigator} />} />
@@ -21,7 +16,7 @@ export default ({ tweets, stateNavigator }) => {
 const Row = ({ id, name, logo, text, stateNavigator }) => (
   <TouchableHighlight underlayColor="white" onPress={() => {
     stateNavigator.navigate('tweet', { id: id });
-  } }>
+  }}>
     <View style={styles.tweet}>
       <Image
         style={styles.logo}
@@ -36,8 +31,6 @@ const Row = ({ id, name, logo, text, stateNavigator }) => (
 );
 
 const styles = StyleSheet.create({
-  view: {
-  },
   home: {
     paddingTop: 10,
     fontSize: 50,
