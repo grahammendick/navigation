83749275a973d3b7e5ce1ae02422ea539677f718
Graham Mendick <graham.mendick@gmail.com>
2016-12-30 16:38:53 +0000

    Created two tabs with their own state navigators

--- NavigationReactNative/sample/twitter/index.ios.js
+++ NavigationReactNative/sample/twitter/index.ios.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Dimensions, AppRegistry, StyleSheet, View } from 'react-native';
 import { SceneNavigator, spring } from 'navigation-react-native';
 import createStateNavigator from './createStateNavigator';
 import Footer from './Footer';
 
-const stateNavigator = createStateNavigator();
+const timelineStateNavigator = createStateNavigator();
+const notificationsStateNavigator = createStateNavigator();
 
 const getStyle = (translate, scale, opacity) => ({
   translate: spring(translate),
@@ -12,36 +13,52 @@ const getStyle = (translate, scale, opacity) => ({
   opacity: spring(opacity)
 });
 
-export default Twitter = () => (
-  <View style={styles.app}>
-    <SceneNavigator
-      startStateKey="home"
-      unmountedStyle={getStyle(1, 1, 1)}
-      mountedStyle={getStyle(0, 1, 1)}
-      crumbStyle={getStyle(0.05, 0.9, 0)}
-      stateNavigator={stateNavigator}>
-      {({translate, scale, opacity}, scene, active) => (
-          <Scene
-            scale={scale}
-            translate={translate}
-            opacity={opacity}
-            pointerEvents={active ? 'auto' : 'none'}
-            dimensions={Dimensions.get('window')}>
-            {scene}
-          </Scene>
-      )}
-    </SceneNavigator>
-    <Footer />
-  </View>
+export default class Twitter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {activeTab: 'timeline'};
+  }
+  render() {
+    const {activeTab} = this.state;
+    return (
+      <View style={styles.app}>
+        <TabNavigator visible={activeTab === 'timeline'} />
+        <TabNavigator visible={activeTab === 'notifications'} />
+        <Footer />
+      </View>
+    );
+  }
+}
+
+const TabNavigator = ({visible}) => (
+  <SceneNavigator
+    startStateKey="home"
+    unmountedStyle={getStyle(1, 1, 1)}
+    mountedStyle={getStyle(0, 1, 1)}
+    crumbStyle={getStyle(0.05, 0.9, 0)}
+    stateNavigator={timelineStateNavigator}>
+    {({translate, scale, opacity}, scene, active) => (
+        <Scene
+          visible={visible}
+          scale={scale}
+          translate={translate}
+          opacity={opacity}
+          pointerEvents={active ? 'auto' : 'none'}
+          dimensions={Dimensions.get('window')}>
+          {scene}
+        </Scene>
+    )}
+  </SceneNavigator>
 );
 
-const Scene = ({translate, scale, opacity, pointerEvents,
+const Scene = ({visible, translate, scale, opacity, pointerEvents,
   dimensions: {width, height}, children}) => (
   <View
     pointerEvents={pointerEvents}
     style={[
       styles.scene,
       {
+        top: visible ? 0 : -height,
         width: width,
         height: height -65,
         opacity: opacity,
