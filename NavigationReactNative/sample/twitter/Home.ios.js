84e98b6d5be19cb5a34d5116122de1fef2ede1ec
Graham Mendick <graham.mendick@gmail.com>
2017-01-02 11:07:41 +0000

    Created separate ios and android home components

---
+++ NavigationReactNative/sample/twitter/Home.ios.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, Text, ScrollView, View} from 'react-native';
+import Tweets from './Tweets';
+
+export default ({tweets, stateNavigator}) => (
+  <View>
+    <View style={styles.banner}>
+      <Text style={styles.title}>Home</Text>
+    </View>
+    <ScrollView style={styles.view}>
+      <Tweets tweets={tweets} stateNavigator={stateNavigator} />
+    </ScrollView>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  banner: {
+    paddingTop: 35,
+    paddingLeft: 50,
+    paddingBottom: 22,
+    borderBottomWidth: 2,
+    borderColor: '#ccd6dd',
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  view: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+  },
+});
