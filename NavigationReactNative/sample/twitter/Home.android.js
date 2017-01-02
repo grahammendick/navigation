a2fd00db7fee4955d8f787b47bb40c24c24a550c
Graham Mendick <graham.mendick@gmail.com>
2017-01-02 20:16:59 +0000

    Tweaked format

--- NavigationReactNative/sample/twitter/Home.android.js
+++ NavigationReactNative/sample/twitter/Home.android.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ScrollableTabView, {DefaultTabBar} from 'react-native-scrollable-tab-view';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 import TabBar from './TabBar';
 import Tweets from './Tweets';
 import Follows from './Follows';
 
 export default ({tweets, follows, stateNavigator}) => (
-  <ScrollableTabView 
-      renderTabBar={() => <TabBar stateNavigator={stateNavigator} />}>
-      <Tweets tweets={tweets} stateNavigator={stateNavigator} tabLabel="Timeline" />
-      <Follows follows={follows} stateNavigator={stateNavigator} tabLabel="Notification" />
+  <ScrollableTabView renderTabBar={() => <TabBar stateNavigator={stateNavigator} />}>
+    <Tweets tweets={tweets} stateNavigator={stateNavigator} tabLabel="Timeline" />
+    <Follows follows={follows} stateNavigator={stateNavigator} tabLabel="Notification" />
   </ScrollableTabView>
 );
