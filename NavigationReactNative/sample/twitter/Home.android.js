378a9136d6f1c55acb985adfee691286ccc9f393
Graham Mendick <graham.mendick@gmail.com>
2017-01-02 20:20:54 +0000

    Prerendered notifications

--- NavigationReactNative/sample/twitter/Home.android.js
+++ NavigationReactNative/sample/twitter/Home.android.js
@@ -5,7 +5,9 @@ import Tweets from './Tweets';
 import Follows from './Follows';
 
 export default ({tweets, follows, stateNavigator}) => (
-  <ScrollableTabView renderTabBar={() => <TabBar stateNavigator={stateNavigator} />}>
+  <ScrollableTabView
+    prerenderingSiblingsNumber={1}
+    renderTabBar={() => <TabBar stateNavigator={stateNavigator} />}>
     <Tweets tweets={tweets} stateNavigator={stateNavigator} tabLabel="Timeline" />
     <Follows follows={follows} stateNavigator={stateNavigator} tabLabel="Notification" />
   </ScrollableTabView>
