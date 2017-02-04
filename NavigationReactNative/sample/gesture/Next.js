6596affa4a11df6c2f33d14e1e4290c2fc91eabe
Graham Mendick <graham.mendick@gmail.com>
2017-02-04 17:56:20 +0000

    Changed Next to class component

--- NavigationReactNative/sample/gesture/Next.js
+++ NavigationReactNative/sample/gesture/Next.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
-export default ({stateNavigator}) => {
-  const {url} = stateNavigator.stateContext;
-  return (
-    <View style={styles.scene}>
-      <TouchableHighlight
-        underlayColor="white"
-        onPress={() => {
-          if (url === stateNavigator.stateContext.url)
-            stateNavigator.navigate('next');
-      }}>
-        <Text>Next</Text>
-      </TouchableHighlight>
-    </View>
-  )
+export default class Next extends React.Component {
+  render() {
+    const {stateNavigator} = this.props;
+    const {url} = stateNavigator.stateContext;
+    return (
+      <View style={styles.scene}>
+        <TouchableHighlight
+          underlayColor="white"
+          onPress={() => {
+            if (url === stateNavigator.stateContext.url)
+              stateNavigator.navigate('next');
+        }}>
+          <Text>Next</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
 };
 
 const styles = StyleSheet.create({
