ba1e9f4a331fa81d96ec6def955678f089048a98
Graham Mendick <graham.mendick@gmail.com>
2017-02-04 18:03:52 +0000

    Added empty pan responder

--- NavigationReactNative/sample/gesture/Next.js
+++ NavigationReactNative/sample/gesture/Next.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+import {StyleSheet, Text, View, TouchableHighlight, PanResponder} from 'react-native';
 
 export default class Next extends React.Component {
+  constructor(props) {
+    super(props);
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
+      onMoveShouldSetPanResponder: this.handleMoveShouldSetPanResponder,
+      onPanResponderGrant: this.handlePanResponderGrant,
+      onPanResponderMove: this.handlePanResponderMove,
+      onPanResponderRelease: this.handlePanResponderEnd,
+      onPanResponderTerminate: this.handlePanResponderEnd,
+    });
+  }
+  handleStartShouldSetPanResponder(e, gestureState) {
+    return true;
+  }
+  handleMoveShouldSetPanResponder(e, gestureState) {
+    return true;
+  }
+  handlePanResponderGrant(e, gestureState) {
+  }
+  handlePanResponderMove(e, gestureState) {
+  }
+  handlePanResponderEnd(e, gestureState) {
+  }
   render() {
     const {stateNavigator} = this.props;
     const {url} = stateNavigator.stateContext;
     return (
-      <View style={styles.scene}>
+      <View
+        style={styles.scene}
+        {...this.panResponder.panHandlers}
+        >
         <TouchableHighlight
           underlayColor="white"
           onPress={() => {
