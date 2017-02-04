56ca97edef6b2ef1f2939e3b213faf10c0cf38a7
Graham Mendick <graham.mendick@gmail.com>
2017-02-04 17:49:00 +0000

    Navigated to next scene
    Added background colour to prevent previous scenes showing through

--- NavigationReactNative/sample/gesture/Scene.js
+++ NavigationReactNative/sample/gesture/Scene.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
-export default ({name}) => (
-  <View style={styles.scene}>
-    <Text>{name}</Text>
-  </View>
-);
+export default ({name, stateNavigator}) => {
+  const {url} = stateNavigator.stateContext;
+  return (
+    <View style={styles.scene}>
+      <TouchableHighlight
+        underlayColor="white"
+        onPress={() => {
+          if (url === stateNavigator.stateContext.url)
+            stateNavigator.navigate('sceneTwo');
+      }}>
+        <Text>{name}</Text>
+      </TouchableHighlight>
+    </View>
+  )
+};
 
 const styles = StyleSheet.create({
   scene: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
