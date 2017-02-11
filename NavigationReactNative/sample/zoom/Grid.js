c5848dd81be4a86fac33ae780f7629a0a777ba6a
Graham Mendick <graham.mendick@gmail.com>
2017-02-11 11:51:33 +0000

    Rendered items grid

--- NavigationReactNative/sample/zoom/Grid.js
+++ NavigationReactNative/sample/zoom/Grid.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
+import {StyleSheet, ScrollView, Text, View, TouchableHighlight} from 'react-native';
 
-export default () => (
+export default ({stateNavigator}) => (
+  <ScrollView>
     <View style={styles.grid}>
-        <Text>Hello</Text>
+      {[...Array(20).keys()].map(item => (
+        <TouchableHighlight
+          key={item}
+          style={styles.item}
+          underlayColor="#fff"
+          onPress={() => {
+            stateNavigator.navigate('detail');
+          }}>
+          <Text style={styles.text}>{item}</Text>
+        </TouchableHighlight>
+      ))}
     </View>
+  </ScrollView>
 );
 
 const styles = StyleSheet.create({
   grid: {
+    marginTop: 50,
     flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'center',
-    alignItems: 'center',
   },
+  item: {
+    width: 100,
+    height: 150,
+    backgroundColor: 'red',
+    padding: 10,
+    margin: 10,
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 40,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
