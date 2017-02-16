3859296da80b24fead157ea57712d9945001161e
Graham Mendick <graham.mendick@gmail.com>
2017-02-16 19:18:22 +0000

    Tweaked format

--- NavigationReactNative/sample/medley/Scene.js
+++ NavigationReactNative/sample/medley/Scene.js
@@ -3,38 +3,38 @@ import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 import {NavigationBackAndroid} from 'navigation-react-native';
 
 const nextDirection = {
-    North: 'East',
-    East: 'South',
-    South: 'West',
-    West: 'North',
+  North: 'East',
+  East: 'South',
+  South: 'West',
+  West: 'North',
 };
 
 export default ({direction, color, stateNavigator}) => {
-    const {url, crumbs} = stateNavigator.stateContext;
-    return (
-      <View style={[
-        styles.scene,
-        {backgroundColor: color}
-      ]}>
-        <NavigationBackAndroid stateNavigator={stateNavigator} />
-        <TouchableHighlight
-          underlayColor={color}
-          onPress={() => {
-            if (url === stateNavigator.stateContext.url)
-              stateNavigator.navigate(`scene${nextDirection[direction]}`);
-        }}>
-          <Text style={styles.text}>{direction} {crumbs.length}</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          underlayColor={color}
-          onPress={() => {
-            if (url === stateNavigator.stateContext.url)
-              stateNavigator.navigateBack(1);
-        }}>
-          <Text style={styles.text}>Back</Text>
-        </TouchableHighlight>
-      </View>
-    )
+  const {url, crumbs} = stateNavigator.stateContext;
+  return (
+    <View style={[
+      styles.scene,
+      {backgroundColor: color}
+    ]}>
+      <NavigationBackAndroid stateNavigator={stateNavigator} />
+      <TouchableHighlight
+        underlayColor={color}
+        onPress={() => {
+          if (url === stateNavigator.stateContext.url)
+            stateNavigator.navigate(`scene${nextDirection[direction]}`);
+      }}>
+        <Text style={styles.text}>{direction} {crumbs.length}</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        underlayColor={color}
+        onPress={() => {
+          if (url === stateNavigator.stateContext.url)
+            stateNavigator.navigateBack(1);
+      }}>
+        <Text style={styles.text}>Back</Text>
+      </TouchableHighlight>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
