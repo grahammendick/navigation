05fe5c9f370a04c709d36169a62b70cc73d7f99e
Graham Mendick <graham.mendick@gmail.com>
2017-02-16 19:21:59 +0000

    Replaced links with on click navigation
    Needs to match native, so don’t want double click/tap firing two
    navigations

--- NavigationReactNative/sample/web/Scene.js
+++ NavigationReactNative/sample/web/Scene.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {NavigationLink, NavigationBackLink} from 'navigation-react';
 
 const nextDirection = {
   North: 'East',
@@ -12,12 +11,20 @@ export default ({direction, color, stateNavigator}) => {
   const {url, crumbs} = stateNavigator.stateContext;
   return (
     <div style={{backgroundColor: color}}>
-      <NavigationLink stateKey={`scene${nextDirection[direction]}`}>
+      <div
+        onClick={() => {
+          if (url === stateNavigator.stateContext.url)
+            stateNavigator.navigate(`scene${nextDirection[direction]}`);          
+        }}>
         {direction} {crumbs.length}
-      </NavigationLink>
-      <NavigationBackLink distance={1}>
+      </div>
+      <div
+        onClick={() => {
+          if (url === stateNavigator.stateContext.url)
+            stateNavigator.navigateBack(1);
+        }}>
         Back
-      </NavigationBackLink>
+      </div>
     </div>
   )
 };
