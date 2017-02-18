fdf98e87a727b5d9d311e675e894a930e8b77e4c
Graham Mendick <graham.mendick@gmail.com>
2017-02-18 15:52:24 +0000

    Switched around north and south translations

--- NavigationReactNative/sample/medley/createStateNavigator.js
+++ NavigationReactNative/sample/medley/createStateNavigator.js
@@ -17,9 +17,9 @@ export default () => {
   sceneSouth.renderScene = (data, moveScene) => <Scene direction="South" color="green" stateNavigator={stateNavigator}/>;
   sceneWest.renderScene = (data, moveScene) => <Scene direction="West" color="black" stateNavigator={stateNavigator}/>;
 
-  sceneNorth.unmountedStyle = () => ({translateY: spring(1, {stiffness: 30})});
+  sceneNorth.unmountedStyle = () => ({translateY: spring(-1, {stiffness: 30})});
   sceneEast.unmountedStyle = () => ({translateX: spring(1, {stiffness: 30})});
-  sceneSouth.unmountedStyle = () => ({translateY: spring(-1, {stiffness: 30})});
+  sceneSouth.unmountedStyle = () => ({translateY: spring(1, {stiffness: 30})});
   sceneWest.unmountedStyle = () => ({translateX: spring(-1, {stiffness: 30})});
 
   sceneNorth.mountedStyle = () => ({translateY: spring(0, {stiffness: 30})});
@@ -27,9 +27,9 @@ export default () => {
   sceneSouth.mountedStyle = () => ({translateY: spring(0, {stiffness: 30})});
   sceneWest.mountedStyle = () => ({translateX: spring(0, {stiffness: 30})});
 
-  sceneNorth.crumbStyle = () => ({translateY: spring(.3, {stiffness: 30})});
+  sceneNorth.crumbStyle = () => ({translateY: spring(-.3, {stiffness: 30})});
   sceneEast.crumbStyle = () => ({translateX: spring(.3, {stiffness: 30})});
-  sceneSouth.crumbStyle = () => ({translateY: spring(-.3, {stiffness: 30})});
+  sceneSouth.crumbStyle = () => ({translateY: spring(.3, {stiffness: 30})});
   sceneWest.crumbStyle = () => ({translateX: spring(-.3, {stiffness: 30})});
   
   return stateNavigator;
