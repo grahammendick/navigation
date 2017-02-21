46c698a9bbbedfcc6312804a3084a2363d505b82
Graham Mendick <graham.mendick@gmail.com>
2017-02-21 20:55:12 +0000

    Removed redundant translation

--- NavigationReactNative/sample/zoom/Zoom.js
+++ NavigationReactNative/sample/zoom/Zoom.js
@@ -2,13 +2,12 @@ import React from 'react';
 import {Dimensions, StyleSheet, View} from 'react-native';
 import {NavigationMotion, spring} from 'navigation-react-native';
 
-const getStyle = ({x, y, w, h, width}, show, translate = false) => ({
+const getStyle = ({x, y, w, h, width}, show) => ({
   x: spring(x || 0, {stiffness: 250}),
   y: spring(y || 0, {stiffness: 250}),
   w: spring(w || 0, {stiffness: 250}),
   h: spring(h || 0, {stiffness: 250}),
   show: spring(show, {stiffness: 250}),
-  translate: spring(0, {stiffness: 250}),
 });
 
 export default ({stateNavigator}) => (
@@ -17,21 +16,14 @@ export default ({stateNavigator}) => (
     unmountedStyle={(state, data, sceneData) => {
       if (sceneData && sceneData.fromX)
         data = {...data, x: sceneData.fromX, y: sceneData.fromY, w: sceneData.fromW, h: sceneData.fromH};
-      return getStyle(data, 0, true);
+      return getStyle(data, 0);
     }}
     mountedStyle={(state, data, sceneData) => getStyle({...data, ...sceneData}, 1)}
     crumbStyle={getStyle({}, 1)}
     style={{flex: 1}}
     stateNavigator={stateNavigator}>
-    {({show, x, y, w, h, translate}, scene, url, state, {color}) => (
-      <View
-        key={url}
-        style={[
-          styles.scene,
-          {transform: [
-            {translateX: Dimensions.get('window').width * translate},
-          ]},
-        ]}>
+    {({show, x, y, w, h}, scene, url, state, {color}) => (
+      <View key={url} style={styles.scene}>
         <View
           style={{
             position: 'absolute',
@@ -45,7 +37,7 @@ export default ({stateNavigator}) => (
         <View
           style={{
             flex: 1,
-            opacity: Math.ceil(translate) || Math.floor(show),
+            opacity: Math.floor(show),
           }}>
           {scene}
         </View>
