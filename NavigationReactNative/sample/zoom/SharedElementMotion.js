de9bd7a5e4625a32670e392e66f1f35e0b2a55aa
Graham Mendick <graham.mendick@gmail.com>
2017-04-10 18:52:07 +0100

    Created Shared Element Motion
    Separate shared element motion instead of combining with navigation
    motion. Simplifies things because don’t have to combine the two
    different ideas. Each Scene can add shared element motion component and
    control the animation.
    Can use Motion instead of TransitionMotion and then use onRest to
    change opacity of shared elements.

---
+++ NavigationReactNative/sample/zoom/SharedElementMotion.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+class SharedElementMotion extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.onRegisterSharedElement = this.onRegisterSharedElement.bind(this);
+        this.state = {url: this.getStateNavigator().stateContext.url, sharedElements: {}};
+    }
+    static contextTypes = {
+        stateNavigator: React.PropTypes.object,
+        onRegisterSharedElement: React.PropTypes.func
+    }
+    getStateNavigator() {
+        return this.props.stateNavigator || this.context.stateNavigator;
+    }
+    componentDidMount() {
+        this.context.onRegisterSharedElement(this.onRegisterSharedElement);
+    }
+    componentWillUnmount() {
+        this.context.offRegisterSharedElement(this.onRegisterSharedElement);
+    }
+    onRegisterSharedElement(url, name, element, measurements) {
+        this.setState(({sharedElements: prevSharedElements}) => {
+            var sharedElements = {...prevSharedElements};
+            var sharedElement = {element, measurements};
+            sharedElements[url] = {...sharedElements[url], [name]: sharedElement};
+            return {sharedElements};
+        });
+    }
+    render() {
+        return null;
+    }
+}
+
+export default SharedElementMotion;
