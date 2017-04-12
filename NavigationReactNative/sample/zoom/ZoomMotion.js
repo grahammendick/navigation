cb59eaf06b3ad12d6128be90159bfc39a77ed0e5
Graham Mendick <graham.mendick@gmail.com>
2017-04-12 09:50:37 +0100

    Changed precision so zoom ends quicker

--- NavigationReactNative/sample/zoom/ZoomMotion.js
+++ NavigationReactNative/sample/zoom/ZoomMotion.js
@@ -3,9 +3,17 @@ import {View} from 'react-native';
 import SharedElementMotion from './SharedElementMotion';
 import spring from './spring';
 
+const getStyle = ({x, y, w, h}) => ({
+  x: spring(x, {precision: 10}),
+  y: spring(y, {precision: 10}),
+  w: spring(w, {precision: 10}),
+  h: spring(h, {precision: 10}),
+});
+
+
 export default ({stateNavigator}) => (
   <SharedElementMotion
-    toStyle={({x, y, w, h}) => ({x: spring(x), y: spring(y), w: spring(w), h: spring(h)})}
+    toStyle={getStyle}
     stateNavigator={stateNavigator}>
     {({x, y, w, h}, sharedElement, name) => (
       <View
