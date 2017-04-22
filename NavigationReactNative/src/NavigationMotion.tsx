fbbc1651684c5aef54632e6356b34d2f5bbf4cfb
Graham Mendick <graham.mendick@gmail.com>
2017-04-22 09:11:38 +0100

    Moved Shared Element changes into plugin

--- NavigationReactNative/src/NavigationMotion.tsx
+++ NavigationReactNative/src/NavigationMotion.tsx
@@ -1,16 +1,35 @@
 import { StateNavigator } from 'navigation';
 import * as React from 'react';
-import { Motion, TransitionMotion } from 'react-motion';
+import { TransitionMotion } from 'react-motion';
 import { View } from 'react-native';
 
 class NavigationMotion extends React.Component<any, any> {
+    private sharedElements = {};
     constructor(props, context) {
         super(props, context);
         this.onNavigate = this.onNavigate.bind(this);
+        this.registerSharedElement = this.registerSharedElement.bind(this);
+        this.unregisterSharedElement = this.unregisterSharedElement.bind(this);
+        this.movingSharedElement = this.movingSharedElement.bind(this);
+        this.getSharedElements = this.getSharedElements.bind(this);
         this.state = {scenes: {}};
     }
     static contextTypes = {
         stateNavigator: React.PropTypes.object
+    }
+    static childContextTypes = {
+        registerSharedElement: React.PropTypes.func,
+        unregisterSharedElement: React.PropTypes.func,
+        movingSharedElement: React.PropTypes.func,
+        getSharedElements: React.PropTypes.func
+    }
+    getChildContext() {
+        return {
+            registerSharedElement: this.registerSharedElement,
+            unregisterSharedElement: this.unregisterSharedElement,
+            movingSharedElement: this.movingSharedElement,
+            getSharedElements: this.getSharedElements
+        };
     }
     private getStateNavigator(): StateNavigator {
         return this.props.stateNavigator || (this.context as any).stateNavigator;
@@ -28,27 +47,55 @@ class NavigationMotion extends React.Component<any, any> {
         this.getStateNavigator().offNavigate(this.onNavigate);
     }
     onNavigate(oldState, state, data) {
-        this.setState(prevState => {
-            var scenes = {...prevState.scenes};
+        this.setState(({scenes: prevScenes}) => {
+            var scenes = {...prevScenes};
             var {url} = this.getStateNavigator().stateContext;
-            var element = state.renderScene(this.getSceneData(data, url, prevState), this.moveScene(url));
+            var element = state.renderScene(this.getSceneData(data, url, prevScenes), this.moveScene(url));
             scenes[url] = {...scenes[url], element};
             return {scenes};
         });
     }
     moveScene(url) {
         return data => {
-            this.setState(prevState => {
-                var scenes = {...prevState.scenes};
+            this.setState(({scenes: prevScenes}) => {
+                var scenes = {...prevScenes};
                 scenes[url] = {...scenes[url], data};
                 return {scenes};
             });
         };
     }
+    registerSharedElement(url, name, ref, measurements, data) {
+        this.sharedElements[url] = this.sharedElements[url] || {};
+        this.sharedElements[url][name] = {ref, measurements, data};
+    }
+    unregisterSharedElement(url, name) {
+        if (this.sharedElements[url])
+            delete this.sharedElements[url][name];
+    }
+    movingSharedElement(url, name, style) {
+        this.sharedElements[url][name].style = style;
+    }
+    getSharedElements() {
+        var {url, oldUrl} = this.getStateNavigator().stateContext;
+        var oldSharedElements = this.sharedElements[oldUrl];
+        var mountedSharedElements = this.sharedElements[url];
+        var sharedElements = [];
+        for(var name in mountedSharedElements) {
+            if (oldSharedElements && oldSharedElements[name]) {
+                sharedElements.push({
+                    name,
+                    oldElement: oldSharedElements[name],
+                    mountedElement: mountedSharedElements[name]
+                })
+            }
+        }
+        return sharedElements;
+    }
     clearScene(url) {
         this.setState(prevState => {
             var scenes = {...prevState.scenes};
             delete scenes[url];
+            delete this.sharedElements[url];
             return {scenes};
         });
     }
@@ -65,9 +112,8 @@ class NavigationMotion extends React.Component<any, any> {
     getStyle(styleProp, {state, data, url}, strip = false) {
         var style = typeof styleProp === 'function' ? styleProp(state, this.getSceneData(data, url)) : styleProp;
         var newStyle: any = {};
-        for(var key in style) {
-            newStyle[key] = (!strip || typeof style[key] === 'number') ? style[key] : style[key].val;
-        }
+        for(var key in style)
+            newStyle[key] = !strip ? style[key] : style[key].val;
         return newStyle;
     }
     render() {
