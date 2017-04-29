82ff9e01e9175c5a5dd1f8d24f3ad25ff9deff47
Graham Mendick <graham.mendick@gmail.com>
2017-04-29 09:05:48 +0100

    Revert "Prevented double clicks and re-rendering"
    This reverts commit a4f2b09e4dcac38d1fc91bd32b124b38a4e31186.

--- NavigationReactNative/sample/twitter/Tweets.js
+++ NavigationReactNative/sample/twitter/Tweets.js
@@ -1,49 +1,40 @@
 import React from 'react';
 import {StyleSheet, Text, Image, View, ListView, TouchableHighlight} from 'react-native';
 
-export default class Tweets extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.url = props.stateNavigator.stateContext.url;
-  }
-  shouldComponentUpdate(props) {
-    return this.url === props.stateNavigator.stateContext.url;
-  }
-  render() {
-    const {tweets, onTimeline, stateNavigator} = this.props;
-    const dataSource = new ListView
-      .DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-      .cloneWithRows(tweets);
-    return (
-      <ListView
-        dataSource={dataSource}
-        renderRow={({account: {id: accountId, name, logo}, id, text}) => (
-          <TouchableHighlight
-            underlayColor="white"
-            onPress={() => {
-              if (this.url === stateNavigator.stateContext.url)
-                stateNavigator.navigate('tweet', {id});
-          }}>
-            <View style={styles.tweet}>
-              <TouchableHighlight
-                underlayColor="white"
-                onPress={() => {
-                  if ((!onTimeline || onTimeline(accountId))
-                    && this.url === stateNavigator.stateContext.url) {
-                    stateNavigator.navigate('timeline', {id: accountId});
-                  }
-              }}>
-                <Image style={styles.logo} source={logo} />
-              </TouchableHighlight>
-              <View style={styles.details}>
-                <Text style={styles.name}>{name}</Text>
-                <Text>{text}</Text>
-              </View>
+export default ({tweets, onTimeline, stateNavigator}) => {
+  const {url} = stateNavigator.stateContext;
+  const dataSource = new ListView
+    .DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    .cloneWithRows(tweets);
+  return (
+    <ListView
+      dataSource={dataSource}
+      renderRow={({account: {id: accountId, name, logo}, id, text}) => (
+        <TouchableHighlight
+          underlayColor="white"
+          onPress={() => {
+            if (url === stateNavigator.stateContext.url)
+              stateNavigator.navigate('tweet', {id});
+        }}>
+          <View style={styles.tweet}>
+            <TouchableHighlight
+              underlayColor="white"
+              onPress={() => {
+                if ((!onTimeline || onTimeline(accountId))
+                  && url === stateNavigator.stateContext.url) {
+                  stateNavigator.navigate('timeline', {id: accountId});
+                }
+            }}>
+              <Image style={styles.logo} source={logo} />
+            </TouchableHighlight>
+            <View style={styles.details}>
+              <Text style={styles.name}>{name}</Text>
+              <Text>{text}</Text>
             </View>
-          </TouchableHighlight>
-        )} />
-    );
-  }
+          </View>
+        </TouchableHighlight>
+      )} />
+  );
 };
 
 const styles = StyleSheet.create({
