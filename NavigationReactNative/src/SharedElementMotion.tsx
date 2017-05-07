98e31de03ca22d40fd7dd3d56aad3655219ef8ec
Graham Mendick <graham.mendick@gmail.com>
2017-05-07 12:02:50 +0100

    Added checks instead of default props

--- NavigationReactNative/src/SharedElementMotion.tsx
+++ NavigationReactNative/src/SharedElementMotion.tsx
@@ -3,20 +3,16 @@ import { Animate } from 'react-move';
 import { View } from 'react-native';
 
 class SharedElementMotion extends React.Component<any, any> {
-    static defaultProps = {
-        onAnimating: () => {},
-        onAnimated: () => {}
-    }
     componentWillReceiveProps({sharedElements, rest}) {
         var {sharedElements: prevSharedElements, onAnimating, onAnimated} = this.props;
-        for (var i = 0; i < sharedElements.length; i++) {
+        for (var i = 0; i < sharedElements.length && onAnimating; i++) {
             var {name, oldElement: old, mountedElement: mounted} = sharedElements[i];
             var prevSharedElement = prevSharedElements.filter(element => element.name === name)[0] || {};
             if (prevSharedElement.mountedElement !== mounted)
                 onAnimating(name, old.ref, mounted.ref, old.data, mounted.data);
         }
         if (rest) {
-            for (var i = 0; i < prevSharedElements.length; i++) {
+            for (var i = 0; i < prevSharedElements.length && onAnimated; i++) {
                 var {name, oldElement, mountedElement: mounted} = prevSharedElements[i];
                 var old = sharedElements.length ? oldElement : {};
                 onAnimated(name, old.ref, mounted.ref, old.data, mounted.data);
