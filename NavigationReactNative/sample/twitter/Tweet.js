647f6fe5fc680b39091adce4449ecd396ece1479
Graham Mendick <graham.mendick@gmail.com>
2017-05-20 18:28:17 +0100

    Re-init'ed twitter sample
    The upgrade script didn’t work. Maybe because it was older than other
    samples or because of svg linked package?

--- NavigationReactNative/sample/twitter/Tweet.js
+++ NavigationReactNative/sample/twitter/Tweet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, Image, ScrollView, View, TouchableHighlight} from 'react-native';
-import {NavigationBackAndroid} from 'navigation-react-native';
+import {NavigationBackHandler} from 'navigation-react-native';
 import Banner from './Banner';
 import Tweets from './Tweets';
 
@@ -17,7 +17,7 @@ export default class Tweet extends React.Component {
       text, time, retweets, likes, replies}, stateNavigator} = this.props;
     return (
       <View style={{flex: 1}}>
-        <NavigationBackAndroid stateNavigator={stateNavigator} />
+        <NavigationBackHandler stateNavigator={stateNavigator} />
         <Banner title="Tweet" stateNavigator={stateNavigator} />
         <ScrollView style={styles.view}>
           <View>
