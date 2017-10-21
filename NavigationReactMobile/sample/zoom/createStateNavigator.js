d0ba8c4cecf8321c14ae61e3975e37763877da2f
Graham Mendick <graham.mendick@gmail.com>
2017-10-21 17:12:20 +0100

    Configured history manager

--- NavigationReactMobile/sample/zoom/createStateNavigator.js
+++ NavigationReactMobile/sample/zoom/createStateNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StateNavigator} from 'navigation';
+import {MobileHistoryManager} from 'navigation-react-mobile';
 import Grid from './Grid';
 import Detail from './Detail';
 
@@ -7,7 +8,7 @@ export default () => {
   const stateNavigator = new StateNavigator([
     {key: 'grid'},
     {key: 'detail', trackCrumbTrail: true},
-  ]);
+  ], new MobileHistoryManager('', true));
 
   const { grid, detail } = stateNavigator.states;
   grid.renderScene = data => <Grid stateNavigator={stateNavigator}/>;
