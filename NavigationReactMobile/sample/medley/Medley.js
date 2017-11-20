c2c8dc678c3c070ed55745d79f44a1e670eedd22
Graham Mendick <graham.mendick@gmail.com>
2017-11-20 14:14:30 +0000

    Changed to client width and height
    The inner width and height changed on mobile as elements were
    translated off screen. Using screen width would’ve worked but it didn’t
    work in desktop.

--- NavigationReactMobile/sample/medley/Medley.js
+++ NavigationReactMobile/sample/medley/Medley.js
@@ -8,15 +8,16 @@ export default ({stateNavigator}) => (
     crumbStyle={state => state.crumbStyle}
     duration={1000}
     stateNavigator={stateNavigator}>
-    {({translateX = 0, translateY = 0}, scene, key, active) => (
-      <div key={key}
+    {({translateX = 0, translateY = 0}, scene, key, active) => {
+      var {clientWidth, clientHeight} = document.documentElement;
+      return <div key={key}
         className="scene"
         style={{
           overflow: active ? 'auto' : 'hidden',
-          transform: `translate(${translateX * innerWidth}px, ${translateY * innerHeight}px)`
+          transform: `translate(${translateX * clientWidth}px, ${translateY * clientHeight}px)`
         }}>
         {scene}
       </div>
-    )}
+    }}
   </NavigationMotion>
 );
