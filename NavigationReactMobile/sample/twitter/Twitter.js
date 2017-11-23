9ef2b95262c98b455c5603037f728580c7878b20
Graham Mendick <graham.mendick@gmail.com>
2017-11-23 10:10:27 +0000

    Rendered NavigationMotion for twitter

---
+++ NavigationReactMobile/sample/twitter/Twitter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationMotion} from 'navigation-react-mobile';
+
+export default ({stateNavigator}) => (
+  <NavigationMotion
+    unmountedStyle={{translate: 1, scale: 1, opacity: 1}}
+    mountedStyle={{translate: 0, scale: 1, opacity: 1}}
+    crumbStyle={{translate: 0.05, scale: 0.9, opacity: 0}}
+    stateNavigator={stateNavigator}>
+    {({translate, scale, opacity}, scene, key, active) => {
+      var {clientWidth} = document.documentElement;
+      return <div key={key}
+        className="scene"
+        style={{
+          overflow: active ? 'auto' : 'hidden',
+          transform: `translate(${translateX * clientWidth}px) scale(${scale}, ${scale})`,
+          opacity
+        }}>
+        {scene}
+      </div>
+    }}
+  </NavigationMotion>
+);
