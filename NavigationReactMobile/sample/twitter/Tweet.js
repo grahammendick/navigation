e8dda27d3493947ba0879f1af827477c863845b5
Graham Mendick <graham.mendick@gmail.com>
2017-11-24 13:42:22 +0000

    Refactored banner into separate component

--- NavigationReactMobile/sample/twitter/Tweet.js
+++ NavigationReactMobile/sample/twitter/Tweet.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import {NavigationBackLink, NavigationLink} from 'navigation-react';
+import {NavigationLink} from 'navigation-react';
+import Banner from './Banner';
 import Tweets from './Tweets';
 
 export default ({tweet: {account: {id: accountId, name, username, logo}, 
   text, time, retweets, likes, replies}, stateNavigator}) => (
   <div>
-    <div className="banner">
-      <NavigationBackLink
-        distance={1}
-        stateNavigator={stateNavigator}>
-        <svg className="back" viewBox="0 0 24 24">
-          <g>
-            <path d="M20 11H7.414l4.293-4.293a1 1 0 0 0-1.414-1.414l-6 6a1 1 0 0 0 0 1.414l6 6a.996.996 0 0 0 1.414 0 1 1 0 0 0 0-1.414L7.414 13H20a1 1 0 1 0 0-2z"></path>
-          </g>
-        </svg>
-      </NavigationBackLink>
-      <h1>Tweet</h1>
-    </div>
+    <Banner title="Tweet" stateNavigator={stateNavigator} />
     <div className="tweet">
       <div className="heading">
         <NavigationLink
