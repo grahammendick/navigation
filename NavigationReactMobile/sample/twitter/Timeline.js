b9bd83da96f2f981267a3c047a2ccefaf8e6402e
Graham Mendick <graham.mendick@gmail.com>
2017-11-24 14:13:26 +0000

    Added and styled details to timeline profile

--- NavigationReactMobile/sample/twitter/Timeline.js
+++ NavigationReactMobile/sample/twitter/Timeline.js
@@ -6,6 +6,15 @@ export default ({timeline: {id, name, username, logo, bio,
   followers, following, tweets}, stateNavigator}) => (
   <div>
     <Banner title={name} stateNavigator={stateNavigator} />
+    <div className="profile">
+      <div className="pic" />
+      <img src={logo} alt={name}/>
+      <div className="details">
+        <div className="name">{name}</div>
+        <div className="username">{username}</div>
+        <div className="bio">{bio}</div>
+        </div>
+      </div>
   </div>
 );
 
