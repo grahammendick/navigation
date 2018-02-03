b6cd9cf48f0bf6db544331abef8567c0122c934a
Graham Mendick <graham.mendick@gmail.com>
2018-02-03 17:56:15 +0000

    Tweaked format

--- NavigationReact/src/NavigationContext.ts
+++ NavigationReact/src/NavigationContext.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 var navigationContext;
 if (React.createContext)
-  navigationContext = React.createContext(null); 
+    navigationContext = React.createContext(null);
 else
-  navigationContext = { Provider: null, Consumer: ({children}) => children(null)}
+    navigationContext = { Provider: null, Consumer: ({children}) => children(null)}
 export default navigationContext;
