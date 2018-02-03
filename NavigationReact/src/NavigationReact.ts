578e15e2fa3b990b0f8025e4d148937c81328112
Graham Mendick <graham.mendick@gmail.com>
2018-02-03 16:33:55 +0000

    Export context and handler from plugin

--- NavigationReact/src/NavigationReact.ts
+++ NavigationReact/src/NavigationReact.ts
@@ -1,5 +1,7 @@
-﻿import NavigationBackLink from './NavigationBackLink';
+﻿import NavigationContext from './NavigationContext';
+import NavigationHandler from './NavigationHandler';
+import NavigationBackLink from './NavigationBackLink';
 import NavigationLink from './NavigationLink';
 import RefreshLink from './RefreshLink';
 
-export { NavigationBackLink, NavigationLink, RefreshLink };
+export { NavigationContext, NavigationHandler, NavigationBackLink, NavigationLink, RefreshLink };
