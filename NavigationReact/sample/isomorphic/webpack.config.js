1c2732c71d501b1d1ee52b866370f5b4496fca54
Graham Mendick <graham.mendick@gmail.com>
2018-02-04 15:10:09 +0000

    Moved path out of filename

--- NavigationReact/sample/isomorphic/webpack.config.js
+++ NavigationReact/sample/isomorphic/webpack.config.js
@@ -1,8 +1,10 @@
+var path = require('path');
+
 module.exports = {
     entry: "./NavigationClient.js",
     output: {
-        path: __dirname,
-        filename: "js/app.js"
+        path: path.join(__dirname, "js"),
+        filename: "app.js"
     },
     module: {
         loaders: [
