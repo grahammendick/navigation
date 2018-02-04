d32e00e334a549a5184f46fa5a4cfeeb48e1ae31
Graham Mendick <graham.mendick@gmail.com>
2018-02-04 11:36:51 +0000

    Threw error if context used with old React

--- NavigationReact/src/NavigationContext.ts
+++ NavigationReact/src/NavigationContext.ts
@@ -3,6 +3,10 @@ import * as React from 'react';
 var navigationContext;
 if (React.createContext)
     navigationContext = React.createContext(null);
-else
-    navigationContext = { Provider: null, Consumer: ({children}) => children(null)}
+else {
+    navigationContext = {
+        Provider: () => { throw Error(`React ${React.version} does not support the new context`) },
+        Consumer: ({children}) => children(null)
+    }
+}
 export default navigationContext;
