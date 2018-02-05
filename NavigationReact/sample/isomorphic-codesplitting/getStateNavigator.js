2aab2c5fb30ce13cbc896cf40825ceb75078030d
Graham Mendick <graham.mendick@gmail.com>
2018-02-05 23:12:26 +0000

    Removed isomorphic-splitting sample
    Better to keep samples as primitives than combine two random ones

--- NavigationReact/sample/isomorphic-codesplitting/getStateNavigator.js
+++
@@ -1,8 +0,0 @@
-import { StateNavigator, HTML5HistoryManager } from 'navigation';
-
-export default function() {
-    return new StateNavigator([
-        {key: 'people', route: '{pageNumber?}', defaults: {pageNumber: 1}},
-        {key: 'person', route: 'person/{id}', defaults: {id: 0}, trackCrumbTrail: true}
-    ], new HTML5HistoryManager());
-};
