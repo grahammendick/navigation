8e51910f7603ca776c2d2c492af669c3f783ef6f
Graham Mendick <graham.mendick@gmail.com>
2018-02-14 14:05:39 +0000

    Changed crumb translation
    Want crumb style to be v different from mounted style so can tell whether server render is return right styles for each scene. It currently isn't. Don't want the client render to animate - the first pass should be correct

--- NavigationReactMobile/sample/isomorphic/Isomorphic.js
+++ NavigationReactMobile/sample/isomorphic/Isomorphic.js
@@ -5,7 +5,7 @@ export default () => (
     <NavigationMotion
         unmountedStyle={{opacity: 1, translate: 100}}
         mountedStyle={{opacity: 1, translate: 0}}
-        crumbStyle={{opacity: 0, translate: 0}}>
+        crumbStyle={{opacity: 0, translate: -100}}>
         {({opacity, translate}, scene, key, active) => (
             <div key={key}
                 className="scene"
