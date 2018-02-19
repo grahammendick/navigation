fffc3b74870b1ed3f9ec837ec38206b028d1ca87
Graham Mendick <graham.mendick@gmail.com>
2018-02-19 19:02:27 +0000

    Added photo zoom prop

--- NavigationReactMobile/sample/twitter/Twitter.js
+++ NavigationReactMobile/sample/twitter/Twitter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavigationMotion} from 'navigation-react-mobile';
+import PhotoZoom from './PhotoZoom';
 
 export default () => (
   <NavigationMotion
@@ -9,7 +10,8 @@ export default () => (
     mountedStyle={{translate: 0, scale: 1, opacity: 1}}
     crumbStyle={(state, data, crumbs, nextState) => (
       nextState.key !== 'photo' ? {translate: 5, scale: 0.9, opacity: 0} : {translate: 0, scale: 1, opacity: 0}
-    )}>
+    )}
+    sharedElementMotion={props => <PhotoZoom {...props} />}>
     {({translate, scale, opacity}, scene, key, active) => (
       <div key={key}
         className="scene"
