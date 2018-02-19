ae75b1d98e46034a0354c6f8fcc29a6131228936
Graham Mendick <graham.mendick@gmail.com>
2018-02-19 16:25:07 +0000

    Styled photo scene

--- NavigationReactMobile/sample/twitter/Photo.js
+++ NavigationReactMobile/sample/twitter/Photo.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import Banner from './Banner';
+import {NavigationBackLink} from 'navigation-react';
 
 export default ({photo}) => (
-  <div>
+  <div className="photo">
+    <NavigationBackLink distance={1}>
+      <svg viewBox="0 0 24 24">
+        <g>
+          <path d="M13.414 12l5.793-5.793a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 0 0 1.414 1.414L12 13.414l5.793 5.793a.996.996 0 0 0 1.414 0 1 1 0 0 0 0-1.414L13.414 12z"></path>
+        </g>
+      </svg>
+    </NavigationBackLink>
     <img src={photo}/>
   </div>
 );
