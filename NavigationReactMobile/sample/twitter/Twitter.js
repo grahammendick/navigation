510a4942719b545155bfad20ceae497c41727562
Graham Mendick <graham.mendick@gmail.com>
2018-02-19 18:54:27 +0000

    Changed style for photo scene
    The crumb fades out and the photo fades in, instead of sliding back and in

--- NavigationReactMobile/sample/twitter/Twitter.js
+++ NavigationReactMobile/sample/twitter/Twitter.js
@@ -3,9 +3,13 @@ import {NavigationMotion} from 'navigation-react-mobile';
 
 export default () => (
   <NavigationMotion
-    unmountedStyle={{translate: 100, scale: 1, opacity: 1}}
+    unmountedStyle={(state) => (
+      state.key !== 'photo' ? {translate: 100, scale: 1, opacity: 1} : {translate: 0, scale: 1, opacity: 0}
+    )}
     mountedStyle={{translate: 0, scale: 1, opacity: 1}}
-    crumbStyle={{translate: 5, scale: 0.9, opacity: 0}}>
+    crumbStyle={(state, data, crumbs, nextState) => (
+      nextState.key !== 'photo' ? {translate: 5, scale: 0.9, opacity: 0} : {translate: 0, scale: 1, opacity: 0}
+    )}>
     {({translate, scale, opacity}, scene, key, active) => (
       <div key={key}
         className="scene"
