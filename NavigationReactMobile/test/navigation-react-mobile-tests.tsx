b74fd9b6bc9e16d00d55d6bd0ea052ba036ff6df
Graham Mendick <graham.mendick@gmail.com>
2018-02-23 11:11:25 +0000

    Added basic typings structure
    Tested declaration merging by adding renderScene function to State

---
+++ NavigationReactMobile/test/navigation-react-mobile-tests.tsx
@@ -0,0 +1,9 @@
+import { StateNavigator } from 'navigation';
+import { NavigationMotion } from 'navigation-react-mobile';
+
+const stateNavigator = new StateNavigator([
+    { key: 'people', route: 'people/{page}' },
+    { key: 'person', route: 'person/{id}', trackCrumbTrail: true }
+]);
+
+stateNavigator.states.people.renderScene = () => {};
