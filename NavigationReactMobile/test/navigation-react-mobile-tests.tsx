2018550c9cc1e3b1e58a2b29da269ed194213a13
Graham Mendick <graham.mendick@gmail.com>
2018-02-24 10:54:10 +0000

    Started mobile typings tests

--- NavigationReactMobile/test/navigation-react-mobile-tests.tsx
+++ NavigationReactMobile/test/navigation-react-mobile-tests.tsx
@@ -6,4 +6,19 @@ const stateNavigator = new StateNavigator([
     { key: 'person', route: 'person/{id}', trackCrumbTrail: true }
 ]);
 
-//stateNavigator.states.people.renderScene = () => {};
+var People = ({ page }) => null;
+var Person = ({ id }) => null;
+
+var { people, person } = stateNavigator.states;
+people.renderScene = ({ page }) => <People page={page}/>;
+person.renderScene = ({ id }) => <Person id={id}/>;
+
+var App = () => (
+    <NavigationMotion>
+        {(style, scene, key) => (
+            <div key={key}>
+                {scene}
+            </div>
+        )}
+    </NavigationMotion>
+);
