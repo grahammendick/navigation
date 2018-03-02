be6b2f11f45b213a1eac5886165d80442f2af1dd
Graham Mendick <graham.mendick@gmail.com>
2018-03-02 16:47:06 +0000

    Moved getNavigationLink inside getDerived

--- NavigationReact/src/NavigationLink.tsx
+++ NavigationReact/src/NavigationLink.tsx
@@ -10,31 +10,20 @@ class NavigationLink extends React.Component<NavigationLinkProps, NavigationLink
         super(props, context);
         var { stateNavigator } = props;
         var { crumbs } = stateNavigator.stateContext;
-        this.state = {
-            link: null,
-            active: false,
-            crumb: crumbs.length
-        }
+        this.state = { link: undefined, active: false, crumb: crumbs.length };
     }
 
     static getDerivedStateFromProps(props, { crumb }) {
-        var { acrossCrumbs, navigationData, stateKey, stateNavigator } = props;
+        var { acrossCrumbs, stateKey, navigationData, includeCurrentData, currentDataKeys, stateNavigator } = props;
         var { crumbs, state } = stateNavigator.stateContext;
         if (!acrossCrumbs && crumb !== undefined && crumb !== crumbs.length)
             return null;
-        return {
-            link: NavigationLink.getNavigationLink(props),
-            active: state && state.key === stateKey && LinkUtility.isActive(stateNavigator, navigationData)
-        };
-    }
-
-    private static getNavigationLink({ stateKey, navigationData, includeCurrentData, currentDataKeys, stateNavigator }): string {
         var navigationData = LinkUtility.getData(stateNavigator, navigationData, includeCurrentData, currentDataKeys);
         try {
-            return stateNavigator.getNavigationLink(stateKey, navigationData);
-        } catch (e) {
-            return null;
-        }
+            var link = stateNavigator.getNavigationLink(stateKey, navigationData);
+        } catch {}
+        var active = state && state.key === stateKey && LinkUtility.isActive(stateNavigator, navigationData);
+        return { link, active };
     }
 
     render() {
