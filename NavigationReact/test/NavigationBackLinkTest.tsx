90e717cb8a229ed6838b4a1944724eda0e9f2811
Graham Mendick <graham.mendick@gmail.com>
2018-03-11 17:03:13 +0000

    Tested back link renders without context

--- NavigationReact/test/NavigationBackLinkTest.tsx
+++ NavigationReact/test/NavigationBackLinkTest.tsx
@@ -35,6 +35,25 @@ describe('NavigationBackLinkTest', function () {
         })
     });
 
+    describe('Without Context Navigation Back Link', function () {
+        it('should render', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 's0', route: 'r0' },
+                { key: 's1', route: 'r1', trackCrumbTrail: true }
+            ]);
+            stateNavigator.navigate('s0');
+            stateNavigator.navigate('s1');
+            var wrapper = mount(
+                <NavigationBackLink distance={1}>
+                    link text
+                </NavigationBackLink>
+            );
+            var link = wrapper.find('a');
+            assert.equal(link.prop('href'), null);
+            assert.equal(link.prop('children'), 'link text');
+        })
+    });
+
     describe('Invalid Navigation Back Link', function () {
         it('should render', function(){
             var stateNavigator = new StateNavigator([
