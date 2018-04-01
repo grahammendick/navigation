cb7a02a9ed3b93c6a12ecc6709e1300dcd83a932
Graham Mendick <graham.mendick@gmail.com>
2018-04-01 19:58:50 +0100

    Added package lock to relay
    Had to change id to type GraphQLID and re-run updateSchema (see https://medium.com/@princeoluwasegunabisagbo/relay-compiler-error-expected-undefined-to-be-a-graphql-leaf-type-1ea1b8691115)

--- NavigationReact/sample/relay/src/__generated__/People_people.graphql.js
+++ NavigationReact/sample/relay/src/__generated__/People_people.graphql.js
@@ -7,57 +7,60 @@
 'use strict';
 
 /*::
-import type {ConcreteFragment} from 'relay-runtime';
+import type { ConcreteFragment } from 'relay-runtime';
+import type { FragmentReference } from 'relay-runtime';
+declare export opaque type People_people$ref: FragmentReference;
 export type People_people = {|
   +persons: ?$ReadOnlyArray<?{|
-    +id: ?string;
-    +name: ?string;
-    +dateOfBirth: ?string;
-  |}>;
+    +id: ?string,
+    +name: ?string,
+    +dateOfBirth: ?string,
+  |}>,
+  +$refType: People_people$ref,
 |};
 */
 
 
-const fragment /*: ConcreteFragment*/ = {
-  "argumentDefinitions": [],
+const node/*: ConcreteFragment*/ = {
   "kind": "Fragment",
-  "metadata": null,
   "name": "People_people",
+  "type": "People",
+  "metadata": null,
+  "argumentDefinitions": [],
   "selections": [
     {
       "kind": "LinkedField",
       "alias": null,
+      "name": "persons",
+      "storageKey": null,
       "args": null,
       "concreteType": "Person",
-      "name": "persons",
       "plural": true,
       "selections": [
         {
           "kind": "ScalarField",
           "alias": null,
-          "args": null,
           "name": "id",
+          "args": null,
           "storageKey": null
         },
         {
           "kind": "ScalarField",
           "alias": null,
-          "args": null,
           "name": "name",
+          "args": null,
           "storageKey": null
         },
         {
           "kind": "ScalarField",
           "alias": null,
-          "args": null,
           "name": "dateOfBirth",
+          "args": null,
           "storageKey": null
         }
-      ],
-      "storageKey": null
+      ]
     }
-  ],
-  "type": "People"
+  ]
 };
-
-module.exports = fragment;
+(node/*: any*/).hash = '248dd0f4828059bab45d651bfa9b88e1';
+module.exports = node;
