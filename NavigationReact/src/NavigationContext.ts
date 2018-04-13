a3a8d50d3f2826a49907d19b7209abd46e582306
Graham Mendick <graham.mendick@gmail.com>
2018-04-13 11:39:37 +0100

    Updated navigation context default

--- NavigationReact/src/NavigationContext.ts
+++ NavigationReact/src/NavigationContext.ts
@@ -1,4 +1,6 @@
-import { StateNavigator } from 'navigation';
+import AsyncStateNavigator from './AsyncStateNavigator';
+import { StateNavigator, StateContext } from 'navigation';
 import * as React from 'react';
 
-export default React.createContext({ oldState: null, state: null, data: {}, stateNavigator: new StateNavigator() });
+var asyncNavigator = new AsyncStateNavigator(null, new StateNavigator(), new StateContext());
+export default React.createContext({ oldState: null, state: null, data: {}, nextState: null, nextData: {}, stateNavigator: asyncNavigator });
