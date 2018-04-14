c1c610bae76138095b7b209b309dee074785e5c1
Graham Mendick <graham.mendick@gmail.com>
2018-04-14 15:01:29 +0100

    Copied more config navigator tests

--- Navigation/test/StateConfigTest.ts
+++ Navigation/test/StateConfigTest.ts
@@ -100,139 +100,326 @@ describe('StateConfigTest', function () {
     });
 
     describe('Default Types', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaultTypes: { 'string': 'string', 'number': 'number', 'boolean': 'boolean' } }
             ]);
-            assert.strictEqual(stateNavigator.states['s'].defaultTypes['string'], 'string');
-            assert.strictEqual(stateNavigator.states['s'].defaultTypes['boolean'], 'boolean');
-            assert.strictEqual(stateNavigator.states['s'].defaultTypes['number'], 'number');
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                assert.strictEqual(stateNavigator.states['s'].defaultTypes['string'], 'string');
+                assert.strictEqual(stateNavigator.states['s'].defaultTypes['boolean'], 'boolean');
+                assert.strictEqual(stateNavigator.states['s'].defaultTypes['number'], 'number');
+            });
+        }
     });
 
     describe('Attributes', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', handler: 'y' }
             ]);
-            assert.equal(stateNavigator.states['s']['handler'], 'y');
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                assert.equal(stateNavigator.states['s']['handler'], 'y');
+            });
+        }
     });
 
     describe('Route', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's0', route: 'r0' },
                 { key: 's1', route: 'r1/{string}/{number}' }
             ]);
-            assert.equal(stateNavigator.states['s0'].route, 'r0');
-            assert.equal(stateNavigator.states['s1'].route, 'r1/{string}/{number}');
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                assert.equal(stateNavigator.states['s0'].route, 'r0');
+                assert.equal(stateNavigator.states['s1'].route, 'r1/{string}/{number}');
+            });
+        }
     });
 
     describe('Default Types String', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { ' &s0': 'a', s1: 'b', s2: 'c', s3: 'd' }, defaultTypes: { s1: 'string', s2: 'boolean' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(defaults[' &s0'], 'a');
-            assert.strictEqual(defaults['s1'], 'b');
-            assert.strictEqual(defaults['s2'], 'c');
-            assert.strictEqual(defaults['s3'], 'd');
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(defaults[' &s0'], 'a');
+                assert.strictEqual(defaults['s1'], 'b');
+                assert.strictEqual(defaults['s2'], 'c');
+                assert.strictEqual(defaults['s3'], 'd');
+            });
+        }
     });
 
     describe('Default Types Bool', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { b1: true, b2: false, b3: true }, defaultTypes: { b1: 'boolean', b2: 'number' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(defaults['b1'], true);
-            assert.strictEqual(defaults['b2'], false);
-            assert.strictEqual(defaults['b3'], true);
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(defaults['b1'], true);
+                assert.strictEqual(defaults['b2'], false);
+                assert.strictEqual(defaults['b3'], true);
+            });
+        }
     });
 
     describe('Default Types Number', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { n1: 0, n2: 1, n3: 2 }, defaultTypes: { n1: 'number', n2: 'date' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(defaults['n1'], 0);
-            assert.strictEqual(defaults['n2'], 1);
-            assert.strictEqual(defaults['n3'], 2);
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(defaults['n1'], 0);
+                assert.strictEqual(defaults['n2'], 1);
+                assert.strictEqual(defaults['n3'], 2);
+            });
+        }
     });
 
     describe('Default Types Date', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { d1: new Date(2010, 3, 7), d2: new Date(2011, 7, 3), d3: new Date(2012, 8, 4) }, defaultTypes: { d1: 'date', d2: 'string' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(+defaults['d1'], +new Date(2010, 3, 7));
-            assert.strictEqual(+defaults['d2'], +new Date(2011, 7, 3));
-            assert.strictEqual(+defaults['d3'], +new Date(2012, 8, 4));
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(+defaults['d1'], +new Date(2010, 3, 7));
+                assert.strictEqual(+defaults['d2'], +new Date(2011, 7, 3));
+                assert.strictEqual(+defaults['d3'], +new Date(2012, 8, 4));
+            });
+        }
     });
 
     describe('Default Types String Array', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { s0: ['a', 'b'], s1: ['c', 'd'], s2: ['e'] }, defaultTypes: { s1: 'stringarray', s2: 'boolean' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(defaults['s0'][0], 'a');
-            assert.strictEqual(defaults['s0'][1], 'b');
-            assert.strictEqual(defaults['s1'][0], 'c');
-            assert.strictEqual(defaults['s1'][1], 'd');
-            assert.strictEqual(defaults['s2'][0], 'e');
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(defaults['s0'][0], 'a');
+                assert.strictEqual(defaults['s0'][1], 'b');
+                assert.strictEqual(defaults['s1'][0], 'c');
+                assert.strictEqual(defaults['s1'][1], 'd');
+                assert.strictEqual(defaults['s2'][0], 'e');
+            });
+        }
     });
 
     describe('Default Types Bool Array', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { b0: [true, false], b1: [false, true], b2: [true] }, defaultTypes: { b1: 'booleanarray', b2: 'number' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(defaults['b0'][0], true);
-            assert.strictEqual(defaults['b0'][1], false);
-            assert.strictEqual(defaults['b1'][0], false);
-            assert.strictEqual(defaults['b1'][1], true);
-            assert.strictEqual(defaults['b2'][0], true);
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(defaults['b0'][0], true);
+                assert.strictEqual(defaults['b0'][1], false);
+                assert.strictEqual(defaults['b1'][0], false);
+                assert.strictEqual(defaults['b1'][1], true);
+                assert.strictEqual(defaults['b2'][0], true);
+            });
+        }
     });
 
     describe('Default Types Number Array', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { n0: [0, 1], n1: [2, 3], n2: [4] }, defaultTypes: { n1: 'numberarray', n2: 'date' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(defaults['n0'][0], 0);
-            assert.strictEqual(defaults['n0'][1], 1);
-            assert.strictEqual(defaults['n1'][0], 2);
-            assert.strictEqual(defaults['n1'][1], 3);
-            assert.strictEqual(defaults['n2'][0], 4);
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(defaults['n0'][0], 0);
+                assert.strictEqual(defaults['n0'][1], 1);
+                assert.strictEqual(defaults['n1'][0], 2);
+                assert.strictEqual(defaults['n1'][1], 3);
+                assert.strictEqual(defaults['n2'][0], 4);
+            });
+        }
     });
 
     describe('Default Types Date Array', function () {
-        it('should configure State Info', function() {
-            var stateNavigator = new StateNavigator([
+        var stateNavigator: StateNavigator;
+        beforeEach(function() {
+            stateNavigator = new StateNavigator([
                 { key: 's', route: 'r', defaults: { d0: [new Date(2010, 3, 7), new Date(2011, 7, 3)], d1: [new Date(2011, 7, 3), new Date(2010, 3, 7)], d2: [new Date(2012, 8, 4)] }, defaultTypes: { d1: 'datearray', d2: 'string' } }
             ]);
-            var defaults = stateNavigator.states['s'].defaults;
-            assert.strictEqual(+defaults['d0'][0], +new Date(2010, 3, 7));
-            assert.strictEqual(+defaults['d0'][1], +new Date(2011, 7, 3));
-            assert.strictEqual(+defaults['d1'][0], +new Date(2011, 7, 3));
-            assert.strictEqual(+defaults['d1'][1], +new Date(2010, 3, 7));
-            assert.strictEqual(+defaults['d2'][0], +new Date(2012, 8, 4));
-        })
+        });
+
+        describe('Array', function() {
+            test();
+        });
+
+        describe('State Navigator', function() {
+            beforeEach(function() {
+                stateNavigator = new StateNavigator(stateNavigator);
+            });
+            test();
+        });
+
+        function test() {
+            it('should configure State Info', function() {
+                var defaults = stateNavigator.states['s'].defaults;
+                assert.strictEqual(+defaults['d0'][0], +new Date(2010, 3, 7));
+                assert.strictEqual(+defaults['d0'][1], +new Date(2011, 7, 3));
+                assert.strictEqual(+defaults['d1'][0], +new Date(2011, 7, 3));
+                assert.strictEqual(+defaults['d1'][1], +new Date(2010, 3, 7));
+                assert.strictEqual(+defaults['d2'][0], +new Date(2012, 8, 4));
+            });
+        }
     });
 
     describe('Invalid Default', function () {
