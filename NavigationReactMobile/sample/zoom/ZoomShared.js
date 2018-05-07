4ede74f533e9d719091a72cdd5950a2e2d65d019
Graham Mendick <graham.mendick@gmail.com>
2018-05-07 16:49:39 +0100

    Shared position styles

--- NavigationReactMobile/sample/zoom/ZoomShared.js
+++ NavigationReactMobile/sample/zoom/ZoomShared.js
@@ -6,40 +6,33 @@ export default (props) => (
     {...props}
     onAnimating={(name, ref) => {ref.style.opacity = 0}}
     onAnimated={(name, ref) => {ref.style.opacity = 1}}>
-    {({fontColor, ...style}, name, {left, top, width, height, fontSize, color}) => (
-      !name.startsWith('text') ? <div key={name}
+    {({fontColor, ...style}, name, {left, top, width, height, fontSize, color}) => {
+      var position = {
+        position: 'absolute',
+        left,
+        top,
+        width,
+        height,
+        transformOrigin: 'top left',
+        transform: `
+          translate(${style.left - left}px, ${style.top - top}px)
+          scale(${style.width / width}, ${style.height / height})
+        `,      
+      };
+      return !name.startsWith('text') ? <div key={name}
         style={{
-          position: 'absolute',
-          left,
-          top,
-          width,
-          height,
+          ...position,
           backgroundColor: color,
-          transformOrigin: 'top left',
-          transform: `
-            translate(${style.left - left}px, ${style.top - top}px)
-            scale(${style.width / width}, ${style.height / height})
-          `,
         }}>
       </div> : <div key={name}     
         style={{
-          position: 'absolute',
-          left,
-          top,
-          width,
-          height,
+          ...position,
           fontSize: `${fontSize}%`,
-          textAlign: 'center',
           color: `rgb(${Array(3).fill(Math.round(fontColor)).join(',')})`,
           zIndex: 1,
-          transformOrigin: 'top left',
-          transform: `
-            translate(${style.left - left}px, ${style.top - top}px)
-            scale(${style.width / width}, ${style.height / height})
-          `,
         }}>
           {color}
         </div>
-    )}
+    }}
   </SharedElementMotion>
 );
