0d6884593cdcb99fb0f3afb01f4cd141f99b3aa9
Graham Mendick <graham.mendick@gmail.com>
2018-05-08 10:20:49 +0100

    Typed scene props

--- NavigationReactMobile/src/Scene.tsx
+++ NavigationReactMobile/src/Scene.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { SceneProps } from './Props';
 
-class Scene extends React.Component<any, any> {
-    shouldComponentUpdate(props) {
+class Scene extends React.Component<SceneProps, any> {
+    shouldComponentUpdate(props: SceneProps) {
         return props.crumbs === props.stateNavigator.stateContext.crumbs.length;
     }
     render() {
