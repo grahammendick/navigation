d9cb06fb537eb43d3efa1958294ee3ba007672f3
Graham Mendick <graham.mendick@gmail.com>
2018-05-08 10:16:30 +0100

    Comapred props directly
    The instance variable didn't add anything because the crumbs prop now holds the index

--- NavigationReactMobile/src/Scene.tsx
+++ NavigationReactMobile/src/Scene.tsx
@@ -1,13 +1,8 @@
 import * as React from 'react';
 
 class Scene extends React.Component<any, any> {
-    private index: number;
-    constructor(props) {
-        super(props);
-        this.index = props.crumbs;
-    }
     shouldComponentUpdate(props) {
-        return this.index === props.stateNavigator.stateContext.crumbs.length;
+        return props.crumbs === props.stateNavigator.stateContext.crumbs.length;
     }
     render() {
         return this.props.children;
