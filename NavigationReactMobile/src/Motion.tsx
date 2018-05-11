f396bab1c9ad5d149ee4af53b8c0288f5c00f835
Graham Mendick <graham.mendick@gmail.com>
2018-05-11 13:56:31 +0100

    Derived next items from props
    Need the first render to be sync when props change so that the new Scene is given the right NavigaitonContext.
    If it's not synchronous then can do these two navigations in a row:
    navigateLink(B?crumb=A)
    navigateLink(C?crumb=A&crumb=B)
    The first navigateLink sets B to render but, before it can, the second navigateLink comes in. When it comes to render then it will render B and C together for the first time and so B will get the wong NavigationContext.

--- NavigationReactMobile/src/Motion.tsx
+++ NavigationReactMobile/src/Motion.tsx
@@ -6,19 +6,15 @@ class Motion<T> extends React.Component<MotionProps<T>, any> {
     constructor(props) {
         super(props);
         this.move = this.move.bind(this);
-        var {data, enter, getKey} = this.props;
-        var items = data.map(item => {
-            var newItem: any = {key: getKey(item), data: item, progress: 1, tick: 0};
-            newItem.start = newItem.end = newItem.style = enter(item);
-            return newItem;
-        });
-        this.state = {items, restart: false};
+        this.state = {items: [], restart: false};
     }
     static defaultProps = {
         progress: 0,
     }
-    static getDerivedStateFromProps() {
-        return {restart: true};
+    static getDerivedStateFromProps(props, {items: prevItems}) {
+        var tick = typeof performance !== 'undefined' ? performance.now() : 0;
+        var {items} = Motion.getItems(tick, prevItems, props);
+        return {items, restart: true};
     }
     componentDidMount() {
         this.moveId = requestAnimationFrame(this.move)
@@ -32,51 +28,55 @@ class Motion<T> extends React.Component<MotionProps<T>, any> {
     }
     move(tick) {
         this.setState(({items: prevItems}) => {
-            var {data, enter, leave, update, progress, getKey, duration, onRest} = this.props;
-            var dataByKey = data.reduce((acc, item, index) => ({...acc, [getKey(item)]: {...(item as any), index}}), {});
-            var itemsByKey = prevItems.reduce((acc, item) => ({...acc, [item.key]: item}), {});
-            var items = prevItems
-                .map((item, index) => {
-                    var matchedItem = dataByKey[item.key];
-                    var nextItem: any = {key: item.key, data: matchedItem || item.data, tick};
-                    nextItem.end = !matchedItem ? (leave || update)(item.data) : update(matchedItem);
-                    nextItem.index = !matchedItem ? data.length + index : matchedItem.index;
-                    var unchanged = this.areEqual(item.end, nextItem.end);
-                    if (unchanged) {
-                        nextItem.start = item.start;
-                        nextItem.rest = item.progress === 1;
-                        var progressDelta = (nextItem.tick - item.tick) / duration;
-                        nextItem.progress = Math.min(item.progress + progressDelta, 1);
-                    } else {
-                        nextItem.rest = false;
-                        var reverse = !unchanged && this.areEqual(item.start, nextItem.end);
-                        nextItem.start = reverse ? item.end : (!progress ? item.style : item.start);
-                        nextItem.progress = reverse ? 1 - item.progress : progress;
-                    }
-                    nextItem.style = this.interpolateStyle(nextItem);
-                    if (onRest && nextItem.rest && !item.rest)
-                        onRest(item.data);
-                    return nextItem;
-                })
-                .filter(item => dataByKey[item.key] || (!item.rest && leave))
-                .concat(data
-                    .filter(item => !itemsByKey[getKey(item)])
-                    .map(item => {
-                        var index = dataByKey[getKey(item)].index;
-                        var newItem: any = {key: getKey(item), data: item, progress, tick, rest: false, index};
-                        newItem.start = newItem.style = enter(item);
-                        newItem.end = update(item);
-                        return newItem;
-                    })
-                )
-                .sort((a, b) => a.index - b.index);
+            var {items, restart} = Motion.getItems(tick, prevItems, this.props);
             this.moveId = null;
             if (items.filter(({rest}) => !rest).length !== 0)
                 this.moveId = requestAnimationFrame(this.move);
-            return {items, restart: false};
+            return {items, restart};
         })
     }
-    areEqual(from = {}, to = {}) {
+    static getItems(tick, prevItems, props) {
+        var {data, enter, leave, update, progress, getKey, duration, onRest} = props;
+        var dataByKey = data.reduce((acc, item, index) => ({...acc, [getKey(item)]: {...(item as any), index}}), {});
+        var itemsByKey = prevItems.reduce((acc, item) => ({...acc, [item.key]: item}), {});
+        var items = prevItems
+            .map((item, index) => {
+                var matchedItem = dataByKey[item.key];
+                var nextItem: any = {key: item.key, data: matchedItem || item.data, tick};
+                nextItem.end = !matchedItem ? (leave || update)(item.data) : update(matchedItem);
+                nextItem.index = !matchedItem ? data.length + index : matchedItem.index;
+                var unchanged = this.areEqual(item.end, nextItem.end);
+                if (unchanged) {
+                    nextItem.start = item.start;
+                    nextItem.rest = item.progress === 1;
+                    var progressDelta = (nextItem.tick - item.tick) / duration;
+                    nextItem.progress = Math.min(item.progress + progressDelta, 1);
+                } else {
+                    nextItem.rest = false;
+                    var reverse = !unchanged && this.areEqual(item.start, nextItem.end);
+                    nextItem.start = reverse ? item.end : (!progress ? item.style : item.start);
+                    nextItem.progress = reverse ? 1 - item.progress : progress;
+                }
+                nextItem.style = this.interpolateStyle(nextItem);
+                if (onRest && nextItem.rest && !item.rest)
+                    onRest(item.data);
+                return nextItem;
+            })
+            .filter(item => dataByKey[item.key] || (!item.rest && leave))
+            .concat(data
+                .filter(item => !itemsByKey[getKey(item)])
+                .map(item => {
+                    var index = dataByKey[getKey(item)].index;
+                    var newItem: any = {key: getKey(item), data: item, progress, tick, rest: false, index};
+                    newItem.start = newItem.style = enter(item);
+                    newItem.end = update(item);
+                    return newItem;
+                })
+            )
+            .sort((a, b) => a.index - b.index);
+        return {items, restart: false};
+    }
+    static areEqual(from = {}, to = {}) {
         if (Object.keys(from).length !== Object.keys(to).length)
             return false;
         for(var key in from) {
@@ -85,7 +85,7 @@ class Motion<T> extends React.Component<MotionProps<T>, any> {
         }
         return true;
     }
-    interpolateStyle({start, end, progress}) {
+    static interpolateStyle({start, end, progress}) {
         var style = {};
         for(var key in end)
             style[key] = start[key] + (progress * (end[key] - start[key]));
