c2ff764f9b8db952a4d8d37810afa4e96104b0dd
Graham Mendick <graham.mendick@gmail.com>
2018-05-26 14:44:46 +0100

    Tested inactive type style

--- NavigationReact/test/RefreshLinkTest.tsx
+++ NavigationReact/test/RefreshLinkTest.tsx
@@ -1128,6 +1128,31 @@ describe('RefreshLinkTest', function () {
         })
     });
 
+    describe('Inactive Type Style Refresh Link', function () {
+        it('should render', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 's', route: 'r', defaultTypes: {x: 'number'} }
+            ]);
+            stateNavigator.navigate('s', {x: '1', y: 'b'});
+            var container = document.createElement('div');
+            ReactDOM.render(
+                <NavigationHandler stateNavigator={stateNavigator}>
+                    <RefreshLink
+                        navigationData={{x: 1}}
+                        activeStyle={{color: 'green', fontWeight: 'bold'}}>
+                        link text
+                    </RefreshLink>
+                </NavigationHandler>,
+                container
+            );
+            var link = container.querySelector<HTMLAnchorElement>('a');
+            assert.equal(link.hash, '#/r?x=1');
+            assert.equal(link.style.color, '');
+            assert.equal(link.style.fontWeight, '');
+            assert.equal(link.innerHTML, 'link text');
+        })
+    });
+
     describe('Inactive Type Css Class Refresh Link', function () {
         it('should render', function(){
             var stateNavigator = new StateNavigator([
