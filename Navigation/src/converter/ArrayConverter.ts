5c25b6a508cde894e2bf611cd8da10cc9352854f
Graham Mendick <graham.mendick@gmail.com>
2018-06-01 11:30:37 +0100

    Changed error message for consistency
    Matches the 'empty string' in convertTo error message

--- Navigation/src/converter/ArrayConverter.ts
+++ Navigation/src/converter/ArrayConverter.ts
@@ -18,20 +18,20 @@ class ArrayConverter extends TypeConverter {
                     if (vals[i].length !== 0)
                         arr.push(this.converter.convertFrom(vals[i].replace(/0-/g, '-')));
                     else
-                        throw Error('\'\' is not a valid array item');
+                        throw Error('Empty string is not a valid array item');
                 }
             } else {
                 if (val.length !== 0)
                     arr.push(this.converter.convertFrom(val));
                 else
-                    throw Error('\'\' is not a valid array item');
+                    throw Error('Empty string is not a valid array item');
             }
         } else {
             for(var i = 0; i < val.length; i++) {
                 if (val[i].length !== 0)
                     arr.push(this.converter.convertFrom(val[i]));
                 else
-                    throw Error('\'\' is not a valid array item');
+                    throw Error('Empty string is not a valid array item');
             }
         }
         return arr;
