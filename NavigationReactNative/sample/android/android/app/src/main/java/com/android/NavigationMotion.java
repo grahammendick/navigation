741b790acece28e299a9a64b951f9fa530766d5a
Graham Mendick <graham.mendick@gmail.com>
2018-06-22 15:55:24 +0100

    Created Scene Activity on navigate

--- NavigationReactNative/sample/android/android/app/src/main/java/com/android/NavigationMotion.java
+++ NavigationReactNative/sample/android/android/app/src/main/java/com/android/NavigationMotion.java
@@ -1,5 +1,8 @@
 package com.android;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -17,5 +20,9 @@ public class NavigationMotion extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void render(int crumb) {
+        Activity currentActivity = getCurrentActivity();
+        Intent intent = new Intent(currentActivity, Scene.class);
+        intent.putExtra("crumb", crumb);
+        currentActivity.startActivity(intent);
     }
 }
---
+++ NavigationReactNative/sample/android/android/app/src/main/java/com/android/Scene.java
@@ -0,0 +1,12 @@
+package com.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Scene extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
