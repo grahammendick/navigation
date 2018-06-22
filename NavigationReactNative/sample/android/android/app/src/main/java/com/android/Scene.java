d575d82a6483ed8f6f858c10c99704bdcbfdfae9
Graham Mendick <graham.mendick@gmail.com>
2018-06-22 16:48:21 +0100

    Rendered next scene
    Shared the react instance manager across activities. Changed MainActivity to inherit from Scene

--- NavigationReactNative/sample/android/android/app/src/main/java/com/android/Scene.java
+++ NavigationReactNative/sample/android/android/app/src/main/java/com/android/Scene.java
@@ -1,32 +1,70 @@
 package com.android;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.os.Bundle;
 
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 
-public class Scene extends Activity {
+public class Scene extends Activity implements DefaultHardwareBackBtnHandler {
+    public static ReactInstanceManager reactInstanceManager;
+    private ReactRootView reactRootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ReactRootView reactRootView = new ReactRootView(this);
-        ReactInstanceManager reactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModulePath("index")
-                .addPackage(new MainReactPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
+        reactRootView = new ReactRootView(this);
+        if (reactInstanceManager == null) {
+            reactInstanceManager = ReactInstanceManager.builder()
+                    .setApplication(getApplication())
+                    .setBundleAssetName("index.android.bundle")
+                    .setJSMainModulePath("index")
+                    .addPackage(new MainReactPackage())
+                    .addPackage(new NavigationPackage())
+                    .setUseDeveloperSupport(BuildConfig.DEBUG)
+                    .setInitialLifecycleState(LifecycleState.RESUMED)
+                    .build();
+        }
         // The string here (e.g. "MyReactNativeApp") has to match
         // the string in AppRegistry.registerComponent() in index.js
         Bundle props = new Bundle();
-        props.putInt("crumb", 1);
+        props.putInt("crumb", getIntent().getIntExtra("crumb", 0));
         reactRootView.startReactApplication(reactInstanceManager, "android", props);
         setContentView(reactRootView);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onHostResume(this, this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (reactRootView != null) {
+            reactRootView.unmountReactApplication();
+        }
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
     }
 }
