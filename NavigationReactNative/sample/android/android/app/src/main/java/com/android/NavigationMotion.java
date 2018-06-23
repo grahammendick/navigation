173b7cbf57b42bb269d4c0c81e9f94bcc3d341d0
Graham Mendick <graham.mendick@gmail.com>
2018-06-23 18:12:08 +0100

    Navigated back multiple crumbs
    The only way could find is NavUtils.navigateUpTo and pass the target Intent. Means have to keep track of the intents as they're added (and removed).
    Also, navigateUpTo closed the app when going back to first Intent?! Don't know why. Changed the launchMode of MainActivity to singleTask and it fixed it

--- NavigationReactNative/sample/android/android/app/src/main/java/com/android/NavigationMotion.java
+++ NavigationReactNative/sample/android/android/app/src/main/java/com/android/NavigationMotion.java
@@ -2,12 +2,16 @@ package com.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import java.util.HashMap;
+
 public class NavigationMotion extends ReactContextBaseJavaModule {
+    private HashMap<Integer, Intent> mIntents = new HashMap<>();
 
     public NavigationMotion(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -21,14 +25,21 @@ public class NavigationMotion extends ReactContextBaseJavaModule {
     @ReactMethod
     public void render(int crumb) {
         Activity currentActivity = getCurrentActivity();
+        if (mIntents.size() == 0) {
+            mIntents.put(0, currentActivity.getIntent());
+        }
         int currentCrumb = currentActivity.getIntent().getIntExtra("crumb", 0);
         if (crumb < currentCrumb) {
-            currentActivity.finish();
+            NavUtils.navigateUpTo(currentActivity, mIntents.get(crumb));
+            for(int i = crumb + 1; i <= currentCrumb; i++) {
+                mIntents.remove(i);
+            }
         }
         if (crumb > currentCrumb) {
             Intent intent = new Intent(getReactApplicationContext(), Scene.class);
             intent.putExtra("crumb", crumb);
             currentActivity.startActivity(intent);
+            mIntents.put(crumb, intent);
         }
     }
 }
