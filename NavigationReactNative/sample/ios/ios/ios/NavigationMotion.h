c892a8afbd1717d84d36886517037d2872727de6
Graham Mendick <graham.mendick@gmail.com>
2018-06-27 10:49:02 +0100

    Added NavigationModule for native nav

---
+++ NavigationReactNative/sample/ios/ios/ios/NavigationMotion.h
@@ -0,0 +1,12 @@
+//
+//  NavigationMotion.h
+//  ios
+//
+//  Created by Graham Mendick on 27/06/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface NavigationMotion : NSObject <RCTBridgeModule>
+@end
---
+++ NavigationReactNative/sample/ios/ios/ios/NavigationMotion.m
@@ -0,0 +1,18 @@
+//
+//  NavigationMotion.m
+//  ios
+//
+//  Created by Graham Mendick on 27/06/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+#import "NavigationMotion.h"
+
+@implementation NavigationMotion
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(render:(NSInteger)crumb appKey:(NSString *)appKey)
+{
+}
+
+@end
