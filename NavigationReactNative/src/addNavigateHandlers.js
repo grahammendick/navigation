d9dc189fdf6eee19741e3fa9ad177b0c5b34d717
Graham Mendick <graham.mendick@gmail.com>
2018-07-05 05:37:02 +0100

    The Navigate event fires on the from crumb
    It used to fire on the to crumb but changed SceneController from didShow (to) to didMove (from)

--- NavigationReactNative/src/addNavigateHandlers.js
+++ NavigationReactNative/src/addNavigateHandlers.js
@@ -15,7 +15,7 @@ var addNavigateHandlers = stateNavigators => {
     var emitter = new NativeEventEmitter(NavigationModule)
     emitter.addListener('Navigate', ({crumb, tab}) => {
         var stateNavigator = stateNavigators[tab];
-        var distance = stateNavigator.stateContext.crumbs.length - crumb;
+        var distance = stateNavigator.stateContext.crumbs.length - crumb + 1;
         if (distance > 0) {
             var url = stateNavigator.getNavigationBackLink(distance);
             stateNavigator.navigateLink(url, undefined, true);
