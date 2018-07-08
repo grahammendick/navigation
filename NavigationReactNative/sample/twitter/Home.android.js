dfa9e80f6b66a89d37644d4c70e9660f11dc1db6
Graham Mendick <graham.mendick@gmail.com>
2018-07-08 16:38:39 +0100

    Rendered view pager for tweets and notifications

--- NavigationReactNative/sample/twitter/Home.android.js
+++ NavigationReactNative/sample/twitter/Home.android.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {StyleSheet, View, ViewPagerAndroid} from 'react-native';
 import Tweets from './Tweets';
+import Notifications from './Notifications';
 
-export default ({tweets}) => (
-  <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.view}>
-    <Tweets tweets={tweets} />
-  </ScrollView>
+export default ({tweets, follows}) => (
+  <ViewPagerAndroid style={{flex: 1}} initialPage={0}>
+    <View style={styles.view} key={1}>
+      <Tweets tweets={tweets} />
+    </View>
+    <View style={styles.view} key={1}>
+      <Notifications follows={follows} />
+    </View>
+  </ViewPagerAndroid>
 );
 
 const styles = StyleSheet.create({
