5a5ace24b2f790bfce966f8bd4beba977fb8804c
Graham Mendick <graham.mendick@gmail.com>
2018-07-08 16:49:05 +0100

    Added toolbar

--- NavigationReactNative/sample/twitter/Home.android.js
+++ NavigationReactNative/sample/twitter/Home.android.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {StyleSheet, View, ViewPagerAndroid} from 'react-native';
+import {StyleSheet, ToolbarAndroid, View, ViewPagerAndroid} from 'react-native';
 import Tweets from './Tweets';
 import Notifications from './Notifications';
 
 export default ({tweets, follows}) => (
-  <ViewPagerAndroid style={{flex: 1}} initialPage={0}>
-    <View style={styles.view} key={1}>
-      <Tweets tweets={tweets} />
-    </View>
-    <View style={styles.view} key={1}>
-      <Notifications follows={follows} />
-    </View>
-  </ViewPagerAndroid>
+  <View style={{flex: 1}}>
+    <ToolbarAndroid title="Home" style={styles.toolbar} />
+    <ViewPagerAndroid style={{flex: 1}} initialPage={0}>
+      <View style={styles.view} key={1}>
+        <Tweets tweets={tweets} />
+      </View>
+      <View style={styles.view} key={1}>
+        <Notifications follows={follows} />
+      </View>
+    </ViewPagerAndroid>
+  </View>
 );
 
 const styles = StyleSheet.create({
-  view: {
-    paddingLeft: 20,
-    paddingRight: 20,
+  toolbar: {
+    height: 50,
   },
 });
