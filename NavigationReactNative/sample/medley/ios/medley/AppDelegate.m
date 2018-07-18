f986ea85ae07b1f791aa8c90389c42d62cc19acd
Graham Mendick <graham.mendick@gmail.com>
2018-07-18 09:53:44 +0100

    Copied in setup sample from native docs

--- NavigationReactNative/sample/medley/ios/medley/AppDelegate.m
+++ NavigationReactNative/sample/medley/ios/medley/AppDelegate.m
@@ -4,25 +4,27 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-#import "NVSceneController.h"
-#import "AppDelegate.h"
 
+#import "AppDelegate.h"
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "NVSceneController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
-
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   
-  self.bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation moduleProvider:nil launchOptions:launchOptions];
-  
+  self.bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation moduleProvider:nil launchOptions:launchOptions];  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [[NVSceneController alloc] init: 0 tab: 0 title: @"North" appKey: @"medley"];
-  UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
+
+  NSString *title = @"North";
+  NSString *appKey = @"medley";
+
+  UIViewController *sceneController = [[NVSceneController alloc] init:0 tab:0 title:title appKey:appKey];
+  UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:sceneController];
+
   self.window.rootViewController = navigationController;
   [self.window makeKeyAndVisible];
   return YES;
