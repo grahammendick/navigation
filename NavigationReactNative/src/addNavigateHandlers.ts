bc1df3f8574254ff8e12baf469da12dd3e7d0c4e
Graham Mendick <graham.mendick@gmail.com>
2018-07-20 11:19:58 +0100

    Added oldState defensive check
    Allows addNavigateHandlers to be called before initial navigate

--- NavigationReactNative/src/addNavigateHandlers.ts
+++ NavigationReactNative/src/addNavigateHandlers.ts
@@ -6,24 +6,26 @@ var addNavigateHandlers = (stateNavigator: StateNavigator | StateNavigator[]) =>
     var stateNavigators = isStateNavigator(stateNavigator) ? [stateNavigator] : stateNavigator;
     stateNavigators.forEach((stateNavigator, tab) => {
         stateNavigator.onNavigate(() => {
-            var {crumbs, history, oldState, oldData, oldUrl} = stateNavigator.stateContext;
+            var {crumbs, history, oldState, oldUrl} = stateNavigator.stateContext;
+            if (!oldState || history)
+                return; 
             var {crumbs: oldCrumbs} = stateNavigator.parseLink(oldUrl);
-            if (!history && crumbs.length !== oldCrumbs.length) {
-                var {state, data, title, oldData, nextCrumb} = stateNavigator.stateContext;
-                var titles = crumbs.map(({title}) => title).concat(title);
-                var appKey = AppRegistry.getAppKeys()[0];
-                if (oldCrumbs.length < crumbs.length) {
-                    var {state: nextState, data: nextData} = crumbs.concat(nextCrumb)[oldCrumbs.length + 1];
-                    var enterAnim = state.getUnmountStyle && state.getUnmountStyle(true, data, crumbs);
-                    var exitAnim = oldState.getCrumbStyle && oldState.getCrumbStyle(false, oldData, oldCrumbs, nextState, nextData);
-                } else {
-                    var nextCrumb = new Crumb(oldData, oldState, null, null, false);
-                    var {state: nextState, data: nextData} = oldCrumbs.concat(nextCrumb)[crumbs.length + 1];
-                    var enterAnim = state.getCrumbStyle && state.getCrumbStyle(true, data, crumbs, nextState, nextData);
-                    var exitAnim = oldState.getUnmountStyle && oldState.getUnmountStyle(false, oldData, oldCrumbs);
-                }
-                NavigationModule.render(crumbs.length, tab, titles, appKey, enterAnim, exitAnim);
+            if (crumbs.length === oldCrumbs.length)
+                return;
+            var {state, data, title, oldData, nextCrumb} = stateNavigator.stateContext;
+            var titles = crumbs.map(({title}) => title).concat(title);
+            var appKey = AppRegistry.getAppKeys()[0];
+            if (oldCrumbs.length < crumbs.length) {
+                var {state: nextState, data: nextData} = crumbs.concat(nextCrumb)[oldCrumbs.length + 1];
+                var enterAnim = state.getUnmountStyle && state.getUnmountStyle(true, data, crumbs);
+                var exitAnim = oldState.getCrumbStyle && oldState.getCrumbStyle(false, oldData, oldCrumbs, nextState, nextData);
+            } else {
+                var nextCrumb = new Crumb(oldData, oldState, null, null, false);
+                var {state: nextState, data: nextData} = oldCrumbs.concat(nextCrumb)[crumbs.length + 1];
+                var enterAnim = state.getCrumbStyle && state.getCrumbStyle(true, data, crumbs, nextState, nextData);
+                var exitAnim = oldState.getUnmountStyle && oldState.getUnmountStyle(false, oldData, oldCrumbs);
             }
+            NavigationModule.render(crumbs.length, tab, titles, appKey, enterAnim, exitAnim);
         });
     });
     new NativeEventEmitter(NavigationModule).addListener('Navigate', ({crumb, tab}) => {
