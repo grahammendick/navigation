aca2d3846e2869b60f47a643f1855e9a6004c444
Graham Mendick <graham.mendick@gmail.com>
2018-08-12 15:23:23 +0100

    Tweaked format

--- NavigationReactNative/sample/zoom/android/app/src/main/java/com/zoom/MainActivity.java
+++ NavigationReactNative/sample/zoom/android/app/src/main/java/com/zoom/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends ReactActivity {
             deviceEventManagerModule.emitNewIntentReceived(uri);
         }
     }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
