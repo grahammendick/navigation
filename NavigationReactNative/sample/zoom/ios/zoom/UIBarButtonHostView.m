ed2e14f11ee1093d791d6c080106a1ca506c072a
Graham Mendick <graham.mendick@gmail.com>
2018-08-15 21:04:18 +0100

    Rendered right UIBarButtonItem!

--- NavigationReactNative/sample/zoom/ios/zoom/UIBarButtonHostView.m
+++ NavigationReactNative/sample/zoom/ios/zoom/UIBarButtonHostView.m
@@ -1,4 +1,6 @@
 #import "UIBarButtonHostView.h"
+#import <UIKit/UIKit.h>
+#import <React/UIView+React.h>
 
 @implementation UIBarButtonHostView
 
@@ -12,6 +14,8 @@
 - (void)didMoveToWindow
 {
   [super didMoveToWindow];
+  UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"Test" style:UIBarButtonItemStylePlain target:nil action:nil];
+  [self.reactViewController.navigationItem setRightBarButtonItem:item animated:YES];
 }
 
 @end
