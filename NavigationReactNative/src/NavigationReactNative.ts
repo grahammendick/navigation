eda77adc396c2a04c048233c5abcb04c7d5157d6
Graham Mendick <graham.mendick@gmail.com>
2018-08-20 20:34:23 +0100

    Implemented left bar javascript side

--- NavigationReactNative/src/NavigationReactNative.ts
+++ NavigationReactNative/src/NavigationReactNative.ts
@@ -1,6 +1,7 @@
 import Scene from './Scene';
 import addNavigateHandlers from './addNavigateHandlers';
+import LeftBarIOS from './LeftBarIOS';
 import RightBarIOS from './RightBarIOS';
 import BarButtonIOS from './BarButtonIOS';
 
-export { addNavigateHandlers, Scene, RightBarIOS, BarButtonIOS };
+export { addNavigateHandlers, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS };
