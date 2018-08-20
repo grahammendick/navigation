2be7ebfd05ca011db9ea9081a75ef33c57a9cf31
Graham Mendick <graham.mendick@gmail.com>
2018-08-20 19:31:55 +0100

    Exported bar button components

--- NavigationReactNative/src/NavigationReactNative.ts
+++ NavigationReactNative/src/NavigationReactNative.ts
@@ -1,4 +1,6 @@
 import Scene from './Scene';
 import addNavigateHandlers from './addNavigateHandlers';
+import RightBarIOS from './RightBarIOS';
+import BarButtonIOS from './BarButtonIOS';
 
-export { addNavigateHandlers, Scene };
+export { addNavigateHandlers, Scene, RightBarIOS, BarButtonIOS };
