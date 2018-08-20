8a311a94c057c90c1d8492ec3e8328734b3d8e7a
Graham Mendick <graham.mendick@gmail.com>
2018-08-20 19:42:49 +0100

    Deleted redundant code

--- NavigationReactNative/src/ios/NVRightBarView.m
+++ NavigationReactNative/src/ios/NVRightBarView.m
@@ -41,15 +41,3 @@
 }
 
 @end
-
-
-
-//
-//  NVRightBarView.m
-//  NavigationReactNative
-//
-//  Created by Graham Mendick on 20/08/2018.
-//  Copyright © 2018 Graham Mendick. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
