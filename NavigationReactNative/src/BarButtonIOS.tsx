94571767ffd556a38063ce61adc27ee0013c68e4
Graham Mendick <graham.mendick@gmail.com>
2018-08-21 11:44:38 +0100

    Switched so custom key takes precedence

--- NavigationReactNative/src/BarButtonIOS.tsx
+++ NavigationReactNative/src/BarButtonIOS.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { requireNativeComponent, Platform } from 'react-native';
 declare var NVBarButton: any;
 
-var BarButtonIOS = props => <NVBarButton {...props} key={props.systemItem} />;
+var BarButtonIOS = props => <NVBarButton key={props.systemItem} {...props} />;
 
 export default Platform.OS === 'ios' ? requireNativeComponent('NVBarButton', BarButtonIOS as any) : () => null;
