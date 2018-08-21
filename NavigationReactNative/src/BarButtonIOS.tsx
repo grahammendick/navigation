25d215bd7ff335b7c10f083e2c5ac8d71d6c5033
Graham Mendick <graham.mendick@gmail.com>
2018-08-21 11:57:22 +0100

    Removed key override - doesn't work
    Leave it up to user to set key if updating system item

--- NavigationReactNative/src/BarButtonIOS.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { requireNativeComponent, Platform } from 'react-native';
-declare var NVBarButton: any;
-
-var BarButtonIOS = props => <NVBarButton {...props} key={props.systemItem} />;
-
-export default Platform.OS === 'ios' ? requireNativeComponent('NVBarButton', BarButtonIOS as any) : () => null;
