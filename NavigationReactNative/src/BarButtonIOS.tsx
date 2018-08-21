a023b90521def7227a577cd80f73d48b1d587a95
Graham Mendick <graham.mendick@gmail.com>
2018-08-21 11:46:07 +0100

    Revert "Switched so custom key takes precedence"
    This reverts commit 94571767ffd556a38063ce61adc27ee0013c68e4.

--- NavigationReactNative/src/BarButtonIOS.tsx
+++ NavigationReactNative/src/BarButtonIOS.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { requireNativeComponent, Platform } from 'react-native';
 declare var NVBarButton: any;
 
-var BarButtonIOS = props => <NVBarButton key={props.systemItem} {...props} />;
+var BarButtonIOS = props => <NVBarButton {...props} key={props.systemItem} />;
 
 export default Platform.OS === 'ios' ? requireNativeComponent('NVBarButton', BarButtonIOS as any) : () => null;
