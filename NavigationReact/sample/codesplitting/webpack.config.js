b24867058bd783ecdacf028abcf864330e4c9072
Graham Mendick <graham.mendick@gmail.com>
2018-12-07 22:27:53 +0000

    Changed to single line for consistency

--- NavigationReact/sample/codesplitting/webpack.config.js
+++ NavigationReact/sample/codesplitting/webpack.config.js
@@ -6,13 +6,7 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-              }
-            }
+            { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
         ]
     },
 };
