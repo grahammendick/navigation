8f2f4eea627254f766f6e7fac9ba808bfac4aabd
Graham Mendick <graham.mendick@gmail.com>
2018-12-18 12:58:54 +0000

    Registered shared view against a crumb

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SharedElementRegistry.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SharedElementRegistry.java
@@ -2,8 +2,25 @@ package com.navigation.reactnative;
 
 import android.view.View;
 
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class SharedElementRegistry {
+    private static final HashMap<Integer, HashSet<WeakReference<View>>> sharedElements;
+
+    static {
+        sharedElements = new HashMap<Integer, HashSet<WeakReference<View>>>();
+    }
 
     static void registerSharedElement(int scene, View view) {
+        if (!sharedElements.containsKey(scene) || !sharedElements.get(scene).contains(view)) {
+            synchronized (SharedElementRegistry.class) {
+                if (!sharedElements.containsKey(scene)) {
+                    sharedElements.put(scene, new HashSet<WeakReference<View>>());
+                }
+                sharedElements.get(scene).add(new WeakReference(view));
+            }
+        }
     }
 }
