ea0362fbdfa396f03f1975528751e5f2e8be911a
Graham Mendick <graham.mendick@gmail.com>
2018-12-21 12:55:06 +0000

    Added listener when view created

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SharedElementManager.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SharedElementManager.java
@@ -20,17 +20,12 @@ public class SharedElementManager extends ViewGroupManager<FrameLayout> {
 
     @Override
     protected FrameLayout createViewInstance(ThemedReactContext reactContext) {
-        return new FrameLayout(reactContext);
-    }
-
-    @SuppressLint("NewApi")
-    @ReactProp(name = "name")
-    public void setName(FrameLayout view, String name) {
-        View rootView = view.getRootView();
+        FrameLayout view = new FrameLayout(reactContext);
         view.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
             public boolean onPreDraw() {
                 view.getViewTreeObserver().removeOnPreDrawListener(this);
+                View rootView = view.getRootView();
                 HashSet<View> sharedElements = (HashSet<View>) rootView.getTag(R.id.sharedElements);
                 if (sharedElements == null) {
                     sharedElements = new HashSet<>();
@@ -40,6 +35,12 @@ public class SharedElementManager extends ViewGroupManager<FrameLayout> {
                 return true;
             }
         });
+        return view;
+    }
+
+    @SuppressLint("NewApi")
+    @ReactProp(name = "name")
+    public void setName(FrameLayout view, String name) {
         view.setTransitionName(name);
     }
 
