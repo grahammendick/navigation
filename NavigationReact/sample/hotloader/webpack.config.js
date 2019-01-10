5d0b584bd027e11efcc7acaaa67b941de308ebd5
Graham Mendick <graham.mendick@gmail.com>
2019-01-10 20:43:17 +0000

    Updated webpack dev server
    Meant updating webpack and babel, too

--- NavigationReact/sample/hotloader/webpack.config.js
+++ NavigationReact/sample/hotloader/webpack.config.js
@@ -1,12 +1,13 @@
 module.exports = {
+    mode: 'development',
     entry: "./index.js",
     output: {
         path: __dirname,
         filename: "app.js"
     },
     module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
         ]
-    }
+    },
 };
