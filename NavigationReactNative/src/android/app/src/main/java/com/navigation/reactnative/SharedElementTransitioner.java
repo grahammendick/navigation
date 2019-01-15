852865605d756aa12d9b8f47e2cf12b1b56cad60
Graham Mendick <graham.mendick@gmail.com>
2019-01-15 05:04:52 +0000

    Loaded elements and started transition

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SharedElementTransitioner.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SharedElementTransitioner.java
@@ -1,15 +1,26 @@
 package com.navigation.reactnative;
 
 import android.app.Activity;
+import android.view.View;
 
 import java.util.HashSet;
 
 public class SharedElementTransitioner {
     private Activity activity;
     private HashSet<String> sharedElements;
+    private HashSet<String> loadedSharedElements;
 
     public SharedElementTransitioner(Activity activity, HashSet<String> sharedElements) {
         this.activity = activity;
         this.sharedElements = sharedElements;
+        this.loadedSharedElements = new HashSet<>();
+    }
+
+    public void load(String sharedElement) {
+        if (sharedElements.contains(sharedElement) && !loadedSharedElements.contains(sharedElement)) {
+            loadedSharedElements.add(sharedElement);
+        }
+        if(sharedElements.size() == loadedSharedElements.size())
+            activity.startPostponedEnterTransition();
     }
 }
