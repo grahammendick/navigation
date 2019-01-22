db4099d57346aee6bc473a3dbd02f8f58f00388c
Graham Mendick <graham.mendick@gmail.com>
2019-01-22 11:44:19 +0000

    Added NavigationMotion to index

--- NavigationReactNative/sample/web/index.web.js
+++ NavigationReactNative/sample/web/index.web.js
@@ -1,4 +1,26 @@
 import React from 'react';
-import { render } from 'react-native';
+import {render} from 'react-native';
+import {NavigationHandler} from 'navigation-react';
+import {NavigationMotion} from 'navigation-react-mobile';
+import createStateNavigator from './createStateNavigator';
 
-render(<div>Hello World</div>, document.getElementById('content'))
+const stateNavigator = createStateNavigator();
+
+stateNavigator.start();
+
+render(
+  <NavigationHandler stateNavigator={stateNavigator}>
+    <NavigationMotion
+        unmountedStyle={{opacity: 0}}
+        mountedStyle={{opacity: 1}}
+        crumbStyle={{opacity: 0}}>
+        {({opacity}, scene, key) => (
+        <div key={key}
+            style={{opacity}}>
+            {scene}
+        </div>
+        )}
+    </NavigationMotion>
+  </NavigationHandler>,
+  document.getElementById('content')
+)
