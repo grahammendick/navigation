f76d230d21e1624c1d8a1ca8d1478ceed029e942
Graham Mendick <graham.mendick@gmail.com>
2019-01-22 11:26:39 +0000

    Built index with webpack

---
+++ NavigationReactNative/sample/web/index.web.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-native';
+
+render(<div>Hello World</div>, document.getElementById('content'))
---
+++ NavigationReactNative/sample/web/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: 'development',
+    entry: "./index.web.js",
+    output: {
+        path: __dirname,
+        filename: "app.js"
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+        ]
+    },
+    resolve: {
+        alias: {
+            'react-native$': 'react-native-web'
+        }
+    }
+};
