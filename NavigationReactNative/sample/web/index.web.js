ff733cfbc9a2ab44e0f9f2bbccdd4db737389ebd
Graham Mendick <graham.mendick@gmail.com>
2019-01-23 11:06:56 +0000

    Created history manager in index.web
    Moves navigation-react-mobile out of the files shared with native

--- NavigationReactNative/sample/web/index.web.js
+++ NavigationReactNative/sample/web/index.web.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {render} from 'react-native';
 import {NavigationHandler} from 'navigation-react';
+import {MobileHistoryManager} from 'navigation-react-mobile';
 import createStateNavigator from './createStateNavigator';
 import Zoom from './Zoom';
 
 const stateNavigator = createStateNavigator();
+
+stateNavigator
+  .configure(stateNavigator, new MobileHistoryManager(url => {
+    var {state, data} = stateNavigator.parseLink(url);
+    return stateNavigator.fluent()
+      .navigate('grid')
+      .navigate(state.key, data).url;
+  }));
 
 stateNavigator.start();
 
