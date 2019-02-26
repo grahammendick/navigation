fd6fa16e3ebcfeac0f1032464732cdab60614677
Graham Mendick <graham.mendick@gmail.com>
2019-02-26 12:04:54 +0000

    Unreserved sharedElements navigation data

--- NavigationReactNative/src/addNavigateHandlers.ts
+++ NavigationReactNative/src/addNavigateHandlers.ts
@@ -12,20 +12,19 @@ var addNavigateHandlers = (stateNavigator: StateNavigator | StateNavigator[]) =>
             var {state, data, title, oldData, oldUrl, crumbs, nextCrumb} = stateNavigator.stateContext;
             var appKey = AppRegistry.getAppKeys()[0];
             var titles = crumbs.map(({data, title}) => data.sceneTitle || title).concat(data.sceneTitle || title);
-            var getSharedElements = ({sharedElements}) => sharedElements;
             var {crumbs: oldCrumbs} = stateNavigator.parseLink(oldUrl);
             if (oldCrumbs.length < crumbs.length) {
                 var {state: nextState, data: nextData} = crumbs.concat(nextCrumb)[oldCrumbs.length + 1];
                 var enterAnim = state.getUnmountStyle && state.getUnmountStyle(true, data, crumbs);
                 var exitAnim = oldState.getCrumbStyle && oldState.getCrumbStyle(false, oldData, oldCrumbs, nextState, nextData);
-                var sharedElements = (state.getSharedElements || getSharedElements)(data, crumbs);
+                var sharedElements = state.getSharedElements && state.getSharedElements(data, crumbs);
             }
             if (crumbs.length < oldCrumbs.length) {
                 var nextCrumb = new Crumb(oldData, oldState, null, null, false);
                 var {state: nextState, data: nextData} = oldCrumbs.concat(nextCrumb)[crumbs.length + 1];
                 var enterAnim = state.getCrumbStyle && state.getCrumbStyle(true, data, crumbs, nextState, nextData);
                 var exitAnim = oldState.getUnmountStyle && oldState.getUnmountStyle(false, oldData, oldCrumbs);
-                var oldSharedElements = (oldState.getSharedElements || getSharedElements)(oldData, oldCrumbs);
+                var oldSharedElements = oldState.getSharedElements && oldState.getSharedElements(oldData, oldCrumbs);
             }
             NavigationModule.render(crumbs.length, tab, titles, appKey, sharedElements, oldSharedElements, enterAnim, exitAnim);
         });
