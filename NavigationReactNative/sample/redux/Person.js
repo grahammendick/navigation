3e6e74efe630ef89c5625114e446181a66c8a6ef
Graham Mendick <graham.mendick@gmail.com>
2019-05-09 21:24:47 +0100

    Styled the person edit

--- NavigationReactNative/sample/redux/Person.js
+++ NavigationReactNative/sample/redux/Person.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, TextInput, View} from 'react-native';
 import {connect} from 'react-redux';
 
 const mapStateToProps = ({byId}, {id}) => ({person: byId[id]});
 
 const Person = ({person: {name}}) => (
   <SafeAreaView>
-    <Text>{name}</Text>
+    <View style={styles.field}>
+      <Text style={styles.label}>Name</Text>
+      <TextInput style={styles.entry}>{name}</TextInput>
+    </View>
   </SafeAreaView>
 );
 
 export default connect(mapStateToProps)(Person);
+
+const styles = StyleSheet.create({
+  field: {
+    flexDirection: 'row',
+    padding: 20,
+  },
+  label: {
+    width: 100,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  entry: {
+    fontSize: 20,
+  },
+});
