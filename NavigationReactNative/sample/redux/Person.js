17eb727fbdc5c3874155a6577bd4f6da04fa0249
Graham Mendick <graham.mendick@gmail.com>
2019-05-09 21:28:40 +0100

    Made non-name fields readonly

--- NavigationReactNative/sample/redux/Person.js
+++ NavigationReactNative/sample/redux/Person.js
@@ -4,11 +4,23 @@ import {connect} from 'react-redux';
 
 const mapStateToProps = ({byId}, {id}) => ({person: byId[id]});
 
-const Person = ({person: {name}}) => (
+const Person = ({person: {name, dateOfBirth, email, phone}}) => (
   <SafeAreaView>
     <View style={styles.field}>
       <Text style={styles.label}>Name</Text>
       <TextInput style={styles.entry}>{name}</TextInput>
+    </View>
+    <View style={styles.field}>
+      <Text style={styles.label}>DOB</Text>
+      <Text style={styles.entry}>{dateOfBirth}</Text>
+    </View>
+    <View style={styles.field}>
+      <Text style={styles.label}>Email</Text>
+      <Text style={styles.entry}>{email}</Text>
+    </View>
+    <View style={styles.field}>
+      <Text style={styles.label}>Phone</Text>
+      <Text style={styles.entry}>{phone}</Text>
     </View>
   </SafeAreaView>
 );
@@ -18,7 +30,8 @@ export default connect(mapStateToProps)(Person);
 const styles = StyleSheet.create({
   field: {
     flexDirection: 'row',
-    padding: 20,
+    margin: 5,
+    marginLeft: 25,
   },
   label: {
     width: 100,
