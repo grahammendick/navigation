a6e4aded3f558efc05b9648aa0da47093aa12ed1
Graham Mendick <graham.mendick@gmail.com>
2019-05-09 14:24:56 +0100

    Added JavaScript setup code and scene

--- NavigationReactNative/sample/redux/App.js
+++ NavigationReactNative/sample/redux/App.js
@@ -1,49 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {StateNavigator} from 'navigation';
+import {NavigationHandler} from 'navigation-react';
+import {addNavigateHandlers, Scene} from 'navigation-react-native';
+import {Text, SafeAreaView} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+var stateNavigator = new StateNavigator([
+  {key: 'grid', title: 'Colors', trackCrumbTrail: true},
+]);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { grid } = stateNavigator.states;
+grid.renderScene = () => <SafeAreaView style={{flex: 1}}><Text>Hello</Text></SafeAreaView>;
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+stateNavigator.navigate('grid');
+addNavigateHandlers(stateNavigator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default ({crumb}) => (
+  <NavigationHandler stateNavigator={stateNavigator}>
+    <Scene crumb={crumb} />
+  </NavigationHandler>
+);
