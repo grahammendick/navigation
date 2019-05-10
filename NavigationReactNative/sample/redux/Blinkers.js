a4a0796f24aadfc3515760b045ad0451493371ae
Graham Mendick <graham.mendick@gmail.com>
2019-05-10 17:59:42 +0100

    Defaulted prop for first crumb on android

--- NavigationReactNative/sample/redux/Blinkers.js
+++ NavigationReactNative/sample/redux/Blinkers.js
@@ -1,7 +1,7 @@
 import React, {useContext, useMemo} from 'react';
 import {ReactReduxContext} from 'react-redux';
 
-export default ({crumb, children}) => {
+const Blinkers = ({crumb, children}) => {
   const {store, subscription} = useContext(ReactReduxContext);
 
   const contextValue = useMemo(() => ({
@@ -24,3 +24,7 @@ export default ({crumb, children}) => {
     </ReactReduxContext.Provider>
   );  
 }
+
+Blinkers.defaultProps = {crumb: 0}
+ 
+export default Blinkers;
