6ed996d78ae9e75ee2c4a446f1284c57e8702bbb
Graham Mendick <graham.mendick@gmail.com>
2019-05-10 09:07:12 +0100

    Replaced consumer with useContext hook

--- NavigationReactNative/sample/redux/PersonItem.js
+++ NavigationReactNative/sample/redux/PersonItem.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {NavigationContext} from 'navigation-react';
 import {connect} from 'react-redux';
 
 const mapStateToProps = ({people: {byId}}, {id}) => ({person: byId[id]});
 
-const PersonItem = ({person: {id, name}}) => (
-  <NavigationContext.Consumer>
-    {({stateNavigator}) => (
-      <TouchableOpacity
-        onPress={() => {
-          stateNavigator.navigate('person', {id});
-        }}>
-        <View>
-          <Text style={styles.name}>{name}</Text>
-        </View>
-      </TouchableOpacity>
-    )}
-  </NavigationContext.Consumer>
-);
+const PersonItem = ({person: {id, name}}) => {
+  const {stateNavigator} = useContext(NavigationContext);
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        stateNavigator.navigate('person', {id});
+      }}>
+      <View>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+};
 
 export default connect(mapStateToProps)(PersonItem);
 
