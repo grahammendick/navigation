c38211f718d8f83017e4e8dc7afa6f112f553b41
Graham Mendick <graham.mendick@gmail.com>
2019-05-14 14:16:13 +0100

    Mounted scene when container did mount

--- NavigationReactMobile/src/SceneProxy.tsx
+++ NavigationReactMobile/src/SceneProxy.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 
-class SceneProxy extends React.Component<{crumb: number}> {
+class SceneProxy extends React.Component<{crumb: number, mountScene: ({crumb: number, el: HTMLDivElement}) => {}}> {
     private sceneEl: React.RefObject<HTMLDivElement>;
     constructor(props) {
         super(props);
         this.sceneEl = React.createRef(); 
     }
-    componentDidMount() {        
+    componentDidMount() {
+        var {crumb, mountScene} = this.props;
+        mountScene({crumb, el: this.sceneEl.current});
     }
     render() {
         return (
