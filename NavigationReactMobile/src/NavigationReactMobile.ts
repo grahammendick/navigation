1822033a40907f5925068db19fcec702a6dcc8f4
Graham Mendick <graham.mendick@gmail.com>
2019-05-14 18:14:36 +0100

    Exported scene on public api

--- NavigationReactMobile/src/NavigationReactMobile.ts
+++ NavigationReactMobile/src/NavigationReactMobile.ts
@@ -1,6 +1,7 @@
 import MobileHistoryManager from './MobileHistoryManager';
 import NavigationMotion from './NavigationMotion';
+import Scene from './Scene';
 import SharedElement from './SharedElement';
 import SharedElementMotion from './SharedElementMotion';
 
-export { MobileHistoryManager, NavigationMotion, SharedElement, SharedElementMotion };
+export { MobileHistoryManager, NavigationMotion, Scene, SharedElement, SharedElementMotion };
