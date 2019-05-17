9cfeb89648a4104a671919ac1547ddee32ac6342
Graham Mendick <graham.mendick@gmail.com>
2019-05-17 11:52:14 +0100

    Inherited proxy registry from registry

--- NavigationReactMobile/src/SharedElementRegistryProxy.ts
+++ NavigationReactMobile/src/SharedElementRegistryProxy.ts
@@ -1,4 +1,6 @@
-class SharedElementRegistryProxy {
+import SharedElementRegistry from './SharedElementRegistry';
+
+class SharedElementRegistryProxy extends SharedElementRegistry {
     registerSharedElement(scene: number, name: string, ref: HTMLElement, data) {
         var eventData = {bubbles: true, detail: {name, data, share: true}};
         ref.dispatchEvent(new CustomEvent('share', eventData));
