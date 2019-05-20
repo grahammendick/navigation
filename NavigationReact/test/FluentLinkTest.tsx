0f7d1f151d18a6f8264f061b047a1f663f5aaf8e
Graham Mendick <graham.mendick@gmail.com>
2019-05-20 14:53:13 +0100

    Tested fluent link renders attributes

--- NavigationReact/test/FluentLinkTest.tsx
+++ NavigationReact/test/FluentLinkTest.tsx
@@ -62,15 +62,13 @@ describe('FluentLinkTest', function () {
                 { key: 's0', route: 'r0' },
                 { key: 's1', route: 'r1', trackCrumbTrail: true }
             ]);
-            stateNavigator.navigate('s0');
-            stateNavigator.navigate('s1');
             var container = document.createElement('div');
             ReactDOM.render(
                 <NavigationHandler stateNavigator={stateNavigator}>
                     <FluentLink navigate={fluentNavigator => (
                         fluentNavigator
-                        .navigate('s0')
-                        .navigate('x')
+                            .navigate('s0')
+                            .navigate('x')
                     )}>
                         link text
                     </FluentLink>
@@ -80,6 +78,39 @@ describe('FluentLinkTest', function () {
             var link = container.querySelector<HTMLAnchorElement>('a');
             assert.equal(link.hash, '');
             assert.equal(link.innerHTML, 'link text');
+        })
+    });
+
+    describe('Attributes Fluent Link', function () {
+        it('should render', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 's0', route: 'r0' },
+                { key: 's1', route: 'r1', trackCrumbTrail: true }
+            ]);
+            var container = document.createElement('div');
+            ReactDOM.render(
+                <NavigationHandler stateNavigator={stateNavigator}>
+                    <FluentLink
+                        navigate={fluentNavigator => (
+                            fluentNavigator
+                                .navigate('s0')
+                                .navigate('s1')
+                        )}
+                        historyAction='replace'
+                        navigating={() => false}
+                        aria-label="z"
+                        target="_blank">
+                        link text
+                    </FluentLink>
+                </NavigationHandler>,
+                container
+            );
+            var link = container.querySelector<HTMLAnchorElement>('a');
+            assert.equal(link.hash, '#/r1?crumb=%2Fr0');
+            assert.equal(link.innerHTML, 'link text');
+            assert.equal(link.getAttribute('aria-label'), 'z');
+            assert.equal(link.target, '_blank');
+            assert.equal(link.attributes.length, 3);
         })
     });
 });
