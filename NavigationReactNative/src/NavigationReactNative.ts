48b06c85b1709c219d348b7752baf1a0af65404d
Graham Mendick <graham.mendick@gmail.com>
2019-05-26 09:37:26 +0100

    Added and exported search bar component

--- NavigationReactNative/src/NavigationReactNative.ts
+++ NavigationReactNative/src/NavigationReactNative.ts
@@ -3,6 +3,7 @@ import addNavigateHandlers from './addNavigateHandlers';
 import LeftBarIOS from './LeftBarIOS';
 import RightBarIOS from './RightBarIOS';
 import BarButtonIOS from './BarButtonIOS';
+import SearchBarIOS from './SearchBarIOS';
 import SharedElementAndroid from './SharedElementAndroid';
 
-export { addNavigateHandlers, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS, SharedElementAndroid };
+export { addNavigateHandlers, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS, SearchBarIOS, SharedElementAndroid };
