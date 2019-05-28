e50bd4d1a305e3cccc159fa73a80fdc57f8313b6
Graham Mendick <graham.mendick@gmail.com>
2019-05-28 08:50:04 +0100

    Added controller to resize on re-orientation

---
+++ NavigationReactNative/src/ios/NVSearchResultsController.h
@@ -0,0 +1,13 @@
+//
+//  NVSearchResultsController.h
+//  NavigationReactNative
+//
+//  Created by Graham Mendick on 28/05/2019.
+//  Copyright © 2019 Graham Mendick. All rights reserved.
+//
+
+#ifndef NVSearchResultsController_h
+#define NVSearchResultsController_h
+
+
+#endif /* NVSearchResultsController_h */
---
+++ NavigationReactNative/src/ios/NVSearchResultsController.m
@@ -0,0 +1,9 @@
+//
+//  NVSearchResultsController.m
+//  NavigationReactNative
+//
+//  Created by Graham Mendick on 28/05/2019.
+//  Copyright © 2019 Graham Mendick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
