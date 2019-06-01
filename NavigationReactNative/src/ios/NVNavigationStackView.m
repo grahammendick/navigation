127ab2e8280547d711e56e5fa15e1cc386f2ee83
Graham Mendick <graham.mendick@gmail.com>
2019-06-01 17:34:32 +0100

    Pushed subview onto navigationController
    Can't call suerp insert because then the react native grumbles that the parent react view controller aren't right. It's expecting the parent to be the host view controller but it's actually the navigation controller. Still need to track the subviews because gonna need them when swipe back and navigate I think because need client and server subviews to match (can't call setState when going back)

--- NavigationReactNative/src/ios/NVNavigationStackView.m
+++ NavigationReactNative/src/ios/NVNavigationStackView.m
@@ -1,12 +1,31 @@
 #import "NVNavigationStackView.h"
 
+#import <UIKit/UIKit.h>
+#import <React/UIView+React.h>
+
 @implementation NVNavigationStackView
+{
+    UINavigationController *_navigationController;
+    NSMutableArray *subviews;
+}
 
 - (id)init
 {
     if (self = [super init]) {
+        _navigationController = [[UINavigationController alloc] init];
+        [self addSubview:_navigationController.view];
+        subviews = [[NSMutableArray alloc] init];
     }
     return self;
+}
+
+- (void)insertReactSubview:(UIView *)subview atIndex:(NSInteger)atIndex
+{
+    //[super insertReactSubview:subview atIndex:atIndex];
+    [subviews insertObject:subview atIndex:atIndex];
+    UIViewController *viewController = [UIViewController new];
+    viewController.view = subview;
+    [_navigationController pushViewController:viewController animated:YES];
 }
 
 @end
