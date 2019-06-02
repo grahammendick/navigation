e3a7604b998cf1542e25acfad85fea3a4f4d885a
Graham Mendick <graham.mendick@gmail.com>
2019-06-02 10:36:02 +0100

    Deleted addNavigateHandlers
    It's all done through props and components now!

--- NavigationReactNative/src/NavigationReactNative.ts
+++ NavigationReactNative/src/NavigationReactNative.ts
@@ -1,9 +1,8 @@
 import NavigationStack from './NavigationStack';
 import Scene from './Scene';
-import addNavigateHandlers from './addNavigateHandlers';
 import LeftBarIOS from './LeftBarIOS';
 import RightBarIOS from './RightBarIOS';
 import BarButtonIOS from './BarButtonIOS';
 import SharedElementAndroid from './SharedElementAndroid';
 
-export { addNavigateHandlers, NavigationStack, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS, SharedElementAndroid };
+export { NavigationStack, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS, SharedElementAndroid };
