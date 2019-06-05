84564040e5bef829fdb6b1c4d491062dc4446b6a
Graham Mendick <graham.mendick@gmail.com>
2019-06-05 15:40:43 +0100

    Added basic tab bar ios components

--- NavigationReactNative/src/NavigationReactNative.ts
+++ NavigationReactNative/src/NavigationReactNative.ts
@@ -3,6 +3,8 @@ import Scene from './Scene';
 import LeftBarIOS from './LeftBarIOS';
 import RightBarIOS from './RightBarIOS';
 import BarButtonIOS from './BarButtonIOS';
+import TabBarIOS from './TabBarIOS';
+import TabBarItemIOS from './TabBarItemIOS';
 import SharedElementAndroid from './SharedElementAndroid';
 
-export { NavigationStack, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS, SharedElementAndroid };
+export { NavigationStack, Scene, LeftBarIOS, RightBarIOS, BarButtonIOS, TabBarIOS, TabBarItemIOS, SharedElementAndroid };
