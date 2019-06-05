2e153fece03e51d8029b5c8123ebb0de750426c0
Graham Mendick <graham.mendick@gmail.com>
2019-06-05 21:45:57 +0100

    Stopped the nav stacks sharing the real estate
    Each navigation stack only had half of the view height. Add another stack and they'd only get a third each! Added stack children to shadow view - trick copied from RCTModalHostViewManager - so they're sized based on whole screen size

--- NavigationReactNative/src/ios/NVNavigationStackManager.m
+++ NavigationReactNative/src/ios/NVNavigationStackManager.m
@@ -1,6 +1,24 @@
 #import "NVNavigationStackManager.h"
 #import "NVNavigationStackView.h"
 
+#import <React/RCTShadowView.h>
+
+@interface NVNavigationStackShadowView : RCTShadowView
+
+@end
+
+@implementation NVNavigationStackShadowView
+
+- (void)insertReactSubview:(id<RCTComponent>)subview atIndex:(NSInteger)atIndex
+{
+    [super insertReactSubview:subview atIndex:atIndex];
+    if ([subview isKindOfClass:[RCTShadowView class]]) {
+        ((RCTShadowView *)subview).size = RCTScreenSize();
+    }
+}
+
+@end
+
 @implementation NVNavigationStackManager
 
 RCT_EXPORT_MODULE()
@@ -8,6 +26,11 @@ RCT_EXPORT_MODULE()
 - (UIView *)view
 {
     return [[NVNavigationStackView alloc] init];
+}
+
+- (RCTShadowView *)shadowView
+{
+    return [NVNavigationStackShadowView new];
 }
 
 RCT_EXPORT_VIEW_PROPERTY(mostRecentEventCount, NSInteger)
