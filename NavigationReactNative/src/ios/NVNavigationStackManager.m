2e1c9006a016b479391d32735f06873c62fa8652
Graham Mendick <graham.mendick@gmail.com>
2019-06-06 00:24:57 +0100

    Removed redundant shadow view
    The bounds change handles the resize

--- NavigationReactNative/src/ios/NVNavigationStackManager.m
+++ NavigationReactNative/src/ios/NVNavigationStackManager.m
@@ -1,24 +1,6 @@
 #import "NVNavigationStackManager.h"
 #import "NVNavigationStackView.h"
 
-#import <React/RCTShadowView.h>
-
-@interface NVNavigationStackShadowView : RCTShadowView
-
-@end
-
-@implementation NVNavigationStackShadowView
-
-- (void)insertReactSubview:(id<RCTComponent>)subview atIndex:(NSInteger)atIndex
-{
-    [super insertReactSubview:subview atIndex:atIndex];
-    if ([subview isKindOfClass:[RCTShadowView class]]) {
-        ((RCTShadowView *)subview).size = RCTScreenSize();
-    }
-}
-
-@end
-
 @implementation NVNavigationStackManager
 
 RCT_EXPORT_MODULE()
@@ -26,11 +8,6 @@ RCT_EXPORT_MODULE()
 - (UIView *)view
 {
     return [[NVNavigationStackView alloc] initWithBridge:self.bridge];
-}
-
-- (RCTShadowView *)shadowView
-{
-    return [NVNavigationStackShadowView new];
 }
 
 RCT_EXPORT_VIEW_PROPERTY(mostRecentEventCount, NSInteger)
