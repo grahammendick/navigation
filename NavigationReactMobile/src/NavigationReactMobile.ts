16d4e27a217fdfa5d178882557a0caee983143d3
Graham Mendick <graham.mendick@gmail.com>
2019-06-15 09:51:15 +0100

    Removed scene from exports

--- NavigationReactMobile/src/NavigationReactMobile.ts
+++ NavigationReactMobile/src/NavigationReactMobile.ts
@@ -1,7 +1,6 @@
 import MobileHistoryManager from './MobileHistoryManager';
 import NavigationMotion from './NavigationMotion';
-import Scene from './Scene';
 import SharedElement from './SharedElement';
 import SharedElementMotion from './SharedElementMotion';
 
-export { MobileHistoryManager, NavigationMotion, Scene, SharedElement, SharedElementMotion };
+export { MobileHistoryManager, NavigationMotion, SharedElement, SharedElementMotion };
