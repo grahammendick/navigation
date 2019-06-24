7b31c18335d5e2cb6d6213348c7858315c666aa3
Graham Mendick <graham.mendick@gmail.com>
2019-06-24 11:24:14 +0100

    Set nav bar hidden from hide property

--- NavigationReactNative/src/ios/NVNavigationBarView.m
+++ NavigationReactNative/src/ios/NVNavigationBarView.m
@@ -1,4 +1,21 @@
 #import "NVNavigationBarView.h"
 
+#import <React/UIView+React.h>
+
 @implementation NVNavigationBarView
+
+- (void)didMoveToWindow
+{
+    [super didMoveToWindow];
+    [self.reactViewController.navigationController setNavigationBarHidden:self.hidden];
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview
+{
+    [super willMoveToSuperview:newSuperview];
+    if (!newSuperview) {
+        [self.reactViewController.navigationController setNavigationBarHidden:false];
+    }
+}
+
 @end
