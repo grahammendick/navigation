d1c71f116b956e07622cfbe8771234d82499826c
Graham Mendick <graham.mendick@gmail.com>
2019-07-04 21:19:08 +0100

    Added 2 scenes and navigated between them

--- NavigationReactNative/sample/temp/App.js
+++ NavigationReactNative/sample/temp/App.js
@@ -1,97 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {StateNavigator} from 'navigation';
+import {NavigationHandler, NavigationContext} from 'navigation-react';
+import {NavigationStack} from 'navigation-react-native';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+var stateNavigator = new StateNavigator([
+  {key: 'hello'},
+  {key: 'world', trackCrumbTrail: true}
+]);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+var {hello, world} = stateNavigator.states;
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
+hello.renderScene = () => (
+  <NavigationContext.Consumer>
+    {({stateNavigator}) => (
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <TouchableOpacity
+          onPress={() => {
+            stateNavigator.navigate('world');
+          }}>
+          <Text>Hello</Text>
+        </TouchableOpacity>
       </SafeAreaView>
-    </Fragment>
-  );
-};
+    )}
+  </NavigationContext.Consumer>
+);
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+world.renderScene = () => (
+  <SafeAreaView>
+    <Text>World</Text>
+  </SafeAreaView>
+);
 
-export default App;
+stateNavigator.navigate('hello');
+
+export default () => (
+  <NavigationHandler stateNavigator={stateNavigator}>
+    <NavigationStack />
+  </NavigationHandler>
+);
