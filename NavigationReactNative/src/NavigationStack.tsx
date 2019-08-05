f8f8b5bf61e0ed4500b43cdd89fbd0f8cf16dad7
Graham Mendick <graham.mendick@gmail.com>
2019-08-05 14:21:38 +0100

    Used prev state context to check for replace
    Button that navigates from A --> B. Click it repeatedly before first navigation completes (add javascript breakpoint to pause first click). The first click sets the oldState to A and state to B. The second click does the same because navigation is idempotent. But this time the prevKeys length matches the keys length so it think it's a replace!!
    Comparing prevKeys and keys so should compare preState and state.
    Could change to compare oldUrl length and keys length but think this would go wrong when it is a replace because the replace would happen twice (need to check!)

--- NavigationReactNative/src/NavigationStack.tsx
+++ NavigationReactNative/src/NavigationStack.tsx
@@ -24,11 +24,12 @@ class NavigationStack extends React.Component<NavigationStackProps, NavigationSt
     static getDerivedStateFromProps({stateNavigator}: NavigationStackProps, {keys: prevKeys, stateNavigator: prevStateNavigator}: NavigationStackState) {
         if (stateNavigator === prevStateNavigator)
             return null;
-        var {oldState, state, crumbs, nextCrumb} = stateNavigator.stateContext;
+        var {state, crumbs, nextCrumb} = stateNavigator.stateContext;
+        var prevState = prevStateNavigator && prevStateNavigator.stateContext.state;
         var currentKeys = crumbs.concat(nextCrumb).map((_, i) => '' + i);
         var newKeys = currentKeys.slice(prevKeys.length);
         var keys = prevKeys.slice(0, currentKeys.length).concat(newKeys);
-        if (prevKeys.length === keys.length && oldState !== state)
+        if (prevKeys.length === keys.length && prevState !== state)
             keys[keys.length - 1] += '+';
         return {keys, stateNavigator};
     }
