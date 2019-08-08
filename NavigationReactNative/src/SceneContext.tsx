7f772fb89626415637e37696178318ffa21a8e8d
Graham Mendick <graham.mendick@gmail.com>
2019-08-08 09:29:08 +0100

    Typed the SceneContext

--- NavigationReactNative/src/SceneContext.tsx
+++ NavigationReactNative/src/SceneContext.tsx
@@ -1,3 +1,4 @@
 import * as React from 'react';
+type SceneContext = { canHandleBack: () => boolean };
 
-export default React.createContext();
+export default React.createContext() as React.Context<SceneContext>;
