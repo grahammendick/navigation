aa91646d5663c710194471c896b05fffaad28f43
Graham Mendick <graham.mendick@gmail.com>
2019-08-18 11:15:53 +0100

    Tweaked format

--- NavigationReactNative/sample/twitter/App.js
+++ NavigationReactNative/sample/twitter/App.js
@@ -9,7 +9,7 @@ import Tweet from './Tweet';
 import Timeline from './Timeline';
 import {getHome, getFollows, getTweet, getTimeline} from './data';
 
-var stateNavigator = new StateNavigator([
+const stateNavigator = new StateNavigator([
   {key: 'home'},
   {key: 'notifications'},
   {key: 'tweet', trackCrumbTrail: true},
@@ -21,14 +21,13 @@ notifications.renderScene = () => <Notifications follows={getFollows()} />;
 tweet.renderScene = ({id}) => <Tweet tweet={getTweet(id)}  />;
 timeline.renderScene = ({id}) => <Timeline timeline={getTimeline(id)}  />;
 
-var notificationsNavigator = new StateNavigator(stateNavigator);
+const notificationsNavigator = new StateNavigator(stateNavigator);
 stateNavigator.navigate('home');
 
-export default() => {
-  const [notified,
-    setNotified] = useState(false);
-  return (Platform.OS === 'ios'
-    ? (<TabBarIOS>
+export default () => {
+  const [notified, setNotified] = useState(false);
+  return (Platform.OS === 'ios' ? (
+      <TabBarIOS>
         <TabBarItemIOS title="Home">
           <NavigationHandler stateNavigator={stateNavigator}>
             <NavigationStack/>
@@ -36,27 +35,22 @@ export default() => {
         </TabBarItemIOS>
         <TabBarItemIOS
           title="Notifications"
-          badge={!notified?getFollows().length:null} 
-          /* badgeColor="red" */
+          badge={!notified ? getFollows().length : null} 
           onPress={() => {
             setNotified(true);
             if (!notificationsNavigator.stateContext.state) 
               notificationsNavigator.navigate('notifications');
-            }}>
+          }}>
           <NavigationHandler stateNavigator={notificationsNavigator}>
             <NavigationStack/>
           </NavigationHandler>
         </TabBarItemIOS>
       </TabBarIOS>
-    )
-    : (<NavigationHandler stateNavigator={stateNavigator}>
+    ) : (
+      <NavigationHandler stateNavigator={stateNavigator}>
         <NavigationStack
-          crumbStyle={from => from
-          ? 'scale_in'
-          : 'scale_out'}
-          unmountStyle={from => from
-          ? 'slide_in'
-          : 'slide_out'}></NavigationStack>
+          crumbStyle={from => from ? 'scale_in' : 'scale_out'}
+          unmountStyle={from => from ? 'slide_in' : 'slide_out'} />
       </NavigationHandler>
     )
   );
