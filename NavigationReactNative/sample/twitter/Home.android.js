bea3e58eae14e5529b5078dcf1422fbf11ef860c
Graham Mendick <graham.mendick@gmail.com>
2019-08-18 11:25:11 +0100

    Converted to useState and useRef

--- NavigationReactNative/sample/twitter/Home.android.js
+++ NavigationReactNative/sample/twitter/Home.android.js
@@ -1,71 +1,74 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import {StyleSheet, BackHandler, Text, TouchableHighlight, ToolbarAndroid, View, ScrollView} from 'react-native';
 import ViewPager from '@react-native-community/viewpager';
 import Tweets from './Tweets';
 import Notifications from './Notifications';
 
-export default class Home extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {page: 0};
-    this.handleBack = this.handleBack.bind(this);
+/*constructor(props, context) {
+  super(props, context);
+  this.state = {page: 0};
+  this.handleBack = this.handleBack.bind(this);
+}
+componentDidMount() {
+  BackHandler.addEventListener('hardwareBackPress', this.handleBack);
+}
+componentWillUnmount() {
+  BackHandler.removeEventListener('hardwareBackPress', this.handleBack);
+}
+handleBack() {
+  if (this.state.page === 1) {
+    this.setPage(0);
+    return true;
   }
-  componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.handleBack);
-  }
-  componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.handleBack);
-  }
-  handleBack() {
-    if (this.state.page === 1) {
-      this.setPage(0);
-      return true;
-    }
-    return false;
-  }
-  setPage(page) {
-    this.setState({page});
-    this.viewPager.setPage(page);
-  }
-  render() {
-    var {tweets, follows} = this.props;
-    return (
-      <View style={{flex: 1}}>
-        <ToolbarAndroid title="Home" style={styles.toolbar} />
-        <View style={styles.tabs}>
-          <TouchableHighlight
-            underlayColor="white"
-            style={styles.tab}
-            onPress={() => this.setPage(0)}>
-            <Text style={styles.text}>Home</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            underlayColor="white"
-            style={styles.tab}
-            onPress={() => this.setPage(1)}>
-            <Text style={styles.text}>Notifications</Text>
-          </TouchableHighlight>
-        </View>
-        <ViewPager
-          ref={el => this.viewPager = el}
-          onPageSelected={({nativeEvent}) => {
-            this.setState({page: nativeEvent.position})
-          }}
-          style={{flex: 1}}>
-          <View key={1}>
-            <ScrollView style={styles.view}>
-              <Tweets tweets={tweets} />
-            </ScrollView>
-          </View>
-          <View key={2}>
-            <ScrollView style={styles.view}>
-              <Notifications follows={follows} />
-            </ScrollView>
-          </View>
-        </ViewPager>
+  return false;
+}*/
+
+
+export default ({tweets, follows}) => {
+  const [page, setPage] = useState(0);
+  const viewPager = useRef(null);
+  return (
+    <View style={{flex: 1}}>
+      <ToolbarAndroid title="Home" style={styles.toolbar} />
+      <View style={styles.tabs}>
+        <TouchableHighlight
+          underlayColor="white"
+          style={styles.tab}
+          onPress={() => {
+            setPage(0);
+            viewPager.current.setPage(0);
+          }}>
+          <Text style={styles.text}>Home</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          underlayColor="white"
+          style={styles.tab}
+          onPress={() => {
+            setPage(1);
+            viewPager.current.setPage(1);
+          }}>
+          <Text style={styles.text}>Notifications</Text>
+        </TouchableHighlight>
       </View>
-    );
-  }
+      <ViewPager
+        ref={viewPager}
+        onPageSelected={({nativeEvent}) => {
+          setPage(nativeEvent.position)
+        }}
+        style={{flex: 1}}>
+        <View key={1}>
+          <ScrollView style={styles.view}>
+            <Tweets tweets={tweets} />
+          </ScrollView>
+        </View>
+        <View key={2}>
+          <ScrollView style={styles.view}>
+            <Notifications follows={follows} />
+          </ScrollView>
+        </View>
+      </ViewPager>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
