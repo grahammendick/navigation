5c86bd1935b70373ed4c13bfe0db932ab1aa8d09
Graham Mendick <graham.mendick@gmail.com>
2019-08-21 10:20:17 +0100

    Emitted url when MainActivity created

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/NavigationStackView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/NavigationStackView.java
@@ -6,6 +6,7 @@ import android.app.SharedElementCallback;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Pair;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.views.image.ReactImageView;
 
@@ -74,8 +76,14 @@ public class NavigationStackView extends ViewGroup {
         Activity currentActivity = ((ThemedReactContext) getContext()).getCurrentActivity();
         if (currentActivity == null)
             return;
-        if (mainActivity == null)
+        if (mainActivity == null) {
             mainActivity = currentActivity;
+            Uri uri = mainActivity.getIntent().getData();
+            if (uri != null) {
+                DeviceEventManagerModule deviceEventManagerModule = ((ThemedReactContext) getContext()).getNativeModule(DeviceEventManagerModule.class);
+                deviceEventManagerModule.emitNewIntentReceived(uri);
+            }
+        }
         if (finish) {
             mainActivity.finish();
             return;
