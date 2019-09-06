223bb7e2dd8fc403909d2804e6ebd94854114e29
peter.zhang <peter.zhang@dji.com>
2019-09-06 10:39:46 +0800

    Implemented BarTitleViewIOS by function component.

--- NavigationReactNative/src/BarTitleViewIOS.tsx
+++ NavigationReactNative/src/BarTitleViewIOS.tsx
@@ -3,18 +3,9 @@ import { requireNativeComponent, Platform, StyleSheet } from 'react-native';
 
 const NVBarTitleView = requireNativeComponent<any>('NVBarTitleView', null)
 
-class BarTitleView extends React.Component {
-  render() {
-    const { style } = this.props as any
-    return (
-      <NVBarTitleView 
-        {...this.props}
-        style={[styles.barTitleView, style]}>
-        {this.props.children}
-      </NVBarTitleView>
-    )
-  }
-}
+const BarTitleView = ({style, ...props}) => (
+  <NVBarTitleView {...props} style={[styles.barTitleView, style]}/>
+)
 
 const styles = StyleSheet.create({
   barTitleView: {
