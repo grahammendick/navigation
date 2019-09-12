7769ad75f444838bc37f930291f53ccf9a251c9b
Graham Mendick <graham.mendick@gmail.com>
2019-09-12 13:29:48 +0100

    Created scene fragment class

---
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/SceneFragment.java
@@ -0,0 +1,25 @@
+package com.navigation.reactnative;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SceneFragment extends Fragment {
+    private String key;
+
+    public SceneFragment(String key) {
+        super();
+        this.key = key;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return NavigationStackView.scenes.get(key);
+    }
+}
