bc2cf29bdff64e8dd2f84ac6f962f31f9d94ed72
Graham Mendick <graham.mendick@gmail.com>
2019-09-14 20:26:24 +0100

    Replaced fragment instead of adding
    Found this out when trying shared element transition

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/NavigationStackView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/NavigationStackView.java
@@ -109,25 +109,17 @@ public class NavigationStackView extends ViewGroup {
             FragmentManager fragmentManager = ((FragmentActivity) mainActivity).getSupportFragmentManager();
             int enter = getAnimationResourceId(enterAnim, activityOpenEnterAnimationId);
             int exit = getAnimationResourceId(exitAnim, activityOpenExitAnimationId);
-            Fragment fragment, prevFragment = null;
             for(int i = 0; i < crumb - currentCrumb; i++) {
                 int nextCrumb = currentCrumb + i + 1;
                 String key = keys.getString(nextCrumb);
                 SceneView scene = NavigationStackView.scenes.get(key);
                 int popEnter = getAnimationResourceId(scene.enterAnim, activityCloseExitAnimationId);
                 int popExit = getAnimationResourceId(scene.exitAnim, activityCloseEnterAnimationId);
-                fragment = new SceneFragment(scene);
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.setCustomAnimations(enter, exit, popEnter, popExit);
-                fragmentTransaction.add(((ViewGroup) this.getParent()).getId(), fragment, key);
-                if (nextCrumb > 0) {
-                    if (prevFragment == null)
-                        prevFragment = fragmentManager.findFragmentByTag(keys.getString(nextCrumb - 1));
-                    fragmentTransaction.hide(prevFragment);
-                }
+                fragmentTransaction.replace(((ViewGroup) this.getParent()).getId(), new SceneFragment(scene), key);
                 fragmentTransaction.addToBackStack(String.valueOf(nextCrumb));
                 fragmentTransaction.commit();
-                prevFragment = fragment;
             }
         }
         if (crumb == currentCrumb && !oldKey.equals(keys.getString(crumb))) {
