03def180c04e24a23a0544bcf7978dc07e27573b
Graham Mendick <graham.mendick@gmail.com>
2019-09-25 13:37:47 +0100

    Passed selected prop to TabBarItem

--- NavigationReactNative/src/TabBarIOS.tsx
+++ NavigationReactNative/src/TabBarIOS.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { requireNativeComponent, StyleSheet } from 'react-native';
 
-class TabBar extends React.Component {
+class TabBar extends React.Component<any, any> {
     constructor(props) {
         super(props);
         this.state = {selectedTab: 0};
     }
     render() {
+        var {children, ...props} = this.props;
         return (
             <NVTabBar
-                {...this.props}
+                {...props}
                 onTabSelected={({nativeEvent}) => {
                     this.setState({selectedTab: nativeEvent.tab})
                 }}
-                style={styles.tabBar} />
+                style={styles.tabBar}>
+                    {React.Children.map(children, (child: any, index) => {
+                        var childProps = {...child.props, selected: index === this.state.selectedTab};
+                        return React.cloneElement(child, childProps);
+                    })}
+            </NVTabBar>
         );
     }
 }
