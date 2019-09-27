cccee8c72224320ec651753af9232542d61edc04
Graham Mendick <graham.mendick@gmail.com>
2019-09-27 12:45:50 +0100

    Changed error message

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabBarManager.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabBarManager.java
@@ -39,7 +39,7 @@ public class TabBarManager extends ViewGroupManager<TabBarView> {
 
     @Override
     public void removeViewAt(TabBarView parent, int index) {
-        throw new UnsupportedOperationException("Removing a tab isn't supported");
+        throw new UnsupportedOperationException("The TabBar doesn't support removing a TabBarItem");
     }
 
     @Override
