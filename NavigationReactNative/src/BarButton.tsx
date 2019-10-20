a567fb0bd9a654d07c5aba107fa5f33c82451f6e
emerson233 <42024893+emerson233@users.noreply.github.com>
2019-10-20 11:19:45 +0800

    Update coding style.

--- NavigationReactNative/src/BarButton.tsx
+++ NavigationReactNative/src/BarButton.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { requireNativeComponent, Image, Platform } from 'react-native';
 
-class NavigationBarButton extends React.Component<any, any> {
-  render() {
-    if (Platform.OS === "android") {
-      return null;
-    }
-    const nativeProps = {...this.props};
-    if (this.props.icon) {
-      nativeProps.icon = Image.resolveAssetSource(this.props.icon);
-    }
-
-    return (
-      <NVBarButton {...nativeProps}>
-        {this.props.children}
-      </NVBarButton>
-    )
-  }
-}
 var NVBarButton = requireNativeComponent<any>('NVBarButton', null);
-
+var NavigationBarButton = Platform.OS === 'ios' ? ({image, children, ...otherProps}) => (
+    <NVBarButton
+        image={Image.resolveAssetSource(image)} 
+        {...otherProps}>
+        {children}
+    </NVBarButton>
+) : () => null;
 export default NavigationBarButton;
--- NavigationReactNative/src/NavigationBar.tsx
+++ NavigationReactNative/src/NavigationBar.tsx
@@ -1,74 +1,39 @@
-import React, { ReactChild, ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import { requireNativeComponent, Image, Platform } from 'react-native';
 import LeftBar from './LeftBar';
 import RightBar from './RightBar';
 
-class NavigationBar extends React.Component<any, any> {
-  render() {
-    if (Platform.OS == "android" && this.props.hidden) {
-      return null
-    }
-
-    var nativeProps = {...this.props};
-    if (this.props.logo) {
-      nativeProps.logo = Image.resolveAssetSource(this.props.logo);
-    }
-    if (this.props.navIcon) {
-      nativeProps.navIcon = Image.resolveAssetSource(this.props.navIcon);
-    }
-    if (this.props.overflowIcon) {
-      nativeProps.overflowIcon = Image.resolveAssetSource(this.props.overflowIcon);
-    }
-
-    if (Platform.OS === 'android') {
-      var {children} = this.props;
-
-      var menuItems = React.Children.toArray(children)
-        .filter((child: ReactElement<any>) => {
-          var type = child.type;
-          return (type === LeftBar || type === RightBar)
-        })
-        .sort((a: ReactElement<any>, b: ReactElement<any>) => {
-          if (a.type === b.type) {
-            return 0
-          }
-          if (a.type === RightBar) {
-            return 1
-          }
-          return -1
-        })
-        .map((child: ReactElement<any>) => (
-          React.Children.toArray(child.props.children)
-            .map((child: ReactElement<any>) => ({
-              ...child.props,
-              image: Image.resolveAssetSource(child.props.image),
-            })
-          )
-        ))
-      
-      // flatten items `[[item]]` -> `[item]`
-      menuItems = [].concat.apply([], menuItems)
-      nativeProps.menuItems = menuItems;
-      return (
-        <NVNavigationBar {...nativeProps} style={{ height: 50 }} onActionSelected={(event) => {
-          var position = event.nativeEvent.position
-          var onPress = nativeProps.menuItems[position].onPress
-          if (onPress) {
-            onPress()
-          }
-        }} >
-          {this.props.children}
-        </NVNavigationBar>
-      )
-    } else {
-      return (
-        <NVNavigationBar {...nativeProps}>
-          {this.props.children}
-        </NVNavigationBar>
-      )
-    }
-
-  }
-}
 var NVNavigationBar = requireNativeComponent<any>('NVNavigationBar', null);
+var NavigationBar = ({hidden, logo, navIcon, overflowIcon, children, ...otherProps}) => {
+    if (Platform.OS === 'android' && hidden)
+        return null
+    var menuItems = (React.Children.toArray(children) as ReactElement<any>[])
+        .filter(({type}) => (type === LeftBar || type === RightBar))
+        .sort((a, b) => (a.type === b.type ? 0 : (a.type === RightBar ? 1 : -1)))
+        .map(({props}) => (
+            (React.Children.toArray(props.children) as ReactElement<any>[])
+                .map(({props}) => ({
+                    ...props,
+                    image: Image.resolveAssetSource(props.image),
+                })
+            )
+        ))
+        .reduce((a, b) => a.concat(b), []);
+    return (
+        <NVNavigationBar
+            menuItems={menuItems}
+            logo={Image.resolveAssetSource(logo)}
+            navIcon={Image.resolveAssetSource(navIcon)}
+            overflowIcon = {Image.resolveAssetSource(overflowIcon)}
+            style={{ height: 50 }}
+            {...otherProps}
+            onActionSelected={({nativeEvent}) => {
+                var onPress = menuItems[nativeEvent.position].onPress;
+                if (onPress)
+                    onPress();
+        }}>
+            {children}
+        </NVNavigationBar>
+    )
+}
 export default NavigationBar;
