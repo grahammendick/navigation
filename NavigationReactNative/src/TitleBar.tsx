4b890becd29cf554519f00ac2938c120c90ed91f
Graham Mendick <graham.mendick@gmail.com>
2019-11-06 22:43:18 +0000

    Hid search button on ios

--- NavigationReactNative/src/TitleBar.tsx
+++ NavigationReactNative/src/TitleBar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { requireNativeComponent, StyleSheet, Platform } from 'react-native';
 
-const NVTitleBar = requireNativeComponent<any>('NVTitleBar', null)
-
 const TitleBar = ({style, ...props}) => (
   <NVTitleBar {...props} style={Platform.OS === 'ios' ? [styles.titleBar, style] : style}/>
 )
+
+const NVTitleBar = requireNativeComponent<any>('NVTitleBar', null)
 
 const styles = StyleSheet.create({
   titleBar: {
