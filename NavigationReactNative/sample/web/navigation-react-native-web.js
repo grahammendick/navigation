cc4a6fdf29781675bdd24a96a075e7d1d8fe8793
Graham Mendick <graham.mendick@gmail.com>
2019-12-03 11:17:49 +0000

    Installed react native web packages

--- NavigationReactNative/sample/web/navigation-react-native-web.js
+++ NavigationReactNative/sample/web/navigation-react-native-web.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
 import {NavigationContext} from 'navigation-react';
-import {SharedElement} from 'navigation-react-mobile';
+import {SharedElement as SharedElementWeb} from 'navigation-react-mobile';
 
 class NavigationBar extends React.Component {
   constructor(props) {
@@ -34,19 +34,19 @@ const NavigationBarIOS = props => (
 const RightBarIOS = () => null;
 const BarButtonIOS = () => null;
 
-class SharedElementAndroid extends React.Component{
+class SharedElement extends React.Component{
   setNativeProps() {
   }
   render() {
     const {style, transition, children, ...props} = this.props;
     return (
       <View style={style}>
-        <SharedElement {...props}>
+        <SharedElementWeb {...props}>
           <div style={{display: 'flex', flex: 1}}>{children}</div>
-        </SharedElement>
+        </SharedElementWeb>
       </View>
     );
   }
 }
 
-export { NavigationBarIOS, RightBarIOS, BarButtonIOS, SharedElementAndroid };
+export { NavigationBarIOS, RightBarIOS, BarButtonIOS, SharedElement };
