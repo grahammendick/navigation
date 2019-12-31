a75cad8995185e7f40da7d5f36d7850d414975cc
Graham Mendick <graham.mendick@gmail.com>
2019-12-31 16:10:58 +0000

    Added setup method to tab view interface

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabNavigationView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabNavigationView.java
@@ -2,11 +2,18 @@ package com.navigation.reactnative;
 
 import android.content.Context;
 
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TabNavigationView extends BottomNavigationView implements TabView {
 
     public TabNavigationView(Context context) {
         super(context);
+    }
+
+    @Override
+    public void setupWithViewPager(@Nullable ViewPager viewPager) {
     }
 }
--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabView.java
@@ -1,4 +1,8 @@
 package com.navigation.reactnative;
 
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
 public interface TabView {
+    void setupWithViewPager(@Nullable ViewPager viewPager);
 }
