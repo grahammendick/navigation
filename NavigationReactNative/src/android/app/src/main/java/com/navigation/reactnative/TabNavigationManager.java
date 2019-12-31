c44c9f480c88edf4fe8b6f97d15abb7410bc2b73
Graham Mendick <graham.mendick@gmail.com>
2019-12-31 15:56:24 +0000

    Added bottom navigation view and manager classes

---
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabNavigationManager.java
@@ -0,0 +1,21 @@
+package com.navigation.reactnative;
+
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
+
+import javax.annotation.Nonnull;
+
+public class TabNavigationManager extends ViewGroupManager<TabNavigationView> {
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    protected TabNavigationView createViewInstance(@Nonnull ThemedReactContext reactContext) {
+        return new TabNavigationView(reactContext);
+    }
+}
---
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/TabNavigationView.java
@@ -0,0 +1,12 @@
+package com.navigation.reactnative;
+
+import android.content.Context;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class TabNavigationView extends BottomNavigationView {
+
+    public TabNavigationView(Context context) {
+        super(context);
+    }
+}
