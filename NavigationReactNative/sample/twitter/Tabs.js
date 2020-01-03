5d3d145d8deb9b5732b2d53b8bd40d3582cdcdc0
Graham Mendick <graham.mendick@gmail.com>
2020-01-03 17:03:53 +0000

    Removed badge because not supported on Android

--- NavigationReactNative/sample/twitter/Tabs.js
+++ NavigationReactNative/sample/twitter/Tabs.js
@@ -1,21 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {TabBar, TabBarItem} from 'navigation-react-native';
 import Home from './Home';
 import Notifications from './Notifications';
 
-export default ({tweets, follows}) => {
-  const [notified, setNotified] = useState(false);
-  return (
-    <TabBar bottomTabs={true} swipeable={false}>
-      <TabBarItem title="Home">
-        <Home tweets={tweets} follows={follows} />
-      </TabBarItem>
-      <TabBarItem
-        title="Notifications"
-        badge={!notified ? follows.length : null}
-        onPress={() => {setNotified(true)}}>
-        <Notifications follows={follows} />
-      </TabBarItem>
-    </TabBar>
-  );
-};
+export default ({tweets, follows}) => (
+  <TabBar bottomTabs={true} swipeable={false}>
+    <TabBarItem title="Home">
+      <Home tweets={tweets} follows={follows} />
+    </TabBarItem>
+    <TabBarItem title="Notifications">
+      <Notifications follows={follows} />
+    </TabBarItem>
+  </TabBar>
+);
