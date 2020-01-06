f039054fd1f1f2f2ec4398f3f0622675fa46f5b3
Graham Mendick <graham.mendick@gmail.com>
2020-01-06 13:42:58 +0000

    Refactored out IconImageInfo

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/IconResolver.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/IconResolver.java
@@ -15,30 +15,34 @@ import com.facebook.drawee.drawable.ForwardingDrawable;
 import com.facebook.imagepipeline.core.ImagePipeline;
 import com.facebook.imagepipeline.image.CloseableBitmap;
 import com.facebook.imagepipeline.image.CloseableImage;
-import com.facebook.imagepipeline.image.ImageInfo;
-import com.facebook.imagepipeline.image.QualityInfo;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.PixelUtil;
 
 class IconResolver {
-    private static class DrawableWithIntrinsicSize extends ForwardingDrawable implements Drawable.Callback {
-        private final ImageInfo imageInfo;
+    private static final String PROP_ICON_URI = "uri";
+    private static final String PROP_ICON_WIDTH = "width";
+    private static final String PROP_ICON_HEIGHT = "height";
 
-        DrawableWithIntrinsicSize(Drawable drawable, ImageInfo imageInfo) {
+    private static class DrawableWithIntrinsicSize extends ForwardingDrawable implements Drawable.Callback {
+        private int width;
+        private int height;
+
+        DrawableWithIntrinsicSize(Drawable drawable, ReadableMap source) {
             super(drawable);
-            this.imageInfo = imageInfo;
+            width = Math.round(PixelUtil.toPixelFromDIP(source.getInt(PROP_ICON_WIDTH)));
+            height = Math.round(PixelUtil.toPixelFromDIP(source.getInt(PROP_ICON_HEIGHT)));
         }
 
         @Override
         public int getIntrinsicWidth() {
-            return imageInfo.getWidth();
+            return width;
         }
 
         @Override
         public int getIntrinsicHeight() {
-            return imageInfo.getHeight();
+            return height;
         }
     }
 
@@ -46,42 +50,13 @@ class IconResolver {
         void setDrawable(Drawable d);
     }
 
-    private static class IconImageInfo implements ImageInfo {
-        private int width;
-        private int height;
-
-        IconImageInfo(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
-
-        @Override
-        public int getWidth() {
-            return width;
-        }
-
-        @Override
-        public int getHeight() {
-            return height;
-        }
-
-        @Override
-        public QualityInfo getQualityInfo() {
-            return null;
-        }
-
-    }
-    
-    static void setIconSource(ReadableMap source, final IconResolverListener iconResolverListener, final Context context) {
+    static void setIconSource(final ReadableMap source, final IconResolverListener iconResolverListener, final Context context) {
         String uri = source != null ? source.getString(PROP_ICON_URI) : null;
         if (uri == null) {
             iconResolverListener.setDrawable(null);
         } else if (uri.startsWith("http://") || uri.startsWith("https://") || uri.startsWith("file://")) {
             ImagePipeline imagePipeline = Fresco.getImagePipeline();
             ImageRequestBuilder builder = ImageRequestBuilder.newBuilderWithSource(Uri.parse(uri));
-
-            final ImageInfo imageInfo = getIconImageInfo(source);
-
             ImageRequest request = builder.build();
             DataSource<CloseableReference<CloseableImage>> dataSource = imagePipeline.fetchDecodedImage(request, context);
             dataSource.subscribe(new BaseDataSubscriber<CloseableReference<CloseableImage>>() {
@@ -90,18 +65,16 @@ class IconResolver {
                     if (!dataSource.isFinished()) {
                         return;
                     }
-
                     CloseableReference<CloseableImage> imageRef = dataSource.getResult();
                     if (imageRef != null) {
                         CloseableImage image = imageRef.get();
                         try {
-
                             if (image instanceof CloseableBitmap) {
                                 Bitmap bitmap = (((CloseableBitmap) image).getUnderlyingBitmap());
                                 if (bitmap != null && !bitmap.isRecycled()) {
                                     Bitmap bitmapCopy = bitmap.copy(bitmap.getConfig(), true);
                                     Drawable drawable = new BitmapDrawable(context.getResources(), bitmapCopy);
-                                    DrawableWithIntrinsicSize sizedDrawable = new DrawableWithIntrinsicSize(drawable, imageInfo);
+                                    DrawableWithIntrinsicSize sizedDrawable = new DrawableWithIntrinsicSize(drawable, source);
                                     iconResolverListener.setDrawable(sizedDrawable);
                                 }
                             }
@@ -131,20 +104,6 @@ class IconResolver {
         int drawableResId = getDrawableResourceByName(name, context);
         if (drawableResId != 0) {
             return context.getResources().getDrawable(getDrawableResourceByName(name, context));
-        } else {
-            return null;
-        }
-    }
-
-    private static final String PROP_ICON_URI = "uri";
-    private static final String PROP_ICON_WIDTH = "width";
-    private static final String PROP_ICON_HEIGHT = "height";
-
-    private static IconImageInfo getIconImageInfo(ReadableMap source) {
-        if (source.hasKey(PROP_ICON_WIDTH) && source.hasKey(PROP_ICON_HEIGHT)) {
-            final int width = Math.round(PixelUtil.toPixelFromDIP(source.getInt(PROP_ICON_WIDTH)));
-            final int height = Math.round(PixelUtil.toPixelFromDIP(source.getInt(PROP_ICON_HEIGHT)));
-            return new IconImageInfo(width, height);
         } else {
             return null;
         }
