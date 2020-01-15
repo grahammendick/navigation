a63e02b13327a6118cca80af4405ab08b8e932ce
Graham Mendick <graham.mendick@gmail.com>
2020-01-15 11:16:17 +0000

    Made coordinatorlayout act like <>...</> on ios

--- NavigationReactNative/src/CoordinatorLayout.tsx
+++ NavigationReactNative/src/CoordinatorLayout.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { requireNativeComponent, Platform } from 'react-native';
 
 const CoordinatorLayout = ({children}) => (
-    Platform.OS === 'android' && <NVCoordinatorLayout style={{flex: 1}}>{children}</NVCoordinatorLayout>
+    <NVCoordinatorLayout style={{flex: 1}}>{children}</NVCoordinatorLayout>
 );
 
 const NVCoordinatorLayout = requireNativeComponent<any>('NVCoordinatorLayout', null)
 
-export default CoordinatorLayout;
+export default Platform.OS === 'android' ? CoordinatorLayout : ({children}) => children;
 
