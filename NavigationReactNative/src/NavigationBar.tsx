3c6cf26416bb87229c312813d682da7ccb464645
Graham Mendick <graham.mendick@gmail.com>
2020-01-15 19:27:59 +0000

    Styled height of collapsing bar

--- NavigationReactNative/src/NavigationBar.tsx
+++ NavigationReactNative/src/NavigationBar.tsx
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 import TitleBar from './TitleBar';
 import CollapsingBar from './CollapsingBar';
 
-var NavigationBar = ({hidden, logo, navigationImage, overflowImage, scrollFlags = [], children, style, ...otherProps}) => {
+var NavigationBar = ({hidden, logo, navigationImage, overflowImage, height = 56, scrollFlags = [], children, style, ...otherProps}) => {
     if (Platform.OS === 'android' && hidden)
         return null
     var constants = (UIManager as any).getViewManagerConfig('NVNavigationBar').Constants;
@@ -28,17 +28,21 @@ var NavigationBar = ({hidden, logo, navigationImage, overflowImage, scrollFlags 
     var collapsingBar = childrenArray.find(({type}) => type === CollapsingBar);
     var Container = ({children}) => (
         !collapsingBar ? children :
-            <CollapsingBar title={otherProps.title} scrollFlags={flags}>{children}</CollapsingBar>
+            <CollapsingBar
+                style={{height}}
+                title={otherProps.title}
+                scrollFlags={flags}>
+                {collapsingBar.props.children}
+                {children}
+            </CollapsingBar>
     )
     return (
         <>
             <NVNavigationBar
                 hidden={hidden}
-                {...otherProps}
-                style={Platform.OS === 'android' ? {height: 56} : undefined}>
+                {...otherProps}>
                 {Platform.OS === 'ios' ? children :
                     <Container>
-                        {collapsingBar && collapsingBar.props.children}
                         <NVToolbar
                             menuItems={menuItems}
                             logo={Image.resolveAssetSource(logo)}
