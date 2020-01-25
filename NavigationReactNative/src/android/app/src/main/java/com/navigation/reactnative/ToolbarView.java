2220067ba0b4f01c5ee64323a2188c1ba5562fc3
Graham Mendick <graham.mendick@gmail.com>
2020-01-25 13:03:30 +0000

    Moved scroll flags setting into setPin of manager

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/ToolbarView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/ToolbarView.java
@@ -37,9 +37,7 @@ public class ToolbarView extends Toolbar {
 
     public ToolbarView(Context context) {
         super(context);
-        AppBarLayout.LayoutParams params = new AppBarLayout.LayoutParams(AppBarLayout.LayoutParams.MATCH_PARENT, AppBarLayout.LayoutParams.MATCH_PARENT);
-        params.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL | AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS);
-        setLayoutParams(params);
+        setLayoutParams(new AppBarLayout.LayoutParams(AppBarLayout.LayoutParams.MATCH_PARENT, AppBarLayout.LayoutParams.MATCH_PARENT));
         defaultTitleTextColor = getDefaultTitleTextColor();
         defaultOverflowIcon = getOverflowIcon();
         logoResolverListener = new IconResolver.IconResolverListener() {
