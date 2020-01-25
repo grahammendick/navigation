a4c04111b19c276be495a8aca3b0ed7404839b5f
Graham Mendick <graham.mendick@gmail.com>
2020-01-25 12:32:38 +0000

    Ensured coordinator scroll when collapsed content short
    This took me about a week to work out! When the collpased content is shorter than the coordinator layout height then it doesn't scroll at all! That means some content can't be seen. Don't know how realistic this scenario is because the content should be long enough. In this case need to measure the gap between the collpased content and coordinator height and subract that from the measure spec. Add 2 to the coordinator height to ensure that the collapsed content clears the height

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/CoordinatorLayoutView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/CoordinatorLayoutView.java
@@ -1,8 +1,11 @@
 package com.navigation.reactnative;
 
 import android.content.Context;
+import android.widget.ScrollView;
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.facebook.react.uimanager.PixelUtil;
 
 public class CoordinatorLayoutView extends CoordinatorLayout {
 
@@ -26,4 +29,16 @@ public class CoordinatorLayoutView extends CoordinatorLayout {
             layout(getLeft(), getTop(), getRight(), getBottom());
         }
     };
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int contentHeight = ((ScrollView) getChildAt(1)).getChildAt(0).getHeight();
+        boolean collapsingBar = ((NavigationBarView) getChildAt(0)).getChildAt(0) instanceof CollapsingBarView;
+        int navigationBarHeight = collapsingBar ? (int) PixelUtil.toPixelFromDIP(56) : 0;
+        int collapsedGap = height + 2 - contentHeight - navigationBarHeight;
+        if (collapsedGap > 0)
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(heightMeasureSpec) - collapsedGap, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 }
