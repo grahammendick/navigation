b02e6f4bf349eb39f4e864896dfe59d1499c7d42
Graham Mendick <graham.mendick@gmail.com>
2020-01-26 12:03:56 +0000

    Tweaked to match real twitter app

--- NavigationReactNative/sample/twitter/Timeline.js
+++ NavigationReactNative/sample/twitter/Timeline.js
@@ -8,7 +8,7 @@ export default ({timeline: {id, name, username, logo, bio,
   followers, following, tweets}}) => {
   const {stateNavigator} = useContext(NavigationContext);
   return (
-    <CoordinatorLayout overlapTop={100}>
+    <CoordinatorLayout overlapTop={110}>
       <NavigationBar
         title={name}
         navigationImage={require('./arrow.png')}
