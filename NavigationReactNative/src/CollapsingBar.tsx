0d4e1b61be42db475f7aa1b430e2cd6311779704
Graham Mendick <graham.mendick@gmail.com>
2020-01-27 22:48:19 +0000

    Used title color for expanded to match ios
    If bring in large title color then need it to work on iOS and android

--- NavigationReactNative/src/CollapsingBar.tsx
+++ NavigationReactNative/src/CollapsingBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { requireNativeComponent, Platform } from 'react-native';
 
-var CollapsingBar: any = ({largeTitle = false, barTintColor, titleColor, largeTitleColor, ...props}) => (
+var CollapsingBar: any = ({largeTitle = false, barTintColor, titleColor, style, ...props}) => (
     <NVCollapsingBar
         titleEnabled={largeTitle}
         contentScrimColor={barTintColor}
         collapsedTitleColor={titleColor}
-        expandedTitleColor={largeTitleColor}
+        expandedTitleColor={titleColor}
         {...props} />
 )
 
