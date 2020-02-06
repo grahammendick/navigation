24c0c56a629b2895084bb429634dac82c487125a
Graham Mendick <graham.mendick@gmail.com>
2020-02-06 22:45:28 +0000

    Hard-coded the search to tab layout
    Don't support other use-cases yet

--- NavigationReactNative/src/NavigationBar.tsx
+++ NavigationReactNative/src/NavigationBar.tsx
@@ -5,6 +5,7 @@ import RightBar from './RightBar';
 import SearchBar from './SearchBar';
 import TitleBar from './TitleBar';
 import CollapsingBar from './CollapsingBar';
+import TabLayout from './TabLayout';
 
 var NavigationBar: any = ({hidden, logo, navigationImage, overflowImage, children, style = {}, ...otherProps}) => {
     if (Platform.OS === 'android' && hidden)
@@ -52,9 +53,7 @@ var NavigationBar: any = ({hidden, logo, navigationImage, overflowImage, childre
                             }}>
                             {childrenArray.find(({type}) => type === TitleBar)}
                         </NVToolbar>
-                        {childrenArray.filter(({type}) => (
-                            ![CollapsingBar, TitleBar, SearchBar].find(el => el === type))
-                        )}
+                        {childrenArray.find(({type}) => type === TabLayout)}
                     </Container>}
             </NVNavigationBar>
             {Platform.OS === 'ios' ? null : childrenArray.find(({type}) => type === SearchBar)}
