8ce748b3a713d14d08c0abb3461024d83087c82b
Graham Mendick <graham.mendick@gmail.com>
2020-02-11 11:09:53 +0000

    Revert "Calculated scroll y drag amount"
    This reverts commit 0eafeadea804436efe7e81efc6095a4153bb8cbc.

--- NavigationReactNative/src/CoordinatorLayout.tsx
+++ NavigationReactNative/src/CoordinatorLayout.tsx
@@ -5,30 +5,9 @@ import SearchBar from './SearchBar';
 
 class CoordinatorLayout extends React.Component<any, any> {
     private ref: React.RefObject<any>;
-    private dragging = false;
-    private y = 0;
     constructor(props) {
         super(props);
         this.ref = React.createRef<any>();
-        this.onTouchStart = this.onTouchStart.bind(this);
-        this.onTouchMove = this.onTouchMove.bind(this);
-        this.onTouchEnd = this.onTouchEnd.bind(this);
-        this.onTouchCancel = this.onTouchCancel.bind(this);
-    }
-    onTouchStart({nativeEvent}) {
-        this.dragging = true;
-        this.y = nativeEvent.locationY;
-    }
-    onTouchMove({nativeEvent}) {
-        if (this.dragging) {
-            var deltaY = nativeEvent.locationY - this.y;
-        }
-    }
-    onTouchEnd() {
-        this.dragging = false;
-    }
-    onTouchCancel() {        
-        this.dragging = false;
     }
     render() {
         var {overlap, children} = this.props;
@@ -48,10 +27,9 @@ class CoordinatorLayout extends React.Component<any, any> {
                     ref={this.ref}
                     overlap={overlap}
                     style={{flex: 1}}
-                    onTouchStart={this.onTouchStart}
-                    onTouchMove={this.onTouchMove}
-                    onTouchEnd={this.onTouchEnd}
-                    onTouchCancel={this.onTouchCancel}>
+                    onTouchStart={e => {
+                        console.log(this.ref);
+                    }}>
                     {clonedChildren}
                 </NVCoordinatorLayout>
                 {searchBar}
