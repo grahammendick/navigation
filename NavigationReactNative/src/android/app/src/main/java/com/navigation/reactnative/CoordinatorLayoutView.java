4f2acb938bc3c4132a355f04a8c176bdd1778230
Graham Mendick <graham.mendick@gmail.com>
2020-02-11 09:27:13 +0000

    Tracked if scroll content tall enough
    When scroll content is too short then nested scrolling doesn't happen. Need to handle scrolling in javascript instead

--- NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/CoordinatorLayoutView.java
+++ NavigationReactNative/src/android/app/src/main/java/com/navigation/reactnative/CoordinatorLayoutView.java
@@ -1,6 +1,7 @@
 package com.navigation.reactnative;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.ScrollView;
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -9,6 +10,7 @@ import com.facebook.react.uimanager.PixelUtil;
 
 public class CoordinatorLayoutView extends CoordinatorLayout {
     int overlap = 0;
+    boolean nestedScrolling = false;
 
     public CoordinatorLayoutView(Context context){
         super(context);
@@ -45,6 +47,18 @@ public class CoordinatorLayoutView extends CoordinatorLayout {
                 return (ScrollView) getChildAt(i);
         }
         return null;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(View child, View target, int axes, int type) {
+        nestedScrolling = true;
+        return super.onStartNestedScroll(child, target, axes, type);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        nestedScrolling = false;
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     /*@Override
