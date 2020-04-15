182e77fbb5c0bc2ee45f56a18e43aa565d0aec1b
Graham Mendick <graham.mendick@gmail.com>
2020-04-15 16:06:30 +0100

    Showed the selected child view and hid others

--- NavigationReactNative/src/ios/NVSegmentedControlView.m
+++ NavigationReactNative/src/ios/NVSegmentedControlView.m
@@ -5,6 +5,14 @@
     NSInteger _selectedIndex;
 }
 
+- (id)init
+{
+    if (self = [super init]) {
+        [self addTarget:self action:@selector(tabPressed) forControlEvents:UIControlEventValueChanged];
+    }
+    return self;
+}
+
 - (void)setTitles:(NSArray<NSString *> *)titles
 {
 
@@ -13,6 +21,26 @@
         [self insertSegmentWithTitle:title atIndex:self.numberOfSegments animated:NO];
     }
     self.selectedSegmentIndex = _selectedIndex;
+}
+
+- (void)didMoveToWindow
+{
+    [super didMoveToWindow];
+    [self selectTab];
+}
+
+- (void)tabPressed
+{
+    _selectedIndex = self.selectedSegmentIndex;
+    [self selectTab];
+}
+
+- (void)selectTab
+{
+    UIView* tabBar = [self.superview.subviews objectAtIndex:1];
+    for(NSInteger i = 0; i < [tabBar.subviews count]; i++) {
+        [tabBar.subviews objectAtIndex:i].alpha = (i == _selectedIndex ? 1 : 0);
+    }
 }
 
 @end
