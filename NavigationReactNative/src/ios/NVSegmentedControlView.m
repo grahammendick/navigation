4a55e31c7d6c278f11e39fba300d73ec62c1c51b
Graham Mendick <graham.mendick@gmail.com>
2020-04-15 09:48:33 +0100

    Initialised selected segment index

--- NavigationReactNative/src/ios/NVSegmentedControlView.m
+++ NavigationReactNative/src/ios/NVSegmentedControlView.m
@@ -1,13 +1,18 @@
 #import "NVSegmentedControlView.h"
 
 @implementation NVSegmentedControlView
+{
+    NSInteger _selectedIndex;
+}
 
 - (void)setTitles:(NSArray<NSString *> *)titles
 {
-  [self removeAllSegments];
-  for (NSString *title in titles) {
-    [self insertSegmentWithTitle:title atIndex:self.numberOfSegments animated:NO];
-  }
+
+    [self removeAllSegments];
+    for (NSString *title in titles) {
+        [self insertSegmentWithTitle:title atIndex:self.numberOfSegments animated:NO];
+    }
+    self.selectedSegmentIndex = _selectedIndex;
 }
 
 @end
