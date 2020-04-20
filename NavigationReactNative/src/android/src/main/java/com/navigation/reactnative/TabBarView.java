9767c664d32b612c95f18bb2f77d8179e29246b8
Graham Mendick <graham.mendick@gmail.com>
2020-04-20 08:38:59 +0100

    Removed unused import

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabBarView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabBarView.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
