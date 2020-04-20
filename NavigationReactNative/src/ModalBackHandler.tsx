be7e97f9d57a816f5f36acaf88f95b9131bc32af
Graham Mendick <graham.mendick@gmail.com>
2020-04-20 17:20:16 +0100

    Future-proofed modal hand back on ios
    On ios, the onRequestClose can be called when showing modal with pageSheet and dismissed with a swipe. Thought this fix was in already, https://github.com/facebook/react-native/commit/8e5fac89bbdcc3028bb5d81a358969a235abf991, but it's not working. Adding the check so it skips on ios when the fix lands

--- NavigationReactNative/src/ModalBackHandler.tsx
+++ NavigationReactNative/src/ModalBackHandler.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import BackHandlerContext from './BackHandlerContext';
 import createBackHandler from './createBackHandler';
 
@@ -6,12 +7,16 @@ class ModalBackHandler extends React.Component<any> {
     private backHandler: any;
     constructor(props) {
         super(props);
+        this.handleBack = this.handleBack.bind(this);
         this.backHandler = createBackHandler();
+    }
+    handleBack() {
+        return Platform.OS === "android" && this.backHandler.handleBack();
     }
     render() {
         return (
             <BackHandlerContext.Provider value={this.backHandler}>
-                {(this.props as any).children(this.backHandler.handleBack)}
+                {(this.props as any).children(this.handleBack)}
             </BackHandlerContext.Provider>
         );
     }
