4ccd72048a5d1ba53b255c102a49d9c3d859131f
Graham Mendick <graham.mendick@gmail.com>
2020-04-29 13:52:08 +0100

    Removed history from build start url in zoom sample

--- NavigationReactMobile/sample/zoom/createStateNavigator.js
+++ NavigationReactMobile/sample/zoom/createStateNavigator.js
@@ -7,9 +7,9 @@ import Detail from './Detail';
 export default () => {
   const buildStartUrl = url => {
     const {state, data} = stateNavigator.parseLink(url);
-    let fluent = stateNavigator.fluent().navigate('grid');
-    stateNavigator.historyManager.addHistory(fluent.url, true);
-    return fluent.navigate(state.key, data).url;
+    return stateNavigator.fluent()
+      .navigate('grid')
+      .navigate(state.key, data).url;
   };
 
   const stateNavigator = new StateNavigator([
