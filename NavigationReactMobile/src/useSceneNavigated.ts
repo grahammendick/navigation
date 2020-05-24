4549860a5026d172bb6ccd3190fd6c4831e32824
Graham Mendick <graham.mendick@gmail.com>
2020-05-24 23:35:45 +0100

    Removed unused import

--- NavigationReactMobile/src/useSceneNavigated.ts
+++ NavigationReactMobile/src/useSceneNavigated.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useRef } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { NavigationContext } from 'navigation-react';
 
 var useSceneNavigated = (handler: () => void) => {
