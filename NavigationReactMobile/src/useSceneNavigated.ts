39b37433d0b6705de434538a93a800d961f91e00
Graham Mendick <graham.mendick@gmail.com>
2020-05-24 22:02:17 +0100

    Removed extra space

--- NavigationReactMobile/src/useSceneNavigated.ts
+++ NavigationReactMobile/src/useSceneNavigated.ts
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useMemo, useRef } from 'react';
 import { NavigationContext } from 'navigation-react';
 
-var useSceneNavigated  = (handler: () => void) => {
+var useSceneNavigated = (handler: () => void) => {
     var prevContext = useRef(null);
     var navigationEvent = useContext(NavigationContext);
     useMemo(() => {
