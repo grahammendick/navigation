9753cf5b982d942189d99e0a051f0ceaf5d02a30
Graham Mendick <graham.mendick@gmail.com>
2020-05-25 20:57:16 +0100

    Tested navigating hook for A -> B
    This shouldn't call the hook because the navigation's already happened

--- NavigationReactMobile/test/UseSceneNavigatingTest.tsx
+++ NavigationReactMobile/test/UseSceneNavigatingTest.tsx
@@ -49,4 +49,49 @@ describe('UseSceneNavigating', function () {
             }
         })
     });
+
+    describe('A -> B', function () {
+        it('should not call navigating hook on on A or B', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 'sceneA' },
+                { key: 'sceneB', trackCrumbTrail: true }
+            ]);
+            stateNavigator.navigate('sceneA');
+            stateNavigator.navigate('sceneB');
+            var {sceneA, sceneB} = stateNavigator.states;
+            var navigatingA, navigatingB;
+            var SceneA = () => {
+                useSceneNavigating(() => {
+                    navigatingA = true;
+                })
+                return <div />;
+            };
+            var SceneB = () => {
+                useSceneNavigating(() => {
+                    navigatingB = true;
+                })
+                return <div />;
+            };
+            sceneA.renderScene = () => <SceneA />;
+            sceneB.renderScene = () => <SceneB />;
+            var container = document.createElement('div');
+            act(() => {
+                navigatingA = navigatingB = false;
+                ReactDOM.render(
+                    <NavigationHandler stateNavigator={stateNavigator}>
+                        <NavigationMotion>
+                            {(_style, scene, key) =>  <div key={key}>{scene}</div>}
+                        </NavigationMotion>
+                    </NavigationHandler>,
+                    container
+                );
+            });
+            try {
+                assert.equal(navigatingA, false);
+                assert.equal(navigatingB, false);
+            } finally {
+                ReactDOM.unmountComponentAtNode(container);
+            }
+        })
+    });
 });
