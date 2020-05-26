560835f273e364b0ea0e5a92cd4f390deea631dc
Graham Mendick <graham.mendick@gmail.com>
2020-05-26 17:45:24 +0100

    Tested unloading hook for A -> B

--- NavigationReactMobile/test/UseSceneUnloadingTest.tsx
+++ NavigationReactMobile/test/UseSceneUnloadingTest.tsx
@@ -50,4 +50,51 @@ describe('UseSceneUnloading', function () {
             }
         })
     });
+
+    describe('A -> B', function () {
+        it('should not call unloading hook on on A or B', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 'sceneA' },
+                { key: 'sceneB', trackCrumbTrail: true }
+            ]);
+            stateNavigator.navigate('sceneA');
+            stateNavigator.navigate('sceneB');
+            var {sceneA, sceneB} = stateNavigator.states;
+            var unloadingA, unloadingB;
+            var SceneA = () => {
+                useSceneUnloading(() => {
+                    unloadingA = true;
+                    return true;
+                })
+                return <div />;
+            };
+            var SceneB = () => {
+                useSceneUnloading(() => {
+                    unloadingB = true;
+                    return true;
+                })
+                return <div />;
+            };
+            sceneA.renderScene = () => <SceneA />;
+            sceneB.renderScene = () => <SceneB />;
+            var container = document.createElement('div');
+            act(() => {
+                unloadingA = unloadingB = false;
+                ReactDOM.render(
+                    <NavigationHandler stateNavigator={stateNavigator}>
+                        <NavigationMotion>
+                            {(_style, scene, key) =>  <div key={key}>{scene}</div>}
+                        </NavigationMotion>
+                    </NavigationHandler>,
+                    container
+                );
+            });
+            try {
+                assert.equal(unloadingA, false);
+                assert.equal(unloadingB, false);
+            } finally {
+                ReactDOM.unmountComponentAtNode(container);
+            }
+        })
+    });
 });
