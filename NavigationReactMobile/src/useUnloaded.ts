fb288567da0d7d7cf848521a1ad949900b6a2e94
Graham Mendick <graham.mendick@gmail.com>
2020-05-27 09:08:57 +0100

    Added unloaded hook

---
+++ NavigationReactMobile/src/useUnloaded.ts
@@ -0,0 +1,24 @@
+import { useContext, useEffect } from 'react';
+import { NavigationContext } from 'navigation-react';
+
+var useUnloaded = (handler: (state, data, stateContext) => void) => {
+    var navigationEvent = useContext(NavigationContext);
+    useEffect(() => {
+        var cancelled = false;
+        var {stateNavigator} = navigationEvent;
+        var navigateHandler = (_oldState, state, data, _asyncData, stateContext) => {
+            var crumb = stateContext.url.split('crumb=').length - 1;
+            var {crumbs: sceneCrumbs, state: sceneState} = stateNavigator.stateContext;
+            if (!(sceneCrumbs.length === crumb && sceneState === state)
+                && stateNavigator.stateContext.url === stateContext.oldUrl && !cancelled)
+                handler(state, data, stateContext);
+        }
+        stateNavigator.onNavigate(navigateHandler);
+        return () => {
+            cancelled = true;
+            stateNavigator.onNavigate(navigateHandler)
+        };
+    }, [navigationEvent, handler]);
+};
+
+export default useUnloaded;
