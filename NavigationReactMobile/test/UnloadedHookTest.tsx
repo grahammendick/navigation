4cb348a7a50b6e50c51ace30812b0d0f2618b103
Graham Mendick <graham.mendick@gmail.com>
2020-05-27 09:49:31 +0100

    Tested unloaded hook for A -> B to A

--- NavigationReactMobile/test/UnloadedHookTest.tsx
+++ NavigationReactMobile/test/UnloadedHookTest.tsx
@@ -141,4 +141,51 @@ describe('UnloadedHook', function () {
             }
         })
     });
-});
+
+    describe('A -> B to A', function () {
+        it('should call unloaded hook on B and not on A', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 'sceneA' },
+                { key: 'sceneB', trackCrumbTrail: true }
+            ]);
+            stateNavigator.navigate('sceneA');
+            var {sceneA, sceneB} = stateNavigator.states;
+            var unloadedA, unloadedB;
+            var SceneA = () => {
+                useUnloaded(() => {
+                    unloadedA = true;
+                })
+                return <div />;
+            };
+            var SceneB = () => {
+                useUnloaded(() => {
+                    unloadedB = true;
+                })
+                return <div />;
+            };
+            sceneA.renderScene = () => <SceneA />;
+            sceneB.renderScene = () => <SceneB />;
+            var container = document.createElement('div');
+            act(() => {
+                ReactDOM.render(
+                    <NavigationHandler stateNavigator={stateNavigator}>
+                        <NavigationMotion>
+                            {(_style, scene, key) =>  <div key={key}>{scene}</div>}
+                        </NavigationMotion>
+                    </NavigationHandler>,
+                    container
+                );
+                stateNavigator.navigate('sceneB');
+            });
+            act(() => {
+                unloadedA = unloadedB = false;
+                stateNavigator.navigateBack(1);
+            });
+            try {
+                assert.equal(unloadedA, false);
+                assert.equal(unloadedB, true);
+            } finally {
+                ReactDOM.unmountComponentAtNode(container);
+            }
+        })
+    });});
