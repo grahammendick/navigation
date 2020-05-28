73c285bf54ed429273822677b51d4ab50271dcbc
Graham Mendick <graham.mendick@gmail.com>
2020-05-28 23:49:28 +0100

    Tested stack for A -> B

--- NavigationReactMobile/test/NavigationMotionTest.tsx
+++ NavigationReactMobile/test/NavigationMotionTest.tsx
@@ -21,7 +21,7 @@ window.cancelAnimationFrame = () => {};
 
 describe('NavigationMotion', function () {
     describe('A', function () {
-        it('should render stack', function(){
+        it('should render A', function(){
             var stateNavigator = new StateNavigator([
                 { key: 'sceneA' }
             ]);
@@ -49,8 +49,42 @@ describe('NavigationMotion', function () {
         })
     });
 
+    describe('A -> B', function () {
+        it('should render [] + B', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 'sceneA' },
+                { key: 'sceneB', trackCrumbTrail: true },
+            ]);
+            stateNavigator.navigate('sceneA');
+            stateNavigator.navigate('sceneB');
+            var {sceneA, sceneB} = stateNavigator.states;
+            var SceneA = () => <div id="sceneA" />;
+            var SceneB = () => <div id="sceneB" />;
+            sceneA.renderScene = () => <SceneA />;
+            sceneB.renderScene = () => <SceneB />;
+            var container = document.createElement('div');
+            ReactDOM.render(
+                <NavigationHandler stateNavigator={stateNavigator}>
+                    <NavigationMotion>
+                        {(_style, scene, key) =>  (
+                            <div className="scene" key={key}>{scene}</div>
+                        )}
+                    </NavigationMotion>
+                </NavigationHandler>,
+                container
+            );
+            try {
+                assert.equal(container.querySelectorAll(".scene").length, 2);
+                assert.equal(container.querySelector("#sceneA"), null);
+                assert.notEqual(container.querySelector("#sceneB"), null);
+            } finally {
+                ReactDOM.unmountComponentAtNode(container);
+            }
+        })
+    });
+
     describe('A to A -> B', function () {
-        it('should render stack', function(){
+        it('should render A + B', function(){
             var stateNavigator = new StateNavigator([
                 { key: 'sceneA' },
                 { key: 'sceneB', trackCrumbTrail: true },
