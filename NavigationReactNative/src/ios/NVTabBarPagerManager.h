63c28a0b700e3be039d5047a2fc351187d045491
Graham Mendick <graham.mendick@gmail.com>
2020-06-27 10:58:03 +0100

    Added tab bar pager files

---
+++ NavigationReactNative/src/ios/NVTabBarPagerManager.h
@@ -0,0 +1,13 @@
+//
+//  NVTabBarPagerManager.h
+//  NavigationReactNative
+//
+//  Created by Graham Mendick on 27/06/2020.
+//  Copyright © 2020 Graham Mendick. All rights reserved.
+//
+
+#ifndef NVTabBarPagerManager_h
+#define NVTabBarPagerManager_h
+
+
+#endif /* NVTabBarPagerManager_h */
---
+++ NavigationReactNative/src/ios/NVTabBarPagerView.h
@@ -0,0 +1,13 @@
+//
+//  NVTabBarPagerView.h
+//  NavigationReactNative
+//
+//  Created by Graham Mendick on 27/06/2020.
+//  Copyright © 2020 Graham Mendick. All rights reserved.
+//
+
+#ifndef NVTabBarPagerView_h
+#define NVTabBarPagerView_h
+
+
+#endif /* NVTabBarPagerView_h */
