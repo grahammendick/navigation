e640a9b063d5e06882a656ac075750a408b31be3
Graham Mendick <graham.mendick@gmail.com>
2020-06-27 20:44:09 +0100

    Added tabcount so props change when tabs do

--- NavigationReactNative/src/ios/NVTabBarPagerManager.m
+++ NavigationReactNative/src/ios/NVTabBarPagerManager.m
@@ -10,6 +10,7 @@ RCT_EXPORT_MODULE()
     return [[NVTabBarPagerView alloc] init];
 }
 
+RCT_EXPORT_VIEW_PROPERTY(tabCount, NSInteger)
 RCT_EXPORT_VIEW_PROPERTY(selectedTab, NSInteger)
 RCT_EXPORT_VIEW_PROPERTY(scrollsToTop, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(mostRecentEventCount, NSInteger)
