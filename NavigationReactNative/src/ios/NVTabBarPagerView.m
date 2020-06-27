3015ce46c1348394ef98f6e100ec1efe62ea6242
Graham Mendick <graham.mendick@gmail.com>
2020-06-27 12:02:48 +0100

    Rendered page view controller

--- NavigationReactNative/src/ios/NVTabBarPagerView.m
+++ NavigationReactNative/src/ios/NVTabBarPagerView.m
@@ -1,4 +1,19 @@
 #import "NVTabBarPagerView.h"
 
 @implementation NVTabBarPagerView
+{
+    UIPageViewController *_pageViewController;
+    NSArray<UIViewController *> *_tabs;
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        _pageViewController = [[UIPageViewController alloc] init];
+        [self addSubview:_pageViewController.view];
+        _tabs = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
 @end
