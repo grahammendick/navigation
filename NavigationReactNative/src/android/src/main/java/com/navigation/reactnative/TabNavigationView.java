f4436a3f4e995bc8c3d932b15dd2b782d8900dd3
Graham Mendick <graham.mendick@gmail.com>
2020-07-01 15:22:17 +0100

    Removed redundant imports

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabNavigationView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabNavigationView.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.viewpager.widget.ViewPager;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.google.android.material.badge.BadgeDrawable;
