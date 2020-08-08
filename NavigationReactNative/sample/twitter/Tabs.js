b250af3a433ff4c12419672dbc72ece6bfb7c2d7
Graham Mendick <graham.mendick@gmail.com>
2020-08-08 22:47:19 +0100

    Moved NavigationStack inline for clarity

--- NavigationReactNative/sample/twitter/Tabs.js
+++ NavigationReactNative/sample/twitter/Tabs.js
@@ -15,12 +15,6 @@ const useStateNavigator = start => {
   }, [])
 };
 
-const Stack = ({stateNavigator}) => (
-  <NavigationHandler stateNavigator={stateNavigator}>
-    <NavigationStack />
-  </NavigationHandler>
-);
-
 export default ({tweets, notifications}) => {
   const [notified, setNotified] = useState(false);
   const homeNavigator = useStateNavigator('home');
@@ -32,18 +26,22 @@ export default ({tweets, notifications}) => {
         primary={true}
         selectedTintColor={Platform.OS === 'android' ? '#1da1f2' : null}>
         <TabBarItem title="Home" image={require('./home.png')}>
-          {Platform.OS === 'android'
-            ? <Home tweets={tweets} />
-            : <Stack stateNavigator={homeNavigator} />}
+          {Platform.OS === 'ios'
+            ? (<NavigationHandler stateNavigator={homeNavigator}>
+                <NavigationStack />
+              </NavigationHandler>)
+            : <Home tweets={tweets} />}
         </TabBarItem>
         <TabBarItem
           title="Notifications"
           image={require('./notifications.png')}
           badge={!notified ? notifications.length : null} 
           onPress={() => {setNotified(true)}}>
-          {Platform.OS === 'android'
-            ? <Notifications notifications={notifications} />
-            : <Stack stateNavigator={notificationsNavigator} />}        
+          {Platform.OS === 'ios'
+            ? (<NavigationHandler stateNavigator={notificationsNavigator}>
+                <NavigationStack />
+              </NavigationHandler>)
+            : <Notifications notifications={notifications} />}
         </TabBarItem>
       </TabBar>
     </>
