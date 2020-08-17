951cfb837b77b8474c58b92c2c2f40905403916f
Graham Mendick <graham.mendick@gmail.com>
2020-08-17 16:36:27 +0100

    Added missing import

--- NavigationReactNative/sample/zoom/ios/zoom/AppDelegate.m
+++ NavigationReactNative/sample/zoom/ios/zoom/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
