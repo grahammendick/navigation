014dae819b1b9516d9ad203ece2a0eb7c99ed5a7
Graham Mendick <graham.mendick@gmail.com>
2020-11-16 14:37:03 +0000

    Tagged the status bar
    So can search for it from the scene controller

--- NavigationReactNative/src/ios/NVStatusBarView.h
+++ NavigationReactNative/src/ios/NVStatusBarView.h
@@ -2,4 +2,6 @@
 
 @interface NVStatusBarView : UIView
 
+#define STATUS_BAR ((int) 78)
+
 @end
--- NavigationReactNative/src/ios/NVStatusBarView.m
+++ NavigationReactNative/src/ios/NVStatusBarView.m
@@ -2,4 +2,12 @@
 
 @implementation NVStatusBarView
 
+- (id)init
+{
+    if (self = [super init]) {
+        self.tag = STATUS_BAR;
+    }
+    return self;
+}
+
 @end
