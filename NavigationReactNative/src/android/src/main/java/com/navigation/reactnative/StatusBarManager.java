ac9361049719973d209d737fe07f1925d7dd773b
Graham Mendick <graham.mendick@gmail.com>
2020-11-17 22:15:10 +0000

    Updated status bar after if attached to window
    This misses the initial update but will drive that from the scene/scenefragment because need to reset the status bar even if there's no statusbar component on the scene

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/StatusBarManager.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/StatusBarManager.java
@@ -1,7 +1,10 @@
 package com.navigation.reactnative;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class StatusBarManager extends ViewGroupManager<StatusBarView> {
     @Override
@@ -12,5 +15,15 @@ public class StatusBarManager extends ViewGroupManager<StatusBarView> {
     @Override
     protected StatusBarView createViewInstance(ThemedReactContext reactContext) {
         return new StatusBarView(reactContext);
+    }
+
+    @ReactProp(name = "hidden")
+    public void setHidden(StatusBarView view, boolean hidden) {
+    }
+
+    @Override
+    protected void onAfterUpdateTransaction(@NonNull StatusBarView view) {
+        super.onAfterUpdateTransaction(view);
+        view.onAfterUpdateTransaction();
     }
 }
--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/StatusBarView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/StatusBarView.java
@@ -1,12 +1,25 @@
 package com.navigation.reactnative;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.view.ViewGroup;
+
+import androidx.core.view.ViewCompat;
+
+import com.facebook.react.bridge.ReactContext;
 
 public class StatusBarView extends ViewGroup {
 
     public StatusBarView(Context context) {
         super(context);
+    }
+
+    void onAfterUpdateTransaction() {
+        if (ViewCompat.isAttachedToWindow(this)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                ((ReactContext) getContext()).getCurrentActivity().getWindow().setStatusBarColor(Color.RED);
+        }
     }
 
     @Override
