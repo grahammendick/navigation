7ec908a32040eda43b9f5aca6a2b8b391c685811
Graham Mendick <graham.mendick@gmail.com>
2020-11-18 08:34:29 +0000

    Renamed prop to tintStyle for consistency

--- NavigationReactNative/src/StatusBar.tsx
+++ NavigationReactNative/src/StatusBar.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { requireNativeComponent, Platform } from 'react-native';
 
-const StatusBar = ({barStyle, hidden, barTintColor}) => (
-    <NVStatusBar
-        barStyle={barStyle && (Platform.OS === 'ios' ? barStyle + '-content' : barStyle)}
+const StatusBar = ({tintStyle, hidden, barTintColor}) => (
+    <NVStatusBar        
         hidden={hidden}
+        tintStyle={tintStyle && (Platform.OS === 'ios' ? tintStyle + '-content' : tintStyle)}
         barTintColor={barTintColor} />
 );
 
