a8121dd35cdce9055db5c8fb004c80d03d1a21f2
Graham Mendick <graham.mendick@gmail.com>
2020-11-29 10:10:28 +0000

    Rendered status bar only if not already one on ios

--- NavigationReactNative/src/NavigationBar.tsx
+++ NavigationReactNative/src/NavigationBar.tsx
@@ -15,9 +15,9 @@ class NavigationBar extends React.Component<any, any> {
     render() {
         var {hidden, logo, navigationImage, overflowImage, children, style = {height: undefined}, ...otherProps} = this.props;
         var childrenArray = (React.Children.toArray(children) as ReactElement<any>[]);
-        var statusBar = childrenArray.find(({type}) => type === StatusBar) || <StatusBar />;
+        var statusBar = childrenArray.find(({type}) => type === StatusBar);
         if (Platform.OS === 'android' && hidden)
-            return statusBar;
+            return statusBar || <StatusBar />;
         var collapsingBar = childrenArray.find(({type}) => type === CollapsingBar);
         return (
             <>
@@ -47,7 +47,7 @@ class NavigationBar extends React.Component<any, any> {
                             </NVToolbar>
                             {childrenArray.find(({type}) => type === TabBar)}
                         </Container>}
-                        {statusBar}
+                        {(Platform.OS === 'android' || !statusBar) && (statusBar || <StatusBar />)}
                 </NVNavigationBar>
                 {Platform.OS === 'ios' ? null : childrenArray.find(({type}) => type === SearchBar)}
             </>
