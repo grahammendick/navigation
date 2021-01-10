bb5ff35a8c2b7967f70c2448eb42753283257c59
Graham Mendick <graham.mendick@gmail.com>
2021-01-10 09:50:27 +0000

    Updated code-splitting to match documentation
    Kept the data in renderScene for minimal changes. Still matches docs enough

--- NavigationReact/sample/codesplitting/index.js
+++ NavigationReact/sample/codesplitting/index.js
@@ -1,7 +1,7 @@
 import { searchPeople, getPerson } from './Data';
 import { StateNavigator } from 'navigation';
 import { NavigationContext, NavigationHandler } from 'navigation-react';
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useContext } from 'react';
 import ReactDOM from 'react-dom';
 var People = lazy(() => import('./People'));
 var Person = lazy(() => import('./Person'));
@@ -12,18 +12,21 @@ var stateNavigator = new StateNavigator([
 ]);
 
 var {people, person} = stateNavigator.states;
-people.renderView = ({pageNumber}) => <People people={searchPeople(pageNumber)} />;
-person.renderView = ({id}) => <Person person={getPerson(id)} />;
+people.renderScene = ({pageNumber}) => <People people={searchPeople(pageNumber)} />;
+person.renderScene = ({id}) => <Person person={getPerson(id)} />;
 
 stateNavigator.start();
 
+const App = () => {
+  const {state, data} = useContext(NavigationContext);
+  return state.renderScene(data)
+};
+
 ReactDOM.render(
-    <NavigationHandler stateNavigator={stateNavigator}>
-        <Suspense fallback={<div>Loading...</div>}>
-            <NavigationContext.Consumer>
-                {({ state, data }) => state.renderView(data)}
-            </NavigationContext.Consumer>
-        </Suspense>
-    </NavigationHandler>,
-    document.getElementById('content')
+  <NavigationHandler stateNavigator={stateNavigator}>
+    <Suspense fallback={<div>Loading...</div>}>
+        <App />
+    </Suspense>
+  </NavigationHandler>,
+  document.getElementById('content')
 );
