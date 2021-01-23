bf132018ddc974eb6a0f5730a051dd283b0d54dc
Joshua Smith <jsmith@stash.energy>
2021-01-23 16:16:14 -0400

    update navigation-tests with key types

--- Navigation/test/navigation-tests.ts
+++ Navigation/test/navigation-tests.ts
@@ -1,17 +1,28 @@
-import { Crumb, HashHistoryManager, StateNavigator, State } from 'navigation';
+import { Crumb, HashHistoryManager, StateNavigator, State, StateInfo } from 'navigation';
 
 // History Manager
-class LogHistoryManager extends HashHistoryManager  {
-    addHistory(url: string) {
-        console.log('add history');
-        super.addHistory(url, false);
-    }
+class LogHistoryManager extends HashHistoryManager {
+  addHistory(url: string) {
+    console.log('add history');
+    super.addHistory(url, false);
+  }
 }
 
 // Configuration
-const config = [
-    { key: 'people', route: ['people/{page}', 'people/{page}/sort/{sort}'], defaults: { page: 1 }, help: 'people.htm' },
-    { key: 'person', route: 'person/{id}', trackTypes: false, defaultTypes: { id: 'number' }, trackCrumbTrail: true }
+const config: StateInfo<'people' | 'person'>[] = [
+  {
+    key: 'people',
+    route: ['people/{page}', 'people/{page}/sort/{sort}'],
+    defaults: { page: 1 },
+    help: 'people.htm',
+  },
+  {
+    key: 'person',
+    route: 'person/{id}',
+    trackTypes: false,
+    defaultTypes: { id: 'number' },
+    trackCrumbTrail: true,
+  },
 ];
 const stateNavigator = new StateNavigator(config);
 stateNavigator.configure(config, new LogHistoryManager());
@@ -27,25 +38,35 @@ const idDefaultType = person.defaultTypes.id;
 // State Controller
 people.dispose = () => {};
 people.navigating = (data, url, navigate) => {
-    navigate([]);
+  navigate([]);
 };
 people.navigated = (data, asyncData) => {};
 person.navigating = (data, url, navigate) => {
-    navigate();
+  navigate();
 };
 person.navigated = (data) => {};
-person.urlEncode = function urlEncode(state: State, key: string, val: string, queryString: boolean): string {
-    return queryString ? val.replace(/\s/g, '+') : encodeURIComponent(val);
+person.urlEncode = function urlEncode(
+  state: State,
+  key: string,
+  val: string,
+  queryString: boolean
+): string {
+  return queryString ? val.replace(/\s/g, '+') : encodeURIComponent(val);
 };
-person.urlDecode = function urlDecode(state: State, key: string, val: string, queryString: boolean): string {
-    return queryString ? val.replace(/\+/g, ' ') : decodeURIComponent(val);
+person.urlDecode = function urlDecode(
+  state: State,
+  key: string,
+  val: string,
+  queryString: boolean
+): string {
+  return queryString ? val.replace(/\+/g, ' ') : decodeURIComponent(val);
 };
 person.validate = (data: any) => data.id > 0;
 person.truncateCrumbTrail = (state: State, data: any, crumbs: Crumb[]) => crumbs;
 
 // Navigation Event
 const navigationListener = (oldState: State, state: State, data: any, asyncData: any) => {
-    stateNavigator.offNavigate(navigationListener);
+  stateNavigator.offNavigate(navigationListener);
 };
 stateNavigator.onNavigate(navigationListener);
 
@@ -73,13 +94,13 @@ link = crumb.url;
 stateNavigator.navigateLink(link, 'none', true);
 
 // Fluent Navigation
-link = stateNavigator.fluent()
-    .navigate('people')
-    .refresh()
-    .refresh({ page: 3 })
-    .navigate('person', { id: 10 })
-    .navigateBack(1)
-    .url;
+link = stateNavigator
+  .fluent()
+  .navigate('people')
+  .refresh()
+  .refresh({ page: 3 })
+  .navigate('person', { id: 10 })
+  .navigateBack(1).url;
 
 // State Context
 let state: State = stateNavigator.stateContext.state;
