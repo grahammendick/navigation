8ed95b6e0c60b4a3c836b3449f42f501fb111342
Graham Mendick <graham.mendick@gmail.com>
2021-02-14 09:45:57 +0000

    Commented how to run typescript tests

--- NavigationReact/test/navigation-react-tests.tsx
+++ NavigationReact/test/navigation-react-tests.tsx
@@ -1,3 +1,4 @@
+// tsc --jsx react --target es3 --lib ES2015,DOM --noImplicitAny true navigation-react-tests.tsx
 import { StateNavigator } from 'navigation';
 import { NavigationHandler, NavigationContext, NavigationBackLink, NavigationLink, RefreshLink } from 'navigation-react';
 import * as React from 'react';
@@ -7,7 +8,7 @@ const stateNavigator = new StateNavigator([
     { key: 'person', route: 'person/{name}', trackCrumbTrail: true }
 ]);
 
-var People = ({ page }) => (
+var People = ({ page }: any) => (
     <div>
         <ul>
             {['Bob', 'Brenda'].map(name => (
@@ -29,7 +30,7 @@ var People = ({ page }) => (
     </div>
 );
 
-var Person = ({ name }) => (
+var Person = ({ name }: any) => (
     <div>
         <NavigationBackLink distance={1}>List</NavigationBackLink>
         <div>{name}</div>
@@ -37,8 +38,8 @@ var Person = ({ name }) => (
 );
 
 var { people, person } = stateNavigator.states;
-people.renderView = ({ page }) => <People page={page} />; 
-person.renderView = ({ name }) => <Person name={name} />;
+people.renderView = ({ page }: any) => <People page={page} />; 
+person.renderView = ({ name }: any) => <Person name={name} />;
 
 var App = () => (
     <NavigationHandler stateNavigator={stateNavigator}>
