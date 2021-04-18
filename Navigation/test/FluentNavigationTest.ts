cc0d0eb4977fc59dfce270a51adf6b0822792943
Graham Mendick <graham.mendick@gmail.com>
2021-04-18 09:36:23 +0100

    Tested hash back fluent navigation

--- Navigation/test/FluentNavigationTest.ts
+++ Navigation/test/FluentNavigationTest.ts
@@ -446,6 +446,24 @@ describe('Fluent', function () {
         });
     });
 
+    describe('Hash Back', function () {
+        it('should navigate', function() {
+            var stateNavigator = new StateNavigator([
+                { key: 's0', route: 'r0' },
+                { key: 's1', route: 'r1' },
+                { key: 's2', route: 'r2', trackCrumbTrail: true }
+            ]);
+            var url = stateNavigator.fluent()
+                .navigate('s0')
+                .navigate('s1', null, 'f')
+                .navigate('s2')
+                .navigateBack(1)
+                .url;
+            assert.strictEqual(url, '/r1#f');
+            assert.strictEqual(stateNavigator.stateContext.url, null);
+        });
+    });
+
     describe('Back Two With Trail', function () {
         it('should navigate', function() {
             var stateNavigator = new StateNavigator([
