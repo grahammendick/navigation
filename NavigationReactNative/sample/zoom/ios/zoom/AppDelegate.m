ed5b8ef56f724d08b7b845747c2776a904bfca32
Graham Mendick <graham.mendick@gmail.com>
2021-05-15 01:09:37 +0100

    Added deep link back on ios

--- NavigationReactNative/sample/zoom/ios/zoom/AppDelegate.m
+++ NavigationReactNative/sample/zoom/ios/zoom/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -57,6 +58,12 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  options:(NSDictionary *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
