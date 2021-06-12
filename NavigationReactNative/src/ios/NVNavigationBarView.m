025791a6e9ac6ccfe6433159bc9565937ea0cee1
Graham Mendick <graham.mendick@gmail.com>
2021-06-12 22:27:07 +0100

    Configured scroll edge appearance
    Only created scrollEdge appearance if there's a large bar tint, otherwise it defaults to the standard appearance. All the other props are large title related and can be taken from standard appearance

--- NavigationReactNative/src/ios/NVNavigationBarView.m
+++ NavigationReactNative/src/ios/NVNavigationBarView.m
@@ -39,27 +39,8 @@
     }
     if (@available(iOS 13.0, *)) {
         [navigationBar setTintColor: self.tintColor];
-        UINavigationBarAppearance *appearance = [UINavigationBarAppearance new];
-        [appearance configureWithDefaultBackground];
-        if (self.barTintColor) {
-            if (CGColorGetAlpha(self.barTintColor.CGColor) == 0)
-                [appearance configureWithTransparentBackground];
-            if (CGColorGetAlpha(self.barTintColor.CGColor) == 1)
-                [appearance configureWithOpaqueBackground];
-        }
-        NSMutableDictionary *attributes = [NSMutableDictionary new];
-        if (self.tintColor != nil) {
-            attributes[NSForegroundColorAttributeName] = self.tintColor;
-        }
-        [appearance setBackgroundColor:self.barTintColor];
-        [appearance.buttonAppearance.normal setTitleTextAttributes:attributes];
-        [appearance.doneButtonAppearance.normal setTitleTextAttributes:attributes];
-        [appearance setTitleTextAttributes:[self titleAttributes]];
-        [appearance setLargeTitleTextAttributes:[self largeTitleAttributes]];
-        appearance.backButtonAppearance = [UIBarButtonItemAppearance new];
-        appearance.backButtonAppearance.normal.titleTextAttributes = [self backAttributes];
-        self.reactViewController.navigationItem.standardAppearance = appearance;
-        self.reactViewController.navigationItem.scrollEdgeAppearance = self.barTintColor ? appearance : nil;
+        self.reactViewController.navigationItem.standardAppearance = [self appearance: self.barTintColor];
+        self.reactViewController.navigationItem.scrollEdgeAppearance = self.largeBarTintColor ? [self appearance: self.largeBarTintColor] : nil;
     } else {
         bool transparent = self.barTintColor && CGColorGetAlpha(self.barTintColor.CGColor) == 0;
         [navigationBar setValue:@(transparent) forKey:@"hidesShadow"];
@@ -90,6 +71,30 @@
     }
 }
 
+- (UINavigationBarAppearance *) appearance: (UIColor *) color
+API_AVAILABLE(ios(13.0)){
+    UINavigationBarAppearance *appearance = [UINavigationBarAppearance new];
+    [appearance configureWithDefaultBackground];
+    if (color) {
+        if (CGColorGetAlpha(color.CGColor) == 0)
+            [appearance configureWithTransparentBackground];
+        if (CGColorGetAlpha(color.CGColor) == 1)
+            [appearance configureWithOpaqueBackground];
+    }
+    NSMutableDictionary *attributes = [NSMutableDictionary new];
+    if (self.tintColor != nil) {
+        attributes[NSForegroundColorAttributeName] = self.tintColor;
+    }
+    [appearance setBackgroundColor:self.barTintColor];
+    [appearance.buttonAppearance.normal setTitleTextAttributes:attributes];
+    [appearance.doneButtonAppearance.normal setTitleTextAttributes:attributes];
+    [appearance setTitleTextAttributes:[self titleAttributes]];
+    [appearance setLargeTitleTextAttributes:[self largeTitleAttributes]];
+    appearance.backButtonAppearance = [UIBarButtonItemAppearance new];
+    appearance.backButtonAppearance.normal.titleTextAttributes = [self backAttributes];
+    return appearance;
+}
+
 - (NSMutableDictionary *) titleAttributes
 {
     NSMutableDictionary *attributes = [NSMutableDictionary new];
@@ -108,14 +113,14 @@
 - (NSMutableDictionary *) largeTitleAttributes
 {
     NSMutableDictionary *attributes = [NSMutableDictionary new];
-    if (self.titleColor != nil) {
-        attributes[NSForegroundColorAttributeName] = self.titleColor;
+    if (self.largeTitleColor != nil) {
+        attributes[NSForegroundColorAttributeName] = self.largeTitleColor;
     }
     if (@available(iOS 11.0, *)) {
-        UIFont *baseFont = !self.titleFontFamily ? [UIFont preferredFontForTextStyle: UIFontTextStyleLargeTitle] : nil;
-        NSNumber *size = @([UIFont preferredFontForTextStyle: UIFontTextStyleLargeTitle].pointSize);
-        UIFont *font = [RCTFont updateFont:baseFont withFamily:self.titleFontFamily size:size weight:self.titleFontWeight style:self.titleFontStyle variant:nil scaleMultiplier:1];
-        if (self.titleFontFamily || self.titleFontWeight || self.titleFontStyle) {
+        UIFont *baseFont = !self.largeTitleFontFamily ? [UIFont preferredFontForTextStyle: UIFontTextStyleLargeTitle] : nil;
+        NSNumber *size = !self.largeTitleFontSize ? @([UIFont preferredFontForTextStyle: UIFontTextStyleLargeTitle].pointSize) : self.largeTitleFontSize;
+        UIFont *font = [RCTFont updateFont:baseFont withFamily:self.largeTitleFontFamily size:size weight:self.largeTitleFontWeight style:self.largeTitleFontStyle variant:nil scaleMultiplier:1];
+        if (self.largeTitleFontFamily || self.largeTitleFontWeight || self.largeTitleFontStyle || self.largeTitleFontSize) {
             attributes[NSFontAttributeName] = font;
         }
     }
