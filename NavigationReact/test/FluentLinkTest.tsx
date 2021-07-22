758a8d711eb1ddcc6bae7f9a833f8b5e7452f4a4
Graham Mendick <graham.mendick@gmail.com>
2021-07-22 17:11:30 +0100

    Revert "Wrapped navigate in its own act"
    This reverts commit d6e02a3574242a56df86d8f2da29d42a9695f1c3.

--- NavigationReact/test/FluentLinkTest.tsx
+++ NavigationReact/test/FluentLinkTest.tsx
@@ -717,7 +717,7 @@ describe('FluentLinkTest', function () {
             });
             assert.equal(stateNavigator.stateContext.state.key, 's0');
             var link = stateNavigator.fluent(true).navigate('s1').url;
-            act(() => stateNavigator.navigateLink(link));
+            stateNavigator.navigateLink(link);
             assert.equal(stateNavigator.stateContext.state.key, 's0');
         })
     });
