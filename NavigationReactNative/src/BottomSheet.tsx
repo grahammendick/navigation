6bd6c53f61495387d80c703615957354adad44f4
Graham Mendick <graham.mendick@gmail.com>
2021-08-28 17:08:48 +0100

    Got bottom sheet to appear
    Needed to set peek height on added behaviour and after it's added as a child of the CoordinatorLayout

--- NavigationReactNative/src/BottomSheet.tsx
+++ NavigationReactNative/src/BottomSheet.tsx
@@ -1,5 +1,20 @@
-import { requireNativeComponent, Platform } from 'react-native';
+import React from 'react';
+import { requireNativeComponent, Platform, StyleSheet } from 'react-native';
+
+var BottomSheet = ({ style, children }) => (
+    <NVBottomSheet style={style}>
+        {children}
+    </NVBottomSheet>
+)
 
 var NVBottomSheet = requireNativeComponent<any>('NVBottomSheet', null);
 
-export default Platform.OS === "android" ? NVBottomSheet : () => null;
+const styles = StyleSheet.create({
+    bottomSheet: {
+        position: 'absolute',
+        top: 0, right: 0,
+        bottom: 0, left: 0,
+    },
+});
+
+export default Platform.OS === "android" ? BottomSheet : () => null;
--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/BottomSheetView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/BottomSheetView.java
@@ -1,11 +1,27 @@
 package com.navigation.reactnative;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.facebook.react.uimanager.PixelUtil;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class BottomSheetView extends ViewGroup {
     public BottomSheetView(Context context) {
         super(context);
+        CoordinatorLayout.LayoutParams params = new CoordinatorLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        params.setBehavior(new BottomSheetBehavior());
+        setLayoutParams(params);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        BottomSheetBehavior.from(this).setPeekHeight((int) PixelUtil.toPixelFromDIP(50));
     }
 
     @Override
