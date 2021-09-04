b553464e7e70e3db8b95fd566db56496afe0ed5f
Graham Mendick <graham.mendick@gmail.com>
2021-09-04 17:23:32 +0100

    Removed redundant transition prop

--- NavigationReactNative/src/SharedElement.tsx
+++ NavigationReactNative/src/SharedElement.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { requireNativeComponent, Platform, View } from 'react-native';
 
-var SharedElement = ({transition, ...props}) => (
-    Platform.OS == 'android' ? (
-        <NVSharedElement
-            enterTransition={typeof transition !== 'function' ? transition : transition(true)}
-            exitTransition={typeof transition !== 'function' ? transition : transition(false)}
-            {...props} />
-    ) : <View {...props} />
+var SharedElement = (props) => (
+    Platform.OS == 'android' ? <NVSharedElement {...props} /> : <View {...props} />
 );
 
 var NVSharedElement = requireNativeComponent<any>('NVSharedElement', null);
