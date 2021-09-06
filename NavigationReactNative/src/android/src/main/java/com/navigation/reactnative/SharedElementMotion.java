27a8d417901c5f8a527cfa6b540d23cdc171ffc1
Graham Mendick <graham.mendick@gmail.com>
2021-09-06 09:20:25 +0100

    Renamed because no clash anymore

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SharedElementMotion.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SharedElementMotion.java
@@ -5,16 +5,16 @@ import com.google.android.material.transition.MaterialContainerTransform;
 class SharedElementMotion {
     private final SceneFragment enterScene;
     private final SceneFragment scene;
-    private final String sharedElementName;
+    private final String sharedElement;
 
-    SharedElementMotion(SceneFragment enterScene, SceneFragment scene, String sharedElementName) {
-        this.sharedElementName = sharedElementName;
+    SharedElementMotion(SceneFragment enterScene, SceneFragment scene, String sharedElement) {
+        this.sharedElement = sharedElement;
         this.enterScene = enterScene;
         this.scene = scene;
     }
 
     void load(SharedElementView sharedElementView) {
-        if (sharedElementName.equals(sharedElementView.getTransitionName())) {
+        if (sharedElement.equals(sharedElementView.getTransitionName())) {
             MaterialContainerTransform transition = sharedElementView.transition;
             transition.setTransitionDirection(enterScene == scene ? MaterialContainerTransform.TRANSITION_DIRECTION_ENTER : MaterialContainerTransform.TRANSITION_DIRECTION_RETURN);
             transition.addTarget(sharedElementView.getTransitionName());
