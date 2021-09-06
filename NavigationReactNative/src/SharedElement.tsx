a649b09dabef2e7d9269bdbf10030e11877a7a3b
Graham Mendick <graham.mendick@gmail.com>
2021-09-06 17:39:53 +0100

    Tweaked format

--- NavigationReactNative/src/SharedElement.tsx
+++ NavigationReactNative/src/SharedElement.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { requireNativeComponent, Platform, View } from 'react-native';
 
 var SharedElement = ({ style, children, ...props }) => (
-    Platform.OS == 'android' ? <NVSharedElement {...props} style={style} children={children} /> : <View style={style} children={children} />
+    Platform.OS == 'android'
+        ? <NVSharedElement {...props} style={style}>{children}</NVSharedElement>
+        : <View style={style}>{children}</View>
 );
 
 var NVSharedElement = requireNativeComponent<any>('NVSharedElement', null);
