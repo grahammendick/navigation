36ef391f1c902c351e44d55cb3090f75ae197177
Graham Mendick <graham.mendick@gmail.com>
2021-09-06 17:29:38 +0100

    Rendered sharedEl (view) children on ios

--- NavigationReactNative/src/SharedElement.tsx
+++ NavigationReactNative/src/SharedElement.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { requireNativeComponent, Platform, View } from 'react-native';
 
-var SharedElement = ({ style, ...props }) => (
-    Platform.OS == 'android' ? <NVSharedElement {...props} style={style} /> : <View style={style} />
+var SharedElement = ({ style, children, ...props }) => (
+    Platform.OS == 'android' ? <NVSharedElement {...props} style={style} /> : <View style={style} children={children} />
 );
 
 var NVSharedElement = requireNativeComponent<any>('NVSharedElement', null);
