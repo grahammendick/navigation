ca2fab0bbbaabbab11d1140d0115b4127478c1e5
Graham Mendick <graham.mendick@gmail.com>
2021-09-06 17:34:07 +0100

    Rendered shared el children on android

--- NavigationReactNative/src/SharedElement.tsx
+++ NavigationReactNative/src/SharedElement.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { requireNativeComponent, Platform, View } from 'react-native';
 
 var SharedElement = ({ style, children, ...props }) => (
-    Platform.OS == 'android' ? <NVSharedElement {...props} style={style} /> : <View style={style} children={children} />
+    Platform.OS == 'android' ? <NVSharedElement {...props} style={style} children={children} /> : <View style={style} children={children} />
 );
 
 var NVSharedElement = requireNativeComponent<any>('NVSharedElement', null);
