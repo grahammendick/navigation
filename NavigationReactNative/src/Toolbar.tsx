c4a3a2ecef1ad8d6a35d0da6d1c2f003de1a06bc
Graham Mendick <graham.mendick@gmail.com>
2021-09-18 19:26:22 +0100

    Created stylesheet for consistency

--- NavigationReactNative/src/Toolbar.tsx
+++ NavigationReactNative/src/Toolbar.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Platform, Image, requireNativeComponent } from 'react-native';
+import { Platform, Image, StyleSheet, requireNativeComponent } from 'react-native';
 
-const Toolbar = ({ bottom, logo, navigationImage, overflowImage, ...props }: any) => {
+const Toolbar = ({ bottom, logo, navigationImage, overflowImage, children, style, ...props }: any) => {
     var Toolbar = !bottom ? NVToolbar : NVToolbarBottom;
     return (
         <Toolbar
             logo={Image.resolveAssetSource(logo)}
             navigationImage={Image.resolveAssetSource(navigationImage)}
-            overflowImage={Image.resolveAssetSource(overflowImage)}
-            {...props}
-            style={{height: 56}}
-        />
+            overflowImage={Image.resolveAssetSource(overflowImage)}            
+            style={styles.toolbar}
+            {...props}>
+            {children}
+        </Toolbar>
     );
 }
 
 var NVToolbar = requireNativeComponent<any>('NVToolbar', null);
 var NVToolbarBottom = requireNativeComponent<any>('NVToolbarBottom', null);
+
+const styles = StyleSheet.create({
+    toolbar: {
+        height: 56
+    },
+});
 
 export default Platform.OS === 'android' ? Toolbar : null;
