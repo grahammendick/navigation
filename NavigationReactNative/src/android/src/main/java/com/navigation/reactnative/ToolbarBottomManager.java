77e04f91a954c19c4e783c24b342016afe30659b
Graham Mendick <graham.mendick@gmail.com>
2021-09-18 21:55:15 +0100

    Displayed BottomAppBar
    Set the height and gravity to bottom (otherwise it appeared at the top, over the navigation bar?!).
    Also need to set paddingBottom on the FlatList,
    contentContainerStyle={{paddingBottom: 56}}

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ToolbarBottomManager.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ToolbarBottomManager.java
@@ -2,8 +2,10 @@ package com.navigation.reactnative;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.uimanager.PixelUtil;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class ToolbarBottomManager extends ViewGroupManager<ToolbarBottomView> {
     @NonNull
@@ -16,5 +18,10 @@ public class ToolbarBottomManager extends ViewGroupManager<ToolbarBottomView> {
     @Override
     protected ToolbarBottomView createViewInstance(@NonNull ThemedReactContext reactContext) {
         return new ToolbarBottomView(reactContext);
+    }
+
+    @ReactProp(name = "height")
+    public void setHeight(ToolbarBottomView view, double height) {
+        view.getLayoutParams().height = (int) PixelUtil.toPixelFromDIP(height);
     }
 }
--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ToolbarBottomView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ToolbarBottomView.java
@@ -1,13 +1,19 @@
 package com.navigation.reactnative;
 
 import android.content.Context;
+import android.view.Gravity;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class ToolbarBottomView extends BottomAppBar {
     public ToolbarBottomView(@NonNull Context context) {
         super(context);
+        CoordinatorLayout.LayoutParams params = new CoordinatorLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.BOTTOM;
+        setLayoutParams(params);
     }
 }
