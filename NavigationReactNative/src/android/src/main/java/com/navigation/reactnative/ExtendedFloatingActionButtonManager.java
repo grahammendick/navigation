f6aa355e8e97aabc6055fb2852fe09a7e6919fd4
Graham Mendick <graham.mendick@gmail.com>
2021-10-07 10:58:25 +0100

    Added gravity prop to extended fab

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ExtendedFloatingActionButtonManager.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ExtendedFloatingActionButtonManager.java
@@ -1,5 +1,7 @@
 package com.navigation.reactnative;
 
+import android.view.Gravity;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.ReadableMap;
@@ -31,6 +33,31 @@ public class ExtendedFloatingActionButtonManager extends SimpleViewManager<Exten
     @ReactProp(name = "text")
     public void setText(ExtendedFloatingActionButtonView view, String text) {
         view.setText(text);
+    }
+
+    @ReactProp(name = "gravity")
+    public void setGravity(ExtendedFloatingActionButtonView view, String gravity) {
+        view.params.gravity = convertGravity(gravity);
+        if (view.getParent() != null) view.getParent().requestLayout();
+    }
+
+    private int convertGravity(String gravity) {
+        if ("topLeft".equals(gravity)) return Gravity.TOP | Gravity.LEFT;
+        if ("topStart".equals(gravity)) return Gravity.TOP | Gravity.START;
+        if ("top".equals(gravity)) return Gravity.TOP | Gravity.CENTER;
+        if ("topRight".equals(gravity)) return Gravity.TOP | Gravity.RIGHT;
+        if ("topEnd".equals(gravity)) return Gravity.TOP | Gravity.END;
+        if ("left".equals(gravity)) return Gravity.CENTER | Gravity.LEFT;
+        if ("start".equals(gravity)) return Gravity.CENTER | Gravity.START;
+        if ("center".equals(gravity)) return Gravity.CENTER;
+        if ("right".equals(gravity)) return Gravity.CENTER | Gravity.RIGHT;
+        if ("end".equals(gravity)) return Gravity.CENTER | Gravity.END;
+        if ("bottomLeft".equals(gravity)) return Gravity.BOTTOM | Gravity.LEFT;
+        if ("bottomStart".equals(gravity)) return Gravity.BOTTOM | Gravity.START;
+        if ("bottom".equals(gravity)) return Gravity.BOTTOM | Gravity.CENTER;
+        if ("bottomRight".equals(gravity)) return Gravity.BOTTOM | Gravity.RIGHT;
+        if ("bottomEnd".equals(gravity)) return Gravity.BOTTOM | Gravity.END;
+        return Gravity.NO_GRAVITY;
     }
 
     @Override
--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ExtendedFloatingActionButtonView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/ExtendedFloatingActionButtonView.java
@@ -3,9 +3,11 @@ package com.navigation.reactnative;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableMap;
@@ -13,10 +15,14 @@ import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 public class ExtendedFloatingActionButtonView extends ExtendedFloatingActionButton {
+    final CoordinatorLayout.LayoutParams params;
     private final IconResolver.IconResolverListener iconResolverListener;
 
     public ExtendedFloatingActionButtonView(@NonNull Context context) {
         super(context);
+        params = new CoordinatorLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setBehavior(getBehavior());
+        setLayoutParams(params);
         iconResolverListener = new IconResolver.IconResolverListener() {
             @Override
             public void setDrawable(Drawable d) {
