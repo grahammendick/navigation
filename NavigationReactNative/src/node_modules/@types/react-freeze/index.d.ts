3a42174e205eb55e17f5ba48bbe10ee9e7b8555e
Graham Mendick <graham.mendick@gmail.com>
2021-11-01 16:37:21 +0000

    Added react-freeze package

---
+++ NavigationReactNative/src/node_modules/@types/react-freeze/index.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+interface Props {
+    freeze: boolean;
+    children: React.ReactNode;
+    placeholder?: React.ReactNode;
+}
+export declare function Freeze({ freeze, children, placeholder }: Props): JSX.Element;
+export {};
