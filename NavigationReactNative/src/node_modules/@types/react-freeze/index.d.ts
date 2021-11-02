1315f4588ee355e124f1261b62d702dd2f476492
Graham Mendick <graham.mendick@gmail.com>
2021-11-02 18:06:54 +0000

    Removed react-freeze module

--- NavigationReactNative/src/node_modules/@types/react-freeze/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-interface Props {
-    freeze: boolean;
-    children: React.ReactNode;
-    placeholder?: React.ReactNode;
-}
-export declare function Freeze({ freeze, children, placeholder }: Props): JSX.Element;
-export {};
