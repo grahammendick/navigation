24685be8f3685217ce53cfc14e3c25d2f5c0a2c8
Graham Mendick <graham.mendick@gmail.com>
2021-11-03 14:06:40 +0000

    Deleted redundant freeze manager and view

--- NavigationReactNative/src/ios/NVFreezeView.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface NVFreezeView : UIView
-
-@end
--- NavigationReactNative/src/ios/NVFreezeView.m
+++
@@ -1,9 +0,0 @@
-#import "NVFreezeView.h"
-
-@implementation NVFreezeView
-
-- (void)setHidden:(BOOL)hidden
-{
-}
-
-@end
