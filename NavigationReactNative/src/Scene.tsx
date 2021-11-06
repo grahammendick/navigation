11497d78a03f1f6fe0edebc0c865cc77820dca4e
Graham Mendick <graham.mendick@gmail.com>
2021-11-06 17:34:29 +0000

    Prevented freeze of exiting scene
    A --> B --> C then ios swipe back to C. This didn't fire useEffect after useUnloaded. That's because the native navigation happens first so don't set rest to false like when navigating in javascript first. So the freeze happens straight away, at same time as javascript navigation so there's no time for a useEffect if setState done in useUnloaded. Want to allow this - not just because it used to work but also there's no need to freeze the leaving scene because it's not in the stack anymore anyway

--- NavigationReactNative/src/Scene.tsx
+++ NavigationReactNative/src/Scene.tsx
@@ -133,7 +133,7 @@ class Scene extends React.Component<SceneProps, SceneState> {
         var stateContext = navigationEvent?.stateNavigator?.stateContext;
         var {state, data} = stateContext || crumbs[crumb];
         return (
-            <Freeze enabled={freezable && crumbs.length !== crumb && navigationEvent
+            <Freeze enabled={freezable && crumb < crumbs.length && navigationEvent
                 && (!stateContext['peek'] || stateContext['peek'] !== this.props.navigationEvent)}>
                 <NVScene
                     ref={(ref: any) => {
