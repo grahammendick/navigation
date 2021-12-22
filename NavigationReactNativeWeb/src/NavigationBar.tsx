dfc6bfb207020438319f1f067332835a2aa902bd
Graham Mendick <graham.mendick@gmail.com>
2021-12-22 12:45:21 +0000

    Added basic navigation and title to navigation bar

--- NavigationReactNativeWeb/src/NavigationBar.tsx
+++ NavigationReactNativeWeb/src/NavigationBar.tsx
@@ -1,1 +1,31 @@
-export default () => null;
+import React from 'react';
+import { View, TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
+
+declare module 'react-native' {
+    interface TextProps {
+      accessibilityRole?: string;
+    }
+}
+
+const NavigationBar = ({navigationImage, onNavigationPress, tintColor, title}) => {
+    return (
+        <View style={{height: 56, flexDirection: 'row'}}>
+            <View>
+                {navigationImage && (
+                    <TouchableOpacity onPress={onNavigationPress}>
+                        <Image source={navigationImage} style={{width: 24, height: 24, tintColor}} />
+                    </TouchableOpacity>
+                )}
+            </View>
+            <Text accessibilityRole="heading" style={{flex: 1}}>{title}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    bar: {
+    },
+});
+
+
+export default NavigationBar;
