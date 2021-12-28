ee981f44d1667ebcce0969abfdf3e8139be38598
Graham Mendick <graham.mendick@gmail.com>
2021-12-28 18:14:41 +0000

    Made content go under the navigation bar
    This copies native apps. Used position fixed and gave the scroll content a paddingTop to match the navigation bar height

--- NavigationReactNativeWeb/src/NavigationBar.tsx
+++ NavigationReactNativeWeb/src/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useRef, useLayoutEffect } from 'react';
 import { View, TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
 import LeftBar from './LeftBar';
 import RightBar from './RightBar';
@@ -11,6 +11,12 @@ declare module 'react-native' {
 
 const NavigationBar = ({hidden = false, navigationImage, onNavigationPress, navigationHref, navigationTestID,
     barTintColor, tintColor, title, titleColor, titleFontFamily, titleFontWeight, titleFontStyle, titleFontSize, children}) => {
+    const barRef = useRef<any>();
+    useLayoutEffect(() => {
+        const content = barRef.current?.nextElementSibling;
+        if (content && !content.style.paddingTop)
+            content.style.paddingTop = '56px';
+    })
     barTintColor = (typeof barTintColor === 'function' ? barTintColor(true) : barTintColor) || '#fff';
     tintColor = (typeof tintColor === 'function' ? tintColor(true) : tintColor) || '#000';
     titleColor = (typeof titleColor === 'function' ? titleColor(true) : titleColor) || '#000';
@@ -21,7 +27,7 @@ const NavigationBar = ({hidden = false, navigationImage, onNavigationPress, navi
     var childrenArray = (React.Children.toArray(children) as ReactElement<any>[]);
     if (hidden) return null;
     return (
-        <View style={[styles.bar, {backgroundColor: barTintColor}]}>
+        <View ref={barRef} style={[styles.bar, {backgroundColor: barTintColor}]}>
             {navigationImage && (
                 <View href={navigationHref} style={{marginEnd: 32}}>
                     <TouchableOpacity testID={navigationTestID} onPress={onNavigationPress}>
@@ -52,6 +58,11 @@ const styles = StyleSheet.create({
         height: 56,
         flexDirection: 'row',
         alignItems: 'center',
+        position: 'fixed' as any,
+        width: '100%',
+        top: 0,
+        left: 0,
+        zIndex: 46,
         paddingStart: 16,
         paddingEnd: 16,
     },
