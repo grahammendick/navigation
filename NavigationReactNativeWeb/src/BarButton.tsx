f0a4cca72e3fbb14c643fec2b454718e93c95516
Graham Mendick <graham.mendick@gmail.com>
2021-12-28 21:42:48 +0000

    Supported custom bar button children

--- NavigationReactNativeWeb/src/BarButton.tsx
+++ NavigationReactNativeWeb/src/BarButton.tsx
@@ -1,16 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, ReactElement } from 'react';
 import { Image, Text, TouchableOpacity } from 'react-native';
 import NavigationBarContext from './NavigationBarContext';
+import ActionBar from './ActionBar';
 
-const BarButton = ({title, image, testID, onPress}) => {
+const BarButton = ({title, image, testID, onPress, children}) => {
     const tintColor = useContext(NavigationBarContext);
-    return (
-        <TouchableOpacity testID={testID} onPress={onPress} style={{marginStart: 24}}>
-            {image
-                ? <Image source={image} style={{width: 24, height: 24, tintColor}} accessibilityLabel={title} />
-                : <Text style={{color: tintColor}}>{title}</Text>}
-        </TouchableOpacity>
-    );
+    const childrenArray = (React.Children.toArray(children) as ReactElement<any>[]);
+    return (childrenArray.length === 0 || childrenArray[0]?.type === ActionBar)
+        ? (
+            <TouchableOpacity testID={testID} onPress={onPress} style={{marginStart: 24}}>
+                {image
+                    ? <Image source={image} style={{width: 24, height: 24, tintColor}} accessibilityLabel={title} />
+                    : <Text style={{color: tintColor}}>{title}</Text>}
+            </TouchableOpacity>
+        ) : children;
 }
 
 export default BarButton;
