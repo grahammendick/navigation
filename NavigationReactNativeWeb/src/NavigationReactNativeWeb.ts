a95b99f52307790c94f6b475aecfb76d8590db7f
Graham Mendick <graham.mendick@gmail.com>
2021-12-28 21:45:50 +0000

    Exported action bar

--- NavigationReactNativeWeb/src/NavigationReactNativeWeb.ts
+++ NavigationReactNativeWeb/src/NavigationReactNativeWeb.ts
@@ -2,10 +2,11 @@ import NavigationBar from './NavigationBar';
 import LeftBar from './LeftBar';
 import RightBar from './RightBar';
 import BarButton from './BarButton';
+import ActionBar from './ActionBar';
 import CollapsingBar from './CollapsingBar';
 import TabBar from './TabBar';
 import TabBarItem from './TabBarItem';
 import CoordinatorLayout from './CoordinatorLayout';
 import useNavigated from './useNavigated';
 
-export { NavigationBar, LeftBar, RightBar, BarButton, CollapsingBar, TabBar, TabBarItem, CoordinatorLayout, useNavigated };
+export { NavigationBar, LeftBar, RightBar, BarButton, ActionBar, CollapsingBar, TabBar, TabBarItem, CoordinatorLayout, useNavigated };
