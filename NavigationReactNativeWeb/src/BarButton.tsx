9da6b7ce2c865f3a1ed0cdec4cff89494f212254
Graham Mendick <graham.mendick@gmail.com>
2021-12-28 15:02:00 +0000

    Supported more props in bar button

--- NavigationReactNativeWeb/src/BarButton.tsx
+++ NavigationReactNativeWeb/src/BarButton.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, Text, TouchableOpacity } from 'react-native';
 
-const BarButton = ({title}) => (
-    <View>{title}</View>
+const BarButton = ({title, image, testID, onPress}) => (
+    <TouchableOpacity testID={testID} onPress={onPress}>
+        {image
+            ? <Image source={image} style={{width: 24, height: 24}} />
+            : <Text>{title}</Text>}
+    </TouchableOpacity>
 );
 
 export default BarButton;
