ca7d00dace2ef99873032c74a78fc417bc3b81af
Graham Mendick <graham.mendick@gmail.com>
2021-12-31 19:22:18 +0000

    Supported href on bar buttons

--- NavigationReactNativeWeb/src/BarButton.tsx
+++ NavigationReactNativeWeb/src/BarButton.tsx
@@ -3,12 +3,12 @@ import { Image, Text, TouchableOpacity } from 'react-native';
 import NavigationBarContext from './NavigationBarContext';
 import ActionBar from './ActionBar';
 
-const BarButton = ({title, image, testID, onPress, children}) => {
+const BarButton = ({title, image, href, testID, onPress, children}) => {
     const tintColor = useContext(NavigationBarContext);
     const childrenArray = (React.Children.toArray(children) as ReactElement<any>[]);
     return (childrenArray.length === 0 || childrenArray[0]?.type === ActionBar)
         ? (
-            <TouchableOpacity testID={testID} onPress={onPress} style={{marginStart: 24}}>
+            <TouchableOpacity href={href} testID={testID} onPress={onPress} style={{marginStart: 24}}>
                 {image
                     ? <Image source={image} style={{width: 24, height: 24, tintColor}} accessibilityLabel={title} />
                     : <Text style={{color: tintColor}}>{title}</Text>}
