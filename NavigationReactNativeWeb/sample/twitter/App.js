be413ee307f8d14ef1ef4862de57cb81469e2287
Graham Mendick <graham.mendick@gmail.com>
2021-12-31 10:34:09 +0000

    Supported custom navigation motion animation
    Accepted unmounted/mounted/crumbed style props (and children) that are passed to underlying NavigationMotion.  Safer to avoid clashing with existing unmount/mount/crumbStyle props on NavigationStack - better for typescript and clearer?

--- NavigationReactNativeWeb/sample/twitter/App.js
+++ NavigationReactNativeWeb/sample/twitter/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Platform} from 'react-native';
+import {Platform, View} from 'react-native';
 import {StateNavigator} from 'navigation';
 import {NavigationHandler} from 'navigation-react';
 import {NavigationStack, TabBar, TabBarItem} from 'navigation-react-native';
@@ -18,7 +18,24 @@ const Stack = ({navigator}) => (
   <NavigationHandler stateNavigator={navigator}>
     <NavigationStack
       crumbStyle={from => from ? 'scale_in' : 'scale_out'}
-      unmountStyle={from => from ? 'slide_in' : 'slide_out'} />
+      unmountStyle={from => from ? 'slide_in' : 'slide_out'}
+      unmountedStyle={{translate: 100, scale: 1, opacity: 1}}
+      mountedStyle={{translate: 0, scale: 1, opacity: 1}}
+      crumbedStyle={{translate: 5, scale: 0.9, opacity: 0}}>
+      {({translate, scale, opacity}, scene, key) => (
+        <View key={key}
+          style={{
+            transform: `translate(${translate}%) scale(${scale}, ${scale})`,
+            opacity,
+            position: 'absolute',
+            backgroundColor: '#fff',
+            left: 0, right: 0, top: 0, bottom: 0,
+            overflow: 'hidden'
+          }}>
+          {scene}
+        </View>
+      )}
+    </NavigationStack>
   </NavigationHandler>
 );
 
--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { View } from 'react-native';
 import { NavigationMotion } from 'navigation-react-mobile';
 
-const NavigationStack = () => (
+const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, children}) => (
     <NavigationMotion
-        unmountedStyle={{translate: 100}}
-        mountedStyle={{translate: 0}}
-        crumbStyle={{translate: 0}}>
-        {({translate}, scene, key) => (
-            <View key={key}
-              style={{
-                transform: `translate(${translate}%)` as any,
-                position: 'absolute',
-                backgroundColor: '#fff',
-                left: 0, right: 0, top: 0, bottom: 0,
-                overflow: 'hidden',
-              }}>
-              {scene}
-            </View>
-          )}
+        unmountedStyle={unmountedStyle || {translate: 100}}
+        mountedStyle={mountedStyle || {translate: 0}}
+        crumbStyle={crumbedStyle || {translate: 0}}>
+          {typeof children !== 'function' ?
+            ({translate}, scene, key) => (
+                <View key={key}
+                  style={{
+                    transform: `translate(${translate}%)` as any,
+                    position: 'absolute',
+                    backgroundColor: '#fff',
+                    left: 0, right: 0, top: 0, bottom: 0,
+                    overflow: 'hidden',
+                  }}>
+                  {scene}
+                </View>
+              ) : children}
     </NavigationMotion>
 )
 
