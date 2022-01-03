824c8901adb5856e7c1e63d1a9a8dee81b56c7e2
Graham Mendick <graham.mendick@gmail.com>
2022-01-03 15:24:22 +0000

    Implemented font props of BarButton
    Also uppercased text only button to match android

--- NavigationReactNativeWeb/src/BarButton.tsx
+++ NavigationReactNativeWeb/src/BarButton.tsx
@@ -3,7 +3,7 @@ import { Image, Text, TouchableOpacity } from 'react-native';
 import NavigationBarContext from './NavigationBarContext';
 import ActionBar from './ActionBar';
 
-const BarButton = ({title, image, search, href, testID, onPress, children}) => {
+const BarButton = ({title, image, search, fontFamily, fontWeight, fontStyle, fontSize = 14, href, testID, onPress, children}) => {
     const tintColor = useContext(NavigationBarContext);
     const childrenArray = (React.Children.toArray(children) as ReactElement<any>[]);
     if (search) return null;
@@ -12,7 +12,7 @@ const BarButton = ({title, image, search, href, testID, onPress, children}) => {
             <TouchableOpacity href={href} testID={testID} onPress={onPress} style={{marginStart: 24}}>
                 {image
                     ? <Image source={image} style={{width: 24, height: 24, tintColor}} accessibilityLabel={title} />
-                    : <Text style={{color: tintColor}}>{title}</Text>}
+                    : <Text style={{color: tintColor, fontFamily, fontWeight, fontStyle, fontSize, textTransform: 'uppercase'}}>{title}</Text>}
             </TouchableOpacity>
         ) : children;
 }
