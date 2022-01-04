2ba83bfc50b935b900dde74ee84dc8fb1b06a79e
Graham Mendick <graham.mendick@gmail.com>
2022-01-04 18:52:16 +0000

    Accepted renderScene prop on NavigationStack

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -3,13 +3,14 @@ import { View } from 'react-native';
 import { NavigationMotion, SharedElementMotion } from 'navigation-react-mobile';
 import { MobileHistoryManager } from 'navigation-react-mobile';
 
-const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElementTransition, duration, children}) => (
+const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElementTransition, duration, renderScene, children}) => (
     <NavigationMotion
         unmountedStyle={unmountedStyle || {translate: 100}}
         mountedStyle={mountedStyle || {translate: 0}}
         crumbStyle={crumbedStyle || {translate: 0}}        
         sharedElementMotion={sharedElementTransition}
-        duration={duration}>
+        duration={duration}
+        renderScene={renderScene}>
           {typeof children !== 'function' ?
             ({translate}, scene, key) => (
                 <View key={key}
