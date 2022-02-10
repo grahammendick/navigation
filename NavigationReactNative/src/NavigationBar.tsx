43f13221fb57e361e2e9117b6db8e585c93e9a0f
Dan Woodward <dan.woodward@ratedpeople.com>
2022-02-10 14:00:01 +0000

    Undid prettier changes

--- NavigationReactNative/src/NavigationBar.tsx
+++ NavigationReactNative/src/NavigationBar.tsx
@@ -1,153 +1,92 @@
-import React, { ReactElement } from "react";
-import {
-  requireNativeComponent,
-  Image,
-  Platform,
-  Animated,
-} from "react-native";
-import LeftBar from "./LeftBar";
-import RightBar from "./RightBar";
-import SearchBar from "./SearchBar";
-import TitleBar from "./TitleBar";
-import CollapsingBar from "./CollapsingBar";
-import TabBar from "./TabBar";
-import StatusBar from "./StatusBar";
-import BottomAppBar from "./BottomAppBar";
+import React, { ReactElement } from 'react';
+import { requireNativeComponent, Image, Platform, Animated } from 'react-native';
+import LeftBar from './LeftBar';
+import RightBar from './RightBar';
+import SearchBar from './SearchBar';
+import TitleBar from './TitleBar';
+import CollapsingBar from './CollapsingBar';
+import TabBar from './TabBar';
+import StatusBar from './StatusBar';
+import BottomAppBar from './BottomAppBar';
 
 class NavigationBar extends React.Component<any, any> {
-  constructor(props) {
-    super(props);
-  }
-  getScrollEdgeProps() {
-    var {
-      barTintColor,
-      titleColor,
-      titleFontFamily,
-      titleFontWeight,
-      titleFontStyle,
-      titleFontSize,
-    } = this.props;
-    return {
-      barTintColor:
-        typeof barTintColor === "function" ? barTintColor(true) : barTintColor,
-      largeBarTintColor:
-        typeof barTintColor === "function" ? barTintColor(false) : barTintColor,
-      titleColor:
-        typeof titleColor === "function" ? titleColor(true) : titleColor,
-      largeTitleColor:
-        typeof titleColor === "function" ? titleColor(false) : titleColor,
-      titleFontFamily:
-        typeof titleFontFamily === "function"
-          ? titleFontFamily(true)
-          : titleFontFamily,
-      largeTitleFontFamily:
-        typeof titleFontFamily === "function"
-          ? titleFontFamily(false)
-          : titleFontFamily,
-      titleFontWeight:
-        typeof titleFontWeight === "function"
-          ? titleFontWeight(true)
-          : titleFontWeight,
-      largeTitleFontWeight:
-        typeof titleFontWeight === "function"
-          ? titleFontWeight(false)
-          : titleFontWeight,
-      titleFontStyle:
-        typeof titleFontStyle === "function"
-          ? titleFontStyle(true)
-          : titleFontStyle,
-      largeTitleFontStyle:
-        typeof titleFontStyle === "function"
-          ? titleFontStyle(false)
-          : titleFontStyle,
-      titleFontSize:
-        typeof titleFontSize === "function"
-          ? titleFontSize(true)
-          : titleFontSize,
-      largeTitleFontSize:
-        typeof titleFontSize === "function" ? titleFontSize(false) : null,
-    };
-  }
-  render() {
-    var {
-      bottomBar,
-      hidden,
-      logo,
-      navigationImage,
-      overflowImage,
-      children,
-      style = { height: undefined },
-      ...otherProps
-    } = this.props;
-    var scrollEdgeProps = this.getScrollEdgeProps();
-    var childrenArray = React.Children.toArray(children) as ReactElement<any>[];
-    var statusBar = childrenArray.find(({ type }) => type === StatusBar);
-    statusBar =
-      (Platform.OS === "android" || !statusBar) && (statusBar || <StatusBar />);
-    if (Platform.OS === "android" && hidden) return statusBar;
-    if (bottomBar) return <BottomAppBar {...this.props} />;
-    var collapsingBar =
-      Platform.OS === "android" &&
-      childrenArray.find(({ type }) => type === CollapsingBar);
-    return (
-      <>
-        <NVNavigationBar
-          hidden={hidden}
-          style={{ height: !!collapsingBar ? style.height : null }}
-          {...otherProps}
-          {...scrollEdgeProps}
-          barTintColor={
-            !collapsingBar
-              ? scrollEdgeProps.barTintColor
-              : scrollEdgeProps.largeBarTintColor
-          }
-        >
-          {Platform.OS === "ios" ? (
-            !hidden && children
-          ) : (
-            <Container
-              collapse={!!collapsingBar}
-              {...otherProps}
-              {...scrollEdgeProps}
-              {...(collapsingBar && collapsingBar.props)}
-            >
-              {collapsingBar && collapsingBar.props.children}
-              <NVToolbar
-                logo={Image.resolveAssetSource(logo)}
-                navigationImage={Image.resolveAssetSource(navigationImage)}
-                overflowImage={Image.resolveAssetSource(overflowImage)}
-                pin={!!collapsingBar}
-                {...otherProps}
-                {...scrollEdgeProps}
-                barTintColor={
-                  !collapsingBar ? scrollEdgeProps.barTintColor : null
-                }
-                style={{ height: 56 }}
-              >
-                {[
-                  childrenArray.find(({ type }) => type === TitleBar),
-                  childrenArray.find(({ type }) => type === LeftBar),
-                  childrenArray.find(({ type }) => type === RightBar),
-                ]}
-              </NVToolbar>
-              {childrenArray.find(({ type }) => type === TabBar)}
-            </Container>
-          )}
-          {Platform.OS === "ios" && statusBar}
-        </NVNavigationBar>
-        {Platform.OS === "android" && statusBar}
-        {Platform.OS === "ios"
-          ? null
-          : childrenArray.find(({ type }) => type === SearchBar)}
-      </>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+    getScrollEdgeProps() {
+        var {barTintColor, titleColor, titleFontFamily, titleFontWeight, titleFontStyle, titleFontSize } = this.props;
+        return {
+            barTintColor: typeof barTintColor === 'function' ? barTintColor(true) : barTintColor,
+            largeBarTintColor: typeof barTintColor === 'function' ? barTintColor(false) : barTintColor,
+            titleColor: typeof titleColor === 'function' ? titleColor(true) : titleColor,
+            largeTitleColor: typeof titleColor === 'function' ? titleColor(false) : titleColor,
+            titleFontFamily: typeof titleFontFamily === 'function' ? titleFontFamily(true) : titleFontFamily,
+            largeTitleFontFamily: typeof titleFontFamily === 'function' ? titleFontFamily(false) : titleFontFamily,
+            titleFontWeight: typeof titleFontWeight === 'function' ? titleFontWeight(true) : titleFontWeight,
+            largeTitleFontWeight: typeof titleFontWeight === 'function' ? titleFontWeight(false) : titleFontWeight,
+            titleFontStyle: typeof titleFontStyle === 'function' ? titleFontStyle(true) : titleFontStyle,
+            largeTitleFontStyle: typeof titleFontStyle === 'function' ? titleFontStyle(false) : titleFontStyle,
+            titleFontSize: typeof titleFontSize === 'function' ? titleFontSize(true) : titleFontSize,
+            largeTitleFontSize: typeof titleFontSize === 'function' ? titleFontSize(false) : null,
+        }
+    }
+    render() {
+        var {bottomBar, hidden, logo, navigationImage, overflowImage, children, style = {height: undefined}, ...otherProps} = this.props;
+        var scrollEdgeProps = this.getScrollEdgeProps()
+        var childrenArray = (React.Children.toArray(children) as ReactElement<any>[]);
+        var statusBar = childrenArray.find(({type}) => type === StatusBar);
+        statusBar = (Platform.OS === 'android' || !statusBar) && (statusBar || <StatusBar />);
+        if (Platform.OS === 'android' && hidden)
+            return statusBar;
+        if (bottomBar)
+            return <BottomAppBar {...this.props} />
+        var collapsingBar = Platform.OS === 'android' && childrenArray.find(({type}) => type === CollapsingBar);
+        return (
+            <>
+                <NVNavigationBar
+                    hidden={hidden}
+                    style={{height: !!collapsingBar ? style.height : null}}                    
+                    {...otherProps}
+                    {...scrollEdgeProps}
+                    barTintColor={!collapsingBar ? scrollEdgeProps.barTintColor : scrollEdgeProps.largeBarTintColor}>
+                    {Platform.OS === 'ios' ? !hidden && children :
+                        <Container
+                            collapse={!!collapsingBar}
+                            {...otherProps}
+                            {...scrollEdgeProps}
+                            {...(collapsingBar && collapsingBar.props)}>
+                            {collapsingBar && collapsingBar.props.children}
+                            <NVToolbar
+                                logo={Image.resolveAssetSource(logo)}
+                                navigationImage={Image.resolveAssetSource(navigationImage)}
+                                overflowImage={Image.resolveAssetSource(overflowImage)}
+                                pin={!!collapsingBar}
+                                {...otherProps}
+                                {...scrollEdgeProps}
+                                barTintColor={!collapsingBar ? scrollEdgeProps.barTintColor : null}
+                                style={{height: 56}}>
+                                {[
+                                    childrenArray.find(({type}) => type === TitleBar),
+                                    childrenArray.find(({type}) => type === LeftBar),
+                                    childrenArray.find(({type}) => type === RightBar)
+                                ]}
+                            </NVToolbar>
+                            {childrenArray.find(({type}) => type === TabBar)}
+                        </Container>}
+                    {Platform.OS === 'ios' && statusBar}
+                </NVNavigationBar>
+                {Platform.OS === 'android' && statusBar}
+                {Platform.OS === 'ios' ? null : childrenArray.find(({type}) => type === SearchBar)}
+            </>
+        )
+    }
 }
 
-var Container: any = ({ collapse, children, ...props }) =>
-  !collapse ? children : <CollapsingBar {...props}>{children}</CollapsingBar>;
+var Container: any = ({collapse, children, ...props}) => (
+    !collapse ? children : <CollapsingBar {...props}>{children}</CollapsingBar>
+)
 
-var NVNavigationBar = requireNativeComponent<any>("NVNavigationBar", null);
-var NVToolbar = requireNativeComponent<any>("NVToolbar", null);
+var NVNavigationBar = requireNativeComponent<any>('NVNavigationBar', null);
+var NVToolbar = requireNativeComponent<any>('NVToolbar', null);
 
 export default Animated.createAnimatedComponent(NavigationBar);
--- NavigationReactNative/src/TabBar.tsx
+++ NavigationReactNative/src/TabBar.tsx
@@ -1,162 +1,124 @@
-import React from "react";
-import {
-  requireNativeComponent,
-  Platform,
-  StyleSheet,
-  View,
-  I18nManager,
-} from "react-native";
-import BackButton from "./BackButton";
+import React from 'react';
+import { requireNativeComponent, Platform, StyleSheet, View, I18nManager } from 'react-native';
+import BackButton from './BackButton';
 
 class TabBar extends React.Component<any, any> {
-  private ref: React.RefObject<View>;
-  private swiping = false;
-  constructor(props) {
-    super(props);
-    this.state = { selectedTab: props.tab || props.defaultTab };
-    this.ref = React.createRef<View>();
-    this.onTabSelected = this.onTabSelected.bind(this);
-    this.onTabSwipeStateChanged = this.onTabSwipeStateChanged.bind(this);
-  }
-  static defaultProps = {
-    defaultTab: 0,
-    scrollable: false,
-    primary: Platform.OS === "ios",
-    scrollsToTop: true,
-  };
-  static getDerivedStateFromProps({ tab }, { selectedTab }) {
-    if (tab != null && tab !== selectedTab) return { selectedTab: tab };
-    return null;
-  }
-  onTabSelected({ nativeEvent }) {
-    var { eventCount: mostRecentEventCount, tab } = nativeEvent;
-    this.ref.current.setNativeProps({ mostRecentEventCount });
-    this.changeTab(tab);
-  }
-  onTabSwipeStateChanged({ nativeEvent }) {
-    this.swiping = nativeEvent.swiping;
-  }
-  changeTab(selectedTab) {
-    var { tab, onChangeTab } = this.props;
-    if (this.state.selectedTab !== selectedTab) {
-      if (tab == null) this.setState({ selectedTab });
-      if (!!onChangeTab) onChangeTab(selectedTab);
-      return true;
+    private ref: React.RefObject<View>;
+    private swiping = false;
+    constructor(props) {
+        super(props);
+        this.state = {selectedTab: props.tab || props.defaultTab};
+        this.ref = React.createRef<View>();
+        this.onTabSelected = this.onTabSelected.bind(this);
+        this.onTabSwipeStateChanged = this.onTabSwipeStateChanged.bind(this);
     }
-    return false;
-  }
-  render() {
-    var {
-      children,
-      labelVisibility = "auto",
-      barTintColor,
-      selectedTintColor,
-      unselectedTintColor,
-      bottomTabs,
-      scrollable,
-      primary,
-      scrollsToTop,
-    } = this.props;
-    bottomTabs = bottomTabs != null ? bottomTabs : primary;
-    var tabBarItems = React.Children.toArray(children).filter(
-      (child) => !!child
-    );
-    var titleOnly = !tabBarItems.find(
-      ({ props }: any) => props.title && props.image
-    );
-    var { fontFamily, fontWeight, fontStyle, fontSize } =
-      (tabBarItems[0] as any)?.props || {};
-    var tabViewHeight = !primary ? (titleOnly ? 48 : 72) : 56;
-    tabViewHeight = Platform.OS === "android" ? tabViewHeight : 28;
-    // Default to auto
-    var labelVisibilityVal = -1;
+    static defaultProps = {
+        defaultTab: 0,
+        scrollable: false,
+        primary: Platform.OS === 'ios',
+        scrollsToTop: true,
+    }
+    static getDerivedStateFromProps({tab}, {selectedTab}) {
+        if (tab != null && tab !== selectedTab)
+            return {selectedTab: tab};
+        return null;
+    }
+    onTabSelected({nativeEvent}) {
+        var {eventCount: mostRecentEventCount, tab} = nativeEvent;
+        this.ref.current.setNativeProps({mostRecentEventCount});
+        this.changeTab(tab);
+    }
+    onTabSwipeStateChanged({nativeEvent}) {
+        this.swiping = nativeEvent.swiping;
+    }
+    changeTab(selectedTab) {
+        var {tab, onChangeTab} = this.props;
+        if (this.state.selectedTab !== selectedTab) {
+            if (tab == null)
+                this.setState({selectedTab});
+            if (!!onChangeTab)
+                onChangeTab(selectedTab);
+            return true;
+        }
+        return false;
+    }
+    render() {
+        var {children, labelVisibility = "auto", barTintColor, selectedTintColor, unselectedTintColor, bottomTabs, scrollable, primary, scrollsToTop} = this.props;
+        bottomTabs = bottomTabs != null ? bottomTabs : primary;
+        var tabBarItems = React.Children.toArray(children).filter(child => !!child);
+        var titleOnly = !tabBarItems.find(({props}: any) => props.title && props.image);
+        var {fontFamily, fontWeight, fontStyle, fontSize} = (tabBarItems[0] as any)?.props || {};
+        var tabViewHeight = !primary ? (titleOnly ? 48 : 72) : 56
+        tabViewHeight = Platform.OS === 'android' ? tabViewHeight : 28;
+        var TabBarPager = (Platform.OS === 'ios' || !I18nManager.isRTL) ? NVTabBarPager : NVTabBarPagerRTL;
+        var TabBar = primary ? NVTabBar : TabBarPager;
+        var TabView = primary ? NVTabNavigation : (!I18nManager.isRTL ? NVTabLayout : NVTabLayoutRTL);
+        TabView = Platform.OS === 'android' ? TabView : NVSegmentedTab;
+        // Default to auto
+        var labelVisibilityVal = -1;
 
-    if (labelVisibility === "show") {
-      labelVisibilityVal = 1;
-    } else if (labelVisibility === "hide") {
-      labelVisibilityVal = 2;
+        if (labelVisibility === "show") {
+          labelVisibilityVal = 1;
+        } else if (labelVisibility === "hide") {
+          labelVisibilityVal = 2;
+        }
+        var tabLayout = (Platform.OS === 'android' || !primary) && (
+            <TabView
+                bottomTabs={bottomTabs}
+                labelVisibility={labelVisibilityVal}
+                selectedTintColor={selectedTintColor}
+                unselectedTintColor={unselectedTintColor}
+                selectedIndicatorAtTop={bottomTabs}
+                tabs={tabBarItems.map(({props: {title, testID}}: any) => ({title, testID}))}
+                fontFamily={fontFamily} fontWeight={fontWeight}
+                fontStyle={fontStyle} fontSize={fontSize}
+                scrollable={scrollable}
+                style={{
+                    height: tabViewHeight,
+                    backgroundColor: barTintColor
+                }} />
+        );
+        return (
+            <>
+                {!bottomTabs && tabLayout}
+                {!!tabBarItems.length && <TabBar
+                    ref={this.ref}
+                    tabCount={tabBarItems.length}
+                    onTabSelected={this.onTabSelected}
+                    onTabSwipeStateChanged={this.onTabSwipeStateChanged}
+                    onMoveShouldSetResponderCapture={() => this.swiping}
+                    selectedTab={this.state.selectedTab}
+                    barTintColor={barTintColor}
+                    selectedTintColor={selectedTintColor}
+                    unselectedTintColor={unselectedTintColor}
+                    scrollsToTop={scrollsToTop}
+                    style={styles.tabBar}>
+                        <BackButton onPress={() => this.changeTab(0)} />
+                        {tabBarItems
+                            .filter(child => !!child)
+                            .map((child: any, index) => {
+                                var selected = index === this.state.selectedTab;
+                                return React.cloneElement(child, {...child.props, index, selected})
+                            })}
+                </TabBar>}
+                {bottomTabs && tabLayout}
+            </>
+        );
     }
-    var TabBarPager =
-      Platform.OS === "ios" || !I18nManager.isRTL
-        ? NVTabBarPager
-        : NVTabBarPagerRTL;
-    var TabBar = primary ? NVTabBar : TabBarPager;
-    var TabView = primary
-      ? NVTabNavigation
-      : !I18nManager.isRTL
-      ? NVTabLayout
-      : NVTabLayoutRTL;
-    TabView = Platform.OS === "android" ? TabView : NVSegmentedTab;
-    var tabLayout = (Platform.OS === "android" || !primary) && (
-      <TabView
-        bottomTabs={bottomTabs}
-        labelVisibility={labelVisibilityVal}
-        selectedTintColor={selectedTintColor}
-        unselectedTintColor={unselectedTintColor}
-        selectedIndicatorAtTop={bottomTabs}
-        tabs={tabBarItems.map(({ props: { title, testID } }: any) => ({
-          title,
-          testID,
-        }))}
-        fontFamily={fontFamily}
-        fontWeight={fontWeight}
-        fontStyle={fontStyle}
-        fontSize={fontSize}
-        scrollable={scrollable}
-        style={{
-          height: tabViewHeight,
-          backgroundColor: barTintColor,
-        }}
-      />
-    );
-    return (
-      <>
-        {!bottomTabs && tabLayout}
-        {!!tabBarItems.length && (
-          <TabBar
-            ref={this.ref}
-            tabCount={tabBarItems.length}
-            onTabSelected={this.onTabSelected}
-            onTabSwipeStateChanged={this.onTabSwipeStateChanged}
-            onMoveShouldSetResponderCapture={() => this.swiping}
-            selectedTab={this.state.selectedTab}
-            barTintColor={barTintColor}
-            selectedTintColor={selectedTintColor}
-            unselectedTintColor={unselectedTintColor}
-            scrollsToTop={scrollsToTop}
-            style={styles.tabBar}
-          >
-            <BackButton onPress={() => this.changeTab(0)} />
-            {tabBarItems
-              .filter((child) => !!child)
-              .map((child: any, index) => {
-                var selected = index === this.state.selectedTab;
-                return React.cloneElement(child, {
-                  ...child.props,
-                  index,
-                  selected,
-                });
-              })}
-          </TabBar>
-        )}
-        {bottomTabs && tabLayout}
-      </>
-    );
-  }
 }
 
-var NVTabLayout = requireNativeComponent<any>("NVTabLayout", null);
-var NVTabLayoutRTL = requireNativeComponent<any>("NVTabLayoutRTL", null);
-var NVTabNavigation = requireNativeComponent<any>("NVTabNavigation", null);
-var NVSegmentedTab = requireNativeComponent<any>("NVSegmentedTab", null);
-var NVTabBar = requireNativeComponent<any>("NVTabBar", null);
-var NVTabBarPager = requireNativeComponent<any>("NVTabBarPager", null);
-var NVTabBarPagerRTL = requireNativeComponent<any>("NVTabBarPagerRTL", null);
+var NVTabLayout = requireNativeComponent<any>('NVTabLayout', null);
+var NVTabLayoutRTL = requireNativeComponent<any>('NVTabLayoutRTL', null);
+var NVTabNavigation = requireNativeComponent<any>('NVTabNavigation', null);
+var NVSegmentedTab = requireNativeComponent<any>('NVSegmentedTab', null);
+var NVTabBar = requireNativeComponent<any>('NVTabBar', null);
+var NVTabBarPager = requireNativeComponent<any>('NVTabBarPager', null);
+var NVTabBarPagerRTL = requireNativeComponent<any>('NVTabBarPagerRTL', null);
 
 const styles = StyleSheet.create({
-  tabBar: {
-    flex: 1,
-  },
+    tabBar: {
+        flex: 1,
+    },
 });
 
 export default TabBar;
