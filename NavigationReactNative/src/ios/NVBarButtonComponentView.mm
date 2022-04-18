11188eb935d3daac717842a6fb32baa23bda5ef5
Graham Mendick <graham.mendick@gmail.com>
2022-04-18 12:09:22 +0100

    Handled system item prop on bar button
    Implemented prepareForRecycle so that changing from system item to title works. Need to reset the button now that views are reused (recycled). So a bar button with system item can be reused as bar button without system item so need to reset it when unmounted

--- NavigationReactNative/src/ios/NVBarButtonComponentView.mm
+++ NavigationReactNative/src/ios/NVBarButtonComponentView.mm
@@ -58,7 +58,40 @@ using namespace facebook::react;
     NSString *testID = [[NSString alloc] initWithUTF8String: newViewProps.testID.c_str()];
     if (self.button.accessibilityIdentifier != testID)
         self.button.accessibilityIdentifier = testID;
+    NSString *systemItemVal = [[NSString alloc] initWithUTF8String: newViewProps.systemItem.c_str()];
+    if (systemItemVal.length) {
+        UIBarButtonSystemItem systemItem = [self systemItem:systemItemVal];
+        self.button = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:systemItem target:self action:@selector(buttonPressed)];
+        self.button.accessibilityIdentifier = testID;
+    }
     [super updateProps:props oldProps:oldProps];
+}
+
+-(UIBarButtonSystemItem)systemItem:(NSString*)val
+{
+    if ([val isEqualToString:@"done"]) return UIBarButtonSystemItemDone;
+    if ([val isEqualToString:@"cancel"]) return UIBarButtonSystemItemCancel;
+    if ([val isEqualToString:@"edit"]) return UIBarButtonSystemItemEdit;
+    if ([val isEqualToString:@"save"]) return UIBarButtonSystemItemSave;
+    if ([val isEqualToString:@"add"]) return UIBarButtonSystemItemAdd;
+    if ([val isEqualToString:@"flexible-space"]) return UIBarButtonSystemItemFlexibleSpace;
+    if ([val isEqualToString:@"fixed-space"]) return UIBarButtonSystemItemFixedSpace;
+    if ([val isEqualToString:@"compose"]) return UIBarButtonSystemItemCompose;
+    if ([val isEqualToString:@"reply"]) return UIBarButtonSystemItemReply;
+    if ([val isEqualToString:@"action"]) return UIBarButtonSystemItemAction;
+    if ([val isEqualToString:@"organize"]) return UIBarButtonSystemItemOrganize;
+    if ([val isEqualToString:@"bookmarks"]) return UIBarButtonSystemItemBookmarks;
+    if ([val isEqualToString:@"search"]) return UIBarButtonSystemItemSearch;
+    if ([val isEqualToString:@"refresh"]) return UIBarButtonSystemItemRefresh;
+    if ([val isEqualToString:@"stop"]) return UIBarButtonSystemItemStop;
+    if ([val isEqualToString:@"camera"]) return UIBarButtonSystemItemCamera;
+    if ([val isEqualToString:@"trash"]) return UIBarButtonSystemItemTrash;
+    if ([val isEqualToString:@"play"]) return UIBarButtonSystemItemPlay;
+    if ([val isEqualToString:@"pause"]) return UIBarButtonSystemItemPause;
+    if ([val isEqualToString:@"rewind"]) return UIBarButtonSystemItemRewind;
+    if ([val isEqualToString:@"fast-forward"]) return UIBarButtonSystemItemFastForward;
+    if ([val isEqualToString:@"undo"]) return UIBarButtonSystemItemUndo;
+    return UIBarButtonSystemItemRedo;
 }
 
 -(void)buttonPressed
@@ -70,6 +103,15 @@ using namespace facebook::react;
     self.frame = [barView convertRect:labelFrameInBar toView:nil];
     std::static_pointer_cast<NVBarButtonEventEmitter const>(_eventEmitter)
         ->onPress(NVBarButtonEventEmitter::OnPress{});
+}
+
+- (void)prepareForRecycle
+{
+    [super prepareForRecycle];
+    self.button = [[UIBarButtonItem alloc] init];
+    self.button.style = UIBarButtonItemStylePlain;
+    self.button.target = self;
+    self.button.action = @selector(buttonPressed);
 }
 
 #pragma mark - RCTComponentViewProtocol
