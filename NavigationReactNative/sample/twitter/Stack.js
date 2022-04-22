bf5d2377d99314d4e4756be9d12f2c19f1c7bdd4
Graham Mendick <graham.mendick@gmail.com>
2022-04-22 21:14:06 +0100

    Tweaked format

--- NavigationReactNative/sample/twitter/Stack.js
+++ NavigationReactNative/sample/twitter/Stack.js
@@ -7,23 +7,23 @@ const Stack = ({ children, ...props }) => {
   const { stateNavigator, state } = useContext(NavigationContext);
   const { scenes, firstScene } = useMemo(() => (
     React.Children.toArray(children)
-      .reduce(({ scenes, firstScene }, { props: { name, view } }) => (
-        { scenes: {...scenes, [name]: view}, firstScene: firstScene || name }
-      ), { scenes: {}, firstScene: null })
+      .reduce(({scenes, firstScene}, {props: {name, view} }) => (
+        {scenes: {...scenes, [name]: view}, firstScene: firstScene || name}
+      ), {scenes: {}, firstScene: null})
   ), [children]);
   const [allScenes, setAllScenes] = useState(scenes);
   useEffect(() => {
     setAllScenes((prevScenes) => ({...prevScenes, ...scenes}));
     allScenes.current = {...allScenes.current, ...scenes};
-    const { crumbs, nextCrumb } = stateNavigator.stateContext;
-    const invalid = [...crumbs, nextCrumb].find(({ state }) => !scenes[state.key])
+    const {crumbs, nextCrumb} = stateNavigator.stateContext;
+    const invalid = [...crumbs, nextCrumb].find(({state}) => !scenes[state.key])
     if (invalid) {
       const url = stateNavigator.fluent().navigate(firstScene).url;
       stateNavigator.navigateLink(url);
     }
   }, [stateNavigator, scenes, firstScene]);
   useEffect(() => {
-    const validate = ({ key }) => !!scenes[key];
+    const validate = ({key}) => !!scenes[key];
     stateNavigator.onBeforeNavigate(validate);
     return () => stateNavigator.offBeforeNavigate(validate);
   }, [stateNavigator, scenes]);
