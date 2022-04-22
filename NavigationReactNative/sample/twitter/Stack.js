5e0a1a57154c9d8fb38ac7771d43a02551d25547
Graham Mendick <graham.mendick@gmail.com>
2022-04-22 20:18:54 +0100

    Fluently navigated to first scene if stack invalid
    Checked if any of the scenes are invalid - not in the children. If so then fluently navigate to the first scene - without context so it starts the stack over. Kept track of all the scene views otherwise will see a blank scene when an invalid scene disappears

--- NavigationReactNative/sample/twitter/Stack.js
+++ NavigationReactNative/sample/twitter/Stack.js
@@ -1,22 +1,34 @@
-import React, {useContext, useMemo} from 'react';
+import React, {useContext, useMemo, useEffect, useRef} from 'react';
 import { StateNavigator } from 'navigation';
 import { NavigationContext } from 'navigation-react';
 import { NavigationStack } from 'navigation-react-native';
 
 const Stack = ({ children, ...props }) => {
-  const { stateNavigator } = useContext(NavigationContext);
-  const { scenes, firstScene } = useMemo(() => {
-    const childrenArray = React.Children.toArray(children);
-    return childrenArray
-      .reduce(({ scenes, firstScene }, { props: { name, view } }) => {
-        scenes[name] = view;
-        firstScene = firstScene || view;
-        return { scenes, firstScene };
-      }, { scenes: {}, firstScene: null });
-  }, [children]);
+  const { stateNavigator, state } = useContext(NavigationContext);
+  const { scenes, firstScene } = useMemo(() => (
+    React.Children.toArray(children)
+      .reduce(({ scenes, firstScene }, { props: { name, view } }) => (
+        { scenes: {...scenes, [name]: view}, firstScene: firstScene || name }
+      ), { scenes: {}, firstScene: null })
+  ), [children]);
+  const allScenes = useRef(scenes);
+  useEffect(() => {
+    allScenes.current = {...allScenes.current, ...scenes};
+    const { crumbs, nextCrumb } = stateNavigator.stateContext;
+    const invalid = [...crumbs, nextCrumb].find(({ state }) => !scenes[state.key])
+    if (invalid) {
+      const url = stateNavigator.fluent().navigate(firstScene).url;
+      stateNavigator.navigateLink(url);
+    }
+  }, [stateNavigator, scenes, firstScene]);
+  useEffect(() => {
+    const validate = ({ key }) => !!scenes[key];
+    stateNavigator.onBeforeNavigate(validate);
+    return () => stateNavigator.offBeforeNavigate(validate);
+  }, [stateNavigator, scenes]);
   return (
       <NavigationStack
-        renderScene={(state, data) => scenes[state.key] || firstScene}
+        renderScene={(state, data) => allScenes.current[state.key]}
         {...props} />
   )
 }
