d30f2b5c5934c684f2713fa241f9e850fd97cdb8
Graham Mendick <graham.mendick@gmail.com>
2022-04-23 23:16:47 +0100

    Changed var to const
    Now using functional components makes sense to change to const

--- NavigationReactMobile/src/NavigationMotion.tsx
+++ NavigationReactMobile/src/NavigationMotion.tsx
@@ -17,39 +17,39 @@ const NavigationMotion = ({unmountedStyle, mountedStyle, crumbStyle, duration = 
     const {stateNavigator} = useContext(NavigationContext);
     const [motionState, setMotionState] = useState<NavigationMotionState>({stateNavigator: null, keys: []});
     const getSharedElements = () => {
-        var {crumbs, oldUrl} = stateNavigator.stateContext;
+        const {crumbs, oldUrl} = stateNavigator.stateContext;
         if (oldUrl !== null) {
-            var oldScene = oldUrl.split('crumb=').length - 1;
+            const oldScene = oldUrl.split('crumb=').length - 1;
             return sharedElementRegistry.current.getSharedElements(crumbs.length, oldScene);
         }
         return [];
     }
     const clearScene = (index) => {
-        var scene = getScenes().filter(scene => scene.key === index)[0];
+        const scene = getScenes().filter(scene => scene.key === index)[0];
         if (!scene)
             sharedElementRegistry.current.unregisterSharedElement(index);
     }
     const getScenes: () => SceneContext[] = () => {
-        var {keys} = motionState;
-        var {crumbs, nextCrumb, oldUrl} = stateNavigator.stateContext;
-        var backward = oldUrl && oldUrl.split('crumb=').length > crumbs.length + 1;
+        const {keys} = motionState;
+        const {crumbs, nextCrumb, oldUrl} = stateNavigator.stateContext;
+        const backward = oldUrl && oldUrl.split('crumb=').length > crumbs.length + 1;
         return crumbs.concat(nextCrumb).map(({state, data, url}, index, crumbsAndNext) => {
-            var preCrumbs = crumbsAndNext.slice(0, index);
-            var {state: nextState, data: nextData} = crumbsAndNext[index + 1] || {state: undefined, data: undefined};
-            var mount = url === nextCrumb.url;
-            var fromUnmounted = mount && !backward;
+            const preCrumbs = crumbsAndNext.slice(0, index);
+            const {state: nextState, data: nextData} = crumbsAndNext[index + 1] || {state: undefined, data: undefined};
+            const mount = url === nextCrumb.url;
+            const fromUnmounted = mount && !backward;
             return {key: keys[index], state, data, url, crumbs: preCrumbs, nextState, nextData, mount, fromUnmounted };
         });
     }
     const getStyle = (mounted: boolean, {state, data, crumbs, nextState, nextData, mount, fromUnmounted}: SceneContext) => {
-        var styleProp = !mounted ? unmountedStyle : (mount ? mountedStyle : crumbStyle);
+        const styleProp = !mounted ? unmountedStyle : (mount ? mountedStyle : crumbStyle);
         fromUnmounted = (mounted && mount) ? fromUnmounted : undefined;
-        var style = typeof styleProp === 'function' ? styleProp(state, data, crumbs, nextState, nextData, fromUnmounted) : styleProp;
+        const style = typeof styleProp === 'function' ? styleProp(state, data, crumbs, nextState, nextData, fromUnmounted) : styleProp;
         return {...style, __marker: !mounted ? 1 : (mount ? 0 : -1)};
     }
     const getMotion = (styles: MotionStyle[]) => {
-        var moving = false, mountMoving = false, mountDuration: number, mountProgress: number;
-        for(var {rest, progress, data: {mount}, style: {duration}} of styles) {
+        let moving = false, mountMoving = false, mountDuration: number, mountProgress: number;
+        for(const {rest, progress, data: {mount}, style: {duration}} of styles) {
             if (mount) {
                 mountMoving = !rest;
                 mountDuration = duration;
@@ -62,17 +62,17 @@ const NavigationMotion = ({unmountedStyle, mountedStyle, crumbStyle, duration = 
     const {keys: prevKeys, stateNavigator: prevStateNavigator} = motionState;
     if (prevStateNavigator !== stateNavigator) {
         setMotionState(() => {
-            var {state, crumbs, nextCrumb} = stateNavigator.stateContext;
-            var prevState = prevStateNavigator && prevStateNavigator.stateContext.state;
-            var currentKeys = crumbs.concat(nextCrumb).map((_, i) => '' + i);
-            var newKeys = currentKeys.slice(prevKeys.length);
-            var keys = prevKeys.slice(0, currentKeys.length).concat(newKeys);
+            const {state, crumbs, nextCrumb} = stateNavigator.stateContext;
+            const prevState = prevStateNavigator && prevStateNavigator.stateContext.state;
+            const currentKeys = crumbs.concat(nextCrumb).map((_, i) => '' + i);
+            const newKeys = currentKeys.slice(prevKeys.length);
+            const keys = prevKeys.slice(0, currentKeys.length).concat(newKeys);
             if (prevKeys.length === keys.length && prevState !== state)
                 keys[keys.length - 1] += '+';
             return {keys, stateNavigator};    
         })
     }
-    var {stateContext: {crumbs, oldState}, stateContext} = stateNavigator;
+    const {stateContext: {crumbs, oldState}, stateContext} = stateNavigator;
     return (stateContext.state &&
         <SharedElementContext.Provider value={sharedElementRegistry.current}>
             <Motion<SceneContext>
@@ -84,11 +84,11 @@ const NavigationMotion = ({unmountedStyle, mountedStyle, crumbStyle, duration = 
                 onRest={({key}) => clearScene(key)}
                 duration={duration}>
                 {styles => {
-                    var {rest, mountRest, mountDuration, mountProgress} = getMotion(styles);
+                    const {rest, mountRest, mountDuration, mountProgress} = getMotion(styles);
                     return (
                         styles.map(({data: {key, state, data}, style: {__marker, duration, ...style}}) => {
-                            var crumb = +key.replace(/\++$/, '');
-                            var scene = <Scene crumb={crumb} rest={rest} renderScene={renderScene} />;
+                            const crumb = +key.replace(/\++$/, '');
+                            const scene = <Scene crumb={crumb} rest={rest} renderScene={renderScene} />;
                             return (
                                 <Freeze key={key} enabled={rest && crumb < getScenes().length - 1}>
                                     {children(style, scene, key, crumbs.length === crumb, state, data)}
--- NavigationReactNative/src/NavigationStack.tsx
+++ NavigationReactNative/src/NavigationStack.tsx
@@ -33,23 +33,23 @@ const NavigationStack = ({underlayColor = '#000', title, crumbStyle = () => null
         return () => stateNavigator.offBeforeNavigate(validate);
     }, [children, stateNavigator, scenes]);
     const onWillNavigateBack = ({nativeEvent}) => {
-        var distance = stateNavigator.stateContext.crumbs.length - nativeEvent.crumb;
+        const distance = stateNavigator.stateContext.crumbs.length - nativeEvent.crumb;
         resumeNavigationRef.current = null;
         if (stateNavigator.canNavigateBack(distance)) {
-            var url = stateNavigator.getNavigationBackLink(distance);
+            const url = stateNavigator.getNavigationBackLink(distance);
             stateNavigator.navigateLink(url, undefined, true, (_stateContext, resumeNavigation) => {
                 resumeNavigationRef.current = resumeNavigation;
             });
         }
     }
     const onNavigateToTop = () => {
-        var {crumbs} = stateNavigator.stateContext;
+        const {crumbs} = stateNavigator.stateContext;
         if (crumbs.length > 0)
             stateNavigator.navigateBack(crumbs.length);
     }
     const onRest = ({nativeEvent}) => {
-        var {crumbs} = stateNavigator.stateContext;
-        var mostRecentEventCount = nativeEvent.eventCount;
+        const {crumbs} = stateNavigator.stateContext;
+        const mostRecentEventCount = nativeEvent.eventCount;
         if (mostRecentEventCount) {
             ref.current.setNativeProps({mostRecentEventCount});
             if (resumeNavigationRef.current)
@@ -59,47 +59,48 @@ const NavigationStack = ({underlayColor = '#000', title, crumbStyle = () => null
         }
     }
     const getAnimation = () => {
-        var {state, data, oldState, oldData, oldUrl, crumbs, nextCrumb} = stateNavigator.stateContext;
+        let {state, data, oldState, oldData, oldUrl, crumbs, nextCrumb} = stateNavigator.stateContext;
         if (!oldState)
             return null;
-        var {crumbs: oldCrumbs} = stateNavigator.parseLink(oldUrl);
+        const {crumbs: oldCrumbs} = stateNavigator.parseLink(oldUrl);
+        let enterAnim, exitAnim, sharedElement, oldSharedElement;
         if (oldCrumbs.length < crumbs.length) {
-            var {state: nextState, data: nextData} = crumbs.concat(nextCrumb)[oldCrumbs.length + 1];
-            var enterAnim = unmountStyle(true, state, data, crumbs);
-            var exitAnim = crumbStyle(false, oldState, oldData, oldCrumbs, nextState, nextData);
-            var sharedElement = getSharedElement(state, data, crumbs);
+            const {state: nextState, data: nextData} = crumbs.concat(nextCrumb)[oldCrumbs.length + 1];
+            enterAnim = unmountStyle(true, state, data, crumbs);
+            exitAnim = crumbStyle(false, oldState, oldData, oldCrumbs, nextState, nextData);
+            sharedElement = getSharedElement(state, data, crumbs);
         }
         if (crumbs.length < oldCrumbs.length) {
-            var nextCrumb = new Crumb(oldData, oldState, null, null, false);
-            var {state: nextState, data: nextData} = oldCrumbs.concat(nextCrumb)[crumbs.length + 1];
-            var enterAnim = crumbStyle(true, state, data, crumbs, nextState, nextData);
-            var exitAnim = unmountStyle(false, oldState, oldData, oldCrumbs);
-            var oldSharedElement = getSharedElement(oldState, oldData, oldCrumbs);
+            nextCrumb = new Crumb(oldData, oldState, null, null, false);
+            const {state: nextState, data: nextData} = oldCrumbs.concat(nextCrumb)[crumbs.length + 1];
+            enterAnim = crumbStyle(true, state, data, crumbs, nextState, nextData);
+            exitAnim = unmountStyle(false, oldState, oldData, oldCrumbs);
+            oldSharedElement = getSharedElement(oldState, oldData, oldCrumbs);
         }
         if (crumbs.length === oldCrumbs.length) {
-            var enterAnim = unmountStyle(true, state, data, crumbs);
-            var exitAnim = unmountStyle(false, oldState, oldData, oldCrumbs, state, data);
+            enterAnim = unmountStyle(true, state, data, crumbs);
+            exitAnim = unmountStyle(false, oldState, oldData, oldCrumbs, state, data);
         }
         return {enterAnim, exitAnim, sharedElement, oldSharedElement};
     }
-    var {stateNavigator: prevStateNavigator, keys, rest} = stackState;
+    const {stateNavigator: prevStateNavigator, keys, rest} = stackState;
     if (prevStateNavigator !== stateNavigator) {
         setStackState((prevStackState) => {
             const {keys: prevKeys, stateNavigator: prevStateNavigator, counter} = prevStackState;
-            var {state, crumbs, nextCrumb, history} = stateNavigator.stateContext;
+            const {state, crumbs, nextCrumb, history} = stateNavigator.stateContext;
             if (!state)
                 return {...prevStackState, keys: []};
-            var prevState = prevStateNavigator && prevStateNavigator.stateContext.state;
-            var currentKeys = crumbs.concat(nextCrumb).map((_, i) => `${counter}-${i}`);
-            var newKeys = currentKeys.slice(prevKeys.length);
-            var keys = prevKeys.slice(0, currentKeys.length).concat(newKeys);
+            const prevState = prevStateNavigator && prevStateNavigator.stateContext.state;
+            const currentKeys = crumbs.concat(nextCrumb).map((_, i) => `${counter}-${i}`);
+            const newKeys = currentKeys.slice(prevKeys.length);
+            const keys = prevKeys.slice(0, currentKeys.length).concat(newKeys);
             if (prevKeys.length === keys.length && prevState !== state)
                 keys[keys.length - 1] = `${counter}-${keys.length - 1}`;
             const refresh = prevKeys.length === keys.length && prevState === state;
             return {keys, stateNavigator, rest: history || refresh, counter: (counter + 1) % 1000};
         });
     }
-    var {crumbs, nextCrumb} = stateNavigator.stateContext;
+    const {crumbs, nextCrumb} = stateNavigator.stateContext;
     return (
         <NVNavigationStack
             ref={ref}
@@ -133,7 +134,7 @@ const NavigationStack = ({underlayColor = '#000', title, crumbStyle = () => null
 
 NavigationStack.Scene = ({children}) => children;
 
-var NVNavigationStack = requireNativeComponent<any>('NVNavigationStack', null);
+const NVNavigationStack = requireNativeComponent<any>('NVNavigationStack', null);
 
 const styles = StyleSheet.create({
     stack: {
