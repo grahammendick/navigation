739a32858ca3b26c9186eaf9bc416676478c5782
Graham Mendick <graham.mendick@gmail.com>
2022-04-24 20:50:21 +0100

    Unit tested A to A -> B to C -> B to C for dynamic

--- NavigationReactMobile/test/NavigationMotionTest.tsx
+++ NavigationReactMobile/test/NavigationMotionTest.tsx
@@ -767,33 +767,60 @@ describe('NavigationMotion', function () {
     });
 
     describe('A to A -> B to C -> B to C', function () {
-        it('should render C', async function(){
-            var stateNavigator = new StateNavigator([
+        var stateNavigator, root, container;
+        var SceneA = () => <div id="sceneA" />;
+        var SceneB = () => <div id="sceneB" />;
+        var SceneC = () => <div id="sceneC" />;
+        beforeEach(() => {
+            stateNavigator = new StateNavigator([
                 { key: 'sceneA' },
                 { key: 'sceneB', trackCrumbTrail: true },
                 { key: 'sceneC' },
             ]);
             stateNavigator.navigate('sceneA');
-            var {sceneA, sceneB, sceneC} = stateNavigator.states;
-            var SceneA = () => <div id="sceneA" />;
-            var SceneB = () => <div id="sceneB" />;
-            var SceneC = () => <div id="sceneC" />;
-            sceneA.renderScene = () => <SceneA />;
-            sceneB.renderScene = () => <SceneB />;
-            sceneC.renderScene = () => <SceneC />;
-            var container = document.createElement('div');
-            const root = createRoot(container)
-            act(() => {
-                root.render(
-                    <NavigationHandler stateNavigator={stateNavigator}>
-                        <NavigationMotion>
-                            {(_style, scene, key) =>  (
-                                <div className="scene" id={key} key={key}>{scene}</div>
-                            )}
-                        </NavigationMotion>
-                    </NavigationHandler>
-                );
+            container = document.createElement('div');
+            root = createRoot(container)
+        });
+        describe('Static', () => {
+            it('should render C', async function(){
+                var {sceneA, sceneB, sceneC} = stateNavigator.states;
+                sceneA.renderScene = () => <SceneA />;
+                sceneB.renderScene = () => <SceneB />;
+                sceneC.renderScene = () => <SceneC />;
+                act(() => {
+                    root.render(
+                        <NavigationHandler stateNavigator={stateNavigator}>
+                            <NavigationMotion>
+                                {(_style, scene, key) =>  (
+                                    <div className="scene" id={key} key={key}>{scene}</div>
+                                )}
+                            </NavigationMotion>
+                        </NavigationHandler>
+                    );
+                });
+                await test();
             });
+        });
+        describe('Dynamic', () => {
+            it('should render C', async function(){
+                act(() => {
+                    root.render(
+                        <NavigationHandler stateNavigator={stateNavigator}>
+                            <NavigationMotion
+                                renderMotion={(_style, scene, key) =>  (
+                                    <div className="scene" id={key} key={key}>{scene}</div>
+                                )}>
+                                <Scene stateKey="sceneA"><SceneA /></Scene>                                
+                                <Scene stateKey="sceneB"><SceneB /></Scene>                                
+                                <Scene stateKey="sceneC"><SceneC /></Scene>                                
+                            </NavigationMotion>
+                        </NavigationHandler>
+                    );
+                });
+                await test();
+            });
+        });
+        const test = async () => {
             act(() => stateNavigator.navigate('sceneB'));
             act(() => {
                 var url = stateNavigator.fluent()
@@ -812,7 +839,7 @@ describe('NavigationMotion', function () {
             } finally {
                 act(() => root.unmount());
             }
-        })
+        }
     });
 
     describe('A to A -> B to C -> D', function () {
