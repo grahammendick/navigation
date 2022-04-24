f782cd116b43d98ac521725fd2147610fd56bedc
Graham Mendick <graham.mendick@gmail.com>
2022-04-24 14:46:19 +0100

    Started unit testing the component api
    Gonna duplicate all the current NavigationMotion tests and have one for the old api and one for new. Called them static and dynamic because that's the key difference between the apis

--- NavigationReactMobile/test/NavigationMotionTest.tsx
+++ NavigationReactMobile/test/NavigationMotionTest.tsx
@@ -2,7 +2,7 @@ import assert from 'assert';
 import 'mocha';
 import { StateNavigator } from 'navigation';
 import { NavigationHandler } from 'navigation-react';
-import { NavigationMotion } from 'navigation-react-mobile';
+import { NavigationMotion, Scene } from 'navigation-react-mobile';
 import React, {useState} from 'react';
 import { createRoot } from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
@@ -23,27 +23,55 @@ window.cancelAnimationFrame = () => {};
 
 describe('NavigationMotion', function () {
     describe('A', function () {
-        it('should render A', function(){
-            var stateNavigator = new StateNavigator([
+        var stateNavigator, root, container;
+        var SceneA = () => <div id="sceneA" />;
+        beforeEach(() => {
+            stateNavigator = new StateNavigator([
                 { key: 'sceneA' }
             ]);
             stateNavigator.navigate('sceneA');
-            var {sceneA} = stateNavigator.states;
-            var SceneA = () => <div id="sceneA" />;
-            sceneA.renderScene = () => <SceneA />;
-            var container = document.createElement('div');
-            const root = createRoot(container)
-            act(() => {
-                root.render(
-                    <NavigationHandler stateNavigator={stateNavigator}>
-                        <NavigationMotion>
-                            {(_style, scene, key) =>  (
-                                <div className="scene" id={key} key={key}>{scene}</div>
-                            )}
-                        </NavigationMotion>
-                    </NavigationHandler>
-                );
+            container = document.createElement('div');
+            root = createRoot(container)
+        });
+
+        describe('Static', () => {
+            it('should render A', function(){
+                var {sceneA} = stateNavigator.states;
+                sceneA.renderScene = () => <SceneA />;
+                act(() => {
+                    root.render(
+                        <NavigationHandler stateNavigator={stateNavigator}>
+                            <NavigationMotion>
+                                {(_style, scene, key) =>  (
+                                    <div className="scene" id={key} key={key}>{scene}</div>
+                                )}
+                            </NavigationMotion>
+                        </NavigationHandler>
+                    );
+                });
+                test();
             });
+        });
+
+        describe('Dynamic', () => {
+            it('should render A', function(){
+                act(() => {
+                    root.render(
+                        <NavigationHandler stateNavigator={stateNavigator}>
+                            <NavigationMotion
+                                renderMotion={(_style, scene, key) =>  (
+                                    <div className="scene" id={key} key={key}>{scene}</div>
+                                )}>
+                                <Scene stateKey="sceneA"><SceneA /></Scene>
+                            </NavigationMotion>
+                        </NavigationHandler>
+                    );
+                });
+                test();
+            });
+        });
+
+        const test = () => {
             try {
                 var scenes = container.querySelectorAll(".scene");                
                 assert.equal(scenes.length, 1);
@@ -52,7 +80,7 @@ describe('NavigationMotion', function () {
             } finally {
                 act(() => root.unmount());
             }
-        })
+        };
     });
 
     describe('A -> B', function () {
