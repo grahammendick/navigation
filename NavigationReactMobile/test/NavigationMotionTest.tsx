cd74e069aea29789f8fd93f3109638565fec1db6
Graham Mendick <graham.mendick@gmail.com>
2022-04-24 17:24:34 +0100

    Unit tested A to B to C for dynamic api

--- NavigationReactMobile/test/NavigationMotionTest.tsx
+++ NavigationReactMobile/test/NavigationMotionTest.tsx
@@ -469,33 +469,60 @@ describe('NavigationMotion', function () {
     });
 
     describe('A to B to C', function () {
-        it('should render C++', function(){
-            var stateNavigator = new StateNavigator([
+        var stateNavigator, root, container;
+        var SceneA = () => <div id="sceneA" />;
+        var SceneB = () => <div id="sceneB" />;
+        var SceneC = () => <div id="sceneC" />;
+        beforeEach(() => {
+            stateNavigator = new StateNavigator([
                 { key: 'sceneA' },
                 { key: 'sceneB' },
                 { key: 'sceneC' },
             ]);
             stateNavigator.navigate('sceneA');
-            var {sceneA, sceneB, sceneC} = stateNavigator.states;
-            var SceneA = () => <div id="sceneA" />;
-            var SceneB = () => <div id="sceneB" />;
-            var SceneC = () => <div id="sceneC" />;
-            sceneA.renderScene = () => <SceneA />;
-            sceneB.renderScene = () => <SceneB />;
-            sceneC.renderScene = () => <SceneC />;
-            var container = document.createElement('div');
-            const root = createRoot(container)
-            act(() => {
-                root.render(
-                    <NavigationHandler stateNavigator={stateNavigator}>
-                        <NavigationMotion>
-                            {(_style, scene, key) =>  (
-                                <div className="scene" id={key} key={key}>{scene}</div>
-                            )}
-                        </NavigationMotion>
-                    </NavigationHandler>
-                );
+            container = document.createElement('div');
+            root = createRoot(container)
+        });
+        describe('Static', () => {
+            it('should render C++', function(){
+                var {sceneA, sceneB, sceneC} = stateNavigator.states;
+                sceneA.renderScene = () => <SceneA />;
+                sceneB.renderScene = () => <SceneB />;
+                sceneC.renderScene = () => <SceneC />;
+                act(() => {
+                    root.render(
+                        <NavigationHandler stateNavigator={stateNavigator}>
+                            <NavigationMotion>
+                                {(_style, scene, key) =>  (
+                                    <div className="scene" id={key} key={key}>{scene}</div>
+                                )}
+                            </NavigationMotion>
+                        </NavigationHandler>
+                    );
+                });
+                test();
             });
+        });
+        describe('Dynamic', () => {
+            it('should render C++', function(){
+                act(() => {
+                    root.render(
+                        <NavigationHandler stateNavigator={stateNavigator}>
+                            <NavigationMotion
+                                renderMotion={(_style, scene, key) =>  (
+                                    <div className="scene" id={key} key={key}>{scene}</div>
+                                )}>
+                                <Scene stateKey="sceneA"><SceneA /></Scene>                                
+                                <Scene stateKey="sceneB"><SceneB /></Scene>                                
+                                <Scene stateKey="sceneC"><SceneC /></Scene>                                
+                            </NavigationMotion>
+                        </NavigationHandler>
+                    );
+                });
+                test();
+            });
+        });
+        const test = () => {
             act(() => stateNavigator.navigate('sceneB'));
             act(() => stateNavigator.navigate('sceneC'));
             try {
@@ -508,7 +535,7 @@ describe('NavigationMotion', function () {
             } finally {
                 act(() => root.unmount());
             }
-        })
+        };
     });
 
     describe('A -> B to C -> B', function () {
