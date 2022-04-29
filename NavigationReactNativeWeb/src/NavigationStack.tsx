a202d1cb8b1ceb65d04cd098d0523e8b1d58764e
Graham Mendick <graham.mendick@gmail.com>
2022-04-29 22:10:15 +0100

    Added component api to nav react native web
    Just a proxy for NavigationMotion but need to make sure it supports static and dynamic api. Also renamed renderMotion to renderTransition for consistency with shared element. Plus motion is specific to navigation react mobile

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { View } from 'react-native';
-import { NavigationMotion, SharedElementMotion } from 'navigation-react-mobile';
+import { NavigationMotion, Scene, SharedElementMotion } from 'navigation-react-mobile';
 import { MobileHistoryManager } from 'navigation-react-mobile';
 
-const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElementTransition, duration, renderScene, children}) => (
+const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElementTransition, duration, renderScene, renderTransition, children}) => (
     <NavigationMotion
         unmountedStyle={unmountedStyle || {translate: 100}}
         mountedStyle={mountedStyle || {translate: 0}}
         crumbStyle={crumbedStyle || {translate: 0}}        
         sharedElementMotion={sharedElementTransition}
         duration={duration}
-        renderScene={renderScene}>
-          {typeof children !== 'function' ?
-            ({translate}, scene, key) => (
-                <View key={key}
-                  style={{
-                    transform: `translate(${translate}%)` as any,
-                    position: 'absolute',
-                    backgroundColor: '#fff',
-                    left: 0, right: 0, top: 0, bottom: 0,
-                    overflow: 'hidden',
-                  }}>
-                  {scene}
-                </View>
-              ) : children}
+        renderScene={renderScene}
+        renderMotion={typeof children === 'function' ? children : (renderTransition || renderMotion)}>
+        {typeof children === 'function' ? null : children}
     </NavigationMotion>
 )
 
+const renderMotion = ({translate}, scene, key) => (
+  <View key={key}
+    style={{
+      transform: `translate(${translate}%)` as any,
+      position: 'absolute',
+      backgroundColor: '#fff',
+      left: 0, right: 0, top: 0, bottom: 0,
+      overflow: 'hidden',
+    }}>
+    {scene}
+  </View>
+);
+
+NavigationStack.Scene = Scene;
 NavigationStack.HistoryManager = MobileHistoryManager;
 NavigationStack.SharedElementTransition = SharedElementMotion;
 
