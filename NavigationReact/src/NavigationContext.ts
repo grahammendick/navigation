11cc473a09755c4ab857887539b9da6da8020064
Graham Mendick <graham.mendick@gmail.com>
2022-04-30 22:04:07 +0100

    Removed next State and Data from NavigationContext
    They're a hangover from original idea for concurrent rendering. Should've removed when supported React 18

--- NavigationReact/src/NavigationContext.ts
+++ NavigationReact/src/NavigationContext.ts
@@ -1,4 +1,4 @@
 import { StateNavigator } from 'navigation';
 import * as React from 'react';
 
-export default React.createContext({ oldState: null, state: null, data: {}, nextState: null, nextData: {}, stateNavigator: new StateNavigator() });
+export default React.createContext({ oldState: null, state: null, data: {}, stateNavigator: new StateNavigator() });
