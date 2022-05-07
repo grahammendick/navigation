fed1585e1874d73c0b3674be3f29d7cb8e62e404
Graham Mendick <graham.mendick@gmail.com>
2022-05-07 16:44:54 +0100

    Rendered segmented control
    Used the contentview because think it's for rendering native view. Had to supply default titles and test ids otherwise React Native threw an error during conversion

--- NavigationReactNative/src/ios/NVSegmentedTabComponentView.h
+++ NavigationReactNative/src/ios/NVSegmentedTabComponentView.h
@@ -6,6 +6,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NVSegmentedTabComponentView : RCTViewComponentView
 
+@property UISegmentedControl *segmentedControl;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- NavigationReactNative/src/ios/NVSegmentedTabComponentView.mm
+++ NavigationReactNative/src/ios/NVSegmentedTabComponentView.mm
@@ -20,8 +20,26 @@ using namespace facebook::react;
     if (self = [super initWithFrame:frame]) {
         static const auto defaultProps = std::make_shared<const NVSegmentedTabProps>();
         _props = defaultProps;
+        _segmentedControl = [[UISegmentedControl alloc] init];
+        self.contentView = _segmentedControl;
     }
     return self;
+}
+
+- (void)updateProps:(const facebook::react::Props::Shared &)props oldProps:(const facebook::react::Props::Shared &)oldProps
+{
+    const auto &oldViewProps = *std::static_pointer_cast<NVSegmentedTabProps const>(_props);
+    const auto &newViewProps = *std::static_pointer_cast<NVSegmentedTabProps const>(props);
+    if (oldViewProps.titles != newViewProps.titles) {
+        NSInteger selectedIndex = _segmentedControl.selectedSegmentIndex;
+        [_segmentedControl removeAllSegments];
+        for (auto i = 0; i < newViewProps.titles.size(); i++) {
+            NSString *title = [[NSString alloc] initWithUTF8String: newViewProps.titles[i].c_str()];
+            [_segmentedControl insertSegmentWithTitle:title atIndex:_segmentedControl.numberOfSegments animated:NO];
+        }
+        _segmentedControl.selectedSegmentIndex = selectedIndex;
+    }
+    [super updateProps:props oldProps:oldProps];
 }
 
 #pragma mark - RCTComponentViewProtocol
