d3f888df7dbe23a337af404706a46974be489da3
Graham Mendick <graham.mendick@gmail.com>
2022-05-21 18:09:55 +0100

    Created NavigationPackage for new architecture
    Otherwise it refers to the managers in the old arch

---
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/newarch/NavigationPackage.java
@@ -0,0 +1,26 @@
+package com.navigation.reactnative;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NavigationPackage implements ReactPackage {
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList(
+                new NavigationStackViewManager(reactContext),
+                new SceneViewManager(reactContext)
+        );
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return new ArrayList<>();
+    }
+}
