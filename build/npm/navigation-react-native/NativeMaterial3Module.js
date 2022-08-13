cc008e33aeb26bdcaee5222874f239db3ac08995
Graham Mendick <graham.mendick@gmail.com>
2022-08-14 00:14:48 +0100

    Deleted native module from build
    Shouldn't have checked this in

--- build/npm/navigation-react-native/NativeMaterial3Module.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
-import { TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-}
-export default (TurboModuleRegistry.get<Spec>(
-'Material3'
-): ?Spec);
