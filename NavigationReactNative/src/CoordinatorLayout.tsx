43422e4382ea7a9a287bfd846dfae0b1087177d0
Graham Mendick <graham.mendick@gmail.com>
2022-09-18 17:13:22 +0100

    Updated react native typings for ColorValue
    It showed error with CoordinatorLayout require - was using 2 parameter one and wasn't doing the fabric thing either

--- NavigationReactNative/src/CoordinatorLayout.tsx
+++ NavigationReactNative/src/CoordinatorLayout.tsx
@@ -4,7 +4,7 @@ import { requireNativeComponent, Platform } from 'react-native';
 const CoordinatorLayout = ({overlap, children}) => (
     <NVCoordinatorLayout overlap={overlap} style={{flex: 1}}>{children}</NVCoordinatorLayout>
 );
-const NVCoordinatorLayout = requireNativeComponent<any>('NVCoordinatorLayout', null)
+const NVCoordinatorLayout = global.nativeFabricUIManager ? require('./CoordinatorLayoutNativeComponent').default : requireNativeComponent('NVCoordinatorLayout');
 
 export default Platform.OS === 'android' ? CoordinatorLayout : ({children}) => children;
 
