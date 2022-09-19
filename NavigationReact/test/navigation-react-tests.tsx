c00524a66838ef6ed0ab092dc64e8194fc15cf3e
Graham Mendick <graham.mendick@gmail.com>
2022-09-19 10:57:06 +0100

    Included baseUrl (and npx) in test scripts
    So can paste in terminal and run

--- NavigationReact/test/navigation-react-tests.tsx
+++ NavigationReact/test/navigation-react-tests.tsx
@@ -1,4 +1,4 @@
-// tsc --jsx react --target es3 --lib ES2015,DOM --esModuleInterop --noImplicitAny true --strict true navigation-react-tests.tsx
+// npx tsc --jsx react --target es3 --lib ES2015,DOM --esModuleInterop --baseUrl ../../types --noImplicitAny true --strict true navigation-react-tests.tsx
 import { StateNavigator } from 'navigation';
 import { NavigationHandler, NavigationContext, NavigationEvent, SceneView, NavigationBackLink, NavigationLink, RefreshLink } from 'navigation-react';
 import React, { useContext } from 'react';
