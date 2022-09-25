6ff4552059bf7c18f08ec6822392974f53751b60
Graham Mendick <graham.mendick@gmail.com>
2022-09-25 17:10:45 +0100

    Handled remapping crumb style for nested scenes
    Can have scenes rendered inside a fragment so have to look for scenes 2 levels deep
    <Stack>
      <>
        <Scene>
      </>
    </Stack>

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -3,6 +3,14 @@ import { View } from 'react-native';
 import { NavigationMotion, Scene, SharedElementMotion } from 'navigation-react-mobile';
 import { MobileHistoryManager } from 'navigation-react-mobile';
 
+const cloneScenes = (children, nested = false) => (
+  React.Children.map(children, scene => (
+    (scene.type === Scene || nested)
+      ? React.cloneElement(scene, { crumbStyle: scene.props.crumbedStyle })
+      : React.cloneElement(scene, null, cloneScenes(scene.props.children, true) as any)
+  ))
+);
+
 const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElementTransition, duration, renderScene, renderTransition, children}) => (
     <NavigationMotion
         unmountedStyle={unmountedStyle || {translate: 100}}
@@ -12,11 +20,7 @@ const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElem
         duration={duration}
         renderScene={renderScene}
         renderMotion={typeof children !== 'function' ? renderTransition || renderMotion : undefined}>
-        {typeof children !== 'function' ? (
-          React.Children.map(children, child => (
-            React.cloneElement(child, { crumbStyle: child.props.crumbedStyle })
-          ))
-        ) : (children || renderMotion)}
+        {typeof children !== 'function' ? cloneScenes(children) : (children || renderMotion)}
     </NavigationMotion>
 );
 
