5a43bcad26c414971062c731cbe7c731290b33ae
Graham Mendick <graham.mendick@gmail.com>
2022-10-18 14:59:10 +0100

    Suspended inactive tabs
    Copied over freeze and TabBarItemContext so can use them in native web

--- NavigationReactNativeWeb/src/TabBarItem.tsx
+++ NavigationReactNativeWeb/src/TabBarItem.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { View } from 'react-native';
+import TabBarItemContext from './TabBarItemContext';
+import Freeze from './Freeze';
 
-const TabBarItem = ({selected, children}) => {
+const TabBarItem = ({selected, lazy = false, children}) => {
+    const [loaded, setLoaded] = useState(selected || lazy);
+    const onLoad = useRef({ onLoad: () => setLoaded(true)});
+    if (!loaded && selected) setLoaded(true);
     return (
-        <View style={{display: selected ? 'flex' : 'none', flex: 1}}>
-            {children}
-        </View>
+        <Freeze enabled={loaded && !selected}>
+            <View style={{display: selected ? 'flex' : 'none', flex: 1}}>
+                <TabBarItemContext.Provider value={onLoad.current}>
+                    {children}
+                </TabBarItemContext.Provider>
+            </View>
+        </Freeze>
     )
 };
 
