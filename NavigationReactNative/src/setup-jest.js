1a282bff7323f984f7b097c1a2f48fa43e996167
Graham Mendick <graham.mendick@gmail.com>
2022-11-05 13:32:21 +0000

    Mocked navigation stack
    The real native one won't work in unit tests because it doesn't pop a scene until it comes to rest. The rest signal comes from native. So in test would end up with scene staying stack after navigating back from it. Simpler to mock and then don't have to worry about PopSync and coming to rest. Can also put the right accessibility labels so can search for top scene (window + selected)

--- NavigationReactNative/src/setup-jest.js
+++ NavigationReactNative/src/setup-jest.js
@@ -1,7 +1,65 @@
 jest.mock('navigation-react-native', () => {
     const React = require('react');
     const ReactNative = require('react-native');
+    const NavigationReact = require('navigation-react');
     const NavigationReactNative = jest.requireActual('navigation-react-native');
+
+    const NavigationStack = ({ stackInvalidatedLink, renderScene, children }) => {
+        const {stateNavigator} = React.useContext(NavigationReact.NavigationContext);
+        const [stackState, setStackState] = React.useState({stateNavigator: null, keys: []});
+        const scenes = {};
+        let firstLink;
+        const findScenes = (elements = children, nested = false) => {
+            for(const scene of React.Children.toArray(elements)) {
+                const {stateKey, children} = scene.props;
+                if (scene.type === NavigationReactNative.Scene) {
+                    firstLink = firstLink || stateNavigator.fluent().navigate(stateKey).url;
+                    scenes[stateKey] = scene;
+                }
+                else if (!nested) findScenes(children, true)
+            }
+        }
+        findScenes();
+        const prevScenes = React.useRef({});
+        const allScenes = {...prevScenes.current, ...scenes};
+        React.useEffect(() => {
+            prevScenes.current = allScenes;
+            const {state, crumbs, nextCrumb} = stateNavigator.stateContext;
+            const validate = ({key}) => !!scenes[key];
+            if (firstLink) {
+                stateNavigator.onBeforeNavigate(validate);
+                let resetLink = !state ? firstLink : undefined;
+                if (!resetLink && [...crumbs, nextCrumb].find(({state}) => !scenes[state.key]))
+                    resetLink = stackInvalidatedLink != null ? stackInvalidatedLink : firstLink;
+                if (resetLink != null) stateNavigator.navigateLink(resetLink);
+            }
+            return () => stateNavigator.offBeforeNavigate(validate);
+        }, [children, stateNavigator, scenes, allScenes, stackInvalidatedLink]);
+        const {stateNavigator: prevStateNavigator, keys} = stackState;
+        if (prevStateNavigator !== stateNavigator && stateNavigator.stateContext.state) {
+            setStackState((prevStackState) => {
+                const {keys: prevKeys, stateNavigator: prevStateNavigator} = prevStackState;
+                const {state, crumbs, nextCrumb, history} = stateNavigator.stateContext;
+                const prevState = prevStateNavigator && prevStateNavigator.stateContext.state;
+                const currentKeys = crumbs.concat(nextCrumb).map((_, i) => `${i}`);
+                const newKeys = currentKeys.slice(prevKeys.length);
+                const keys = prevKeys.slice(0, currentKeys.length).concat(newKeys);
+                if (prevKeys.length === keys.length && prevState !== state)
+                    keys[keys.length - 1] = `${keys.length - 1}`;
+                const refresh = prevKeys.length === keys.length && prevState === state;
+                return {keys, stateNavigator};
+            });
+        }
+        const {crumbs, nextCrumb} = stateNavigator.stateContext;
+        return crumbs.concat(nextCrumb || []).map(({ state, data }, index) => {
+            renderScene = firstLink ? ({key}) => allScenes[key] : renderScene;
+            return (
+                <ReactNative.View accessibilityRole="window" key={state.key}>
+                    {renderScene(state, data)}
+                </ReactNative.View>
+            );
+        });
+    }
 
     const TabBar = ({tab, defaultTab = 0, onChangeTab, bottomTabs, primary = true, ...props}) => {
         const [selectedTab, setSelectedTab] = React.useState(tab || defaultTab);
@@ -48,7 +106,7 @@ jest.mock('navigation-react-native', () => {
     );
 
     return  {
-        NavigationStack: NavigationReactNative.NavigationStack,
+        NavigationStack,
         Scene: NavigationReactNative.Scene,
         NavigationBar: ({ title, children, ...props }) => (
             <ReactNative.View accessibilityRole="toolbar" {...props}>
