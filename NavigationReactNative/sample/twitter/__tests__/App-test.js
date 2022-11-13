70221beaf08383c46a7bed43775045413ea7b7b8
Graham Mendick <graham.mendick@gmail.com>
2022-11-13 12:10:35 +0000

    Tested notifications tab

--- NavigationReactNative/sample/twitter/__tests__/App-test.js
+++ NavigationReactNative/sample/twitter/__tests__/App-test.js
@@ -5,7 +5,7 @@ import { render, screen, fireEvent, within } from '@testing-library/react-native
 
 describe('Twitter', () => {
   it('should display Home and Notifications tabs with Home selected', () => {
-    const { getAllByRole, getByRole } = render(<App />);
+    const { getAllByRole } = render(<App />);
     const tabsScene = getAllByRole('window', {selected: true})[0];
     const homeTab = within(tabsScene).getByRole('tab', {name: 'Home', selected: true});
     const notificationsTab = within(tabsScene).getByRole('tab', {name: 'Notifications', selected: false});
@@ -59,5 +59,25 @@ describe('Home', () => {
     const timelineScene = within(homeTab).getByRole('window', {selected: true});
     const header = within(timelineScene).getByRole('header', {name: 'Dan Abramov'});
     expect(header).toBeTruthy(); 
+  });
+});
+
+describe('Notifications', () => {
+  it('should have title Notification', () => {
+    const { getAllByRole } = render(<App />);
+    const tabsScene = getAllByRole('window', {selected: true})[0];
+    const notificationsTab = within(tabsScene).getByRole('tabpanel', {name: 'Notifications'});
+    const header = within(notificationsTab).getByRole('header', {name: 'Notifications'});
+    expect(header).toBeTruthy();    
+  });
+
+  it('should display All and Mentions tabs with All selected', () => {
+    const { getAllByRole } = render(<App />);
+    const tabsScene = getAllByRole('window', {selected: true})[0];
+    const notificationsScene = within(tabsScene).getAllByRole('window', {selected: true})[0];
+    const allTab = within(notificationsScene).getByRole('tab', {name: 'All', selected: true});
+    const mentionsTab = within(notificationsScene).getByRole('tab', {name: 'Mentions', selected: false});
+    expect(allTab).toBeTruthy();    
+    expect(mentionsTab).toBeTruthy();    
   });
 });
