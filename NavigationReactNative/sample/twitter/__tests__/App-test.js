86323f7a054e53cb6d3cd868854448b699536984
Graham Mendick <graham.mendick@gmail.com>
2022-11-13 11:48:37 +0000

    Tested Tabs and Home scene
    Also mocked Animated to prevent warning from Timeline scene

--- NavigationReactNative/sample/twitter/__tests__/App-test.js
+++ NavigationReactNative/sample/twitter/__tests__/App-test.js
@@ -1,27 +1,47 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-import { Platform } from 'react-native';
-import { TabBarItem } from 'navigation-react-native';
 import { render, screen, fireEvent, within } from '@testing-library/react-native';
-// Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const { getByTestId } = render(<TabBarItem testID="hello" onPress={() => console.log('yyyy')} />);
-  fireEvent.press(getByTestId('hello'), { stopPropagation: () => {} });
+describe('Twitter', () => {
+  it('should display Home and Notifications tabs with Home selected', () => {
+    const { getAllByRole, getByRole } = render(<App />);
+    const tabsScene = getAllByRole('window', {selected: true})[0];
+    const homeTab = within(tabsScene).getByRole('tab', {name: 'Home', selected: true});
+    const notificationsTab = within(tabsScene).getByRole('tab', {name: 'Notifications', selected: false});
+    expect(homeTab).toBeTruthy();    
+  });
 });
 
-it('renders App correctly', () => {
-  const { getAllByText, getByText, getAllByRole, getByRole } = render(<App />);
-  const scene = getAllByRole('window', {selected: true})[0];
-  const tab = within(scene).getByRole('tab', {name: 'Home', selected: true});
-  const panel = getAllByRole('tabpanel', {name: 'All'});
-  let title = getByRole('header', {name: 'Home'});
-  fireEvent.press(getAllByText('Dan Abramov')[0]);
-  title = getByRole('header', {name: 'Tweet'});
+describe('Home', () => {
+  it('should have title Home', () => {
+    const { getAllByRole, getByRole } = render(<App />);
+    const tabsScene = getAllByRole('window', {selected: true})[0];
+    const homeTab = within(tabsScene).getByRole('tabpanel', {name: 'Home'});
+    const header = within(homeTab).getByRole('header', {name: 'Home'});
+    expect(header).toBeTruthy();    
+  });
+
+  it('should navigate to Tweet', () => {
+    const { getAllByRole, getByRole } = render(<App />);
+    const tabsScene = getAllByRole('window', {selected: true})[0];
+    const homeTab = within(tabsScene).getByRole('tabpanel', {name: 'Home'});
+    const tweetButton = within(homeTab).getAllByRole('button', {name: 'Dan Abramov'})[0];
+    fireEvent.press(tweetButton);
+    const tweetScene = within(homeTab).getByRole('window', {selected: true});
+    const header = within(tweetScene).getByRole('header', {name: 'Tweet'});
+    expect(header).toBeTruthy(); 
+  });
+
+  it('should navigate to Timeline', () => {
+    const { getAllByRole, getByRole } = render(<App />);
+    const tabsScene = getAllByRole('window', {selected: true})[0];
+    const homeTab = within(tabsScene).getByRole('tabpanel', {name: 'Home'});
+    const tweetButton = within(homeTab).getAllByRole('button', {name: 'Dan Abramov'})[0];
+    const timelineButton = within(tweetButton).getAllByRole('button')[1];
+    fireEvent.press(timelineButton);
+    const timelineScene = within(homeTab).getByRole('window', {selected: true});
+    const header = within(timelineScene).getByRole('header', {name: 'Dan Abramov'});
+    expect(header).toBeTruthy(); 
+  });
 });
