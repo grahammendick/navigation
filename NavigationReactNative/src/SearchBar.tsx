18756ee973c92da6fcc6cb302cd55754dbe3cc66
Graham Mendick <graham.mendick@gmail.com>
2023-01-01 13:40:45 +0000

    Changed from class to function component

--- NavigationReactNative/src/SearchBar.tsx
+++ NavigationReactNative/src/SearchBar.tsx
@@ -1,63 +1,49 @@
-import React from 'react';
-import { requireNativeComponent, Platform, StyleSheet, UIManager, View } from 'react-native';
+import React, {useState} from 'react';
+import { requireNativeComponent, Platform, StyleSheet, UIManager } from 'react-native';
 
-class SearchBar extends React.Component<any, any> {
-    private ref: React.RefObject<View>;
-    constructor(props) {
-        super(props);
-        this.state = {show: false, mostRecentEventCount: 0};
-        this.ref = React.createRef<View>();
-        this.onChangeText = this.onChangeText.bind(this);
-        this.onChangeScopeButton = this.onChangeScopeButton.bind(this);
-    }
-    static defaultProps = {
-        obscureBackground: true,
-        hideNavigationBar: true,
-        hideWhenScrolling: false,
-        autoCapitalize: 'sentences',
-        placeholder: Platform.OS === 'ios' ? 'Search' : undefined,
-    }
-    onChangeText({nativeEvent}) {
-        var {onChangeText} = this.props as any;
+const SearchBar = ({obscureBackground = true, hideNavigationBar= true, hideWhenScrolling = false, autoCapitalize = 'sentences', onChangeText,
+    placeholder = Platform.OS === 'ios' ? 'Search' : undefined, children, bottomBar, scopeButton, scopeButtons, onChangeScopeButton, toolbar, ...props}) => {
+    const [show, setShow] = useState(false);
+    const [mostRecentEventCount, setMostRecentEventCount] = useState(0);
+    const [mostRecentButtonEventCount, setMostRecentButtonEventCount] = useState(0);
+    const changeText = ({nativeEvent}) => {
         var {eventCount: mostRecentEventCount, text} = nativeEvent;
-        this.setState({mostRecentEventCount});
+        setMostRecentEventCount(mostRecentEventCount);
         if (onChangeText)
             onChangeText(text)
     }
-    onChangeScopeButton({nativeEvent}) {
-        var {onChangeScopeButton, scopeButtons} = this.props as any;
+    const changeScopeButton = ({nativeEvent}) => {
         var {eventCount: mostRecentButtonEventCount, scopeButton} = nativeEvent;
-        this.setState({mostRecentButtonEventCount});
+        setMostRecentButtonEventCount(mostRecentButtonEventCount);
         if (onChangeScopeButton)
             onChangeScopeButton(scopeButtons[scopeButton])
     }
-    render() {
-        var {show, mostRecentEventCount, mostRecentButtonEventCount} = this.state;
-        var {autoCapitalize, children, bottomBar, scopeButton, scopeButtons, toolbar, ...props} = this.props;
-        var constants = (UIManager as any).getViewManagerConfig('NVSearchBar').Constants;
-        autoCapitalize = Platform.OS === 'android' ? constants.AutoCapitalize[autoCapitalize] : autoCapitalize;
-        var showStyle = Platform.OS === 'android' && {top: !bottomBar ? 56 : 0, bottom: !bottomBar ? 0: 56, height: show ? 'auto' : 0, zIndex: show ? 58 : -58}
-        var SearchBar = Platform.OS === 'ios' || !toolbar ? NVSearchBar : NVSearchResults;
-        return (
-            <SearchBar
-                {...props}
-                ref={this.ref}
-                bottomBar={bottomBar}
-                autoCapitalize={'' + autoCapitalize}
-                scopeButton={scopeButton ? scopeButtons?.indexOf(scopeButton) : 0}
-                scopeButtons={scopeButtons}
-                mostRecentEventCount={mostRecentEventCount}
-                mostRecentButtonEventCount={mostRecentButtonEventCount}
-                onChangeText={this.onChangeText}
-                onChangeScopeButton={this.onChangeScopeButton}
-                onExpand={() => this.setState({show: true})}
-                onCollapse={() => this.setState({show: false})}
-                style={[styles.searchBar, showStyle]}>
-                {children}
-            </SearchBar>
-        );
-    }
-}
+    var constants = (UIManager as any).getViewManagerConfig('NVSearchBar').Constants;
+    autoCapitalize = Platform.OS === 'android' ? constants.AutoCapitalize[autoCapitalize] : autoCapitalize;
+    var showStyle = Platform.OS === 'android' && {top: !bottomBar ? 56 : 0, bottom: !bottomBar ? 0: 56, height: show ? 'auto' : 0, zIndex: show ? 58 : -58}
+    var SearchBar = Platform.OS === 'ios' || !toolbar ? NVSearchBar : NVSearchResults;
+    return (
+        <SearchBar
+            {...props}
+            placeholder={placeholder}
+            bottomBar={bottomBar}
+            obscureBackground={obscureBackground}
+            hideNavigationBar={hideNavigationBar}
+            hideWhenScrolling={hideWhenScrolling}
+            autoCapitalize={'' + autoCapitalize}
+            scopeButton={scopeButton ? scopeButtons?.indexOf(scopeButton) : 0}
+            scopeButtons={scopeButtons}
+            mostRecentEventCount={mostRecentEventCount}
+            mostRecentButtonEventCount={mostRecentButtonEventCount}
+            onChangeText={changeText}
+            onChangeScopeButton={changeScopeButton}
+            onExpand={() => setShow(true)}
+            onCollapse={() => setShow(false)}
+            style={[styles.searchBar, showStyle]}>
+            {children}
+        </SearchBar>
+    );
+};
 
 var NVSearchBar = global.nativeFabricUIManager ? require('./SearchBarNativeComponent').default : requireNativeComponent('NVSearchBar');
 var NVSearchResults = global.nativeFabricUIManager ? require('./SearchBarNativeComponent').default : requireNativeComponent('NVSearchResults');
