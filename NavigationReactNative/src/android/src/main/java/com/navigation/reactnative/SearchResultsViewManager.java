f15ebf43d3dddc678f5f80138df551a0a09e0140
Graham Mendick <graham.mendick@gmail.com>
2023-02-04 14:44:26 +0000

    Decorated results props with ReactProp attribute

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SearchResultsViewManager.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SearchResultsViewManager.java
@@ -8,6 +8,7 @@ import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.ViewManagerDelegate;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.viewmanagers.NVSearchResultsManagerDelegate;
 import com.facebook.react.viewmanagers.NVSearchResultsManagerInterface;
 
@@ -40,31 +41,37 @@ public class SearchResultsViewManager extends ViewGroupManager<SearchResultsView
     }
 
     @Override
+    @ReactProp(name = "placeholder")
     public void setPlaceholder(SearchResultsView view, @Nullable String placeholder) {
         view.setHint(placeholder);
     }
 
     @Override
+    @ReactProp(name = "text")
     public void setText(SearchResultsView view, @Nullable String text) {
         view.setText(text);
     }
 
     @Override
+    @ReactProp(name = "active")
     public void setActive(SearchResultsView view, boolean active) {
         view.setActive(active);
     }
 
     @Override
+    @ReactProp(name = "mostRecentEventCount")
     public void setMostRecentEventCount(SearchResultsView view, int mostRecentEventCount) {
         view.mostRecentEventCount = mostRecentEventCount;
     }
 
     @Override
+    @ReactProp(name = "mostRecentActiveEventCount")
     public void setMostRecentActiveEventCount(SearchResultsView view, int mostRecentActiveEventCount) {
         view.mostRecentActiveEventCount = mostRecentActiveEventCount;
     }
 
     @Override
+    @ReactProp(name = "barTintColor", customType = "Color")
     public void setBarTintColor(SearchResultsView view, @Nullable Integer barTintColor) {
         if (barTintColor != null)
             view.getToolbar().setBackgroundColor(barTintColor);
