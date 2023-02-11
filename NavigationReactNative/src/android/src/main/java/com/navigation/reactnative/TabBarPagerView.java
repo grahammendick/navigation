de352c7ea2c9740e66c28c1227e1ecf94add26bb
Graham Mendick <graham.mendick@gmail.com>
2023-02-11 12:31:32 +0000

    Removed redundant import

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabBarPagerView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/TabBarPagerView.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
