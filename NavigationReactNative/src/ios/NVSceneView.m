32573d2c505964e3119055fa6f1ffd80d5c83624
Graham Mendick <graham.mendick@gmail.com>
2023-04-15 14:00:50 +0100

    Suppported landscape on ios (< 16)
    Think the simulator behaves oddly so can't be 100% it's correct. There's interface orientation and device orientation. When rotating the simulator that changes both of these. When programmatically changing the orientation it also seems to change both but without rotating the simulator. So it's hard to know what position the 'real' device is in. But assuming that the device orientation is reliable indicator of its position. So when changing to landscape programmatically the device is actually rotated (even though simulator isn't). Basically can't replicate orientation landscape but device portrait in simulator

--- NavigationReactNative/src/ios/NVSceneView.m
+++ NavigationReactNative/src/ios/NVSceneView.m
@@ -4,6 +4,7 @@
 @implementation NVSceneView
 {
     BOOL _notifiedPeekable;
+    BOOL _landscape;
 }
 
 - (id)init
@@ -11,6 +12,46 @@
     if (self = [super init]) {
     }
     return self;
+}
+
+- (void)setLandscape:(BOOL)landscape
+{
+    if (landscape != _landscape) {
+        _landscape = landscape;
+        [self setOrientation];
+    }
+}
+
+- (void)didMoveToWindow
+{
+    [super didMoveToWindow];
+    [self setOrientation];
+}
+
+- (void)setOrientation {
+    if (self.reactViewController && self.reactViewController == self.reactViewController.navigationController.topViewController) {
+        [[UIDevice currentDevice] setValue:[NSNumber numberWithInt:[self interfaceOrientation]] forKey:@"orientation"];
+    }
+}
+
+- (int)interfaceOrientation
+{
+    UIDeviceOrientation deviceOrientation = [[UIDevice currentDevice] orientation];
+    if (_landscape) {
+        return deviceOrientation != UIDeviceOrientationLandscapeLeft ? UIInterfaceOrientationLandscapeLeft : UIInterfaceOrientationLandscapeRight;
+    }
+    switch (deviceOrientation) {
+        case UIDeviceOrientationPortrait:
+          return UIInterfaceOrientationPortrait;
+        case UIDeviceOrientationPortraitUpsideDown:
+          return UIInterfaceOrientationPortraitUpsideDown;
+        case UIDeviceOrientationLandscapeLeft:
+          return UIInterfaceOrientationLandscapeRight;
+        case UIDeviceOrientationLandscapeRight:
+          return UIInterfaceOrientationLandscapeLeft;
+        default:
+          return UIInterfaceOrientationUnknown;
+    }
 }
 
 -(void)didPop
