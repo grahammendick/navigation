262a55321699e568c93b4e2524ed1dc56b94f577
Graham Mendick <graham.mendick@gmail.com>
2023-05-04 20:36:37 +0100

    Inherited from html5 history from hash history
    With rewriting need to store the real url in history state because it might not be in the href anymore. Could rewrite open modal url as details href. And only html5 history supports state. Can still use hash history via html5 history

--- Navigation/src/history/HashHistoryManager.ts
+++ Navigation/src/history/HashHistoryManager.ts
@@ -1,60 +1,24 @@
 ﻿import HistoryManager from './HistoryManager';
+import HTML5HistoryManager from './HTML5HistoryManager';
 
-class HashHistoryManager implements HistoryManager {
-    private navigateHistory: () => void = null;
+class HashHistoryManager extends HTML5HistoryManager {
     private replaceQueryIdentifier: boolean = false;
-    private rewrite: (url: string) => string | undefined;
-    disabled: boolean = (typeof window === 'undefined') || !('onhashchange' in window);
     
     constructor(replaceQueryIdentifier: boolean = false) {
+        super('');
         this.replaceQueryIdentifier = replaceQueryIdentifier;
-    }
-
-    init(navigateHistory, rewrite) {
-        if (!this.rewrite) this.rewrite = rewrite;
-        if (!this.disabled && !this.navigateHistory) {
-            this.navigateHistory = () => navigateHistory();
-            if (window.addEventListener)
-                window.addEventListener('hashchange', this.navigateHistory);
-            else
-                window['attachEvent']('onhashchange', this.navigateHistory);
-        }
-    }
-
-    addHistory(url: string, replace: boolean) {
-        var href = this.getHref(url);
-        if (!this.disabled && window.location.hash !== href) {
-            if (!replace)            
-                window.location.hash = href;
-            else
-                window.location.replace(href);
-        }
-    }
-
-    getCurrentUrl(): string {
-        return this.getUrl(window.location);
     }
 
     getHref(url: string): string {
         if (url == null)
             throw new Error('The Url is invalid');
-        return '#' + this.encode(this.rewrite?.(url) || url);
+        return '#' + this.encode(super.getHref(url));
     }
 
     getUrl(hrefElement: HTMLAnchorElement | Location) {
         return this.decode(hrefElement.hash.substring(1));
     }
     
-    stop() {
-        if (this.navigateHistory) {
-            if (window.removeEventListener)
-                window.removeEventListener('hashchange', this.navigateHistory);
-            else
-                window['detachEvent']('onhashchange', this.navigateHistory);
-        }
-        this.navigateHistory = null;
-    }
-
     private encode(url: string): string {
         if (!this.replaceQueryIdentifier)
             return url;
