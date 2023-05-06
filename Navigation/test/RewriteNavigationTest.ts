550e4eefdca30f3896708bcd52913514e0d23866
Graham Mendick <graham.mendick@gmail.com>
2023-05-06 12:42:37 +0100

    Tested rewrite back with trail

--- Navigation/test/RewriteNavigationTest.ts
+++ Navigation/test/RewriteNavigationTest.ts
@@ -506,4 +506,78 @@ describe('Rewrite Navigation', () => {
             test(stateNavigator => stateNavigator.fluent(true) .navigateBack(1).url);
         });
     });
+
+    describe('Back With Trail Rewrite', () => {
+        const test = navigate => {
+            it('should populate href', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's0', route: 'r0' },
+                    { key: 's1', route: 'r1', trackCrumbTrail: true },
+                    { key: 's2', route: 'r2', trackCrumbTrail: true },
+                    { key: 's3', route: 'r3' },
+                ]);
+                const {s1} = stateNavigator.states;
+                s1.rewrite = () => ({
+                    stateKey: 's3'
+                });
+                stateNavigator.navigate('s0');
+                stateNavigator.navigate('s1');
+                stateNavigator.navigate('s2');
+                const link = navigate(stateNavigator);
+                assert.equal(stateNavigator.historyManager.getHref(link), '#/r3');
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.navigateBack(1);
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getNavigationBackLink(1));
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent(true) .navigateBack(1).url);
+        });
+    });
+
+    describe('Back With Trail Rewrite With Trail', () => {
+        const test = navigate => {
+            it('should populate href', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's0', route: 'r0' },
+                    { key: 's1', route: 'r1', trackCrumbTrail: true },
+                    { key: 's2', route: 'r2', trackCrumbTrail: true },
+                    { key: 's3', route: 'r3', trackCrumbTrail: true },
+                ]);
+                const {s1} = stateNavigator.states;
+                s1.rewrite = () => ({
+                    stateKey: 's3'
+                });
+                stateNavigator.navigate('s0');
+                stateNavigator.navigate('s1');
+                stateNavigator.navigate('s2');
+                const link = navigate(stateNavigator);
+                assert.equal(stateNavigator.historyManager.getHref(link), '#/r3?crumb=%2Fr0');
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.navigateBack(1);
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getNavigationBackLink(1));
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent(true) .navigateBack(1).url);
+        });
+    });
 });
