f04d55dccf7eaa141f21af530a6d5efb0b5a1382
Graham Mendick <graham.mendick@gmail.com>
2023-05-08 17:30:08 +0100

    Tested back twice rewrite with trail

--- Navigation/test/RewriteNavigationTest.ts
+++ Navigation/test/RewriteNavigationTest.ts
@@ -1461,6 +1461,50 @@ describe('Rewrite Navigation', () => {
         });
     });
 
+    describe('Back Twice With Trail Rewrite With Trail', () => {
+        const test = navigate => {
+            it('should populate context', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's0', route: 'r0' },
+                    { key: 's1', route: 'r1', trackCrumbTrail: true },
+                    { key: 's2', route: 'r2', trackCrumbTrail: true },
+                    { key: 's3', route: 'r3', trackCrumbTrail: true },
+                    { key: 's4', route: 'r4', trackCrumbTrail: true },
+                ]);
+                const {s1} = stateNavigator.states;
+                s1.rewriteNavigation = () => ({
+                    stateKey: 's4'
+                });
+                stateNavigator.navigate('s0');
+                stateNavigator.navigate('s1');
+                stateNavigator.navigate('s2');
+                stateNavigator.navigate('s3');
+                const link = navigate(stateNavigator);
+                const rewrittenLink = stateNavigator.historyManager.getHref(link).substring(1);
+                stateNavigator.navigateLink(rewrittenLink);
+                assert.equal(stateNavigator.stateContext.url, '/r4?crumb=%2Fr0');
+                assert.equal(stateNavigator.stateContext.state.key, 's4');
+                assert.equal(stateNavigator.stateContext.crumbs.length, 1);
+                assert.equal(stateNavigator.stateContext.crumbs[0].state.key, 's0');
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.navigateBack(2);
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getNavigationBackLink(2));
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent(true).navigateBack(2).url);
+        });
+    });
+
     describe('Rewrite Custom Trail', () => {
         const test = navigate => {
             it('should populate context', () => {
