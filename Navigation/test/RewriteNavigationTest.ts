dfe6889a4087c3cc7138bd1fe7536d9e4b2e3c91
Graham Mendick <graham.mendick@gmail.com>
2023-05-08 17:05:43 +0100

    Tested transition, refresh and back defaults

--- Navigation/test/RewriteNavigationTest.ts
+++ Navigation/test/RewriteNavigationTest.ts
@@ -301,6 +301,40 @@ describe('Rewrite Navigation', () => {
         });
     });
 
+    describe('Rewrite Data Defaults', () => {
+        const test = navigate => {
+            it('should populate href', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's', route: 'r', defaults: {a: 'b'} }
+                ]);
+                const {s} = stateNavigator.states;
+                s.rewriteNavigation = ({a}) => (a === 'b' ? {
+                    stateKey: 's',
+                    navigationData: {
+                        a: 'c'
+                    }
+                } : null);
+                const link = navigate(stateNavigator);
+                assert.equal(stateNavigator.historyManager.getHref(link), '#/r?a=c');
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.navigate('s');
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getNavigationLink('s'));
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent().navigate('s').url);
+        });
+    });
+
     describe('Rewrite Data Null', () => {
         const test = navigate => {
             it('should populate context', () => {
@@ -788,6 +822,48 @@ describe('Rewrite Navigation', () => {
         });
     });
 
+    describe('Transition Rewrite Data Defaults', () => {
+        const test = navigate => {
+            it('should populate context', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's0', route: 'r0' },
+                    { key: 's1', route: 'r1', defaults: {a: 'b'} },
+                    { key: 's2', route: 'r2' },
+                ]);
+                const {s1} = stateNavigator.states;
+                s1.rewriteNavigation = ({a}) => (a === 'b' ? {
+                    stateKey: 's2',
+                    navigationData: {
+                        a: 'c'
+                    }
+                } : null);
+                stateNavigator.navigate('s0');
+                const link = navigate(stateNavigator);
+                const rewrittenLink = stateNavigator.historyManager.getHref(link).substring(1);
+                stateNavigator.navigateLink(rewrittenLink);
+                assert.equal(stateNavigator.stateContext.url, '/r2?a=c');
+                assert.equal(stateNavigator.stateContext.state.key, 's2');
+                assert.equal(stateNavigator.stateContext.data.a, 'c');
+                assert.equal(stateNavigator.stateContext.crumbs.length, 0);
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.navigate('s1');
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getNavigationLink('s1'));
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent(true).navigate('s1').url);
+        });
+    });
+
     describe('Transition Rewrite Hash', () => {
         const test = navigate => {
             it('should populate context', () => {
@@ -961,6 +1037,47 @@ describe('Rewrite Navigation', () => {
                 assert.equal(stateNavigator.stateContext.url, '/r1?a=b');
                 assert.equal(stateNavigator.stateContext.state.key, 's1');
                 assert.equal(stateNavigator.stateContext.data.a, 'b');
+                assert.equal(stateNavigator.stateContext.crumbs.length, 0);
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.refresh();
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getRefreshLink());
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent(true).refresh().url);
+        });
+    });
+
+    describe('Refresh Rewrite Data Defaults', () => {
+        const test = navigate => {
+            it('should populate context', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's0', route: 'r0', defaults: {a: 'b'} },
+                    { key: 's1', route: 'r1' },
+                ]);
+                const {s0} = stateNavigator.states;
+                s0.rewriteNavigation = ({a}) => (a === 'b' ? {
+                    stateKey: 's1',
+                    navigationData: {
+                        a: 'c'
+                    }
+                } : null);
+                stateNavigator.navigate('s0');
+                const link = navigate(stateNavigator);
+                const rewrittenLink = stateNavigator.historyManager.getHref(link).substring(1);
+                stateNavigator.navigateLink(rewrittenLink);
+                assert.equal(stateNavigator.stateContext.url, '/r1?a=c');
+                assert.equal(stateNavigator.stateContext.state.key, 's1');
+                assert.equal(stateNavigator.stateContext.data.a, 'c');
                 assert.equal(stateNavigator.stateContext.crumbs.length, 0);
             });
         }
@@ -1159,6 +1276,49 @@ describe('Rewrite Navigation', () => {
                 assert.equal(stateNavigator.stateContext.url, '/r2?a=b');
                 assert.equal(stateNavigator.stateContext.state.key, 's2');
                 assert.equal(stateNavigator.stateContext.data.a, 'b');
+                assert.equal(stateNavigator.stateContext.crumbs.length, 0);
+            });
+        }
+
+        describe('Navigate', () => {
+            test(stateNavigator => {
+                stateNavigator.navigateBack(1);
+                return stateNavigator.stateContext.url
+            });
+        });
+
+        describe('Navigate Link', () => {
+            test(stateNavigator => stateNavigator.getNavigationBackLink(1));
+        });
+
+        describe('Fluent Navigate', () => {
+            test(stateNavigator => stateNavigator.fluent(true).navigateBack(1).url);
+        });
+    });
+
+    describe('Back Rewrite Data Defaults', () => {
+        const test = navigate => {
+            it('should populate context', () => {
+                const stateNavigator = new StateNavigator([
+                    { key: 's0', route: 'r0', defaults: {a: 'b'} },
+                    { key: 's1', route: 'r1', trackCrumbTrail: true },
+                    { key: 's2', route: 'r2' },
+                ]);
+                const {s0} = stateNavigator.states;
+                s0.rewriteNavigation = ({a}) => (a === 'b' ? {
+                    stateKey: 's2',
+                    navigationData: {
+                        a: 'c'
+                    }
+                } : null);
+                stateNavigator.navigate('s0');
+                stateNavigator.navigate('s1');
+                const link = navigate(stateNavigator);
+                const rewrittenLink = stateNavigator.historyManager.getHref(link).substring(1);
+                stateNavigator.navigateLink(rewrittenLink);
+                assert.equal(stateNavigator.stateContext.url, '/r2?a=c');
+                assert.equal(stateNavigator.stateContext.state.key, 's2');
+                assert.equal(stateNavigator.stateContext.data.a, 'c');
                 assert.equal(stateNavigator.stateContext.crumbs.length, 0);
             });
         }
