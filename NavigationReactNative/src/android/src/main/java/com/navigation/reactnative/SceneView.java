fdf949de0fe9dc66c99eb5a554766941d8bf3682
Graham Mendick <graham.mendick@gmail.com>
2023-08-05 22:04:06 +0100

    Ensured scene transitions as a single unit
    Without this each child transitions independently - the toolbar separate to the scroll view, for example

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SceneView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SceneView.java
@@ -26,6 +26,7 @@ public class SceneView extends ReactViewGroup {
 
     public SceneView(Context context) {
         super(context);
+        setTransitionGroup(true);
     }
 
     protected void setLandscape(boolean landscape) {
