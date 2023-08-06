7c606980f86025d60b15c0d50802636cd8c39730
Graham Mendick <graham.mendick@gmail.com>
2023-08-06 14:17:51 +0100

    Removed redundant pause listener
    It threw an error when pressing very fast going forward with long duration transition - because the parent was an Overlay and not the NavigationStackView. Also it's not needed because if it's paused it means another transition has taken over so can let that one call rest. Only need the latest enter transition to call rest - previous ones in flight aren't relevant

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SceneFragment.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SceneFragment.java
@@ -19,20 +19,6 @@ import java.util.concurrent.TimeUnit;
 
 public class SceneFragment extends Fragment {
     private SceneView scene;
-    private final TransitionListenerAdapter enterTransitionListener = new TransitionListenerAdapter(){
-        @Override
-        public void onTransitionEnd(@NonNull Transition transition) {
-            super.onTransitionEnd(transition);
-            if (scene.getParent() != null)
-                ((NavigationStackView) scene.getParent()).onRest(scene.crumb);
-        }
-
-        @Override
-        public void onTransitionPause(@NonNull Transition transition) {
-            super.onTransitionPause(transition);
-            ((NavigationStackView) scene.getParent()).onRest(scene.crumb);
-        }
-    };
 
     public SceneFragment() {
         super();
@@ -89,14 +75,28 @@ public class SceneFragment extends Fragment {
     public void setEnterTransition(@Nullable Object transition) {
         super.setEnterTransition(transition);
         if (transition == null) return;
-        ((Transition) transition).addListener(enterTransitionListener);
+        ((Transition) transition).addListener(new TransitionListenerAdapter(){
+            @Override
+            public void onTransitionEnd(@NonNull Transition transition) {
+                super.onTransitionEnd(transition);
+                if (scene.getParent() instanceof NavigationStackView)
+                    ((NavigationStackView) scene.getParent()).onRest(scene.crumb);
+            }
+        });
     }
 
     @Override
     public void setReenterTransition(@Nullable Object transition) {
         super.setReenterTransition(transition);
         if (transition == null) return;
-        ((Transition) transition).addListener(enterTransitionListener);
+        ((Transition) transition).addListener(new TransitionListenerAdapter(){
+            @Override
+            public void onTransitionEnd(@NonNull Transition transition) {
+                super.onTransitionEnd(transition);
+                if (scene.getParent() instanceof NavigationStackView)
+                    ((NavigationStackView) scene.getParent()).onRest(scene.crumb);
+            }
+        });
     }
 
     @Override
