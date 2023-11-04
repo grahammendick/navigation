55409d4d097823d988e6fd866b9fdffb7d1045d3
Graham Mendick <graham.mendick@gmail.com>
2023-11-04 12:11:29 +0000

    Logged out the height of the sheet as it animates
    Nothing was called when the sheet was animated - not viewDidLayoutSubviews. The frame size didn't even change - tried logging it out. But managed to get it on the presentationLayer! Got the [clue from react native screens ](https://github.com/software-mansion/react-native-screens/pull/1870/files#diff-ad35cf2dc9a86855afc1d2443a42a133ccf0dd8b17589719cb1aab4a81d20604R127)

--- NavigationReactNative/src/ios/NVBottomSheetView.m
+++ NavigationReactNative/src/ios/NVBottomSheetView.m
@@ -1,12 +1,60 @@
 #import "NVBottomSheetView.h"
 
+#import <UIKit/UIKit.h>
+#import "UIView+React.h"
+
+@interface NVBottomSheetController : UIViewController
+
+@end
+
+@implementation NVBottomSheetController
+
+@end
+
 @implementation NVBottomSheetView
+{
+    NVBottomSheetController *_bottomSheetController;
+    int _oldHeight;
+}
 
 - (id)init
 {
     if (self = [super init]) {
+        _bottomSheetController = [[NVBottomSheetController alloc] init];
+        CADisplayLink *displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateView)];
+        [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
     }
     return self;
 }
 
+- (void)updateView {
+    int newHeight = [[_bottomSheetController.view.layer.presentationLayer valueForKeyPath:@"frame.size.height"] intValue];
+    if (_oldHeight != newHeight) {
+        NSLog(@"animated value: %d\n", newHeight);
+        _oldHeight = newHeight;
+    }
+}
+
+- (void)insertReactSubview:(UIView *)subview atIndex:(NSInteger)atIndex
+{
+    [super insertReactSubview:subview atIndex:atIndex];
+    _bottomSheetController.view = subview;
+}
+
+- (void)removeReactSubview:(UIView *)subview
+{
+    [super removeReactSubview:subview];
+}
+
+- (void)didMoveToWindow
+{
+    [super didMoveToWindow];
+    if (@available(iOS 15.0, *)) {
+        UISheetPresentationController *sheet = _bottomSheetController.sheetPresentationController;
+        [sheet setDetents:@[UISheetPresentationControllerDetent.mediumDetent, UISheetPresentationControllerDetent.largeDetent]];
+        [[self reactViewController] presentViewController:_bottomSheetController animated:true completion:nil];
+    }
+}
+
 @end
+
