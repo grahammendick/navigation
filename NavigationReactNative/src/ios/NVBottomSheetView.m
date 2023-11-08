6e4641810629d9545b9aa103bd7e51e1c9c022e0
Graham Mendick <graham.mendick@gmail.com>
2023-11-08 16:04:29 +0000

    Excluded hidden and expanded from largest undimmed
    Neither of them make sense as largest undimmed. Has to be open have to be able to see the scene behind

--- NavigationReactNative/src/ios/NVBottomSheetView.m
+++ NavigationReactNative/src/ios/NVBottomSheetView.m
@@ -100,7 +100,7 @@
         [sheet animateChanges:^{
             [sheet setLargestUndimmedDetentIdentifier:nil];
             if ([_largestUndimmedDetent length]) {
-                [sheet setLargestUndimmedDetentIdentifier:[_largestUndimmedDetent isEqual: @"collapsed"] ? [self collapsedIdentifier] : ([_largestUndimmedDetent isEqual: @"expanded"] ? [self expandedIdentifier] : [self halfExpandedIdentifier])];
+                [sheet setLargestUndimmedDetentIdentifier:[_largestUndimmedDetent isEqual: @"collapsed"] ? [self collapsedIdentifier] : [self halfExpandedIdentifier]];
             }
             [sheet setDetents: [[self halfExpandedIdentifier] isEqual:UISheetPresentationControllerDetentIdentifierLarge] ? @[_collapsedDetent, _expandedDetent] : @[_collapsedDetent, _halfExpandedDetent, _expandedDetent]];
             if (_skipCollapsed && ![_detent isEqual:@"collapsed"]) {
