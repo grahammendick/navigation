64732887943e81b87fcfadd4027532761e3a2560
Graham Mendick <graham.mendick@gmail.com>
2023-12-29 10:18:54 +0000

    Ensured bottom sheet animates open on android
    Have to always render non-modal bottom sheet on android otherwise it doesn't animate when it opens

--- NavigationReactNative/src/BottomSheet.tsx
+++ NavigationReactNative/src/BottomSheet.tsx
@@ -31,7 +31,7 @@ const BottomSheet = ({detent, defaultDetent = 'collapsed', expandedHeight, expan
         }
     }
     const BottomSheetView = Platform.OS === 'ios' || !modal ? NVBottomSheet : NVBottomSheetDialog;
-    if (sheetState.dismissed && sheetState.selectedDetent === 'hidden') return null;
+    if ((Platform.OS === 'ios' || modal) && sheetState.dismissed && sheetState.selectedDetent === 'hidden') return null;
     return (
         <BottomSheetView
             detent={Platform.OS === 'android' ? '' + detents[sheetState.selectedDetent] : sheetState.selectedDetent}
