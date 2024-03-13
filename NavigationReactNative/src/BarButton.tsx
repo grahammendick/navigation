d776dc09bb9789a900775d35215da0c965c7ca94
Gerzon Zorrilla <gerzonc@icloud.com>
2024-03-12 23:45:15 -0400

    chore: clean up js code

--- NavigationReactNative/src/BarButton.tsx
+++ NavigationReactNative/src/BarButton.tsx
@@ -3,9 +3,9 @@ import { requireNativeComponent, Image, Platform, UIManager } from 'react-native
 import ActionBar from './ActionBar';
 
 const BarButton = React.forwardRef<any, any>(({image, systemItem, show, search = false, size = 48, style, children, testID, ...props}, ref) => {
-   var constants = (UIManager as any).getViewManagerConfig('NVNavigationBar').Constants;
-   var actionBar = !!((React.Children.toArray(children) as ReactElement<any>[])[0]?.type === ActionBar);
-   return (Platform.OS === 'android' || !search) && (
+    var constants = (UIManager as any).getViewManagerConfig('NVNavigationBar').Constants;
+    var actionBar = !!((React.Children.toArray(children) as ReactElement<any>[])[0]?.type === ActionBar);
+    return (Platform.OS === 'android' || !search) && (
         <NVBarButton
             ref={ref}
             key={Platform.OS === 'ios' && typeof image === 'string' ? image : Platform.OS === 'ios' ? systemItem : testID}
