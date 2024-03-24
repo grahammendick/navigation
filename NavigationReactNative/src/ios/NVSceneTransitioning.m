fec39a8c9ca7731335efdd80689f8f65aa15dfb1
Graham Mendick <graham.mendick@gmail.com>
2024-03-24 11:35:23 +0000

    Turned transitions into transforms
    Combined the array for transitions into affine transformations. Applied next one on top of the last so they all run together. Not sure how to do different durations so keeping it simple to start with

--- NavigationReactNative/src/ios/NVSceneTransitioning.m
+++ NavigationReactNative/src/ios/NVSceneTransitioning.m
@@ -1,4 +1,5 @@
 #import "NVSceneTransitioning.h"
+#import "NVSceneController.h"
 
 #import <UIKit/UIKit.h>
 
@@ -8,8 +9,32 @@
 {
     UIView *containerView = [transitionContext containerView];
     UIView *toScene = [transitionContext viewForKey:UITransitionContextToViewKey];
+    NVSceneController *toSceneController = [transitionContext
+                viewControllerForKey:UITransitionContextToViewControllerKey];
     [containerView addSubview:toScene];
-    [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    for(NSInteger i = 0; i < toSceneController.enterTrans.count; i++) {
+        NVTransition *transition = toSceneController.enterTrans[i];
+        if ([transition.type isEqualToString:@"translate"]) {
+            transform = CGAffineTransformTranslate(transform, transition.x.percent ? transitionContext.containerView.bounds.size.width * transition.x.val / 100 : transition.x.val, transition.y.percent ? transitionContext.containerView.bounds.size.height * transition.y.val / 100 : transition.y.val);
+        }
+        if ([transition.type isEqualToString:@"scale"]) {
+            transform = CGAffineTransformScale(transform, transition.x.val, transition.y.val);
+        }
+        if ([transition.type isEqualToString:@"rotate"]) {
+            transform = CGAffineTransformRotate(transform, transition.x.val * M_PI / 360);
+        }
+        if ([transition.type isEqualToString:@"alpha"]) {
+            toSceneController.view.alpha = transition.x.val;
+        }
+    }
+    toSceneController.view.transform = transform;
+    [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+        toSceneController.view.transform = CGAffineTransformIdentity;
+        toSceneController.view.alpha = 1.0;
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+    }];
 }
 
 - (NSTimeInterval)transitionDuration:(nullable id<UIViewControllerContextTransitioning>)transitionContext {
