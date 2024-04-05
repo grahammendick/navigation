c475153702d7f3fe582e1482259af40b674b85d3
Graham Mendick <graham.mendick@gmail.com>
2024-04-05 14:13:18 +0100

    Tweaked format

--- NavigationReactNative/src/ios/NVBarButtonView.m
+++ NavigationReactNative/src/ios/NVBarButtonView.m
@@ -52,7 +52,7 @@
         }
         [[_bridge moduleForName:@"ImageLoader"] loadImageWithURLRequest:source.request size:source.size scale:source.scale clipped:NO resizeMode:RCTResizeModeCover progressBlock:nil partialLoadBlock:nil completionBlock:^(NSError *error, UIImage *image) {
             dispatch_async(dispatch_get_main_queue(), ^{
-                self -> _button.image = image;
+                self->_button.image = image;
             });
         }];
     } else {
