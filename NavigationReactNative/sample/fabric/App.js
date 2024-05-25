f02f372b1605ed5482a010c7848d4022864e1a9a
Graham Mendick <graham.mendick@gmail.com>
2024-05-25 21:53:38 +0100

    Reverted accidental change

--- NavigationReactNative/sample/fabric/App.js
+++ NavigationReactNative/sample/fabric/App.js
@@ -9,7 +9,7 @@ import Timeline from './Timeline';
 import {NavigationStack, Scene} from 'navigation-react-native';
 
 const stateNavigator = new StateNavigator([
-  {key: 'tabs', trackCrumbTrail: true},
+  {key: 'tabs'},
   {key: 'home'},
   {key: 'notifications'},
   {key: 'tweet', trackCrumbTrail: true},
