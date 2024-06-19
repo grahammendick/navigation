38d2a7dd08deda39af0a396985f327eda0a931c9
Graham Mendick <graham.mendick@gmail.com>
2024-06-19 19:52:43 +0100

    Removed redundant import

--- NavigationReactNative/src/Dialog.tsx
+++ NavigationReactNative/src/Dialog.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { requireNativeComponent, StyleSheet } from 'react-native';
 import FragmentContext from './FragmentContext';
 
