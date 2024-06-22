d711553f6358e5a2c768d657285fca7d214b4788
Graham Mendick <graham.mendick@gmail.com>
2024-06-22 11:32:15 +0100

    Forgot to commit this with recent changes

--- NavigationReactNative/src/Dialog.tsx
+++ NavigationReactNative/src/Dialog.tsx
@@ -1,30 +1,51 @@
-import React, { useMemo, useContext } from 'react';
-import { requireNativeComponent, StyleSheet, View } from 'react-native';
-import { NavigationContext, NavigationEvent } from 'navigation-react';
+import React, { useMemo, useContext, useState, useRef } from 'react';
+import { requireNativeComponent, StyleSheet, View, Platform, UIManager } from 'react-native';
+import { NavigationContext } from 'navigation-react';
 import FragmentContext from './FragmentContext';
 
-const Dialog = ({open = false, modal = true, onChangeOpen, children}) => {
+const Dialog = ({detent, defaultDetent = 'collapsed', modal = true, onChangeDetent, children}) => {
+    const [sheetState, setSheetState]  = useState({selectedDetent: detent || defaultDetent, mostRecentEventCount: 0, dismissed: (detent || defaultDetent) === 'hidden'})
+    const dragging = useRef(false);
+    const changeDetent = (selectedDetent) => {
+        if (sheetState.selectedDetent !== selectedDetent) {
+            if (detent == null)
+                setSheetState(prevSheetState => ({...prevSheetState, selectedDetent}));
+            if (!!onChangeDetent)
+                onChangeDetent(selectedDetent);
+        }
+    }
     const stackId = React.useId?.();
     const ancestorStackIds = useContext(FragmentContext);
     const navigationEvent = useContext(NavigationContext);
     const stackIds = useMemo(() => stackId ? [...ancestorStackIds, stackId] : [], [ancestorStackIds, stackId]);
-    const onShowChanged = ({nativeEvent}) => {
-        const {show} = nativeEvent;
-        if (show !== open)
-            onChangeOpen(show);
+    if (detent != null && detent !== sheetState.selectedDetent)
+        setSheetState(prevSheetState => ({...prevSheetState, selectedDetent: detent, dismissed: detent === 'hidden' && sheetState.dismissed}));
+    const detents = (UIManager as any).getViewManagerConfig('NVBottomSheet').Constants?.Detent;
+    const onDetentChanged = ({nativeEvent}) => {
+        const {eventCount: mostRecentEventCount, detent: nativeDetent} = nativeEvent;
+        const selectedDetent = Platform.OS === 'android'? Object.keys(detents).find(name => detents[name] === nativeDetent) : nativeDetent;
+        dragging.current = !selectedDetent;
+        if (selectedDetent) {
+            changeDetent(selectedDetent);
+            setSheetState(prevSheetState => ({...prevSheetState, mostRecentEventCount}));
+        }
     }
-    if (!open) return null;
+    if (sheetState.dismissed && sheetState.selectedDetent === 'hidden') return null;
     const RootView = modal ? NVDialogRoot : View;
     const DialogView = modal ? NVDialog : NVSheet;
     const crumb = navigationEvent.stateNavigator.stateContext.crumbs.length;
     return (
         <FragmentContext.Provider value={stackIds}>
             <DialogView
-                show={open}
+                detent={Platform.OS === 'android' ? '' + detents[sheetState.selectedDetent] : sheetState.selectedDetent}
+                dismissed={sheetState.dismissed}
                 stackId={stackId}
                 ancestorStackIds={ancestorStackIds}
                 crumb={crumb}
-                onShowChanged={onShowChanged}
+                mostRecentEventCount={sheetState.mostRecentEventCount}
+                onMoveShouldSetResponderCapture={() => dragging.current}
+                onDetentChanged={onDetentChanged}
+                onDismissed={() => setSheetState(prevSheetState => ({...prevSheetState, dismissed: true}))}
                 style={styles.dialog}>
                 <RootView style={styles.dialog} collapsable={false}>
                     {children}
@@ -42,6 +63,7 @@ const styles = StyleSheet.create({
     dialog: {
         position: 'absolute',
         elevation: 5,
+        backgroundColor: 'transparent',
         top:0, right: 0, bottom: 0, left: 0
     },
 });
