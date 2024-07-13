1dc30bce6fd544dcc901fa1a0904becd70523513
Graham Mendick <graham.mendick@gmail.com>
2024-07-13 13:51:32 +0100

    Prevented scene interaction when drawer open
    The drawer is modal so should block interaction with the content behind. Copied from react native ReactDrawerLayout https://github.com/facebook/react-native/blob/65a3259f039416394d2b945ec10565d38b3cad9e/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/drawer/ReactDrawerLayout.java#L67-L92 (dropped the try/catch because not sure what it's for)

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/DrawerLayoutView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/DrawerLayoutView.java
@@ -1,12 +1,36 @@
 package com.navigation.reactnative;
 
 import android.content.Context;
+import android.view.MotionEvent;
 
 import androidx.annotation.NonNull;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.facebook.react.uimanager.events.NativeGestureUtil;
+
 public class DrawerLayoutView extends DrawerLayout {
+    boolean dragging;
     public DrawerLayoutView(@NonNull Context context) {
         super(context);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (super.onInterceptTouchEvent(ev)) {
+            NativeGestureUtil.notifyNativeGestureStarted(this, ev);
+            dragging = true;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getActionMasked();
+        if (action == MotionEvent.ACTION_UP && dragging) {
+            NativeGestureUtil.notifyNativeGestureEnded(this, ev);
+            dragging = false;
+        }
+        return super.onTouchEvent(ev);
     }
 }
