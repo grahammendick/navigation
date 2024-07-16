ddde4bca6627940026fb9b3133f13bb6b8444cef
Graham Mendick <graham.mendick@gmail.com>
2024-07-16 17:46:14 +0100

    Removed code for old drawer
    I checked back and this seemed to be for handling activities - apparently drawer didn't open on second activity (https://github.com/grahammendick/navigation/pull/266). This seems to be a good time to remove this old code

--- NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SceneView.java
+++ NavigationReactNative/src/android/src/main/java/com/navigation/reactnative/SceneView.java
@@ -47,11 +47,6 @@ public class SceneView extends ReactViewGroup {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         setOrientation();
-        View child = getChildAt(0);
-        if (child != null && child.getClass().getSimpleName().contains("DrawerLayout")) {
-            child.requestLayout();
-            post(measureAndLayoutDrawer);
-        }
     }
 
     private void setOrientation() {
@@ -60,18 +55,6 @@ public class SceneView extends ReactViewGroup {
             activity.setRequestedOrientation(this.landscape ? ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE : ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
     }
-
-    private final Runnable measureAndLayoutDrawer = new Runnable() {
-        @Override
-        public void run() {
-            View drawer = getChildAt(0);
-            if (drawer == null) return;
-            drawer.measure(
-                MeasureSpec.makeMeasureSpec(drawer.getWidth(), MeasureSpec.EXACTLY),
-                MeasureSpec.makeMeasureSpec(drawer.getHeight(), MeasureSpec.EXACTLY));
-            drawer.layout(drawer.getLeft(), drawer.getTop(), drawer.getRight(), drawer.getBottom());
-        }
-    };
 
     protected void popped() {
         ReactContext reactContext = (ReactContext) getContext();
