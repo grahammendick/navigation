93b4f8db68b4f891a6a31a0616b229e6f6375ad8
Graham Mendick <graham.mendick@gmail.com>
2024-08-10 15:27:25 +0100

    Started the web sheet
    Trying to get modal sheet to fill out the page - but there's default margin provided by browser. Think would need pseudo classes to override this but react native web doesn't support.
    Hand rendering dialog instead of using View because don't think react native web respects the dialog role

---
+++ NavigationReactNativeWeb/src/Sheet.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+const Sheet = ({detent, defaultDetent = 'collapsed', expandedHeight, expandedOffset, modal = true, onChangeDetent, children}) => {
+    const [selectedDetent, setSelectedDetent]  = useState(detent || defaultDetent);
+    const dialogEl = useRef<HTMLDialogElement | null>(null);
+    if (detent != null && detent !== selectedDetent)
+        setSelectedDetent(detent);
+    useEffect(() => {
+        if (selectedDetent !== 'hidden') {
+            if (modal) dialogEl.current.showModal();
+            else dialogEl.current.show();
+        } else {
+            dialogEl.current.close();
+        }
+    }, [selectedDetent, modal])
+    const onClose = () => {
+        if (selectedDetent !== 'hidden') {
+            if (detent == null) setSelectedDetent('hidden');
+            if (!!onChangeDetent) onChangeDetent('hidden');
+        }
+    };
+    return (
+        <dialog ref={dialogEl} onClose={onClose} style={{
+            display: selectedDetent !== 'hidden' ? 'flex' : 'none',
+            width: '100vw',
+            color: 'red',
+            height: expandedHeight == null ? '100vh' : `${expandedHeight}px`,
+            border: 0,
+            padding: 0,
+            zIndex: 54,
+        }}>
+            {children}
+        </dialog>
+    );
+}
+
+export const BottomSheet = Sheet;
+export default Sheet;
