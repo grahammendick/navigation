6057fa19026b5b27490994b17002f9abb86c7365
Graham Mendick <graham.mendick@gmail.com>
2024-08-25 14:48:28 +0100

    Sketched in idea for native style props on web
    Idea is to use unmountStyle and crumbStyle props from native on the web. Original though was to change navigation motion to use these props - but new idea is to convert them into props that navigation motion understands. This makes more sense because navigation motion is like the 'primitive' so don't want to change that. The navigation native web layer turns props into native primitives so that feels like the right place to do it. Want navigation motion to stay independent of native

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -3,18 +3,78 @@ import { View } from 'react-native';
 import { NavigationMotion, Scene, SharedElementMotion } from 'navigation-react-mobile';
 import { MobileHistoryManager } from 'navigation-react-mobile';
 
-const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, sharedElementTransition, duration, renderScene, renderTransition, children}) => (
-    <NavigationMotion
-        unmountedStyle={unmountedStyle || {translate: 100}}
-        mountedStyle={mountedStyle || {translate: 0}}
-        crumbStyle={crumbedStyle || {translate: 0}}        
-        sharedElementMotion={sharedElementTransition}
-        duration={duration}
-        renderScene={renderScene}
-        renderMotion={typeof children !== 'function' ? renderTransition || renderMotion : undefined}>
-        {typeof children !== 'function' ? cloneScenes(children) : (children || renderMotion)}
-    </NavigationMotion>
-);
+const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, unmountStyle = () => null, crumbStyle = () => null,
+  sharedElementTransition, duration, renderScene, renderTransition, children}) => {
+  const customRender = typeof children === 'function' || renderTransition;
+  // if !customRender then turn unmountStyle into unmounted style (and mountedStyle)
+  // and crumbStyle into crumbStyle
+  // (what about if they're empty like in the zoom sample?)
+  const returnOrCall = (item, ...args) => typeof item !== 'function' ? item : item(...args);
+  return (
+      <NavigationMotion
+          unmountedStyle={unmountedStyle || ((state, data, crumbs) => {
+            let trans = returnOrCall(unmountStyle, true, state, data, crumbs);
+            if (!trans || typeof trans === 'string')
+              trans = {type: 'translate',  startX: 100};
+            trans = !Array.isArray(trans) ? trans : {items: trans};
+            const transStyle = {duration};
+            const addStyle = (type: string, start: string | number) => {
+              if (start === undefined) return;
+              const percent = typeof start === 'string' && start.endsWith('%')
+              transStyle[type + (percent ? '_pc' : '')] = percent ? +(start as string).slice(0, -1) : +start;
+            }
+            const convertTrans = ({type, start, from, startX, fromX, startY, fromY, items}) => {
+              if (type === 'translate') {
+                addStyle('translateX', startX ?? fromX);
+                addStyle('translateY', startY ?? fromY);
+              }
+              if (type === 'scale') {
+                addStyle('scaleX', startX ?? fromX);
+                addStyle('scaleY', startY ?? fromY);
+              }
+              if (type === 'alpha') addStyle('alpha', start ?? from);
+              if (type === 'rotate') addStyle('rotate', start ?? from); // can do pivot? transform origin?
+              items?.forEach(convertTrans);
+            };
+            convertTrans(trans);
+            return transStyle;
+          })}
+          mountedStyle={mountedStyle || {translateX_pc: 0, alpha: 1, scaleX: 1, scaleY: 1}}
+          crumbStyle={crumbedStyle || ((state, data, crumbs, nextState, nextData) => {
+            let trans = returnOrCall(crumbStyle, true, state, data, crumbs, nextState, nextData);
+            if (!trans || typeof trans === 'string')
+              trans = {type: 'translate',  startX: 0};
+            trans = !Array.isArray(trans) ? trans : {items: trans};
+            const transStyle = {duration};
+            const addStyle = (type: string, start: string | number) => {
+              if (start === undefined) return;
+              const percent = typeof start === 'string' && start.endsWith('%')
+              transStyle[type + (percent ? '_pc' : '')] = percent ? +(start as string).slice(0, -1) : +start;
+            }
+            const convertTrans = ({type, start, from, startX, fromX, startY, fromY, items}) => {
+              if (type === 'translate') {
+                addStyle('translateX', startX ?? fromX);
+                addStyle('translateY', startY ?? fromY);
+              }
+              if (type === 'scale') {
+                addStyle('scaleX', startX ?? fromX);
+                addStyle('scaleY', startY ?? fromY);
+              }
+              if (type === 'alpha') addStyle('alpha', start ?? from);
+              if (type === 'rotate') addStyle('rotate', start ?? from); // can do pivot? transform origin?
+              items?.forEach(convertTrans);
+            };
+            convertTrans(trans);
+            return transStyle;
+          })}
+          sharedElementMotion={sharedElementTransition}
+          duration={duration}
+          renderScene={renderScene}
+          renderMotion={typeof children !== 'function' ? renderTransition || renderMotion : undefined}>
+          {typeof children !== 'function' ? cloneScenes(children) : (children || renderMotion)}
+      </NavigationMotion>
+  );
+}
 
 const renderMotion = ({translate}, scene, key) => (
   <View key={key}
