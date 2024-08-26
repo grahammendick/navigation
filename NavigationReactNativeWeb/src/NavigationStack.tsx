931d95e7c643e04a2ba37cd465482bff9e083cc9
Graham Mendick <graham.mendick@gmail.com>
2024-08-26 17:06:39 +0100

    Tweaked format for consistency

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -3,19 +3,19 @@ import { View } from 'react-native';
 import { NavigationMotion, Scene, SharedElementMotion } from 'navigation-react-mobile';
 import { MobileHistoryManager } from 'navigation-react-mobile';
 
-const NavigationStack = ({ unmountedStyle, mountedStyle, crumbedStyle, unmountStyle = () => null, crumbStyle = () => null,
-    sharedElementTransition, duration, renderScene, renderTransition, children }) => {
-    const emptyStyle = { duration: undefined, translateX: 0, translateX_pc: 0, scaleX: 1, scaleX_pc: 100, alpha: 1, rotate: 0 };
+const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, unmountStyle = () => null, crumbStyle = () => null,
+    sharedElementTransition, duration, renderScene, renderTransition, children}) => {
+    const emptyStyle = {duration: undefined, translateX: 0, translateX_pc: 0, scaleX: 1, scaleX_pc: 100, alpha: 1, rotate: 0};
     const returnOrCall = (item, ...args) => typeof item !== 'function' ? item : item(...args);
     const getStyle = (trans) => {
-        trans = !Array.isArray(trans) ? trans : { items: trans };
-        const transStyle = { ...emptyStyle };
+        trans = !Array.isArray(trans) ? trans : {items: trans};
+        const transStyle = {...emptyStyle};
         const addStyle = (type: string, start: string | number) => {
             if (start === undefined) return;
             const suffix = `${start}`.endsWith('%') ? '_pc' : '';
             transStyle[type + suffix] = +(suffix ? `${start}`.slice(0, -1) : start);
         }
-        const convertTrans = ({ type, start, from, startX, fromX, startY, fromY, items, duration }) => {
+        const convertTrans = ({type, start, from, startX, fromX, startY, fromY, items, duration}) => {
             if (type === 'translate' || type === 'scale') addStyle(`${type}X`, startX ?? fromX);
             if (type === 'translate' || type === 'scale') addStyle(`${type}Y`, startY ?? fromY);
             if (type === 'alpha' || type === 'rotate') addStyle(type, start ?? from);
@@ -29,15 +29,15 @@ const NavigationStack = ({ unmountedStyle, mountedStyle, crumbedStyle, unmountSt
         <NavigationMotion
             unmountedStyle={unmountedStyle || ((state, data, crumbs) => {
                 const trans = returnOrCall(unmountStyle, true, state, data, crumbs);
-                return getStyle(trans && typeof trans !== 'string' ? trans : trans ? { type: 'translate', startX: '100%' } : { duration: 0 });
+                return getStyle(trans && typeof trans !== 'string' ? trans : trans ? {type: 'translate', startX: '100%'} : {duration: 0});
             })}
             mountedStyle={mountedStyle || ((state, data, crumbs, _nextState, _nextData, from) => {
                 const trans = returnOrCall(from ? unmountStyle : crumbStyle, true, state, data, crumbs);
-                return { ...emptyStyle, duration: trans && typeof trans !== 'string' ? getStyle(trans).duration : undefined };
+                return {...emptyStyle, duration: trans && typeof trans !== 'string' ? getStyle(trans).duration : undefined};
             })}
             crumbStyle={crumbedStyle || ((state, data, crumbs, nextState, nextData) => {
                 const trans = returnOrCall(crumbStyle, true, state, data, crumbs, nextState, nextData);
-                return getStyle(trans && typeof trans !== 'string' ? trans : trans ? { type: 'translate', startX: '0%' } : { duration: 0 });
+                return getStyle(trans && typeof trans !== 'string' ? trans : trans ? {type: 'translate', startX: '0%'} : {duration: 0});
             })}
             sharedElementMotion={sharedElementTransition}
             duration={duration}
@@ -48,7 +48,7 @@ const NavigationStack = ({ unmountedStyle, mountedStyle, crumbedStyle, unmountSt
     );
 }
 
-const renderMotion = ({ translateX, translateX_pc, scaleX, scaleX_pc, alpha, rotate }, scene, key) => (
+const renderMotion = ({translateX, translateX_pc, scaleX, scaleX_pc, alpha, rotate}, scene, key) => (
     <View key={key}
         style={{
             transform: `
@@ -69,7 +69,7 @@ const renderMotion = ({ translateX, translateX_pc, scaleX, scaleX_pc, alpha, rot
 const cloneScenes = (children, nested = false) => (
     React.Children.map(children, scene => (
         (scene.type === Scene || nested)
-            ? React.cloneElement(scene, { crumbStyle: scene.props.crumbedStyle })
+            ? React.cloneElement(scene, {crumbStyle: scene.props.crumbedStyle})
             : React.cloneElement(scene, null, cloneScenes(scene.props.children, true))
     ))
 );
