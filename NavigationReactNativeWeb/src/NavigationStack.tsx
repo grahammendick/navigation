41ecbdceeb9d598e41df16130e7871e782f36615
Graham Mendick <graham.mendick@gmail.com>
2024-08-26 12:39:20 +0100

    Simplified format

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -35,16 +35,12 @@ const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, unmountSty
       <NavigationMotion
           unmountedStyle={unmountedStyle || ((state, data, crumbs) => {
             let trans = returnOrCall(unmountStyle, true, state, data, crumbs);
-            if (!trans || typeof trans === 'string')
-              trans = {type: 'translate',  startX: '100%'};
-            return getStyle(trans);
+            return getStyle(trans && typeof trans !== 'string' ? trans : {type: 'translate',  startX: '100%'});
           })}
           mountedStyle={mountedStyle || {...emptyStyle}}
           crumbStyle={crumbedStyle || ((state, data, crumbs, nextState, nextData) => {
             let trans = returnOrCall(crumbStyle, true, state, data, crumbs, nextState, nextData);
-            if (!trans || typeof trans === 'string')
-              trans = {type: 'translate',  startX: '0%'};
-            return getStyle(trans);
+            return getStyle(trans && typeof trans !== 'string' ? trans : {type: 'translate',  startX: '0%'});
           })}
           sharedElementMotion={sharedElementTransition}
           duration={duration}
