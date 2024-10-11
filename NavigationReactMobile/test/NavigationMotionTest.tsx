e3302f463fccea3938e85bb4bd81f72c0efc27fb
Graham Mendick <graham.mendick@gmail.com>
2024-10-11 18:02:19 +0100

    Split up fluent & back test for unmounted asserts

--- NavigationReactMobile/test/NavigationMotionTest.tsx
+++ NavigationReactMobile/test/NavigationMotionTest.tsx
@@ -2286,6 +2286,43 @@ describe('NavigationMotion', function () {
                 await test();
             })
         });
+        const test = async () => {
+            await act(async () => {
+                var url = stateNavigator.fluent(true)
+                    .navigate('sceneB')
+                    .navigate('sceneC').url;
+                stateNavigator.navigateLink(url);
+            });
+            await act(async () => {
+                stateNavigator.navigateBack(2);
+            });
+            try {
+                var scenes = container.querySelectorAll(".scene");
+                assert.equal(scenes.length, 1);
+                assert.notEqual(scenes[0].querySelector("#sceneA"), null);
+                assert.equal(container.querySelector("#sceneB"), null);
+                assert.equal(container.querySelector("#sceneC"), null);
+            } finally {
+                act(() => root.unmount());
+            }
+        };
+    });
+
+    describe('A to A -> B -> C to A', function () {
+        var stateNavigator, root, container;
+        var SceneA = () => <div id="sceneA" />;
+        var SceneB = () => <div id="sceneB" />;
+        var SceneC = () => <div id="sceneC" />;
+        beforeEach(() => {
+            stateNavigator = new StateNavigator([
+                { key: 'sceneA' },
+                { key: 'sceneB', trackCrumbTrail: true },
+                { key: 'sceneC', trackCrumbTrail: true },
+            ]);
+            stateNavigator.navigate('sceneA');
+            container = document.createElement('div');
+            root = createRoot(container)
+        });
         describe('Static Stack', () => {            
             it('should render A', async function(){
                 var {sceneA, sceneB, sceneC} = stateNavigator.states;
@@ -2324,14 +2361,19 @@ describe('NavigationMotion', function () {
                     .navigate('sceneB')
                     .navigate('sceneC').url;
                 stateNavigator.navigateLink(url);
+            });
+            await act(async () => {
                 stateNavigator.navigateBack(2);
             });
             try {
                 var scenes = container.querySelectorAll(".scene");
-                assert.equal(scenes.length, 1);
+                assert.equal(scenes.length, 3);
                 assert.notEqual(scenes[0].querySelector("#sceneA"), null);
+                assert.notEqual(scenes[0].style.display, 'none');
+                assert.equal(scenes[1].style.display, 'none');
+                assert.notEqual(scenes[2].querySelector("#sceneC"), null);
+                assert.equal(scenes[2].style.display, 'none');
                 assert.equal(container.querySelector("#sceneB"), null);
-                assert.equal(container.querySelector("#sceneC"), null);
             } finally {
                 act(() => root.unmount());
             }
