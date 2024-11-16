849d12f85d43d18bbc2be5cdf16262f151ae9665
Graham Mendick <graham.mendick@gmail.com>
2024-11-16 16:07:45 +0000

    Deleted redundant code
    This was needed when the image loading was in the c++ but now can check for sf symbols directly in the component

--- NavigationReactNative/src/ios/NVSystemImageValidator.mm
+++
@@ -1,9 +0,0 @@
-#import "NVSystemImageValidator.h"
-#import <UIKit/UIKit.h>
-
-bool isSystemImage(std::string imageName) {
-    if (@available(iOS 13.0, *)) {
-        return [UIImage systemImageNamed:[NSString stringWithUTF8String:imageName.c_str()]];
-    }
-    return false;
-}
