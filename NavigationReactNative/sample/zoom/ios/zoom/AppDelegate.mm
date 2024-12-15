87d15a4fb98edcb36c6040f10423a36233b46800
Graham Mendick <graham.mendick@gmail.com>
2024-12-15 15:44:10 +0000

    Enabled deep linking on ios
    Took the latest [AppDelegate code from the react native docs](https://reactnative.dev/docs/linking?syntax=ios#open-links-and-deep-links-universal-links)

--- NavigationReactNative/sample/zoom/ios/zoom/AppDelegate.mm
+++ NavigationReactNative/sample/zoom/ios/zoom/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -10,7 +11,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -26,6 +27,11 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
