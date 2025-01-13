4e41a0f4227834a9ec936d25dc0267e8c887e63d
Graham Mendick <graham.mendick@gmail.com>
2025-01-13 18:01:26 +0000

    Rendered person details and link back to search

--- NavigationReact/sample/rsc/Person.tsx
+++ NavigationReact/sample/rsc/Person.tsx
@@ -1,3 +1,28 @@
-const Person = () => <h1>Person</h1>;
+import { getPerson } from './data';
+import { NavigationBackLink, useNavigationEvent } from 'navigation-react';
+
+const Person = async () => {
+    const {data} = useNavigationEvent();
+    const person: any = await getPerson(data.id);
+      return (
+        <>
+            <h1>Person</h1>
+            <div>
+                <NavigationBackLink distance={1}>
+                    Person Search
+                </NavigationBackLink>
+                <div>
+                    <h2>{person.name}</h2>
+                    <div className="label">Date of Birth</div>
+                    <div>{person.dateOfBirth}</div>
+                    <div className="label">Email</div>
+                    <div>{person.email}</div>
+                    <div className="label">Phone</div>
+                    <div>{person.phone}</div>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default Person;
