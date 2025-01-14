0830b3b2e910fed604c76406914a8497263f5cab
Graham Mendick <graham.mendick@gmail.com>
2025-01-14 21:29:51 +0000

    Changed for clarity

--- NavigationReact/sample/rsc/Filter.tsx
+++ NavigationReact/sample/rsc/Filter.tsx
@@ -8,17 +8,17 @@ const Filter = () => {
             <div>
                 <label htmlFor="name">Name</label>
                 <input id="name" value={data.name || ''} onChange={({target}) => {
-                    stateNavigator.refresh({...data, name: target.value, page: null});
+                    stateNavigator.refresh({...data, name: target.value, page: 1});
                 }} />
             </div>
             Page size
             <RefreshLink
-                navigationData={{size: 5, page: null}}
+                navigationData={{size: 5, page: 1}}
                 includeCurrentData>
                 5
             </RefreshLink>
             <RefreshLink
-                navigationData={{size: 10, page: null}}
+                navigationData={{size: 10, page: 1}}
                 includeCurrentData
             >
                 10
