9dfdd5186d6481d8cfcce9ad78a74bb1126dd768
Graham Mendick <graham.mendick@gmail.com>
2025-01-19 15:23:14 +0000

    Used null to demonstrate reusability
    Could reuse filter on scene without paging - not so much here because page size part of filter. But don't want to specify non-filter props so could drop it into other scenes where there's no paging, for example

--- NavigationReact/sample/rsc/Filter.tsx
+++ NavigationReact/sample/rsc/Filter.tsx
@@ -12,12 +12,12 @@ const Filter = () => {
                 <label htmlFor="name">Name</label>
                 <input id="name" value={optimisticName || ''} onChange={({target: {value}}) => {
                     setOptimisticName(value);
-                    stateNavigator.refresh({...data, name: value, page: 1});
+                    stateNavigator.refresh({...data, name: value, page: null});
                 }} />
             </div>
             Page size
-            <RefreshLink navigationData={{size: 5, page: 1}} includeCurrentData>5</RefreshLink>
-            <RefreshLink navigationData={{size: 10, page: 1}} includeCurrentData>10</RefreshLink>
+            <RefreshLink navigationData={{size: 5, page: null}} includeCurrentData>5</RefreshLink>
+            <RefreshLink navigationData={{size: 10, page: null}} includeCurrentData>10</RefreshLink>
         </div>
     );
 }
