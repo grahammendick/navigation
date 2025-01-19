9b7e333190706c547a2e02fe9669adaf9e34ee18
Graham Mendick <graham.mendick@gmail.com>
2025-01-19 14:54:32 +0000

    Removed redundant code
    NavigationContext isn't used on the server

--- NavigationReact/src/NavigationReact.server.ts
+++ NavigationReact/src/NavigationReact.server.ts
@@ -1,5 +1,4 @@
 ﻿import AsyncStateNavigator from './AsyncStateNavigator';
-import NavigationContext from './NavigationContext.server';
 import NavigationHandler from './NavigationHandler.server';
 import NavigationBackLink from './NavigationBackLink';
 import NavigationLink from './NavigationLink';
@@ -9,4 +8,4 @@ import SceneView from './SceneView.server';
 import SceneRSCView from './SceneRSCView';
 import useNavigationEvent from './useNavigationEvent.server';
 
-export { AsyncStateNavigator, NavigationContext, NavigationHandler, NavigationBackLink, NavigationLink, RefreshLink, FluentLink, SceneView, SceneRSCView, useNavigationEvent };
+export { AsyncStateNavigator, NavigationHandler, NavigationBackLink, NavigationLink, RefreshLink, FluentLink, SceneView, SceneRSCView, useNavigationEvent };
