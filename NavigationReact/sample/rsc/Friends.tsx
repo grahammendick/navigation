c22fdfa6a5069cefc6bffff5c31a1ef8919d0044
Graham Mendick <graham.mendick@gmail.com>
2025-01-19 11:01:18 +0000

    Moved link out of suspense boundary to simplify

--- NavigationReact/sample/rsc/Friends.tsx
+++ NavigationReact/sample/rsc/Friends.tsx
@@ -6,12 +6,15 @@ import { RefreshLink, useNavigationEvent } from 'navigation-react';
 
 const Friends = async () => {
   const {data: {show}} = useNavigationEvent();
-  if (!show)
-    return <RefreshLink navigationData={{show: true}} includeCurrentData>Show Friends</RefreshLink>
   return (
-    <Suspense fallback={<><RefreshLink navigationData={{show: true}} includeCurrentData>Show Friends</RefreshLink><h2>Loading...</h2></>}>
-      <List />
-    </Suspense>
+    <>
+      <RefreshLink navigationData={{show: !show}} includeCurrentData>{`${!show ? 'Show' : 'Hide'} Friends`}</RefreshLink>
+      {show && (
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <List />
+        </Suspense>
+      )}
+    </>
   );
 }
 
@@ -19,14 +22,11 @@ const List = async () => {
   const {data: {id}} = useNavigationEvent();
   const friends = await getFriends(id);
   return (
-    <>
-      <RefreshLink navigationData={{show: false}} includeCurrentData>Hide Friends</RefreshLink>
-      <ul>
-        {friends.map(({id, name}) => (
-          <li><RefreshLink navigationData={{id}} includeCurrentData>{name}</RefreshLink></li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {friends.map(({id, name}) => (
+        <li><RefreshLink navigationData={{id}} includeCurrentData>{name}</RefreshLink></li>
+      ))}
+    </ul>
   );
 }
 
