b971c0269190512afa92ad9c5164c2514ab868d6
Graham Mendick <graham.mendick@gmail.com>
2025-01-19 15:20:59 +0000

    Removed parcel comments
    Copied this code over from parcel rsc examples. The comments aren't appropriate now in the Navigation router

--- NavigationReact/sample/rsc/client.tsx
+++ NavigationReact/sample/rsc/client.tsx
@@ -1,18 +1,16 @@
-"use client-entry";
+'use client-entry';
 
 import {use, startTransition, ReactElement} from 'react';
 import ReactDOM from 'react-dom/client';
 import {createFromReadableStream} from 'react-server-dom-parcel/client';
 import {rscStream} from 'rsc-html-stream/client';
 
-// Stream in initial RSC payload embedded in the HTML.
 let initialRSCPayload = createFromReadableStream<ReactElement>(rscStream);
 
 function Content() {
   return use(initialRSCPayload);
 }
 
-// Hydrate initial page content.
 startTransition(() => {
   ReactDOM.hydrateRoot(document, <Content />);
 });
--- NavigationReact/sample/rsc/server.tsx
+++ NavigationReact/sample/rsc/server.tsx
@@ -1,12 +1,9 @@
-// Server dependencies.
 import express, {type Request as ExpressRequest, type Response as ExpressResponse} from 'express';
 import {Readable} from 'node:stream';
 import type { ReadableStream as NodeReadableStream } from 'stream/web';
 import {renderToReadableStream} from 'react-server-dom-parcel/server.edge';
 import {injectRSCPayload} from 'rsc-html-stream/server';
 
-// Client dependencies, used for SSR.
-// These must run in the same environment as client components (e.g. same instance of React).
 import {createFromReadableStream} from 'react-server-dom-parcel/client' with {env: 'react-client'};
 import {renderToReadableStream as renderHTMLToReadableStream} from 'react-dom/server' with {env: 'react-client'};
 import ReactClient, {ReactElement} from 'react' with {env: 'react-client'};
@@ -15,7 +12,6 @@ import { StateNavigator } from 'navigation';
 import { NavigationHandler } from 'navigation-react';
 import stateNavigator from './stateNavigator';
 
-// Page components. These must have "use server-entry" so they are treated as code splitting entry points.
 import App from './App';
 import Person from './Person';
 import People from './People';
@@ -67,7 +63,6 @@ app.post('*', async (req, res) => {
 });
 
 async function render(req: ExpressRequest, res: ExpressResponse, component: ReactElement, actionResult?: any) {
-  // Render RSC payload.
   let root: any = component;
   if (actionResult) {
     root = {result: actionResult, root};
@@ -76,7 +71,6 @@ async function render(req: ExpressRequest, res: ExpressResponse, component: Reac
   if (req.accepts('text/html')) {
     res.setHeader('Content-Type', 'text/html');
 
-    // Use client react to render the RSC payload to HTML.
     let [s1, s2] = stream.tee();
     let data = createFromReadableStream<ReactElement>(s1);
     function Content() {
@@ -95,7 +89,6 @@ async function render(req: ExpressRequest, res: ExpressResponse, component: Reac
 let server = app.listen(3001);
 console.log('Server listening on port 3001');
 
-// Restart the server when it changes.
 if (module.hot) {
   module.hot.dispose(() => {
     server.close();
