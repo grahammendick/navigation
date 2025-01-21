23355bdc469d7c1f270b2b6bb3e71e50c94d06c1
Graham Mendick <graham.mendick@gmail.com>
2025-01-21 20:09:05 +0000

    Tweaked format

--- NavigationReact/sample/rsc/Friends.tsx
+++ NavigationReact/sample/rsc/Friends.tsx
@@ -6,10 +6,10 @@ import { getFriends } from './data';
 import Gender from './Gender';
 
 const Friends = async () => {
-  const { data: { show } } = useNavigationEvent();
+  const {data: {show}} = useNavigationEvent();
   return (
     <>
-      <RefreshLink navigationData={{ show: !show }} includeCurrentData>{`${!show ? 'Show' : 'Hide'} Friends`}</RefreshLink>
+      <RefreshLink navigationData={{show: !show}} includeCurrentData>{`${!show ? 'Show' : 'Hide'} Friends`}</RefreshLink>
       {show && (
         <Suspense fallback={<h2>Loading...</h2>}>
           <List />
@@ -20,14 +20,14 @@ const Friends = async () => {
 }
 
 const List = async () => {
-  const { data: { id, gender } } = useNavigationEvent();
+  const {data: {id, gender}} = useNavigationEvent();
   const friends = await getFriends(id, gender);
   return (
     <>
       <Gender />
       <ul>
-        {friends.map(({ id, name }) => (
-          <li key={id}><RefreshLink navigationData={{ id }} includeCurrentData>{name}</RefreshLink></li>
+        {friends.map(({id, name}) => (
+          <li key={id}><RefreshLink navigationData={{id}} includeCurrentData>{name}</RefreshLink></li>
         ))}
       </ul>
     </>
