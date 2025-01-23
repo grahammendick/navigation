ffa8189c00d598d46662b823dea15f4629404483
Rickard Natt och Dag <rickard.natt-och-dag@seb.se>
2025-01-23 15:09:13 +0100

    Added support for React Native 0.77

--- NavigationReactNative/src/BackButton.tsx
+++ NavigationReactNative/src/BackButton.tsx
@@ -1,30 +1,16 @@
-import React from 'react';
-import { BackHandler } from 'react-native';
+import { useContext, useEffect } from 'react';
 import BackHandlerContext from './BackHandlerContext';
 
-class BackButton extends React.Component<{backHandler: BackHandler, onPress: () => boolean}> {
-    constructor(props) {
-        super(props);
-        this.handleBack = this.handleBack.bind(this);
-    }
-    componentDidMount() {
-        this.props.backHandler.addEventListener('hardwareBackPress', this.handleBack);
-    }
-    componentWillUnmount() {
-        this.props.backHandler.removeEventListener('hardwareBackPress', this.handleBack);
-    }
-    handleBack() {
-        return this.props.onPress();
-    }
-    render() {
-        return null;
-    }
+const BackButton = ({onPress}: { onPress: () => boolean }) => {
+  const backHandler = useContext(BackHandlerContext);
+
+  useEffect(() => {
+    const subscription = backHandler.addEventListener('hardwareBackPress', onPress);
+
+    return () => subscription.remove();
+  }, [onPress])
+
+  return null;
 }
 
-export default props => (
-    <BackHandlerContext.Consumer>
-        {backHandler => (
-            <BackButton {...props} backHandler={backHandler} />
-        )}
-    </BackHandlerContext.Consumer>
-);
+export default BackButton;
