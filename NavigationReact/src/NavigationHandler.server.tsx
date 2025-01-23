1d6566ca063238d40bad8e4e6ec2195adf51d697
Graham Mendick <graham.mendick@gmail.com>
2025-01-23 07:47:53 +0000

    Corrected typo
    See https://github.com/parcel-bundler/rsc-examples/pull/2#issuecomment-2608823422
    Not sure why it worked on my machine - maybe because navigator global was in typescript lib?!

--- NavigationReact/src/NavigationHandler.server.tsx
+++ NavigationReact/src/NavigationHandler.server.tsx
@@ -5,7 +5,7 @@ export const getCache = cache(() => ({navigationEvent: null}));
 
 const NavigationHandler = ({ stateNavigator, children }: { stateNavigator: StateNavigator, children: any }) => {
     const { oldState, state, data, asyncData } = stateNavigator.stateContext;
-    getCache().navigationEvent = { oldState, state, data, asyncData, stateNavigator: navigator };
+    getCache().navigationEvent = { oldState, state, data, asyncData, stateNavigator };
     return children;
 };
 export default NavigationHandler;
