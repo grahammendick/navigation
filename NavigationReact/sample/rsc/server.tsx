f75516fd3ba43430520f25aa5f46bf7ee10fce0d
Graham Mendick <graham.mendick@gmail.com>
2025-01-27 17:53:40 +0000

    Handled navigation during ssr
    If the navigation happens sync then catch and turn into a 302 redirect. If it happens async, because suspended, then this is already handled by the error boundary

--- NavigationReact/sample/rsc/server.tsx
+++ NavigationReact/sample/rsc/server.tsx
@@ -21,11 +21,17 @@ app.get('/favicon.ico', function (req, res) {
 app.get('*', async (req, res) => {
   const navigator = new StateNavigator(stateNavigator);
   navigator.navigateLink(req.url);
-  await renderRequest(req, res, (
-    <NavigationHandler stateNavigator={navigator}>
-      <App url={req.url} />
-    </NavigationHandler>
-  ), {component: App});
+  try {
+    await renderRequest(req, res, (
+      <NavigationHandler stateNavigator={navigator}>
+        <App url={req.url} />
+      </NavigationHandler>
+    ), {component: App});
+  } catch(e) {
+    const [name, url] = (e as Error).message.split(';');
+    if (name === 'navigateLink') res.redirect(url);
+    else throw e;
+  }
 });
 
 app.post('*', async (req, res) => {
