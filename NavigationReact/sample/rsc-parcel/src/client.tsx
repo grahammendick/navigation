a2b4c7add40ae4c1b24ae83940b127acece0e40f
Graham Mendick <graham.mendick@gmail.com>
2025-03-07 19:13:38 +0000

    Removed redundant import

--- NavigationReact/sample/rsc-parcel/src/client.tsx
+++ NavigationReact/sample/rsc-parcel/src/client.tsx
@@ -1,5 +1,5 @@
 'use client-entry';
-import { useState, startTransition, useEffect } from 'react';
+import { useState, startTransition } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createFromReadableStream } from 'react-server-dom-parcel/client';
 import { rscStream } from 'rsc-html-stream/client';
