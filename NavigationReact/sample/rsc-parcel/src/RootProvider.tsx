0556411141e1c7cc21bf1d2f73a82b4478e4fda1
Graham Mendick <graham.mendick@gmail.com>
2025-03-07 19:13:27 +0000

    Moved hmr reload fetch into the RootProvider
    Created HmrContext so can set the shell root from the RootProvider. It works much better in RootProvider because can access the stateNavigator and context. Creating a new one was clumsy plus could get the wrong url because the current href won't necessarily match the state context (history action none, for example)

--- NavigationReact/sample/rsc-parcel/src/RootProvider.tsx
+++ NavigationReact/sample/rsc-parcel/src/RootProvider.tsx
@@ -1,16 +1,37 @@
 'use client'
-import { useMemo } from "react";
+import { useMemo, useContext, useEffect } from "react";
 import { fetchRSC } from '@parcel/rsc/client';
 import { StateNavigator, HTML5HistoryManager } from 'navigation';
 import { NavigationHandler } from "navigation-react";
 import stateNavigator from "./stateNavigator";
+import HmrContext from "./HmrContext";
 
 const RootProvider = ({url, children}: any) => {
+  const setRoot = useContext(HmrContext);
   const navigator = useMemo(() => {
-    const navigator = new StateNavigator(stateNavigator, new HTML5HistoryManager());
+    // remove app appPath!
+    const navigator = new StateNavigator(stateNavigator, new HTML5HistoryManager('app'));
     navigator.navigateLink(url);
     return navigator;
   }, []);
+  useEffect(() => {
+    const onHmrReload = (e: any) => {
+      e.preventDefault();
+      const {stateContext: {state, data, crumbs, nextCrumb: {crumblessUrl}}} = navigator;
+      const root = fetchRSC(navigator.historyManager.getHref(crumblessUrl), {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: {
+              crumbs: crumbs.map(({state, data}) => ({state: state.key, data})),
+              state: state.key,
+              data
+          }
+      });
+      setRoot(root);
+    }
+    window.addEventListener('parcelhmrreload', onHmrReload);
+    return () => window.removeEventListener('parcelhmrreload', onHmrReload);
+  });
   return (
     <NavigationHandler stateNavigator={navigator} fetchRSC={fetchRSC}>
       {children}
--- NavigationReact/sample/rsc-parcel/src/client.tsx
+++ NavigationReact/sample/rsc-parcel/src/client.tsx
@@ -2,41 +2,12 @@
 import { useState, startTransition, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createFromReadableStream } from 'react-server-dom-parcel/client';
-import { fetchRSC } from '@parcel/rsc/client';
 import { rscStream } from 'rsc-html-stream/client';
-import { StateNavigator, HTML5HistoryManager } from 'navigation';
-import stateNavigator from "./stateNavigator";
+import HmrContext from './HmrContext';
 
 function Shell() {
-    let [root, setRoot] = useState(() => (
-        createFromReadableStream(rscStream)
-    ));
-    useEffect(() => {
-        const onHmrReload = (e: any) => {
-            e.preventDefault();
-            // remove app appPath!
-            const navigator = new StateNavigator(stateNavigator, new HTML5HistoryManager('app'));
-            navigator.historyManager.stop();
-            navigator.start();
-            const {stateContext: {state, data, crumbs, nextCrumb: {crumblessUrl}}} = navigator;
-            const root = fetchRSC(navigator.historyManager.getHref(crumblessUrl), {
-                method: 'put',
-                headers: {
-                    Accept: 'text/x-component',
-                    'Content-Type': 'application/json'
-                },
-                body: {
-                    crumbs: crumbs.map(({state, data}) => ({state: state.key, data})),
-                    state: state.key,
-                    data
-                }
-            });
-            setRoot(root);
-        }
-        window.addEventListener('parcelhmrreload', onHmrReload);
-        return () => window.removeEventListener('parcelhmrreload', onHmrReload);
-    })
-    return root;
+    let [root, setRoot] = useState(() => createFromReadableStream(rscStream));
+    return <HmrContext.Provider value={setRoot}>{root}</HmrContext.Provider>;
 }
 
 startTransition(() => {
