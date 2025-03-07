f2b0b1239499537299d195ef8248709b42dc1e0f
Graham Mendick <graham.mendick@gmail.com>
2025-03-07 18:33:52 +0000

    Moved hmr reload into Shell component
    Got the idea from the webpack rsc react example where it hydrates into a Shell. Plan is to move the hmr reload into the RootProvider so can read the stateContext (and don't have to create a new StateNavigator). It's better because the href might not match the state context, if history action none, for example.
    Went back to [early version of the parcel client](https://github.com/parcel-bundler/rsc-examples/blob/1b916d1fd544d95dce8d9272ec71c8f1d18b3dc8/examples/server/src/client.tsx) to check how it was done - before [parcel hid this away](https://github.com/parcel-bundler/parcel/blob/1ed569ca9d93901345897df0a27b2ef347983b97/packages/utils/rsc/src/client.tsx#L58)

--- NavigationReact/sample/rsc-parcel/src/client.tsx
+++ NavigationReact/sample/rsc-parcel/src/client.tsx
@@ -1,26 +1,47 @@
 'use client-entry';
-import { hydrate, fetchRSC } from '@parcel/rsc/client';
+import { useState, startTransition, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { createFromReadableStream } from 'react-server-dom-parcel/client';
+import { fetchRSC } from '@parcel/rsc/client';
+import { rscStream } from 'rsc-html-stream/client';
 import { StateNavigator, HTML5HistoryManager } from 'navigation';
 import stateNavigator from "./stateNavigator";
 
-const updateRoot = hydrate({
-    onHmrReload() {
-        const navigator = new StateNavigator(stateNavigator, new HTML5HistoryManager());
-        navigator.historyManager.stop();
-        navigator.start();
-        const {stateContext: {state, data, crumbs, nextCrumb: {crumblessUrl}}} = navigator;
-        const root = fetchRSC(navigator.historyManager.getHref(crumblessUrl), {
-            method: 'put',
-            headers: {
-                Accept: 'text/x-component',
-                'Content-Type': 'application/json'
-            },
-            body: {
-                crumbs: crumbs.map(({state, data}) => ({state: state.key, data})),
-                state: state.key,
-                data
-            }
-        });
-        updateRoot(root as any);
-    }
+function Shell() {
+    let [root, setRoot] = useState(() => (
+        createFromReadableStream(rscStream)
+    ));
+    useEffect(() => {
+        const onHmrReload = (e: any) => {
+            e.preventDefault();
+            const navigator = new StateNavigator(stateNavigator, new HTML5HistoryManager('app'));
+            navigator.historyManager.stop();
+            navigator.start();
+            const {stateContext: {state, data, crumbs, nextCrumb: {crumblessUrl}}} = navigator;
+            const root = fetchRSC(navigator.historyManager.getHref(crumblessUrl), {
+                method: 'put',
+                headers: {
+                    Accept: 'text/x-component',
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    crumbs: crumbs.map(({state, data}) => ({state: state.key, data})),
+                    state: state.key,
+                    data
+                }
+            });
+            setRoot(root);
+        }
+        window.addEventListener('parcelhmrreload', onHmrReload);
+        return () => window.removeEventListener('parcelhmrreload', onHmrReload);
+    })
+    return root;
+}
+
+startTransition(() => {
+    ReactDOM.hydrateRoot(
+        document,
+        <Shell />,
+        {}
+    );
 });
