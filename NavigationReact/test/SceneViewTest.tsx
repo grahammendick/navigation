9547586deccf13cca3af595589699bcbdcebae43
Graham Mendick <graham.mendick@gmail.com>
2025-03-22 10:08:56 +0000

    Tested error fallback prop on scene view

--- NavigationReact/test/SceneViewTest.tsx
+++ NavigationReact/test/SceneViewTest.tsx
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import * as mocha from 'mocha';
 import { StateNavigator } from 'navigation';
-import { SceneView, NavigationHandler } from 'navigation-react';
+import { SceneView, NavigationHandler, useNavigationEvent } from 'navigation-react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
@@ -335,6 +335,65 @@ describe('SceneViewTest', function () {
                 );
             });
             assert.equal(container.innerHTML, '');
+        })
+    });
+
+    describe('Scene View Error', function () {
+        it('should render fallback', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 's' }
+            ]);
+            stateNavigator.navigate('s');
+            var container = document.createElement('div');
+            var root = createRoot(container)
+            const Scene = () => {
+                throw Error();
+            }
+            const err = console.error;
+            console.error = () => {};
+            act(() => {
+                root.render(
+                    <NavigationHandler stateNavigator={stateNavigator}>
+                        <SceneView active="s" errorFallback={<h1>error</h1>}>
+                            <Scene />
+                        </SceneView>
+                    </NavigationHandler>
+                );
+            });
+            console.error = err;
+            assert.equal(container.innerHTML, '<h1>error</h1>');
+        })
+    });
+
+    describe('Scene View Error Off', function () {
+        it('should hide fallback', function(){
+            var stateNavigator = new StateNavigator([
+                { key: 's' }
+            ]);
+            stateNavigator.navigate('s', {x: 'a'});
+            var container = document.createElement('div');
+            var root = createRoot(container)
+            const Scene = () => {
+                const {data: {x}} = useNavigationEvent();
+                if (x === 'a') throw Error();
+                return <div>scene</div>;
+            }
+            const err = console.error;
+            console.error = () => {};
+            act(() => {
+                root.render(
+                    <NavigationHandler stateNavigator={stateNavigator}>
+                        <SceneView active="s" errorFallback={<h1>error</h1>}>
+                            <Scene />
+                        </SceneView>
+                    </NavigationHandler>
+                );
+            });
+            console.error = err;
+            act(() => {
+                stateNavigator.navigate('s', {x: 'b'})
+            });
+            assert.equal(container.innerHTML, '<div>scene</div>');
         })
     });
 });
