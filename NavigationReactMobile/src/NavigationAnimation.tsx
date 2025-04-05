f3752a7c473ea93cca681eba579c03611190f357
Graham Mendick <graham.mendick@gmail.com>
2025-04-05 20:19:04 +0100

    Fixed double click back to search from details
    The mutation observer ran as soon as the dom committed the new count attribute and this was with the stale scenes state. Need the mutation observer to run after the mutation observer is added with the latest scenes. So did the actual move in a subsequent useEffect - the mutation observer just indicates the move needs to happen

--- NavigationReactMobile/src/NavigationAnimation.tsx
+++ NavigationReactMobile/src/NavigationAnimation.tsx
@@ -2,105 +2,110 @@ import React, {useRef, useState, useEffect} from 'react';
 
 const NavigationAnimation  = ({children, data: nextScenes, history, onRest, oldState, duration: defaultDuration, pause}) => {
     const [scenes, setScenes] = useState({prev: null, all: [], count: 0});
+    const [move, setMove] = useState(null);
     const container = useRef(null);
     useEffect(() => {
-        let cancel = false;
-        const moveScenes = () => {
-            scenes.all.forEach(({key, url, pushEnter, popExit, pushExit, popEnter, unmountStyle, crumbStyle, unmounted}, i) => {
-                const scene = container.current.children[i];
-                const prevNavState = scene.navState || scene.prevNavState;
-                if (!scene.animate) {
-                    if (popExit && !unmounted) {
+        if (scenes.all.length !== container.current.children.length) return;
+        scenes.all.forEach(({key, url, pushEnter, popExit, pushExit, popEnter, unmountStyle, crumbStyle, unmounted}, i) => {
+            const scene = container.current.children[i];
+            const prevNavState = scene.navState || scene.prevNavState;
+            if (!scene.animate) {
+                if (popExit && !unmounted) {
+                    setScenes(({all, ...rest}) => (
+                        {all: all.map((s, index) => index !== i ? s : {...s, unmounted: true}), ...rest}
+                    ));
+                }
+                if ((pushEnter && prevNavState !== 'pushEnter') || (popEnter && prevNavState !== 'popEnter')) onRest({key, url});
+                scene.prevNavState = pushEnter ? 'pushEnter' : popExit ? 'popExit' : pushExit ? 'pushExit' : 'popEnter';
+                return;
+            };
+            if (scene.pushPlayState) scene.pushEnter.play();
+            if (scene.popPlayState) scene.popEnter.play();
+            const afterPushEnter = scene.pushEnter?.finished || {then: (f) => f()};
+            const afterPopEnter = scene.popEnter?.finished || {then: (f) => f()};
+            afterPopEnter.then(() => {
+                if (!unmountStyle) return;
+                if (!scene.pushEnter) {
+                    const {duration = defaultDuration, keyframes = unmountStyle} = unmountStyle;
+                    scene.pushEnter = scene.animate(keyframes, {duration, fill: 'both'});
+                    scene.pushEnter.persist();
+                }
+                if (pushEnter && prevNavState !== 'pushEnter') {
+                    scene.navState = 'pushEnter';
+                    if (prevNavState === 'popExit') scene.pushEnter.reverse();
+                    else if (prevNavState) scene.pushEnter.finish();
+                    else scene.pushEnter.play();
+                    if (!oldState) scene.pushEnter.finish();
+                }
+                if (popExit && prevNavState !== 'popExit') {
+                    if (prevNavState === 'pushExit') {
+                        scene.prevNavState = 'popEnter';
+                        scene.popEnter.cancel();
+                        scene.popEnter = null;
+                    }
+                    scene.navState = 'popExit';
+                    scene.pushEnter.reverse();
+                }
+                scene.pushEnter?.finished.then(() => {
+                    if (!scene.navState) return;
+                    if (popExit) {
                         setScenes(({all, ...rest}) => (
                             {all: all.map((s, index) => index !== i ? s : {...s, unmounted: true}), ...rest}
                         ));
                     }
-                    if ((pushEnter && prevNavState !== 'pushEnter') || (popEnter && prevNavState !== 'popEnter')) onRest({key, url});
-                    scene.prevNavState = pushEnter ? 'pushEnter' : popExit ? 'popExit' : pushExit ? 'pushExit' : 'popEnter';
-                    return;
-                };
-                if (scene.pushPlayState) scene.pushEnter.play();
-                if (scene.popPlayState) scene.popEnter.play();
-                const afterPushEnter = scene.pushEnter?.finished || {then: (f) => f()};
-                const afterPopEnter = scene.popEnter?.finished || {then: (f) => f()};
-                afterPopEnter.then(() => {
-                    if (cancel || !unmountStyle) return;
-                    if (!scene.pushEnter) {
-                        const {duration = defaultDuration, keyframes = unmountStyle} = unmountStyle;
-                        scene.pushEnter = scene.animate(keyframes, {duration, fill: 'both'});
-                        scene.pushEnter.persist();
-                    }
-                    if (pushEnter && prevNavState !== 'pushEnter') {
-                        scene.navState = 'pushEnter';
-                        if (prevNavState === 'popExit') scene.pushEnter.reverse();
-                        else if (prevNavState) scene.pushEnter.finish();
-                        else scene.pushEnter.play();
-                        if (!oldState) scene.pushEnter.finish();
-                    }
-                    if (popExit && prevNavState !== 'popExit') {
-                        if (prevNavState === 'pushExit') {
-                            scene.prevNavState = 'popEnter';
-                            scene.popEnter.cancel();
-                            scene.popEnter = null;
-                        }
-                        scene.navState = 'popExit';
-                        scene.pushEnter.reverse();
-                    }
-                    scene.pushEnter?.finished.then(() => {
-                        if (cancel || !scene.navState) return;
-                        if (popExit) {
-                            setScenes(({all, ...rest}) => (
-                                {all: all.map((s, index) => index !== i ? s : {...s, unmounted: true}), ...rest}
-                            ));
-                        }
-                        if (pushEnter || popExit) {
-                            onRest({key, url});
-                            scene.prevNavState = scene.navState;
-                            scene.navState = undefined;
-                        }
-                    });
-                    scene.pushPlayState = undefined;
-                    if (pause && scene.pushEnter?.playState === 'running') {
-                        scene.pushPlayState = 'running';
-                        scene.pushEnter.pause();
+                    if (pushEnter || popExit) {
+                        onRest({key, url});
+                        scene.prevNavState = scene.navState;
+                        scene.navState = undefined;
                     }
                 });
-                afterPushEnter.then(() => {
-                    if (cancel || !crumbStyle) return;
-                    if (!scene.popEnter && (pushExit || popEnter)) {       
-                        const {duration = defaultDuration, keyframes = crumbStyle} = crumbStyle;
-                        scene.popEnter = scene.animate(keyframes, {duration, fill: 'backwards'});
-                        scene.popEnter.persist();
-                    }
-                    if (popEnter && prevNavState !== 'popEnter') {
-                        scene.navState = 'popEnter';
-                        if (prevNavState === 'pushExit') scene.popEnter.reverse();
-                        else if (prevNavState) scene.popEnter.finish();
-                        else scene.popEnter.play();
-                    }
-                    if (pushExit && prevNavState !== 'pushExit') {
-                        scene.navState = 'pushExit';
-                        scene.popEnter.reverse();
-                        if (!oldState) scene.popEnter.finish();
-                    }
-                    scene.popEnter?.finished.then(() => {
-                        if (cancel || !scene.navState) return;
-                        if (pushExit || popEnter) {
-                            onRest({key, url});
-                            scene.prevNavState = scene.navState;
-                            scene.navState = undefined;
-                        }
-                    });
-                    scene.popPlayState = undefined;
-                    if (pause && scene.popEnter?.playState === 'running') {
-                        scene.popPlayState = 'running';
-                        scene.popEnter.pause();
-                    }
-                });
+                scene.pushPlayState = undefined;
+                if (pause && scene.pushEnter?.playState === 'running') {
+                    scene.pushPlayState = 'running';
+                    scene.pushEnter.pause();
+                }
             });
+            afterPushEnter.then(() => {
+                if (!crumbStyle) return;
+                if (!scene.popEnter && (pushExit || popEnter)) {       
+                    const {duration = defaultDuration, keyframes = crumbStyle} = crumbStyle;
+                    scene.popEnter = scene.animate(keyframes, {duration, fill: 'backwards'});
+                    scene.popEnter.persist();
+                }
+                if (popEnter && prevNavState !== 'popEnter') {
+                    scene.navState = 'popEnter';
+                    if (prevNavState === 'pushExit') scene.popEnter.reverse();
+                    else if (prevNavState) scene.popEnter.finish();
+                    else scene.popEnter.play();
+                }
+                if (pushExit && prevNavState !== 'pushExit') {
+                    scene.navState = 'pushExit';
+                    scene.popEnter.reverse();
+                    if (!oldState) scene.popEnter.finish();
+                }
+                scene.popEnter?.finished.then(() => {
+                    if (!scene.navState) return;
+                    if (pushExit || popEnter) {
+                        onRest({key, url});
+                        scene.prevNavState = scene.navState;
+                        scene.navState = undefined;
+                    }
+                });
+                scene.popPlayState = undefined;
+                if (pause && scene.popEnter?.playState === 'running') {
+                    scene.popPlayState = 'running';
+                    scene.popEnter.pause();
+                }
+            });
+        });
+    }, [move, scenes])
+    useEffect(() => {
+        let cancel = false;
+        const moveScenes = () => {
+            if (!cancel) setMove({});
         };
         const scenesObserver = new MutationObserver(moveScenes);
-        scenesObserver.observe(container.current, {attributes: true, childList: true})
+        scenesObserver.observe(container.current, {attributes: true, childList: true});
         return () => {
             cancel = true;
             scenesObserver.disconnect();
