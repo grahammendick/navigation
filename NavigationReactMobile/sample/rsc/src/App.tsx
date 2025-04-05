e7ad035ff58126fc08673f180cf04b5503d67827
Graham Mendick <graham.mendick@gmail.com>
2025-04-05 18:34:15 +0100

    Added transition so can see what's happening

--- NavigationReactMobile/sample/rsc/src/App.tsx
+++ NavigationReactMobile/sample/rsc/src/App.tsx
@@ -13,7 +13,18 @@ const App = async ({url}: any) => {
       </head>
       <body>
         <RootProvider url={url}>
-          <NavigationStack>
+          <NavigationStack
+            unmountStyle={[{transform: 'translateX(100%)'}, {transform: 'translateX(0)'}]}
+            crumbStyle={[{transform: 'translateX(5%) scale(0.9)', opacity: 0},{transform: 'translateX(0) scale(1)', opacity: 1}]}
+            style={{
+              position: 'fixed',
+              left: '0',
+              right: '0',
+              top: '0',
+              bottom: '0',
+              overflow: 'auto',
+              backgroundColor: '#fff',      
+            }}>
             <Scene stateKey="people"><People /></Scene>
             <Scene stateKey="person"><Person /></Scene>
           </NavigationStack>
