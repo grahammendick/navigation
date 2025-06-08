bc6f10a06b74d101260195e14bf4d81e98865ecb
Graham Mendick <graham.mendick@gmail.com>
2025-06-08 15:50:55 +0100

    Exported the scene view context (client only)

--- NavigationReact/src/NavigationReact.ts
+++ NavigationReact/src/NavigationReact.ts
@@ -1,5 +1,6 @@
 ﻿import NavigationContext from './NavigationContext.js';
 import BundlerContext from './BundlerContext.js';
+import SceneViewContext from './SceneViewContext.js';
 import NavigationHandler from './NavigationHandler.js';
 import NavigationBackLink from './NavigationBackLink.js';
 import NavigationLink from './NavigationLink.js';
@@ -8,4 +9,4 @@ import FluentLink from './FluentLink.js';
 import SceneView from './SceneView.js';
 import useNavigationEvent from './useNavigationEvent.js';
 
-export { NavigationContext, BundlerContext, NavigationHandler, NavigationBackLink, NavigationLink, RefreshLink, FluentLink, SceneView, useNavigationEvent };
+export { NavigationContext, BundlerContext, SceneViewContext, NavigationHandler, NavigationBackLink, NavigationLink, RefreshLink, FluentLink, SceneView, useNavigationEvent };
