65969e1effcd22b398d6e53b9fbabeb9f76d8231
Graham Mendick <graham.mendick@gmail.com>
2025-06-08 15:55:49 +0100

    Changed to match new name

--- NavigationReact/sample/rsc-parcel/src/App.tsx
+++ NavigationReact/sample/rsc-parcel/src/App.tsx
@@ -18,7 +18,7 @@ const App = async ({url}: any) => {
             <SceneView active="people">
               <People />
             </SceneView>
-            <SceneView active="person" dataKeyDeps={['id']}>
+            <SceneView active="person" refetch={['id']}>
               <Person />
             </SceneView>
           </HmrProvider>
