3c2508f9ea5f248ebaa243e573955ffdfa5fa911
Graham Mendick <graham.mendick@gmail.com>
2025-06-08 16:54:38 +0100

    Added suffix so works with webpack sample

--- NavigationReact/src/useRefetch.ts
+++ NavigationReact/src/useRefetch.ts
@@ -1,6 +1,6 @@
 import { StateContext } from 'navigation';
 import { useContext } from 'react';
-import RSCContext from './RSCContext';
+import RSCContext from './RSCContext.js';
 
 const useRefetch = (refetch?: string[] | ((stateContext: StateContext) => boolean) | null) => {
     const {setRefetch} = useContext(RSCContext);
