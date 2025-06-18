b1c33f976401d984b16c1a8c528af88884f3d05f
Graham Mendick <graham.mendick@gmail.com>
2025-06-18 20:58:10 +0100

    Supported refetching the whole scene
    Pass true to the refetch and it's the whole scene, otherwise it's just the scene view
    const refetch = useRefetch();
    refetch(true)
    Tried setState(prevContext => nextContext) but this infinitely looped. Don't think it's supported in startTransition

--- NavigationReact/src/useRefetch.ts
+++ NavigationReact/src/useRefetch.ts
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
 import { StateContext } from 'navigation';
 import RSCContext from './RSCContext.js';
+import SceneRSCContext from './SceneRSCContext.js';
 
 const useRefetch = (refetch?: string[] | ((stateContext: StateContext) => boolean) | null) => {
     const {setRefetch, refetcher} = useContext(RSCContext);
+    const sceneRefetcher = useContext(SceneRSCContext);
     setRefetch(refetch);
-    return refetcher;
+    return (scene = false) => !scene ? refetcher() : sceneRefetcher();
 }
 export default useRefetch;
