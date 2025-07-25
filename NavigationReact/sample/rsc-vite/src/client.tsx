c031c38395dbe1a13313651d8aadf8702a733a58
Graham Mendick <graham.mendick@gmail.com>
2025-07-25 18:21:27 +0100

    Tweaked for consistency with other nav rsc samples

--- NavigationReact/sample/rsc-vite/src/client.tsx
+++ NavigationReact/sample/rsc-vite/src/client.tsx
@@ -5,21 +5,19 @@ import { rscStream } from 'rsc-html-stream/client'
 import { createFromFetch } from "@vitejs/plugin-rsc/browser";
 import { BundlerContext } from 'navigation-react';
 
-async function hydrate() {
-    const initialPayload = await ReactClient.createFromReadableStream<{root: React.ReactNode}>(rscStream)
-    function Shell() {
-        const [root, setRoot] = useState(initialPayload.root);
-        const bundler = useMemo(() => ({setRoot, deserialize: fetchRSC}), []);
-        return (
-            <BundlerContext.Provider value={bundler}>
-                {root}
-            </BundlerContext.Provider>
-        );
-    }
-    ReactDOM.hydrateRoot(document, <Shell />);
-}
 async function fetchRSC(url: string, {body, ...options}: any) {
     const payload = await createFromFetch(fetch(url, {...options, body: JSON.stringify(body)})) as any;
     return payload.root;
 }
-hydrate();
+
+const initialPayload = await ReactClient.createFromReadableStream<{root: React.ReactNode}>(rscStream)
+function Shell() {
+    const [root, setRoot] = useState(initialPayload.root);
+    const bundler = useMemo(() => ({setRoot, deserialize: fetchRSC}), []);
+    return (
+        <BundlerContext.Provider value={bundler}>
+            {root}
+        </BundlerContext.Provider>
+    );
+}
+ReactDOM.hydrateRoot(document, <Shell />);
