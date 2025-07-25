e5268a9e23554b5d931d5b41f6fc908e232ff5b1
Graham Mendick <graham.mendick@gmail.com>
2025-07-25 18:18:29 +0100

    Tweaked for consistency with other nav rsc samples

--- NavigationReact/sample/rsc-vite/src/server.tsx
+++ NavigationReact/sample/rsc-vite/src/server.tsx
@@ -48,23 +48,11 @@ export default async function handler(request: Request): Promise<Response> {
   );
   const rscStream = ReactServer.renderToReadableStream({ root });
   if (request.method !== 'GET') {
-    return new Response(rscStream, {
-      headers: {
-        'content-type': 'text/x-component;charset=utf-8',
-        vary: 'accept',
-      },
-    })
+    return new Response(rscStream, {headers: {'Content-type': 'text/x-component'}});
   }
-  const ssrEntryModule = await import.meta.viteRsc.loadModule<
-    typeof import('./server.ssr.tsx')
-  >('ssr', 'index');
+  const ssrEntryModule = await import.meta.viteRsc.loadModule<typeof import('./server.ssr.tsx')>('ssr', 'index');
   const htmlStream = await ssrEntryModule.renderHTML(rscStream);
-  return new Response(htmlStream, {
-    headers: {
-      'Content-type': 'text/html',
-      vary: 'accept',
-    },
-  });
+  return new Response(htmlStream, {headers: {'Content-type': 'text/html'}});
 }
 
 if (import.meta.hot) {
