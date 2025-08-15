f69b312c03c2b8bfc01b0048de04a8222fd68e27
Graham Mendick <graham.mendick@gmail.com>
2025-08-15 12:37:20 +0100

    Supported unit testing with new SafeAreaProvider
    Followed the safe area testing docs for setup https://appandflow.github.io/react-native-safe-area-context/testing

--- NavigationReactNative/sample/fabric/jest.config.js
+++ NavigationReactNative/sample/fabric/jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   preset: 'react-native',
   setupFiles: [
     "./node_modules/navigation-react-native/setup-jest.js",
+    "./setup-jest.js",
   ],
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|navigation-react-native)/)"
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|navigation-react-native|react-native-safe-area-context)/)"
   ]
 };
