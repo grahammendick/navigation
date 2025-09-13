93cccbb53549ed08fc838bf11b6cf91c2d676177
Graham Mendick <graham.mendick@gmail.com>
2025-09-13 10:13:27 +0100

    Started converting NavigationHandler to function
    Don't like the NavigationHandlerInner and should be able to get rid of it when NavigationHandler is a function. Also should help with setting the history (resumeNavigation) when the transition/deferred has settled.
    Moving the AsyncStateNavigator into the function component should also help with this because it has access to all the stuff in closure.
    Lots of errors but want to commit because it's an ok start

--- NavigationReact/src/NavigationHandler.tsx
+++ NavigationReact/src/NavigationHandler.tsx
@@ -1,13 +1,66 @@
 'use client'
-import React, { Component, startTransition, useDeferredValue } from 'react';
-import { StateNavigator, State } from 'navigation';
-import AsyncStateNavigator from './AsyncStateNavigator.js';
+import React, { Component, startTransition, useDeferredValue, useEffect, useRef, useState } from 'react';
+import { StateNavigator, StateContext, State } from 'navigation';
 import NavigationContext from './NavigationContext.js';
 import RefetchContext from './RefetchContext.js';
 import NavigationDeferredContext from './NavigationDeferredContext.js';
 import useNavigationEvent from './useNavigationEvent.js';
-type NavigationHandlerState = { context: { ignoreCache?: boolean, oldState: State, state: State, data: any, asyncData: any, stateNavigator: AsyncStateNavigator } };
+type NavigationHandlerState = {ignoreCache?: boolean, oldState: State, state: State, data: any, asyncData: any, stateNavigator: StateNavigator};
 
+const NavigationHandler = ({stateNavigator, children}: {stateNavigator: StateNavigator, children: any}) => {
+    const [navigationEvent, setNavigationEvent] = useState<NavigationHandlerState>();
+    const createNavigationEvent = (stateContext: StateContext = stateNavigator.stateContext) => {
+        const AsyncStateNavigator = class AsyncStateNavigator extends StateNavigator {
+            constructor() {
+                super(stateNavigator, stateNavigator.historyManager);
+                this.stateContext = stateContext;
+                this.configure = stateNavigator.configure.bind(stateNavigator);
+                this.onBeforeNavigate = stateNavigator.onBeforeNavigate.bind(stateNavigator);
+                this.offBeforeNavigate = stateNavigator.offBeforeNavigate.bind(stateNavigator);
+                this.onNavigate = stateNavigator.onNavigate.bind(stateNavigator);
+                this.offNavigate = stateNavigator.offNavigate.bind(stateNavigator);
+            }
+        
+            navigateLink(url: string, historyAction: 'add' | 'replace' | 'none' = 'add', history = false,
+                suspendNavigation?: (stateContext: StateContext, resumeNavigation: () => void) => void,
+                currentContext = this.stateContext) {
+                if (!suspendNavigation)
+                    suspendNavigation = (_stateContext, resumeNavigation) => resumeNavigation();
+                stateNavigator.navigateLink(url, historyAction, history, (stateContext, resumeNavigation) => {
+                    suspendNavigation(stateContext, () => {
+                        const {oldState, state, history, crumbs} = stateContext;
+                        const refresh = oldState === state && crumbs.length === this.stateContext.crumbs.length;
+                        const startTransition = (!history && !refresh && React.startTransition) || ((transition) => transition());
+                        startTransition(() => {
+                            setNavigationEvent(createNavigationEvent(stateContext));
+                        });
+                    })
+                }, currentContext);
+            }
+        }
+        const asyncNavigator = new AsyncStateNavigator()
+        const {oldState, state, data, asyncData} = asyncNavigator.stateContext;
+        return {oldState, state, data, asyncData, stateNavigator: asyncNavigator};
+    }
+    if (!navigationEvent) setNavigationEvent(createNavigationEvent());
+    useEffect(() => {
+        const onNavigate = () => {
+            if (navigationEvent.stateNavigator.stateContext !== stateNavigator.stateContext)
+                setNavigationEvent(createNavigationEvent());
+        };
+        stateNavigator.onNavigate(onNavigate);
+        return () => stateNavigator.offNavigate(onNavigate);
+    }, [stateNavigator, navigationEvent]);
+    return (
+        <NavigationContext.Provider value={navigationEvent}>
+            <RefetchContext.Provider value={this.refetchControl}>
+                <NavigationHandlerInner>
+                    {children}
+                </NavigationHandlerInner>
+            </RefetchContext.Provider>
+        </NavigationContext.Provider>
+    )
+}
 const NavigationHandlerInner = ({ children }: any) => {
     const navigationEvent = useNavigationEvent();
     const navigationDeferredEvent = useDeferredValue(navigationEvent);
