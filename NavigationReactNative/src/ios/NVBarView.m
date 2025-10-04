f2428f30f2180a8253e8396d26bc3a9cbba15aae
Graham Mendick <graham.mendick@gmail.com>
2025-10-04 01:24:17 +0100

    Supported barbutton as shared element on old arch
    Copied over changes from new arch

--- NavigationReactNative/src/ios/NVBarView.m
+++ NavigationReactNative/src/ios/NVBarView.m
@@ -16,27 +16,35 @@
 - (void)insertReactSubview:(UIView *)subview atIndex:(NSInteger)atIndex
 {
     [super insertReactSubview:subview atIndex:atIndex];
-    [self.buttons insertObject:((NVBarButtonView *) subview).button atIndex:atIndex];
+    [self.buttons insertObject:(NVBarButtonView *) subview atIndex:atIndex];
     [self setBarButtons:self.buttons];
+    [((UIViewController<NVSharedElementController> *) self.reactViewController).sharedElements addObject:(NVBarButtonView *) subview];
 }
 
 - (void)removeReactSubview:(UIView *)subview
 {
     [super removeReactSubview:subview];
-    [self.buttons removeObject:((NVBarButtonView *) subview).button];
+    [self.buttons removeObject:(NVBarButtonView *) subview];
     [self setBarButtons:self.buttons];
+    [((UIViewController<NVSharedElementController> *) self.reactViewController).sharedElements removeObject:(NVBarButtonView *) subview];
 }
 
 - (void)didMoveToWindow
 {
     [super didMoveToWindow];
     [self setBarButtons:self.buttons];
+    for (NVBarButtonView *button in self.buttons) {
+        [((UIViewController<NVSharedElementController> *) self.reactViewController).sharedElements addObject:button];
+    }
 }
 
 - (void)willMoveToSuperview:(nullable UIView *)newSuperview
 {
     [super willMoveToSuperview:newSuperview];
     if (!newSuperview) {
+        for (NVBarButtonView *button in self.buttons) {
+            [((UIViewController<NVSharedElementController> *) self.reactViewController).sharedElements removeObject:button];
+        }
         [self setBarButtons:nil];
     }
 }
--- NavigationReactNative/src/ios/NVRightBarView.m
+++ NavigationReactNative/src/ios/NVRightBarView.m
@@ -1,12 +1,17 @@
 #import "NVRightBarView.h"
 
 #import <React/UIView+React.h>
+#import "NVBarButtonView.h"
 
 @implementation NVRightBarView
 
 -(void)setBarButtons:(NSMutableArray *)buttons
 {
-    [self.reactViewController.navigationItem setRightBarButtonItems:buttons];
+    NSMutableArray *barButtons = [NSMutableArray arrayWithCapacity:buttons.count];
+    for (NVBarButtonView *button in buttons) {
+        [barButtons addObject:button.button];
+    }
+    [self.reactViewController.navigationItem setRightBarButtonItems:barButtons];
 }
 
 @end
