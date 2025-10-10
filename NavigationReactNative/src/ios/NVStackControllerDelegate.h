e2e911fe85de2430367be949275baf9f31a33114
Graham Mendick <graham.mendick@gmail.com>
2025-10-10 15:14:15 +0100

    Added delegate stub obj-c
    The only way to opt into the default pop behavior is by not implementing animationControllerForOperation - as soon as implement it then never get the built-in pop gesture support. If don't get the built-in then can't get the ios26 full screen gesture properly - where it doesn't conflict with horizontal scroll view, for example. Tried returning NO/YES from various gesture recognizer stuff but can never get the non-conflict.
    Plan to move the delegate off the NavigationStackView into separate classes. Then can add the right delegate depending on whether want custom animation or not. 99% of times there's no custom animation so add delegate without animationControllerForOperation and get default pop gestures

---
+++ NavigationReactNative/src/ios/NVStackControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  NVStackControllerDelegate.h
+//  NavigationReactNative
+//
+//  Created by Graham Mendick on 10/10/2025.
+//  Copyright © 2025 Graham Mendick. All rights reserved.
+//
+
+#ifndef NVStackControllerDelegate_h
+#define NVStackControllerDelegate_h
+
+
+#endif /* NVStackControllerDelegate_h */
---
+++ NavigationReactNative/src/ios/NVStackControllerDelegate.m
@@ -0,0 +1,9 @@
+//
+//  NVStackControllerDelegate.m
+//  NavigationReactNative
+//
+//  Created by Graham Mendick on 10/10/2025.
+//  Copyright © 2025 Graham Mendick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
