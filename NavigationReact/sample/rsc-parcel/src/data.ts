cb7f3abb006bba3260f4b1a23029ff0f7972d006
Graham Mendick <graham.mendick@gmail.com>
2025-11-28 15:02:49 +0000

    Reverted testing changes

--- NavigationReact/sample/rsc-parcel/src/data.ts
+++ NavigationReact/sample/rsc-parcel/src/data.ts
@@ -27,7 +27,7 @@ const searchPeople = async (name: string, pageNumber: number, pageSize: number, 
                 people: filteredPeople.slice(start, start + pageSize),
                 count: filteredPeople.length
             });
-        }, 2000);
+        }, 10);
     })
 };
 
