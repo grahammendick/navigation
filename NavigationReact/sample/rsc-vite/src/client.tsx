635d7c0851a5d0182c6ff64a4451f387e7eb5c6d
Graham Mendick <graham.mendick@gmail.com>
2025-11-30 12:27:16 +0000

    Changed for consistency with parcel sample
    Want them to look as similar as possible - plus, going to be removing setRoot when copying over the rsc navigate parcel changes (hmr is done with refetch not setRoot now)

--- NavigationReact/sample/rsc-vite/src/client.tsx
+++ NavigationReact/sample/rsc-vite/src/client.tsx
@@ -1,6 +1,6 @@
-import * as ReactClient from '@vitejs/plugin-rsc/browser'
-import { useState, useMemo } from "react";
+import { useMemo } from "react";
 import ReactDOM from "react-dom/client";
+import { createFromReadableStream } from '@vitejs/plugin-rsc/browser'
 import { rscStream } from 'rsc-html-stream/client'
 import { createFromFetch } from "@vitejs/plugin-rsc/browser";
 import { BundlerContext } from 'navigation-react';
@@ -9,10 +9,10 @@ async function fetchRSC(url: string, {body, ...options}: any) {
     return createFromFetch(fetch(url, {...options, body: JSON.stringify(body)}));
 }
 
-const initialPayload = ReactClient.createFromReadableStream<any>(rscStream)
+const initialPayload = createFromReadableStream<any>(rscStream)
 function Shell() {
-    const [root, setRoot] = useState(initialPayload);
-    const bundler = useMemo(() => ({setRoot, deserialize: fetchRSC}), []);
+    const root = useMemo(() => initialPayload, []);
+    const bundler = useMemo(() => ({setRoot: () => {}, deserialize: fetchRSC}), []);
     return (
         <BundlerContext.Provider value={bundler}>
             {root}
