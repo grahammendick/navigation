88e094e331f5ce47b398e965feca9ea6bb0cd26d
Graham Mendick <graham.mendick@gmail.com>
2026-04-05 14:55:55 +0100

    Removed redundant wrapper function

--- NavigationReact/sample/rsc-parcel/src/Name.tsx
+++ NavigationReact/sample/rsc-parcel/src/Name.tsx
@@ -2,19 +2,16 @@
 import { useActionScene } from 'navigation-react';
 import updateName from "./updateName";
 
-const Name = ({id, name}: {id: number, name: string}) => {
+const Name = ({ id, name }: { id: number, name: string }) => {
   const saveName = useActionScene(updateName);
-  const save = async (formData: FormData) => {
-    await saveName(formData);
-  }
-    return (
-        <form action={save}>
-          <input type="hidden" name="id" value={id} />
-          <input type="text" name="name" defaultValue={name} />
-          <button type="submit">Save</button>
-          <button type="reset">Reset</button>
-        </form>
-    )
+  return (
+    <form action={saveName}>
+      <input type="hidden" name="id" value={id} />
+      <input type="text" name="name" defaultValue={name} />
+      <button type="submit">Save</button>
+      <button type="reset">Reset</button>
+    </form>
+  )
 }
 
 export default Name;
