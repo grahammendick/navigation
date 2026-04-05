ca9a58c6723eeffbf4699db7251c2bba5cd1ff6d
Graham Mendick <graham.mendick@gmail.com>
2026-04-05 21:36:19 +0100

    Copied server action changes from parcel sample
    Also copied over the gender changes where moved the hide/show so text updates when toggled (should've done this at the time)

--- NavigationReactMobile/sample/rsc/src/client.tsx
+++ NavigationReactMobile/sample/rsc/src/client.tsx
@@ -1,7 +1,7 @@
 'use client-entry';
 import { useMemo, startTransition } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createFromReadableStream } from 'react-server-dom-parcel/client';
+import { createFromReadableStream, setServerCallback } from 'react-server-dom-parcel/client';
 import { rscStream } from 'rsc-html-stream/client';
 import { fetchRSC } from '@parcel/rsc/client';
 import { BundlerContext } from 'navigation-react';
@@ -27,6 +27,19 @@ function Shell() {
         </BundlerContext.Provider>
     );
 }
+
+setServerCallback(async (actionId: string, args: any[]) => {
+    const ind = args.findIndex(arg => typeof arg === 'function');
+    if (ind !== -1) {
+        const deserializeScene = args[ind];
+        return deserializeScene(actionId, [...args.slice(0, ind), ...args.slice(ind +1)]);
+    }
+    const res = await fetchRSC(window.location.href, {
+        method: 'post',
+        body: {actionId, args}
+    }) as any;
+    return res.data;
+});
 
 startTransition(() => {
     ReactDOM.hydrateRoot(document, <Shell />);
