eb9633968ea6143020e4f31fcbbe21eeee944afa
Graham Mendick <graham.mendick@gmail.com>
2026-04-06 10:21:34 +0100

    Added flex direction column to fix regression
    Think this got lost when switching to css animations - introduced a div data-scene wrapper and this doesn't get the default react-native-web styles. So defaults to flex direction row and the tabs on the twitter sample were on the right of screen instead of bottom

--- NavigationReactNativeWeb/src/NavigationStack.tsx
+++ NavigationReactNativeWeb/src/NavigationStack.tsx
@@ -39,6 +39,7 @@ const NavigationStack = ({unmountedStyle, mountedStyle, crumbedStyle, unmountSty
                 left: 0, right: 0, top: 0, bottom: 0,
                 overflow: 'hidden',
                 display: 'flex',
+                flexDirection: 'column',
                 ...style
             }}
             renderScene={renderScene}
