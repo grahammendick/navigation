d3831ef65fec4e469ec80007e2a906a5da023490
Graham Mendick <graham.mendick@gmail.com>
2026-04-12 18:53:07 +0100

    Reverted unnecessary change

--- NavigationReact/sample/rsc-parcel/src/client.tsx
+++ NavigationReact/sample/rsc-parcel/src/client.tsx
@@ -31,8 +31,8 @@ function Shell() {
 setServerCallback(async (actionId: string, args: any[]) => {
     const ind = args.findIndex(arg => typeof arg === 'function');
     if (ind !== -1) {
-        const callback = args[ind];
-        return callback(actionId, [...args.slice(0, ind), ...args.slice(ind +1)]);
+        const deserializeScene = args[ind];
+        return deserializeScene(actionId, [...args.slice(0, ind), ...args.slice(ind +1)]);
     }
     const res = await fetchRSC(window.location.href, {
         method: 'post',
