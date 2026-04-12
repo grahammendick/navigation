8e32e12eed37ed57d78ecf1065f1a5d609a8ed53
Graham Mendick <graham.mendick@gmail.com>
2026-04-12 18:58:43 +0100

    Reverted formatting tweaks for consistency
    Matches filter in other rsc samples

--- NavigationReact/sample/rsc-parcel/src/Filter.tsx
+++ NavigationReact/sample/rsc-parcel/src/Filter.tsx
@@ -3,23 +3,23 @@ import { startTransition, useOptimistic } from 'react';
 import { RefreshLink, useNavigationEvent } from 'navigation-react';
 
 const Filter = () => {
-  const {data, stateNavigator} = useNavigationEvent();
-  const {name} = data;
+  const { data, stateNavigator } = useNavigationEvent();
+  const { name } = data;
   const [optimisticName, setOptimisticName] = useOptimistic(name || '');
   return (
     <div>
       <div>
         <label htmlFor="name">Name</label>
-        <input id="name" value={optimisticName} onChange={({target: {value}}) => {
+        <input id="name" value={optimisticName} onChange={({ target: { value } }) => {
           startTransition(() => {
             setOptimisticName(value);
-            stateNavigator.refresh({...data, name: value, page: null});
+            stateNavigator.refresh({ ...data, name: value, page: null });
           });
         }} />
       </div>
       Page size
-      <RefreshLink navigationData={{size: 5, page: null}} includeCurrentData>5</RefreshLink>
-      <RefreshLink navigationData={{size: 10, page: null}} includeCurrentData>10</RefreshLink>
+      <RefreshLink navigationData={{ size: 5, page: null }} includeCurrentData>5</RefreshLink>
+      <RefreshLink navigationData={{ size: 10, page: null }} includeCurrentData>10</RefreshLink>
     </div>
   );
 }
