81debc220994ae70f43c067ff6c747ad85a3ce16
Graham Mendick <graham.mendick@gmail.com>
2026-05-10 22:29:55 +0100

    Reverted now tested the signal abort

--- NavigationReact/sample/rsc-parcel/src/server.tsx
+++ NavigationReact/sample/rsc-parcel/src/server.tsx
@@ -32,9 +32,6 @@ app.get('*', async (req, res) => {
 });
 
 app.post('*', async (req, res) => {
-  await new Promise(res => {
-    setTimeout(res, 2000);
-  })
   const sceneViews: any = {
     people: People,
     list: List,
