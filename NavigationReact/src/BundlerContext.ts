33dff40e89c9d6ac1667390ba0f46e90f40968bd
Graham Mendick <graham.mendick@gmail.com>
2026-05-12 17:28:19 +0100

    Tweaked format for consistency

--- NavigationReact/src/BundlerContext.ts
+++ NavigationReact/src/BundlerContext.ts
@@ -1,3 +1,3 @@
 import { createContext } from 'react';
 
-export default createContext<{createFromFetch(promiseForResponse: Promise<Response>, options?: object): Promise<any>, createTemporaryReferenceSet(): any, encodeReply: (v: any[], options?: any) => Promise<string | FormData>, onHmrReload: (hmrReload: () => void) => () => void}>({createFromFetch: null, createTemporaryReferenceSet: null, encodeReply: null, onHmrReload: null});
+export default createContext<{createFromFetch: (promiseForResponse: Promise<Response>, options?: object) => Promise<any>, createTemporaryReferenceSet: () => any, encodeReply: (v: any[], options?: any) => Promise<string | FormData>, onHmrReload: (hmrReload: () => void) => () => void}>({createFromFetch: null, createTemporaryReferenceSet: null, encodeReply: null, onHmrReload: null});
