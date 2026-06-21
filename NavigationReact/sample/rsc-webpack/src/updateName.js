ff68fb1af36cf1bd12752ce71f8eddb40de4588e
Graham Mendick <graham.mendick@gmail.com>
2026-06-21 14:13:05 +0100

    Removed webpack sample
    Too hard to keep up to date and not needed now have vite - need to have 2 rsc samples to make sure anything built works on both (don't want to accidentally build something that only works on parce, for example). Webpack sample was always too rough - it felt much more like an internal project for react than anything for the public to use.
    It started throwing hydration errors when update to latest react-server-dom-webpack - no need to investigate now have vite and parcel samples

--- NavigationReact/sample/rsc-webpack/src/updateName.js
+++
@@ -1,8 +0,0 @@
-'use server'
-import { getPerson } from './data.js';
-
-export default async function updateName(data, { refetch }) {
-	const person = await getPerson(+`${data.get('id')}`);
-	person.name = `${data.get('name')}`;
-	refetch();
-}
