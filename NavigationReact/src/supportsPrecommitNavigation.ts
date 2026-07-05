5fccaa25b76adad111d39d67c2ab198262ebfbb8
Graham Mendick <graham.mendick@gmail.com>
2026-07-05 09:50:32 +0100

    Ensured boolean returned

--- NavigationReact/src/supportsPrecommitNavigation.ts
+++ NavigationReact/src/supportsPrecommitNavigation.ts
@@ -1,2 +1,2 @@
-export default typeof window !== 'undefined' && window.NavigationPrecommitController
+export default typeof window !== 'undefined' && !!window.NavigationPrecommitController
     && !!(window.navigator as any).userAgentData?.brands?.some(({brand}) => brand === 'Microsoft Edge');
