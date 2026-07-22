80901a80ec777f4b3d78d7c4cb1190bbbcfc8dd8
Graham Mendick <graham.mendick@gmail.com>
2026-07-22 22:52:59 +0100

    Reverted commit  d640f70
    Reverting [commit as redundant](https://github.com/grahammendick/navigation/commit/d640f705d621d15eabe621dc3d2ab92ccb2f396a) after [passing latest navigation event instead of deferred if not rendered](https://github.com/grahammendick/navigation/commit/d755fdb22a0db2d630364bf974c44a197ee09c92). Passing the deferred navigation event to a scene view that hasn't rendered yet is wrong because it can cause it to fetch - it was never rendered so its data isn't in cache - the fix this reverts was a workaround to try and stop it fetching. But now the correct fix is in don't need it anymore

--- NavigationReact/src/SceneRSCView.tsx
+++ NavigationReact/src/SceneRSCView.tsx
@@ -49,7 +49,7 @@ const SceneView = ({active, name, refetch, pending, errorFallback, children}: Sc
     const fetching = fetchingFn(navigationEvent);
     const firstScene = !oldUrl && !ignoreCache;
     if (!cachedSceneViews[sceneViewKey] && !firstScene && !ancestorFetching && fetching) {
-        cachedSceneViews[sceneViewKey] = deserialize(navigationEvent, sceneViewKey);
+        cachedSceneViews[sceneViewKey] = deserialize(sceneViewKey);
     }
     const sceneView = (() => {
         if (!getShow(state?.key)) return null;
--- NavigationReact/src/useSceneView.ts
+++ NavigationReact/src/useSceneView.ts
@@ -1,13 +1,11 @@
 import { useCallback, useContext } from 'react';
 import RefetchContext from './RefetchContext.js';
-import useNavigationEvent from './useNavigationEvent.js';
 
 const useSceneView = (fn: (...args: any) => any) => {
     const {sceneViewKey, deserialize} = useContext(RefetchContext);
-    const navigationEvent = useNavigationEvent();
     if (typeof window === 'undefined') return fn;
     const deserializeScene = useCallback(fn.bind(null, async (actionId: string, args: any[]) => (
-        deserialize(navigationEvent, sceneViewKey, actionId, args)
+        deserialize(sceneViewKey, actionId, args)
     )), [fn, sceneViewKey, deserialize])
     return deserializeScene;
 }
