69aac5dc35ccbefff1f5eed0d284e37067147916
Graham Mendick <graham.mendick@gmail.com>
2026-07-25 20:45:21 +0100

    Reverted now tested navigating while suspended

--- NavigationReact/sample/rsc-instant/src/Person.tsx
+++ NavigationReact/sample/rsc-instant/src/Person.tsx
@@ -1,12 +1,11 @@
 'use client'
-import { NavigationBackLink, RefreshLink } from 'navigation-react';
+import { NavigationBackLink } from 'navigation-react';
 
 const Person = ({children}: any) => {
   return (
     <>
       <h1>Person</h1>
       <NavigationBackLink distance={1}>Person Search</NavigationBackLink>
-      <RefreshLink navigationData={{gender: 'male'}} includeCurrentData>Male</RefreshLink>
       {children}
     </>
   )
