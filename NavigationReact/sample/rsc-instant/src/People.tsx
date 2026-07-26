9463fe235ad69190a5020e84cd9ab442a3a76a8b
Graham Mendick <graham.mendick@gmail.com>
2026-07-26 21:05:49 +0100

    Moved filter because people client component
    For non-instant people is server component so filter has to be separate. But clearer to move filter inline into people component for instant because shows it can go in the client shell

--- NavigationReact/sample/rsc-instant/src/People.tsx
+++ NavigationReact/sample/rsc-instant/src/People.tsx
@@ -1,12 +1,22 @@
 'use client'
-import Filter from './Filter';
+import { RefreshLink, useNavigationEvent } from 'navigation-react';
 
 const People = ({children}: any) => {
+  const { data, stateNavigator } = useNavigationEvent();
+  const { name } = data;
   return (
     <>
       <title>People</title>
       <h1>People</h1>
-      <Filter />
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" autoComplete="off" value={name || ''} autoFocus onChange={({ target: { value } }) => {
+          stateNavigator.refresh({ ...data, name: value, page: null });
+        }} />
+      </div>
+      Page size
+      <RefreshLink navigationData={{ size: 5, page: null }} includeCurrentData>5</RefreshLink>
+      <RefreshLink navigationData={{ size: 10, page: null }} includeCurrentData>10</RefreshLink>
       {children}
     </>
   );
