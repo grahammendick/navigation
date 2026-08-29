005ac7d40ce57622bafaa89dcf884497ffadeeae
Graham Mendick <graham.mendick@gmail.com>
2026-08-29 22:44:41 +0100

    Prevented double fetching friends
    The dynamically rendered friends scene view is always the tricky one. Scenario is People to Person then show friends. This works ok, only fetches once. But then close friends and show again and this would fetch twice.
    Even with the rendered check back in scene rsc view - which is needed - still renders twice. It's because the 2nd time person ancestor isn't fetching on the old navigation event so it fetches. It's correctly given the optimistic/next navigation event but when it fetches the navigation handler uses the navigation event instead of the next navigation event. Ordinarily, for non-dynamically revealed scene views, this doesn't matter because they only fetch on the background render - and on that render navigation event is the next navigation event - because it's the background render. But in this scenario the friends scene view appears on the non-background render - so for this scenario it's essential for the navigation handler to always use the next navigation event to fetch. For all normal background renders this makes no difference because navigation event = next navigation event. But for the dynamically revealed it matters because it renders on the foreground render - so it needs to fetch using the next navigation event and then it only fetches once because the foreground and background render use the same navigation event and rsc cache.
    Also meant had to add a fallback to gender scene view otherwise it wouldn't change to 'hide friends' immediately - it did before this change (but only the first time) and that's because it friends actually wasn't fetching - now it is.

--- NavigationReact/src/SceneRSCView.tsx
+++ NavigationReact/src/SceneRSCView.tsx
@@ -88,6 +88,7 @@ const SceneView = ({active, name, refetch, __checkCrumb, pending, fallback, erro
 
 const SceneRSCView = (props: SceneViewProps & {active: string | string[]}) => {
     const {active, refetch, name} = props;
+    const rendered = useRef(false);
     const {refetcher, registerSceneView, deserialize} = useContext(RefetchContext);
     const ancestorNavigationEvent = useNavigationEvent();
     const {navigationEvent, nextNavigationEvent} = useContext(NavigationDeferredContext);
@@ -95,6 +96,9 @@ const SceneRSCView = (props: SceneViewProps & {active: string | string[]}) => {
     useEffect(() => {
         registerSceneView(sceneViewKey, active);
     }, [registerSceneView, sceneViewKey, active]);
+    useEffect(() => {
+        rendered.current = true;
+    }, []);    
     const refetchControl = useMemo(() => ({
         sceneViewKey,
         refetcher: (scene: boolean) => refetcher(scene || sceneViewKey),
@@ -108,15 +112,16 @@ const SceneRSCView = (props: SceneViewProps & {active: string | string[]}) => {
             typeof active === 'string' ? stateKey === active : active.indexOf(stateKey) !== -1
         )
     );
-    const hidingOrFetching = (() => {
-        if (!getShow(state?.key) || !getShow(oldState?.key) || !refetch) return true;
+    const optimistic = (() => {
+        if (!rendered.current) return true;
+        if (!getShow(state?.key) || !getShow(oldState?.key) || !refetch) return false;
         for(let i = 0; i < refetch.length; i++) {
-            if (data[refetch[i]] !== oldData[refetch[i]]) return true;
+            if (data[refetch[i]] !== oldData[refetch[i]]) return false;
         }
-        return false;
+        return true;
     })();
     return (
-        <NavigationContext.Provider value={hidingOrFetching ? navigationEvent : ancestorNavigationEvent}>
+        <NavigationContext.Provider value={optimistic ? ancestorNavigationEvent : navigationEvent}>
             <RefetchContext.Provider value={refetchControl}>
                 <SceneView {...props} pending={navigationEvent !== nextNavigationEvent} />
             </RefetchContext.Provider>
